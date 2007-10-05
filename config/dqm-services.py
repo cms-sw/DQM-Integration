5c16d525037880b315dce0c1399d7f1dfab20c2d
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2007-10-05 09:10:01 +0000

    GRES config and layouts

---
+++ config/dqm-services.py
@@ -0,0 +1,4 @@
+services = [
+  ('Online', 'http://cmsmon.cern.ch/dqm/online'),
+  ('Online test', 'http://cmsmon.cern.ch/dqm/online-test')
+]
---
+++ config/online-views.py
@@ -0,0 +1,40 @@
+views = [
+ ('DT', [
+   "Layouts/DT Layouts/DTIntegrityCheck_Summary",
+   "Layouts/DT Layouts/W1_St1_Signal",
+   "Layouts/DT Layouts/W1_St2_Signal",
+   "Layouts/DT Layouts/W1_St3_Signal",
+   "Layouts/DT Layouts/W1_St4_Signal",
+   "Layouts/DT Layouts/DTLocalTrigger_Station_1",
+   "Layouts/DT Layouts/DTLocalTrigger_Station_2",
+   "Layouts/DT Layouts/DTLocalTrigger_Station_3",
+   "Layouts/DT Layouts/DTLocalTrigger_Station_4",
+   "Layouts/DT Layouts/W1_St1_OccupancyPerLayer",
+   "Layouts/DT Layouts/W1_St2_OccupancyPerLayer",
+   "Layouts/DT Layouts/W1_St3_OccupancyPerLayer",
+   "Layouts/DT Layouts/W1_St4_OccupancyPerLayer",
+   "Layouts/DT Layouts/DTIntegrityCheck_station1_first",
+   "Layouts/DT Layouts/DTIntegrityCheck_station1_second",
+   "Layouts/DT Layouts/DTIntegrityCheck_station2_first",
+   "Layouts/DT Layouts/DTIntegrityCheck_station2_second",
+   "Layouts/DT Layouts/DTIntegrityCheck_station3_first",
+   "Layouts/DT Layouts/DTIntegrityCheck_station3_second",
+   "Layouts/DT Layouts/DTIntegrityCheck_station4_first",
+   "Layouts/DT Layouts/DTIntegrityCheck_station4_second" ]),
+ ('EB', [
+   "Layouts/EB Layouts/Summary/0-Global-Summary",
+   "Layouts/EB Layouts/Summary/1-Integrity-Summary",
+   "Layouts/EB Layouts/Summary/2-Occupancy-Summary",
+   "Layouts/EB Layouts/Summary/3-Cosmic-Summary",
+   "Layouts/EB Layouts/Summary/4-PedestalOnline-Summary",
+   "Layouts/EB Layouts/Summary/5-LaserL1-Summary" ]),
+ ('L1T', [
+   "Layouts/L1T Layouts/Summary/GT decision bit correlation",
+   "Layouts/L1T Layouts/Summary/GT FE Bx",
+   "Layouts/L1T Layouts/Summary/GT decision bits",
+   "Layouts/L1T Layouts/Summary/DTTF_quality",
+   "Layouts/L1T Layouts/Summary/DTTF_eta_value",
+   "Layouts/L1T Layouts/Summary/DTTF_phi_value",
+   "Layouts/L1T Layouts/Summary/DTTF_ntrack"])
+ 
+]
