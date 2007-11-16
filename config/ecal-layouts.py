f6c888ea2fbb9f1be3c86e81d6a62e776f37c3ea
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2007-11-16 09:43:56 +0000

    - add Cosmics sub-layout

--- config/ecal-layouts.py
+++ config/ecal-layouts.py
@@ -1,7 +1,7 @@
 
 def ecallayout(i, p, *rows): i["Layouts/ECAL Layouts/" + p] = DQMItem(layout=rows)
 
-ecallayout(dqmitems, "0-Global-Summary",
+ecallayout(dqmitems, "00-Global-Summary",
   [None,
    "EcalEndcap/EESummaryClient/EE global summary EE +",
    None],
