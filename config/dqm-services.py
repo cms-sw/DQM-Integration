a4785b3252372a9a3f63b47cd2e066f2928ba66f
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2007-11-17 17:40:19 +0000

    *** empty log message ***

--- config/dqm-services.py
+++ config/dqm-services.py
@@ -1,4 +1,5 @@
 services = [
   ('Online', 'http://cmsmon.cern.ch/dqm/online'),
   ('Online test', 'http://cmsmon.cern.ch/dqm/online-test')
+  ('GUI test', 'http://cmsmon.cern.ch/dqm/gui-test')
 ]
---
+++ config/server-conf-online.py
@@ -0,0 +1,29 @@
+import os.path, socket
+CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
+PORT = 8030
+
+envsetup = """
+ source ~cmssw/cmsset_default.sh
+ source /home/dqm/rpms/slc4_ia32_gcc345/cms/webtools/1.0.0/etc/profile.d/dependencies-setup.sh
+ export QUIET_ASSERT=a
+"""
+
+server = DQMServerSpec (
+  port       = PORT,
+  localBase   = '%s:%d' % (socket.gethostname(), PORT),
+  serverDir   = '/home/dqm/gui',
+  baseUrl     = '/dqm/online',
+  title       = 'CMS data quality',
+  views       = CONFIGDIR + '/online-views.py',
+  services    = CONFIGDIR + '/dqm-services.py',
+  serviceName = "Online",
+  backends    = [
+    DQMBackendSpec('dqm', 'Client', [ '--collector localhost:9090' ], waitFor = 'commands'),
+    DQMBackendSpec('dt', 'Layout', [ CONFIGDIR + '/dt-layouts.py' ]),
+    DQMBackendSpec('l1t', 'Layout', [ CONFIGDIR + '/l1t-layouts.py' ]),
+    DQMBackendSpec('ecal', 'Layout', [ CONFIGDIR + '/ecal-layouts.py' ]),
+    DQMBackendSpec('eb', 'Layout', [ CONFIGDIR + '/eb-layouts.py' ]),
+    DQMBackendSpec('ee', 'Layout', [ CONFIGDIR + '/ee-layouts.py' ]),
+    DQMBackendSpec('strip','Layout', [ CONFIGDIR + '/strip-layouts.py' ])
+  ])
+
