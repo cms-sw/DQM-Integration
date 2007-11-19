84952fc1f698ee2124767c311fbe2bd7fa39308b
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2007-11-19 14:56:17 +0000

    update for GREN: L1TMonitor->L1T

--- config/l1t-layouts.py
+++ config/l1t-layouts.py
@@ -1,20 +1,20 @@
 def l1tlayout(i, p, *rows): i["Layouts/L1T Layouts/" + p] = DQMItem(layout=rows)
 
 l1tlayout(dqmitems, "Summary/GT Decision bit correlations",
-  ["L1TMonitor/L1TGT/GT decision bit correlation"])
+  ["L1T/L1TGT/GT decision bit correlation"])
 
 l1tlayout(dqmitems, "Summary/GT FE Bx",
-  ["L1TMonitor/L1TGT/GT FE Bx"])
+  ["L1T/L1TGT/GT FE Bx"])
 
 l1tlayout(dqmitems, "Summary/GT decision bits",
-  ["L1TMonitor/L1TGT/GT decision bits"])
+  ["L1T/L1TGT/GT decision bits"])
 
 l1tlayout(dqmitems, "Summary/DTTF_quality",
-          ["L1TMonitor/L1TDTTF/DTTF_quality"])
+          ["L1T/L1TDTTF/DTTF_quality"])
 l1tlayout(dqmitems,"Summary/DTTF_eta_value",
-          ["L1TMonitor/L1TDTTF/DTTF_eta_value"])
+          ["L1T/L1TDTTF/DTTF_eta_value"])
 l1tlayout(dqmitems, "Summary/DTTF_phi_value",
-          ["L1TMonitor/L1TDTTF/DTTF_phi_value"])
+          ["L1T/L1TDTTF/DTTF_phi_value"])
 
 l1tlayout(dqmitems, "Summary/DTTF_ntrack",
-          ["L1TMonitor/L1TDTTF/DTTF_ntrack"])
+          ["L1T/L1TDTTF/DTTF_ntrack"])
