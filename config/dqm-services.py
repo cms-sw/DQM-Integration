55a0f1ac4e32c7e6f3bfa5c03c545c73a6d5b52e
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2007-11-29 10:38:28 +0000

    setting up a separate webserver for playback

--- config/dqm-services.py
+++ config/dqm-services.py
@@ -1,5 +1,6 @@
 services = [
   ('Online', 'http://cmsmon.cern.ch/dqm/online'),
   ('Online test', 'http://cmsmon.cern.ch/dqm/online-test'),
-  ('GUI test', 'http://cmsmon.cern.ch/dqm/gui-test')
+  ('GUI test', 'http://cmsmon.cern.ch/dqm/gui-test'),
+  ('Online Playback', 'http://cmsmon.cern.ch/dqm/online-playback')
 ]
