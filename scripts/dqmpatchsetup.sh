7a49c68dfa3f764442e82ec4dbb1f44eea9410de
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2007-12-02 16:16:51 +0000

    update for DQMPATCH:02

--- scripts/dqmpatchsetup.sh
+++ scripts/dqmpatchsetup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo 
+echo Please use DQMPATCH:02, i.e. type dqmpatchsetup.sh [-a] 2
+echo 
+
 if [ "$#" -lt 1 ] ; then  echo "  Usage $0 [-a ] dqmpatch" ;  exit  ; fi
 
 while getopts a: opt
@@ -16,7 +20,7 @@ echo "  dqmpatchversion: $dqmpatch"
 
 if [ -z "$LOCALRT" ]; then echo "  First setup your scramv1 area for CMSSW_1_7_1 - exiting " ; exit ; fi
 if [ $CMSSW_VERSION != CMSSW_1_7_1 ] ; then echo "  First setup your area for CMSSW_1_7_1 - exiting " ; exit ; fi
-if [ $dqmpatch != "1" ]  ; then echo "  This DQM patch version not available - exiting " ; exit ; fi
+if [ $dqmpatch != "2" ]  ; then echo "  This DQM patch version not available - exiting " ; exit ; fi
 
 echo "  installing tags for DQMPATCH:$dqmpatch in your project area:"
 if [ $all != "true" ] ; 
@@ -29,19 +33,26 @@ fi
 cd $LOCALRT/src
 source $CMS_PATH/utils/cmscvsroot.sh CMSSW
 
-cvs co -r V00-05-11 DQMServices/Core
-cvs co -r V00-05-11 DQMServices/CoreROOT
+cvs co -r V00-05-12 DQMServices/Core
+cvs co -r V00-05-12 DQMServices/CoreROOT
 cvs co -r V00-05-11 DQMServices/Components
 cvs co -r V00-05-11 DQMServices/Examples
 
-cvs co -r V00-00-10 DQM/Integration
+cvs co -r V00-00-11 DQM/Integration
+cvs co -r V00-00-08 DQM/RenderPlugins
 
 if [ $all == "true" ] ; 
 then echo "  now checking out subsystem tags  ... " ; sleep 2
 
-cvs co -r V00-07-05 DQM/HcalMonitorModule
-cvs co -r V00-07-04 DQM/HcalMonitorClient
-cvs co -r V00-07-04 DQM/HcalMonitorTasks
+cvs co -r V01-00-01 L1Trigger/HardwareValidation
+cvs co -r V02-03-03-03 DataFormats/L1Trigger
+
+cvs co -r V00-05-01 EventFilter/EcalRawToDigiDev
+
+cvs co -r V00-07-05 DQM/HcalMonitorClient
+cvs co -r V00-07-05 DQM/HcalMonitorTasks
+cvs co -r V00-07-06 DQM/HcalMonitorModule 
+
 cvs co -r V00-06-00 DQM/RPCMonitorDigi
 
 cvs co -r V02-00-03 DQM/CSCMonitorModule 
