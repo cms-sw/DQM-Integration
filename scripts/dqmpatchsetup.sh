d8beb7a52043c1356bbaca3d71331cc1719e88ae
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2007-12-04 14:42:36 +0000

    moving to version 3

--- scripts/dqmpatchsetup.sh
+++ scripts/dqmpatchsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 
-echo Please use DQMPATCH:02, i.e. type dqmpatchsetup.sh [-a] 2
+echo Please use DQMPATCH:03, i.e. type dqmpatchsetup.sh [-a] 3
 echo 
 
 if [ "$#" -lt 1 ] ; then  echo "  Usage $0 [-a ] dqmpatch" ;  exit  ; fi
@@ -20,7 +20,7 @@ echo "  dqmpatchversion: $dqmpatch"
 
 if [ -z "$LOCALRT" ]; then echo "  First setup your scramv1 area for CMSSW_1_7_1 - exiting " ; exit ; fi
 if [ $CMSSW_VERSION != CMSSW_1_7_1 ] ; then echo "  First setup your area for CMSSW_1_7_1 - exiting " ; exit ; fi
-if [ $dqmpatch != "2" ]  ; then echo "  This DQM patch version not available - exiting " ; exit ; fi
+if [ $dqmpatch != "3" ]  ; then echo "  Please use patch version 3 " ; exit ; fi
 
 echo "  installing tags for DQMPATCH:$dqmpatch in your project area:"
 if [ $all != "true" ] ; 
@@ -33,17 +33,21 @@ fi
 cd $LOCALRT/src
 source $CMS_PATH/utils/cmscvsroot.sh CMSSW
 
-cvs co -r V00-05-12 DQMServices/Core
+cvs co -r V00-05-13 DQMServices/Core
 cvs co -r V00-05-12 DQMServices/CoreROOT
 cvs co -r V00-05-11 DQMServices/Components
 cvs co -r V00-05-11 DQMServices/Examples
 
-cvs co -r V00-00-11 DQM/Integration
-cvs co -r V00-00-08 DQM/RenderPlugins
+cvs co -r V00-00-12 DQM/Integration
+cvs co -r V00-00-09 DQM/RenderPlugins
 
 if [ $all == "true" ] ; 
 then echo "  now checking out subsystem tags  ... " ; sleep 2
 
+cvs co -r CMSSW_1_7_2 EventFilter/Processor  
+cvs co -r CMSSW_1_7_1 DQM/SiStripMonitorClient/test 
+
+cvs co -r V02-00-20 DQM/L1TMonitor       
 cvs co -r V01-00-01 L1Trigger/HardwareValidation
 cvs co -r V02-03-03-03 DataFormats/L1Trigger
 
@@ -51,12 +55,12 @@ cvs co -r V00-05-01 EventFilter/EcalRawToDigiDev
 
 cvs co -r V00-07-05 DQM/HcalMonitorClient
 cvs co -r V00-07-05 DQM/HcalMonitorTasks
-cvs co -r V00-07-07 DQM/HcalMonitorModule 
+cvs co -r V00-07-07 DQM/HcalMonitorModule
 
-cvs co -r V00-06-00 DQM/RPCMonitorDigi
+cvs co -r V00-06-03 DQM/RPCMonitorDigi  
 
-cvs co -r V02-00-03 DQM/CSCMonitorModule 
-cvs co -r V01-08-10 EventFilter/CSCRawToDigi 
+cvs co -r V02-00-03 DQM/CSCMonitorModule
+cvs co -r V01-08-10 EventFilter/CSCRawToDigi
 
 cvs co -r V00-16-05 DQM/DTMonitorModule
 cvs co -r V00-03-04 DQM/DTMonitorClient
