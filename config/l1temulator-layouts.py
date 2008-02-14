3cc8735b9144f7373fe60b2bf5e9202acad76c93
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2008-02-14 23:52:55 +0000

    *** empty log message ***

--- config/l1temulator-layouts.py
+++ config/l1temulator-layouts.py
@@ -3,5 +3,5 @@ def l1tlayout(i, p, *rows): i["Layouts/L1TEMU Layouts/" + p] = DQMItem(layout=ro
 l1tlayout(dqmitems, "Summary/Data Emulator comparison",
           ["L1TEMU/errorflag",
            "L1TEMU/sysrates"],
-          ["L1TEMU/sysncadData",
-           "L1TEMU/sysncadEmul"])
+          ["L1TEMU/sysncandData",
+           "L1TEMU/sysncandEmul"])
