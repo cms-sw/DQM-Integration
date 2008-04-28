7982b3590ddf08a00e006f6583f972d7dd34b186
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2008-04-28 12:46:53 +0000

    Commit Andreas' server config.

--- config/server-conf-online.py
+++ config/server-conf-online.py
@@ -1,22 +1,15 @@
 import os.path
-
+global CONFIGDIR
 CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
-PORT = 8030
-
-# Define a list of allowed bases, then rewrite tools.proxy to
-# select a base that was used in the request ('X-Forwarded-Host'
-# or 'Host'; allow several in each).
-#
-# bases    = [ 'localhost:%d' % PORT,
-#              '%s:%d' % (gethostname(), PORT),
-#              'lxcms73.cern.ch' ],
+LAYOUTS = ("%s/%s-layouts.py" % (CONFIGDIR, x) for x in
+	   ("csc", "dt", "eb", "ecal", "ee", "hcal", "l1temulator", "l1t",
+	    "pixel", "rpc", "strip"))
 
 modules = ("GuiDQM",)
-
 envsetup = """
-  source /home/dqm/rpms/slc4_ia32_gcc345/cms/webtools/1.0.0/etc/profile.d/dependencies-setup.sh
-  # source $HOME/sw/rpms/slc4_ia32_gcc345/cms/webtools/1.0.0/etc/profile.d/dependencies-setup.sh
-  export QUIET_ASSERT=a
+ source /home/dqm/rpms/cmsset_default.sh
+ source /home/dqm/rpms/slc4_ia32_gcc345/cms/webtools/1.3.0/etc/profile.d/dependencies-setup.sh
+ export QUIET_ASSERT=a
 """
 
 server.serverDir   = '/home/dqm/gui'
@@ -24,20 +17,9 @@ server.baseUrl     = '/dqm/online'
 server.title       = 'CMS data quality'
 server.serviceName = 'Online'
 
-# server.source('dt',    'DQMLive', '--listen 9092', '--collector usc55ucr04.cms:9090')
-# server.source('ecal',  'DQMLive', '--listen 9093', '--collector ecalod-22.cms:9090')
-# server.source('hcal',  'DQMLive', '--listen 9094', '--collector rubus2d16-12:9090')
-# server.source('hcalt', 'DQMLive', '--listen 9095', '--collector rubus2d16-12:9090')
-# server.source('l1t',   'DQMLive', '--listen 9096', '--collector rubus2d16-14:9090')
-server.source('test',    'DQMLive', '--listen 9091', '--collector localhost:9090')
-#server.source('file',    'DQMArchive', '/home/dqm/dqm.db', '--listen 9097')
-#server.source('layouts', 'DQMLayout',
-#              CONFIGDIR + '/test-layouts.py',
-#              CONFIGDIR + '/dt-layouts.py',
-#              CONFIGDIR + '/eb-layouts.py',
-#              CONFIGDIR + '/l1t-layouts.py')
+server.source('dqm', 'DQMLive', '--listen 9091', '--collector localhost:9090')
+server.source('file', 'DQMArchive', '/home/dqm/dqm.db', '--listen 9097')
+#server.source('layouts', 'DQMLayout', *LAYOUTS)
 
-# server.extend('DQMFileAccess', '/dev/null', '/tmp/lat/dqmdata')
-
-execfile(CONFIGDIR + "/dqm-services.py", globals(), locals())
-execfile(CONFIGDIR + "/online-workspaces.py", globals(), locals())
+execfile(CONFIGDIR + "/dqm-services.py")
+execfile(CONFIGDIR + "/online-workspaces.py")
