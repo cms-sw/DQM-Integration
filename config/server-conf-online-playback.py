32218cb311c5cc97f4f539917bbaf7493467b76c
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2008-04-30 20:56:02 +0000

    update for new config file format for playback

--- config/server-conf-online-playback.py
+++ config/server-conf-online-playback.py
@@ -1,23 +1,26 @@
 import os.path
 global CONFIGDIR
 CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
-LAYOUTS = ("%s/%s-layouts.py" % (CONFIGDIR, x) for x in ("dt", "eb", "ee", "l1t"))
+LAYOUTS = ("%s/%s-layouts.py" % (CONFIGDIR, x) for x in
+           ("csc", "dt", "eb", "ecal", "ee", "hcal", "l1temulator", "l1t",
+            "pixel", "rpc", "strip"))
 
 modules = ("GuiDQM",)
 envsetup = """
- source ~cmssw/cmsset_default.sh
- source /home/dqm/rpms/slc4_ia32_gcc345/cms/webtools/1.0.0/etc/profile.d/dependencies-setup.sh
+ source /home/dqm/rpms/cmsset_default.sh
+ source /home/dqm/rpms/slc4_ia32_gcc345/cms/webtools/1.3.0/etc/profile.d/dependencies-setup.sh
  export QUIET_ASSERT=a
 """
 
 server.serverDir   = '/home/dqm/gui'
 server.baseUrl     = '/dqm/online-playback'
 server.title       = 'CMS data quality'
-server.serviceName = 'Online playback'
+server.serviceName = 'Online Playback'
 
 server.source('dqm', 'DQMLive', '--listen 9091', '--collector localhost:9090')
 server.source('file', 'DQMArchive', '/home/dqm/dqm.db', '--listen 9097')
-server.source('layouts', 'DQMLayout', *LAYOUTS)
+#server.source('layouts', 'DQMLayout', *LAYOUTS)
 
 execfile(CONFIGDIR + "/dqm-services.py")
 execfile(CONFIGDIR + "/online-workspaces.py")
+
