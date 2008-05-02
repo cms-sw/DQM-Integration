d97093ceddb201c6da490ad9f35a469f2604c155
Carlo Battilana <sha1-2f2a88ddb08fa41d1d85b122a8afd8e62eeb85cd@cern.ch>
2008-05-02 16:34:46 +0000

    DT layouts for CRUZET

---
+++ config/shift_dt_layout.py
@@ -0,0 +1,23 @@
+def shiftdtlayout(i, p, *rows): i["Shift/DT" + p] = DQMItem(layout=rows)
+
+shiftdtlayout(dqmitems, "01-DataIntegrity",
+  ["DT/DataIntegrity/FED770/FED770_ROSSummary",
+   "DT/DataIntegrity/FED771/FED771_ROSSummary",
+   "DT/DataIntegrity/FED772/FED772_ROSSummary"],
+  ["DT/DataIntegrity/FED773/FED773_ROSSummary",
+   "DT/DataIntegrity/FED774/FED774_ROSSummary"])
+
+shiftdtlayout(dqmitems, "02-CorrelatedTriggers",
+  ["DT/Tests/DTLocalTrigger/Wheel-2/LocalTriggerPhi/CorrFraction_Phi_W-2",
+   "DT/Tests/DTLocalTrigger/Wheel-1/LocalTriggerPhi/CorrFraction_Phi_W-1",
+   "DT/Tests/DTLocalTrigger/Wheel0/LocalTriggerPhi/CorrFraction_Phi_W0"],
+  ["DT/Tests/DTLocalTrigger/Wheel1/LocalTriggerPhi/CorrFraction_Phi_W1",
+   "DT/Tests/DTLocalTrigger/Wheel2/LocalTriggerPhi/CorrFraction_Phi_W2"])
+
+shiftdtlayout(dqmitems, "03-Efficiency",
+  ["DT/Tests/DTEfficiency/SummaryPlot/ESummary_testFailedByAtLeastBadSL"],
+  ["DT/Tests/DTEfficiency/SummaryPlot/UESummary_testFailedByAtLeastBadSL"])
+
+shiftdtlayout(dqmitems, "04-Resolution",
+  ["DT/Tests/DTResolution/SummaryPlot/MeanSummaryRes_testFailedByAtLeastBadSL_STEP3"],
+  ["DT/Tests/DTResolution/SummaryPlot/SigmaSummaryRes_testFailedByAtLeastBadSL_STEP3"])
