8cc0c701d16e470e0faf9e13ca5e2091500b4d79
Hyunkwan Seo <sha1-885d53b681c72a58fe6bb49e412be85d8087d0ef@cern.ch>
2008-05-06 12:24:44 +0000

    Modified to be run on cmsmon(srv-c2c06-02)

--- scripts/mergeAndRegister_online.py
+++ scripts/mergeAndRegister_online.py
@@ -5,13 +5,19 @@ import os,time,sys,shutil
 def filereg(db,file,logfile):
     tmpdb = '/cms/mon/data/.dropbox_test/dqm-tmp.db'
     newdb = db[:-3]+'-new.db'
+    server = 'srv-c2d05-19'
     if os.path.exists(tmpdb): os.remove(tmpdb)
-    if os.path.exists(db): shutil.copy2(db, tmpdb)
+    #if os.path.exists(db): shutil.copy2(db, tmpdb)
+    logfile.write(os.popen('scp '+server+ ':'+db+' '+tmpdb).read())
     logfile.write('*** File Register ***\n')
     logfile.write(os.popen('visDQMRegisterFile '+ tmpdb +' "/Global/Online/ALL" "Global run" '+ file).read())
-    shutil.move(tmpdb, newdb)
-    if os.path.exists(db): os.remove(db)
-    os.rename(newdb,db)
+    #shutil.move(tmpdb, newdb)
+    logfile.write(os.popen('scp '+tmpdb+' '+server+':'+newdb).read())
+    os.remove(tmpdb)
+    #if os.path.exists(db): os.remove(db)
+    #os.rename(newdb,db)
+    # logfile.write(os.popen('ssh '+server+' -t rm '+db).read())
+    logfile.write(os.popen('ssh '+server+' -t mv '+newdb+' '+db).read())
 
 def uniq_chk(filename):
     if os.path.exists(filename):
@@ -28,8 +34,10 @@ def uniq_chk(filename):
 # runs for testing 403  409  420  426  429
 # find /cms/mon/data/dqm/results -name 'DQM_*_R000038403*.root' | grep -v _RPCexpert_ | xargs visDQMRegisterFile dqm.db /Global/Online/ALL "Global run"
 DIR = '/cms/mon/data/dropbox'  # directory to search new files
+#DIR = '/cms/mon/data/dropbox_test'  # directory to search new files
 #DIR = '/cms/mon/data/dqm/results/000/038/483'
-DB = '/cms/mon/data/dqm/dqm.db'
+#DB = '/cms/mon/data/dqm/dqm.db'
+DB = '/home/dqm/dqm.db'
 #FILEDIR = '/cms/mon/data/dropbox_test' # directory, to which merged file is stored
 FILEDIR = '/cms/mon/data/dqm/results' # directory, to which merged file is stored
 TMPDIR = '/cms/mon/data/.dropbox_test' # directory, in which merged file is created
@@ -69,12 +77,12 @@ while 1:
     #### loop for new files
     for NEW in NEW_:
         if NEW==NEW_[0]: os.system('touch '+ TMPDIR +'/timeTag')
-        print NEW
         run = NEW[-14:-5] #run number of new file
         SUBDIR = '/'+run[0:3]+'/'+run[3:6]+'/'+run[6:9]
         NEWDIR = FILEDIR + SUBDIR # directory, to which merged file is stored
         #NEWDIR = NEW[0:NEW.find('DQM')-1]
         TMP_NEW = TMPDIR + '/'+ NEW[NEW.find('DQM'):]
+        print NEW +' is being copied.'
         shutil.copy2(NEW,TMP_NEW)
 
 
@@ -90,7 +98,11 @@ while 1:
             TMP_MERGED = MERGED.replace(NEWDIR,TMPDIR)
             TMP_LOG = TMP_MERGED[:-4]+'log'
             shutil.copy2(MERGED,TMP_MERGED)
+            if not os.path.exists(LOG):
+                os.system('touch '+ LOG)
+                print 'log file was lost for unknown reason. It was recreated!'
             shutil.copy2(LOG,TMP_LOG)
+                
             irun = TMP_MERGED[-14:-5]
             FILE_EXIST = 1
         else:
@@ -147,6 +159,7 @@ while 1:
             
             shutil.copy2(TMP_LOG,TMP_NEW_LOG)
             LOGFILE = open(TMP_NEW_LOG,'a')
+            print 'merging '+NEW
             #LOGFILE.write(os.popen('hadd '+TMP_NEW_MERGED+' '+NEW+ ' '+TMP_MERGED).read())
             LOGFILE.write(os.popen('hadd '+TMP_NEW_MERGED+' '+TMP_NEW+ ' '+TMP_MERGED).read())
             LOGFILE.flush()
