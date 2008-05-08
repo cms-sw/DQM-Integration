4cd1661b3d41837e45656899813f26d40c9adae7
Jason St John <sha1-13e6ca3f617e2679b2e43c5867c3ac26779e685c@cern.ch>
2008-05-08 18:35:11 +0000

    Stripped down to just the data format monitor

--- config/hcal-layouts.py
+++ config/hcal-layouts.py
@@ -3,117 +3,11 @@ def hcallayout(i, p, *rows): i["Hcal/Layouts/" + p] = DQMItem(layout=rows)
 hcallayout(dqmitems, "HCAL Data Format Summary",
            [{ 'path': "Hcal/DataFormatMonitor/HTR Error Word by Crate",
                    'description': 'Ignore only OpDat Err, Clock Err, Link Err, CapID Err, and FE Format Err' },
-            "Hcal/DataFormatMonitor/Invalid HTR Data",
-            "Hcal/DigiMonitor/Bad Digi Fraction"],
-           ["Hcal/DataFormatMonitor/Event Fragment Size for each FED",
+            "Hcal/DataFormatMonitor/Invalid HTR Data"],
+           ["Hcal/DataFormatMonitor/Ev Frag Size 2d",
             "Hcal/DataFormatMonitor/EvN Inconsistent - HTR vs Ref HTR"],
            ["Hcal/DataFormatMonitor/Common Data Format violations",
             "Hcal/DataFormatMonitor/DCC Event Format violation",
             "Hcal/DataFormatMonitor/DCC Error and Warning",
             "Hcal/DataFormatMonitor/DCC Nonzero Spigot Conditions"]
            )
-
-hcallayout(dqmitems, "HCAL Digitization Summary",
-           ["Hcal/DigiMonitor/Digi Depth 1 Occupancy Map",
-            "Hcal/DigiMonitor/Digi Depth 2 Occupancy Map",
-            "Hcal/DigiMonitor/Digi Depth 3 Occupancy Map",
-            "Hcal/DigiMonitor/Digi Depth 4 Occupancy Map"],
-           ["Hcal/DigiMonitor/HB/HB # of Digis",
-            "Hcal/DigiMonitor/HE/HE # of Digis",
-            "Hcal/DigiMonitor/HF/HF # of Digis",
-            "Hcal/DigiMonitor/HO/HO # of Digis"],
-           ["Hcal/DigiMonitor/HB/HB Digi Shape - over thresh",
-            "Hcal/DigiMonitor/HE/HE Digi Shape - over thresh",
-            "Hcal/DigiMonitor/HF/HF Digi Shape - over thresh",
-            "Hcal/DigiMonitor/HO/HO Digi Shape - over thresh"]
-           )
-
-hcallayout(dqmitems, "HCAL Reconstruction Summary",
-           ["Hcal/RecHitMonitor/RecHit Total Energy"],
-           ["Hcal/RecHitMonitor/HB/HB RecHit Energies",
-            "Hcal/RecHitMonitor/HE/HE RecHit Energies",
-            "Hcal/RecHitMonitor/HF/HF Short RecHit Energies",
-            "Hcal/RecHitMonitor/HF/HF Long RecHit Energies",
-            "Hcal/RecHitMonitor/HO/HO RecHit Energies"],
-           ["Hcal/RecHitMonitor/HB/HB RecHit Times",
-            "Hcal/RecHitMonitor/HE/HE RecHit Times",
-            "Hcal/RecHitMonitor/HF/HF Short RecHit Times",
-            "Hcal/RecHitMonitor/HF/HF Long RecHit Times",
-            "Hcal/RecHitMonitor/HO/HO RecHit Times"]
-           )
-
-hcallayout(dqmitems, "HCAL Reconstruction Threshold Summary",
-           ["Hcal/RecHitMonitor/RecHit Total Energy - Threshold"],
-           ["Hcal/RecHitMonitor/HB/HB RecHit Total Energy - Threshold",
-            "Hcal/RecHitMonitor/HE/HE RecHit Total Energy - Threshold",
-            "Hcal/RecHitMonitor/HF/HF RecHit Total Energy - Threshold",
-            "Hcal/RecHitMonitor/HO/HO RecHit Total Energy - Threshold"],
-           ["Hcal/RecHitMonitor/HB/HB RecHit Times - Threshold",
-            "Hcal/RecHitMonitor/HE/HE RecHit Times - Threshold",
-            "Hcal/RecHitMonitor/HF/HF RecHit Times - Threshold",
-            "Hcal/RecHitMonitor/HO/HO RecHit Times - Threshold"]
-           )
-
-hcallayout(dqmitems, "HCAL Hot Cell Summary",
-           ["Hcal/HotCellMonitor/HCAL/HCALEnergy"],
-           ["Hcal/HotCellMonitor/HB/HBHotCellOccMapThresh0",
-            "Hcal/HotCellMonitor/HE/HEHotCellOccMapThresh0",
-            "Hcal/HotCellMonitor/HF/HFHotCellOccMapThresh0",
-            "Hcal/HotCellMonitor/HO/HOHotCellOccMapThresh0"]
-           )
-
-hcallayout(dqmitems, "HCAL Hot Cell NADA Summary",
-           ["Hcal/HotCellMonitor/HCAL/HCALnadaNumHotCells",
-            "Hcal/HotCellMonitor/HCAL/HCALnadaNumNegCells"],
-           ["Hcal/HotCellMonitor/HB/HBnadaOccMap",
-            "Hcal/HotCellMonitor/HE/HEnadaOccMap",
-            "Hcal/HotCellMonitor/HF/HFnadaOccMap",
-            "Hcal/HotCellMonitor/HO/HOnadaOccMap"]
-           )
-
-
-hcallayout(dqmitems, "HCAL Barrel Summary",
-           ["Hcal/DigiMonitor/HB/HB # of Digis",
-            "Hcal/DigiMonitor/HB/HB QIE ADC Value"],
-           ["Hcal/RecHitMonitor/HB/HB RecHit Energies",
-            "Hcal/RecHitMonitor/HB/HB RecHit Times",
-            "Hcal/RecHitMonitor/HB/HB RecHit Total Energy - Threshold",
-            "Hcal/RecHitMonitor/HB/HB RecHit Times - Threshold"],
-           ["Hcal/HotCellMonitor/HB/HBHotCellOccMapThresh0",
-            "Hcal/HotCellMonitor/HB/HBnadaOccMap"]
-)
-
-hcallayout(dqmitems, "HCAL Endcap Summary",
-           ["Hcal/DigiMonitor/HE/HE # of Digis",
-            "Hcal/DigiMonitor/HE/HE QIE ADC Value"],
-           ["Hcal/RecHitMonitor/HE/HE RecHit Energies",
-            "Hcal/RecHitMonitor/HE/HE RecHit Times",
-            "Hcal/RecHitMonitor/HE/HE RecHit Total Energy - Threshold",
-            "Hcal/RecHitMonitor/HE/HE RecHit Times - Threshold"],
-           ["Hcal/HotCellMonitor/HE/HEHotCellOccMapThresh0",
-            "Hcal/HotCellMonitor/HE/HEnadaOccMap"]
-)
-
-hcallayout(dqmitems, "HCAL Forward Summary",
-           ["Hcal/DigiMonitor/HF/HF # of Digis",
-            "Hcal/DigiMonitor/HF/HF QIE ADC Value"],
-           ["Hcal/RecHitMonitor/HF/HF Short RecHit Energies",
-            "Hcal/RecHitMonitor/HF/HF Long RecHit Energies",
-            "Hcal/RecHitMonitor/HF/HF RecHit Times",
-            "Hcal/RecHitMonitor/HF/HF RecHit Total Energy - Threshold",
-            "Hcal/RecHitMonitor/HF/HF RecHit Times - Threshold"],
-           ["Hcal/HotCellMonitor/HF/HFHotCellOccMapThresh0",
-            "Hcal/HotCellMonitor/HF/HFnadaOccMap"]
-)
-
-hcallayout(dqmitems, "HCAL Outer Summary",
-           ["Hcal/DigiMonitor/HO/HO # of Digis",
-            "Hcal/DigiMonitor/HO/HO QIE ADC Value"],
-           ["Hcal/RecHitMonitor/HO/HO RecHit Energies",
-            "Hcal/RecHitMonitor/HO/HO RecHit Times",
-            "Hcal/RecHitMonitor/HO/HO RecHit Total Energy - Threshold",
-            "Hcal/RecHitMonitor/HO/HO RecHit Times - Threshold"],
-           ["Hcal/HotCellMonitor/HO/HOHotCellOccMapThresh0",
-            "Hcal/HotCellMonitor/HO/HOnadaOccMap"]
-)
-
--- config/shift_hcal_layout.py
+++ config/shift_hcal_layout.py
@@ -2,22 +2,14 @@ def shiftHcalLayout(i, p, *rows): i["Shift/Hcal/" + p] = DQMItem(layout=rows)
 
 
 shiftHcalLayout(dqmitems,"Shifter Hcal Summary",
-                # DataFormatMonitor, DigiMonitor
-                [{ 'path': "Hcal/DataFormatMonitor/HTR Error Word by Crate",
+           [{ 'path': "Hcal/DataFormatMonitor/HTR Error Word by Crate",
                    'description': 'Ignore only OpDat Err, Clock Err, Link Err, CapID Err, and FE Format Err' },
-                 "Hcal/DigiMonitor/Bad Digi Fraction",
-                 "Hcal/DataFormatMonitor/Ev Frag Size 2d"],
-                 ["Hcal/DigiMonitor/Digi Depth 1 Occupancy Map",
-                  "Hcal/DigiMonitor/Digi Depth 2 Occupancy Map",
-                  "Hcal/DigiMonitor/Digi Depth 3 Occupancy Map",
-                  "Hcal/DigiMonitor/Digi Depth 4 Occupancy Map"],
+            "Hcal/DataFormatMonitor/Invalid HTR Data"],
+           ["Hcal/DataFormatMonitor/Ev Frag Size 2d",
+            "Hcal/DataFormatMonitor/EvN Inconsistent - HTR vs Ref HTR"],
+           ["Hcal/DataFormatMonitor/Common Data Format violations",
+            "Hcal/DataFormatMonitor/DCC Event Format violation",
+            "Hcal/DataFormatMonitor/DCC Error and Warning",
+            "Hcal/DataFormatMonitor/DCC Nonzero Spigot Conditions"]
+           )
 
-                # 2 hists each from RecHitMonitor, TrigPrim Monitor
-                ["Hcal/RecHitMonitor/RecHit Eta Energy Map",
-                 "Hcal/RecHitMonitor/RecHit Phi Energy Map",
-                 "Hcal/TrigPrimMonitor/TrigPrim Geo Energy Map",
-                 "Hcal/TrigPrimMonitor/TP Timing"],
-
-                # HotCellMonitor
-                ["Hcal/HotCellMonitor/HCAL/HCALHotCellEnergyMapThresh0",
-                 "Hcal/HotCellMonitor/HCAL/HCALnadaOccMap"])
