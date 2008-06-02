37f4ff2662f6369ce932b8bdc5770de769701b5b
David Lomidze <sha1-caee96601a9b34acf0a8be2f22a26eb4dd6cd64d@cern.ch>
2008-06-02 14:10:47 +0000

    rpc-layouts.py

--- config/rpc-layouts.py
+++ config/rpc-layouts.py
@@ -9,27 +9,3 @@ rpclayout(dqmitems, "RPC_Summary",
    "RPC/RecHits/SumarryHistograms/ClusterSize_for_Wheel_0_2",
    "RPC/RecHits/SumarryHistograms/ClusterSize_for_Wheel_0_1",
    "RPC/RecHits/SumarryHistograms/ClusterSize_for_Wheel_0_0"])
-
-rpclayout(dqmitems, "SectorOccubancy_W1_W0",
-  ["RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_0_Sector_1",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_0_Sector_2",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_0_Sector_3",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_0_Sector_10",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_0_Sector_11",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_0_Sector_12"],
-
-  ["RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_1_Sector_1",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_1_Sector_2",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_1_Sector_3",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_1_Sector_10",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_1_Sector_11",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_1_Sector_12"])
-
-rpclayout(dqmitems, "SectorOccubancy_W2",
-  ["RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_2_Sector_10",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_2_Sector_11",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_2_Sector_12",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_2_Sector_1",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_2_Sector_2",
-   "RPC/RecHits/SumarryHistograms/SectorOccupancy_Wheel_2_Sector_3"])
-
