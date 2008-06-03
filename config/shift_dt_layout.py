a094f95d9317dcf96a4d644403d352c782000f46
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-06-03 16:46:36 +0000

    Summary plots

--- config/shift_dt_layout.py
+++ config/shift_dt_layout.py
@@ -1,15 +1,14 @@
 def shiftdtlayout(i, p, *rows): i["Shift/DT/" + p] = DQMItem(layout=rows)
 
-shiftdtlayout(dqmitems, "01-DataIntegrity",
-  ["DT/DataIntegrity/FED770/FED770_ROSSummary",
-   "DT/DataIntegrity/FED771/FED771_ROSSummary",
-   "DT/DataIntegrity/FED772/FED772_ROSSummary"],
-  ["DT/DataIntegrity/FED773/FED773_ROSSummary",
-   "DT/DataIntegrity/FED774/FED774_ROSSummary"])
+shiftdtlayout(dqmitems, "00-DataIntegritySummary",
+  [{ 'path': "DT/DataIntegrity/DataIntegritySummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description</a>" }])
 
-shiftdtlayout(dqmitems, "02-CorrelatedTriggers",
-  ["DT/Tests/DTLocalTrigger/Wheel-2/LocalTriggerPhi/CorrFraction_Phi_W-2",
-   "DT/Tests/DTLocalTrigger/Wheel-1/LocalTriggerPhi/CorrFraction_Phi_W-1",
-   "DT/Tests/DTLocalTrigger/Wheel0/LocalTriggerPhi/CorrFraction_Phi_W0"],
-  ["DT/Tests/DTLocalTrigger/Wheel1/LocalTriggerPhi/CorrFraction_Phi_W1",
-   "DT/Tests/DTLocalTrigger/Wheel2/LocalTriggerPhi/CorrFraction_Phi_W2"])
+shiftdtlayout(dqmitems, "01-OccupancySummary",
+  [{ 'path': "DT/Digi/OccupancySummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description</a>" }])
+
+shiftdtlayout(dqmitems, "02-SegmentSummary",
+  [{ 'path': "DT/Segments/segmentSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description</a>" }])
+
+shiftdtlayout(dqmitems, "03-LocalTriggerSummary",
+  [{ 'path': "DT/LocalTrigger/DDU_CorrFractionSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description</a>" }])
+
