9f967dff74190671cc838434d9a2338476335400
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2008-06-05 07:57:42 +0000

    new top level folder for shift layout

--- config/shift_pixel_layout.py
+++ config/shift_pixel_layout.py
@@ -1,18 +1,14 @@
-def pixellayout(i, p, *rows): i["Layouts/Pixel Layouts/" + p] = DQMItem(layout=rows)
-pixellayout(dqmitems, "Pixel_Digi_Toplevel_Summary",
-  ["Pixel/Barrel/SUMDIG_adc_Barrel",
-   "Pixel/Barrel/SUMDIG_ndigis_Barrel"],
-  ["Pixel/Endcap/SUMDIG_adc_Endcap",
-   "Pixel/Endcap/SUMDIG_ndigis_Endcap"])
-pixellayout(dqmitems, "Pixel_PIB_Summary",
-  ["Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_errorType_Panel_2",
-   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_NErrors_Panel_2",
-   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_TBMType_Panel_2"],
-  ["Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_EvtNbr_Panel_2",
-   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_ROCId_Panel_2",
-   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_DCOLId_Panel_2"],
-  ["Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_PXId_Panel_2",
-   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_ROCNmbr_Panel_2",
-   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_TBMMessage_Panel_2"],
+def pixellayout(i, p, *rows): i["00 Shift/Pixel/" + p] = DQMItem(layout=rows)
+pixellayout(dqmitems, "Pixel_DIGI_Summary",
   ["Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMDIG_adc_Panel_2",
    "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMDIG_ndigis_Panel_2"])
+pixellayout(dqmitems, "Pixel_FEDError_Summary",   
+  ["Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_NErrors_Panel_2",
+   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_errorType_Panel_2",
+   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_TBMType_Panel_2",
+   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_TBMMessage_Panel_2"],
+  ["Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_EvtNbr_Panel_2",
+   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_ROCNmbr_Panel_2"],
+  ["Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_ROCId_Panel_2",
+   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_DCOLId_Panel_2",
+   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_PXId_Panel_2"])
