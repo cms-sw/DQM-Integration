04bbddcef6166ddff598f0a125aadff9ee918a4e
Cms Dqm <sha1-ab433251379c0653a4d5fcb697bb9f400ca219d0@cern.ch>
2008-06-13 16:05:07 +0000

    Update offline configurations for server upgrade.

--- config/server-conf-offline-dev.py
+++ config/server-conf-offline-dev.py
@@ -6,6 +6,10 @@ modules = ("GuiDQM",)
 envsetup = """
  source /data/sw/cmsset_default.sh
  source /data/sw/slc4_ia32_gcc345/cms/webtools/1.3.0/etc/profile.d/dependencies-setup.sh
+ source /data/sw/slc4_ia32_gcc345/external/py2-pil/1.1.6/etc/profile.d/init.sh
+ source /data/sw/slc4_ia32_gcc345/external/libpng/1.2.10/etc/profile.d/init.sh
+ source /data/sw/slc4_ia32_gcc345/external/libjpg/6b/etc/profile.d/init.sh
+ source /data/sw/slc4_ia32_gcc345/external/libtiff/3.8.2/etc/profile.d/init.sh
  export QUIET_ASSERT=a
 """
 
