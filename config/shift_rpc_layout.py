b4000b89b7c5ebffe6047da0cf13963b818f4608
David Lomidze <sha1-caee96601a9b34acf0a8be2f22a26eb4dd6cd64d@cern.ch>
2008-06-25 17:41:27 +0000

    shifter layout for rpc

--- config/shift_rpc_layout.py
+++ config/shift_rpc_layout.py
@@ -9,6 +9,18 @@ shiftrpclayout(dqmitems, "01-BarrelClusterSize",
 shiftrpclayout(dqmitems, "02-BarrelNumberOfDigis",
   [{ 'path': "RPC/RecHits/SummaryHistograms/NumberOfDigi_for_Barrel", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
 
-shiftrpclayout(dqmitems, "03-BarrelBX",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/SameBXDigis", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
+shiftrpclayout(dqmitems, "Wheel_+2_Occupancy",
+  [{ 'path': "RPC/RecHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_2", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
 
+shiftrpclayout(dqmitems, "Wheel_+1_Occupancy",
+  [{ 'path': "RPC/RecHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_1", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
+
+shiftrpclayout(dqmitems, "Wheel_0_Occupancy",
+  [{ 'path': "RPC/RecHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_0", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
+
+
+shiftrpclayout(dqmitems, "Wheel_-1_Occupancy",
+  [{ 'path': "RPC/RecHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_-1", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
+
+shiftrpclayout(dqmitems, "Wheel_-2_Occupancy",
+  [{ 'path': "RPC/RecHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_-2", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
