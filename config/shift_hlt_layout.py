91fa91bc762bf8c6ace9e5ba45197488c4c8bd0c
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-06-30 14:00:01 +0000

    included 2d ele/muons plots

--- config/shift_hlt_layout.py
+++ config/shift_hlt_layout.py
@@ -1,5 +1,26 @@
 def hltlayout(i, p, *rows): i["00 Shift/HLT/" + p] = DQMItem(layout=rows)
   
-hltlayout(dqmitems,"00 FourVectors",
-  	[{'path': "HLT/FourVectorHLT/HLT1Electron_etaphi", 'description': "Dummy. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+hltlayout(dqmitems,"00 Filtered ET Muon",
+  	[{'path': "HLT/HLTMonhltMonMu/hltZMML2Filteredet", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
 
+hltlayout(dqmitems,"01 Filtered Eta Muon",
+  	[{'path': "HLT/HLTMonhltMonMu/hltZMML2Filteredeta", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+
+hltlayout(dqmitems,"02 Total Efficiency Muon",
+  	[{'path': "HLT/HLTMonhltMonMu/total eff", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+
+hltlayout(dqmitems,"03 Occupancy Muon",
+  	[{'path': "HLT/HLTMonhltMonMu/hltZMML2Filteredeta_phi", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+
+
+hltlayout(dqmitems,"00 Filtered ET Electron",
+  	[{'path': "HLT/HLTMonhltMonE/hltL1seedRelaxedSingleEt8et", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+
+hltlayout(dqmitems,"01 Filtered Eta Electron",
+  	[{'path': "HLT/HLTMonhltMonE/hltL1seedRelaxedSingleEt8eta", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+
+hltlayout(dqmitems,"02 Total Efficiency Electron",
+  	[{'path': "HLT/HLTMonhltMonE/total eff", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+
+hltlayout(dqmitems,"03 Occupancy Electron",
+  	[{'path': "HLT/HLTMonhltMonE/hltL1seedRelaxedSingleEt8eta_phi", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
