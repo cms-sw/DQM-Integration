c0e9f06019f1ef183babc35aa786a8cbb93e81b1
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2008-07-04 15:54:17 +0000

    for cruzet3- changed search string

--- config/rpc-layouts.py
+++ config/rpc-layouts.py
@@ -6,35 +6,38 @@ rpclayout(dqmitems, "RPC_Summary",
    "RPC/RecHits/SummaryHistograms/SameBXDigis"],
 
   ["RPC/RecHits/SummaryHistograms/ClusterSize_for_Barrel",
-   "RPC/REcHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_0",
-   "RPC/REcHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_1",
-   "RPC/REcHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_2"])
+   "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_0",
+   "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_1",
+   "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_2"])
+
 
 rpclayout(dqmitems, "RPC Cluster size by Wheels",  
-   ["RPC/RecHits/SummaryHistograms/ClusterSize_for_Wheel_0_0",
-   "RPC/RecHits/SummaryHistograms/ClusterSize_for_Wheel_0_1",
-   "RPC/RecHits/SummaryHistograms/ClusterSize_for_Wheel_0_2"])
+   ["RPC/RecHits/SummaryHistograms/ClusterSize_for_Wheel_0",
+   "RPC/RecHits/SummaryHistograms/ClusterSize_for_Wheel_1",
+   "RPC/RecHits/SummaryHistograms/ClusterSize_for_Wheel_2"])
 
-rpclayout(dqmitems, "SectorOccupancy_W1_W0",
-  ["RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_0_Sector_1",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_0_Sector_2",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_0_Sector_3",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_0_Sector_10",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_0_Sector_11",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_0_Sector_12"],
+rpclayout(dqmitems, "SectorOccupancy_W0",
+  ["RPC/RecHits/SummaryHistograms/Occupancy_Wheel_0_Sector_1",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_0_Sector_2",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_0_Sector_3",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_0_Sector_10",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_0_Sector_11",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_0_Sector_12"])
 
-  ["RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_1_Sector_1",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_1_Sector_2",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_1_Sector_3",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_1_Sector_10",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_1_Sector_11",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_1_Sector_12"])
+
+rpclayout(dqmitems, "SectorOccupancy_W1",
+  ["RPC/RecHits/SummaryHistograms/Occupancy_Wheel_1_Sector_1",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_1_Sector_2",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_1_Sector_3",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_1_Sector_10",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_1_Sector_11",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_1_Sector_12"])
 
 rpclayout(dqmitems, "SectorOccupancy_W2",
-  ["RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_2_Sector_10",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_2_Sector_11",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_2_Sector_12",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_2_Sector_1",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_2_Sector_2",
-   "RPC/RecHits/SummaryHistograms/SectorOccupancy_Wheel_2_Sector_3"])
+  ["RPC/RecHits/SummaryHistograms/Occupancy_Wheel_2_Sector_10",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_2_Sector_11",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_2_Sector_12",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_2_Sector_1",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_2_Sector_2",
+   "RPC/RecHits/SummaryHistograms/Occupancy_Wheel_2_Sector_3"])
 
