4f0f48515d1742c33da548cc4e5b8882f1cf32de
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2008-07-04 11:28:02 +0000

    for cruzet3- changed search string

--- config/shift_rpc_layout.py
+++ config/shift_rpc_layout.py
@@ -10,17 +10,17 @@ shiftrpclayout(dqmitems, "02-BarrelNumberOfDigis",
   [{ 'path': "RPC/RecHits/SummaryHistograms/NumberOfDigi_for_Barrel", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
 
 shiftrpclayout(dqmitems, "Wheel_+2_Occupancy",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_2", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
+  [{ 'path': "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_2", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
 
 shiftrpclayout(dqmitems, "Wheel_+1_Occupancy",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_1", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
+  [{ 'path': "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_1", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
 
 shiftrpclayout(dqmitems, "Wheel_0_Occupancy",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_0", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
+  [{ 'path': "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_0", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
 
 
 shiftrpclayout(dqmitems, "Wheel_-1_Occupancy",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_-1", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
+  [{ 'path': "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_-1", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
 
 shiftrpclayout(dqmitems, "Wheel_-2_Occupancy",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/Wheel1DOccupancy_Wheel_0_-2", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
+  [{ 'path': "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_-2", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
