fd82aa7fb6b1f644543be0a64451b0a5ee44c0e8
Hyunkwan Seo <sha1-885d53b681c72a58fe6bb49e412be85d8087d0ef@cern.ch>
2008-07-15 09:19:41 +0000

    activate to execute dqmPostProcess_online.py in real production directory.

--- scripts/alivecheck_dqmPostProcessing.sh
+++ scripts/alivecheck_dqmPostProcessing.sh
@@ -1,14 +1,14 @@
 #! /bin/csh -f
 
-source /cms/mon/data/dqm/filereg_test/setupForFileReg.csh
-set EXE=/cms/mon/data/dqm/filereg_test/dqmPostProcessing_online.py
+source /cms/mon/data/dqm/filereg/setupForFileReg.csh
+set EXE=/cms/mon/data/dqm/filereg/dqmPostProcessing_online.py
 set RUN_STAT = `ps -ef | grep dqmPostProcessing_online.py | grep -v grep | wc | awk '{print $1}'`
 
 if ($RUN_STAT != 0) then
 echo dqmPostProcessing_online.py is running
 else
 echo dqmPostProcessing_online.py stopped by unknown reason and restarted now.
-set LOG=/cms/mon/data/dqm/filereg_test/log/LOG.postprocess.$$
+set LOG=/cms/mon/data/dqm/filereg/log/LOG.postprocess.$$
 $EXE >& $LOG &
 date >> $LOG
 echo dqmPostProcessing_online.py stopped by unknown reason and restarted at $HOST. >> $LOG
