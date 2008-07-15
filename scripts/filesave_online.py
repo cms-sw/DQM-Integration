97521b12433e8ec186280863c78a05f82076a790
Hyunkwan Seo <sha1-885d53b681c72a58fe6bb49e412be85d8087d0ef@cern.ch>
2008-07-15 09:10:35 +0000

    check incomplet files

--- scripts/filesave_online.py
+++ scripts/filesave_online.py
@@ -1,18 +1,53 @@
 #! /usr/bin/env python
 
-import os,time,sys,shutil
+import os,time,sys,shutil,stat,glob
+from sets import Set
+from ROOT import TFile
 
-server = 'srv-c2c06-02' #machine to which files are transfered
-#DIR = '/tmp/dqmpro/output'  #directory to search new files
-#TMPDIR = '/cms/mon/data/.dropbox_tmp' # stealth area on cmsmon
-#FILEDIR = '/cms/mon/data/dropbox' # directory, to which files are stored
-#TimeTag = '/tmp/dqmpro/output/timetag' #file for time tag for searching new file
+def filecheck(rootfile):
+	f = TFile(rootfile)
+	if (f.IsZombie()):
+		#print "File corrupted"
+		f.Close()
+		return 0
+	else:
+		hist = f.FindObjectAny("reportSummaryContents")
+		if (hist == None):
+			#print "File is incomplete"
+			f.Close()
+			return 0
+		else:
+			#print "File is OK"
+			f.Close()
+			return 1
 
-### Fore test and development
+
+def copy2dropbox(org,tmp,final):
+	server = 'srv-c2c06-02' #machine to which files are transfered
+	file_stats = os.stat(org)
+	t1 = time.localtime(file_stats[stat.ST_CTIME])[0:5]
+	t2 = time.localtime()[0:5]
+	while t1 == t2:
+		print t1, t2
+		print 'waiting for file creation is completed...'
+		time.sleep(60)
+		t2 = time.localtime()[0:5]
+	### copy files to stealth area on cmsmon and move to final area
+	os.popen('scp '+org+' '+server+':'+tmp).read()
+	os.popen('ssh '+server+' -t mv '+tmp+' '+final).read()
+		
+						    
+
 DIR = '/home/dqmprolocal/output'  #directory to search new files
 TMPDIR = '/cms/mon/data/.dropbox_tmp' # stealth area on cmsmon
-FILEDIR = '/cms/mon/data/dropbox_test' # directory, to which files are stored
+FILEDIR = '/cms/mon/data/dropbox' # directory, to which files are stored
 TimeTag = '/home/dqmprolocal/output/timetag' #file for time tag for searching new file
+
+### Fore test and development
+#DIR = '/cms/mon/data/dqm/filereg_test/output'  #directory to search new files
+#TMPDIR = '/cms/mon/data/.dropbox_tmp' # stealth area on cmsmon
+#FILEDIR = '/cms/mon/data/dropbox_test' # directory, to which files are stored
+#TimeTag = DIR+'/timetag' #file for time tag for searching new file
 
 WAITTIME = 120 # waiting time for new files (sec)
 
@@ -21,37 +56,71 @@ TempTag = TimeTag + '-tmp'
 if not os.path.exists(TimeTag):
         os.system('touch -t 01010000 '+ TimeTag)
 
+
+
 ####### ENDLESS LOOP WITH SLEEP
 while 1:
-    #### search new files
-    #NEW_ = os.popen('find '+ DIR +'/ -type f -name "DQM_*_R?????????.root" -newer '+ TimeTag +' | grep -v DQM_V').read().split()
-    NEW_ = os.popen('find '+ DIR +'/ -type f -name "DQM_*_R?????????.root" -newer '+ TimeTag).read().split()
+    #### search new tag files
+    #NEW_ = os.popen('find '+ DIR +'/ -type f -name "DQM_*_R?????????.root" -newer '+ TimeTag).read().split()
+    NEW_ = os.popen('find '+ DIR +'/ -type f -name "tagfile_runend_?????_*" -newer '+ TimeTag).read().split()
+    
     if len(NEW_)==0:
         print 'waiting for new files...'
         time.sleep(WAITTIME)
         continue
 
     os.system('touch '+ TempTag)
-    print 'Found '+str(len(NEW_))+' new file(s).'
-    print os.popen('ls -l '+TimeTag).read()
 
-    #### sort new files by run number
+    #print 'Found '+str(len(NEW_))+' new file(s).'
+    #print os.popen('ls -l '+TimeTag).read()
+
+    #### sort tag files by run number
     pairs = []
-    for fname in NEW_:
-        run = fname[-14:-5] #run number of new file
-        pairs.append((run,fname))
+    for tagfile in NEW_:
+	    i = tagfile.find('/tagfile_runend_')
+	    run = tagfile[i+16:i+21] #run number of new tagfile
+	    pairs.append((run,tagfile))
     pairs.sort()
 
-    #### loop for new files
+    #### extract uniq runs
+    Runs = []
     for pair in pairs:
-        fname = pair[1]
-        tmpfile = fname.replace(fname[:fname.find('/DQM_')],TMPDIR)
-        file = fname.replace(fname[:fname.find('/DQM_')],FILEDIR)
-        print os.popen('ls -l '+fname).read()
-        
-        ### copy files to stealth area cmsmon and move to final area
-        os.popen('scp '+fname+' '+server+':'+tmpfile).read()
-        os.popen('ssh '+server+' -t mv '+tmpfile+' '+file).read()
+	    Runs.append(pair[0])
+    UniqRuns = list(Set(Runs)) #remove duplicate run
+    UniqRuns.sort()
+		
+    #### extract new files & copy to dropbox
+    for run in UniqRuns:#loop for runs
+	    subfiles = glob.glob(DIR + '/DQM_*_R????' + run + '_T*.root')
+
+	    ## extract head of file name (ex.'/home/dqmprolocal/output/DQM_SiStrip')
+	    fheads = []
+	    for fname in subfiles:
+		    i = fname.rfind('_R')
+		    fhead = fname[:i]
+		    fheads.append(fhead)
+	    UniqHeads = list(Set(fheads))
+
+	    ## extract single good file for single subsys
+	    for fhead in UniqHeads:
+		    fname = fhead +'_R0000'+run+'.root'
+		    fname_Ts = os.popen('ls -rt '+fhead+'_R????'+run+'_T*.root').read().split()
+		    if os.path.exists(fname): fname_Ts.append(fname)
+		    
+		    numbers = range(len(fname_Ts))
+		    numbers.reverse()
+		    for i in numbers:
+			    fname_T = fname_Ts[i]
+			    if filecheck(fname_T) == 0:
+				    print fname_T +' is incomplete'
+			    else:
+				    print fname_T +' is OK'
+				    tmpfile = fname.replace(fname[:fname.find('/DQM_')],TMPDIR)
+				    file = fname.replace(fname[:fname.find('/DQM_')],FILEDIR)
+				    copy2dropbox(fname_T,tmpfile,file)
+				    #if not fname_T == fname:
+					    #shutil.copy2(fname_T,fname)
+				    break								    
 
     shutil.copy2(TempTag,TimeTag)
     
