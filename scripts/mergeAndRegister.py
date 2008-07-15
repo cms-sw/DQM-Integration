603bd7482f3cb72ef8cd904ec94a84ae671dbea9
Hyunkwan Seo <sha1-885d53b681c72a58fe6bb49e412be85d8087d0ef@cern.ch>
2008-07-15 09:11:42 +0000

    check if merged output exist

--- scripts/mergeAndRegister.py
+++ scripts/mergeAndRegister.py
@@ -2,12 +2,14 @@
 
 import os,time,sys,shutil
 from sets import Set
+from datetime import datetime
 
 def fileunreg(db,bakdb,tmpdb,cmsdb,file,logfile):
-    server = 'srv-c2d05-18'
+    server = 'srv-c2d05-19'
     newdb = db[:-3]+'-new.db'
-    tstamp = time.localtime()
-    bakdb = bakdb+'.'+str(tstamp[0])+'.'+str(tstamp[1])+'.'+str(tstamp[2])+'.'+str(tstamp[3])+':'+str(tstamp[4])+':'+str(tstamp[5])
+    t = datetime.now()
+    tstamp = t.strftime("%Y%m%d_%H%M%S")
+    bakdb = bakdb+'.'+tstamp
     logfile.write(os.popen('scp '+server+ ':'+db+' '+bakdb).read())
     if os.path.exists(tmpdb): os.remove(tmpdb)
     logfile.write(os.popen('scp '+server+ ':'+db+' '+tmpdb).read())
@@ -18,10 +20,11 @@ def fileunreg(db,bakdb,tmpdb,cmsdb,file,logfile):
     logfile.write(os.popen('ssh '+server+' -t mv '+newdb+' '+db).read())
 
 def filereg(db,bakdb,tmpdb,cmsdb,file,logfile):
-    server = 'srv-c2d05-18'
+    server = 'srv-c2d05-19'
     newdb = db[:-3]+'-new.db'
-    tstamp = time.localtime()
-    bakdb = bakdb+'.'+str(tstamp[0])+'.'+str(tstamp[1])+'.'+str(tstamp[2])+'.'+str(tstamp[3])+':'+str(tstamp[4])+':'+str(tstamp[5])
+    t = datetime.now()
+    tstamp = t.strftime("%Y%m%d_%H%M%S")
+    bakdb = bakdb+'.'+tstamp
     logfile.write(os.popen('scp '+server+ ':'+db+' '+bakdb).read())
     if os.path.exists(tmpdb): os.remove(tmpdb)
     logfile.write(os.popen('scp '+server+ ':'+db+' '+tmpdb).read())
@@ -34,24 +37,25 @@ def filereg(db,bakdb,tmpdb,cmsdb,file,logfile):
 
 CMSMON = 'srv-c2c06-02' # machine to which merged file is transfered
 
-#DIR = '/cms/mon/data/dropbox'  # directory to search new files
-#DB = '/home/dqm/dqm.db' #master db
-#BAKDB = '/cms/mon/data/dqm/filereg/backups/dqm.db' #bakcup db (timestamp will be attatched)
-#TMPDB = '/cms/mon/data/dropbox/dqm-tmp.db' # temporal db
-#CMSDB = '/cms/mon/data/dqm/filereg/dqm.db' # master db on CMSMON
-#FILEDIR = '/cms/mon/data/dqm/results' # directory, to which merged file is stored
-#TMPDIR = '/cms/mon/data/.dropbox_tmp' # directory, in which merged file is created
-#TimeTag = '/cms/mon/data/dropbox/timetag' #file for time tag for searching new file
+DIR = '/cms/mon/data/dropbox'  # directory to search new files
+DB = '/home/dqm/dqm.db' #master db
+BAKDB = '/cms/mon/data/dqm/filereg/backups/dqm.db' #bakcup db (timestamp will be attatched)
+TMPDB = '/cms/mon/data/dropbox/dqm-tmp.db' # temporal db
+CMSDB = '/cms/mon/data/dqm/filereg/dqm.db' # master db on CMSMON
+FILEDIR = '/cms/mon/data/dqm/results' # directory, to which merged file is stored
+TMPDIR = '/cms/mon/data/.dropbox_tmp' # directory, in which merged file is created
+TimeTag = '/cms/mon/data/dropbox/timetag' #file for time tag for searching new file
+LOGDIR = '/cms/mon/data/dqm/filereg/log'
 
 #### Directories and files for test and development
-DIR = '/cms/mon/data/dropbox_test'  # directory to search new files
-DB = '/home/dqm/dqm.db' #master db
-BAKDB = '/cms/mon/data/dqm/filereg_test/backups/dqm.db' #bakcup db (timestamp will be attatched)
-TMPDB = '/cms/mon/data/dropbox_test/dqm-tmp.db' # temporal db
-CMSDB = '/cms/mon/data/dqm/filereg_test/dqm.db' # master db on CMSMON
-FILEDIR = '/cms/mon/data/dqm/results_test' # directory, to which merged file is stored
-TMPDIR = '/cms/mon/data/.dropbox_tmp' # directory, in which merged file is created
-TimeTag = '/cms/mon/data/dropbox_test/timetag' #file for time tag for searching new file
+#DIR = '/cms/mon/data/dropbox_test'  # directory to search new files
+#DB = '/home/dqm/dqm.db' #master db
+#BAKDB = '/cms/mon/data/dqm/filereg_test/backups/dqm.db' #bakcup db (timestamp will be attatched)
+#TMPDB = '/cms/mon/data/dropbox_test/dqm-tmp.db' # temporal db
+#CMSDB = '/cms/mon/data/dqm/filereg_test/dqm.db' # master db on CMSMON
+#FILEDIR = '/cms/mon/data/dqm/results_test' # directory, to which merged file is stored
+#TMPDIR = '/cms/mon/data/.dropbox_tmp' # directory, in which merged file is created
+#TimeTag = '/cms/mon/data/dropbox_test/timetag' #file for time tag for searching new file
 
 #SIZE_LIMIT = 5000000000  # size limit for merged file (5~10 Gbyte)
 WAITTIME = 120 # waiting time for new files (sec)
@@ -64,14 +68,15 @@ if not os.path.exists(TimeTag):
 
 
 #### search new files
-#NEW_ = os.popen('find '+ DIR +'/ -type f -name "DQM_*_R?????????.root" -newer '+ TimeTag +' | grep -v DQM_V').read().split()
-NEW_ = os.popen('find '+ DIR +'/ -type f -name "DQM_*_R?????????.root" -newer '+ TimeTag).read().split()
+NEW_ = os.popen('find '+ DIR +'/ -type f -name "DQM_*_R?????????.root" -newer '+ TimeTag +' | grep -v DQM_SiStrip | grep -v DQM_Reference').read().split()
+#NEW_ = os.popen('find '+ DIR +'/ -type f -name "DQM_*_R?????????.root" -newer '+ TimeTag).read().split()
 if len(NEW_)==0:
     print 'Have not found new files...'
     os._exit(99)
 print os.popen('ls -l '+TimeTag).read()
 #os.system('touch '+ TimeTag)
 os.system('touch '+ TempTag)
+print datetime.now()
 print 'Found '+str(len(NEW_))+' new file(s).'
 
 
@@ -150,12 +155,20 @@ for run in UniqRuns:
         if os.path.exists(TMP_MERGED): os.remove(TMP_MERGED)
         LOGFILE = open(TMP_LOG,'a')
         LOGFILE.write(os.popen('visDQMMergeFile '+TMP_MERGED+' '+TMPDIR+'/DQM_*_R'+run+'.root').read())
-        print 'moving merged file to the master directory'
-        #os.system('scp '+TMP_MERGED+' '+CMSMON+':'+MERGED)
-        os.popen('scp '+TMP_MERGED+' '+CMSMON+':'+MERGED)
-        os.remove(TMP_MERGED)
-        os.remove(TMP_OLD_MERGED)
-        
+        if os.path.exists(TMP_MERGED):
+            print 'moving merged file to the master directory'
+            os.popen('scp '+TMP_MERGED+' '+CMSMON+':'+MERGED)
+            os.remove(TMP_MERGED)
+            os.remove(TMP_OLD_MERGED)
+        else:
+            os.remove(TMP_OLD_MERGED)
+            LOGFILE.write('Failed merging files for run '+run)
+            LOGFILE.close()
+            shutil.move(TMP_LOG,LOGDIR)
+            for file in TMP_FILES:
+                os.remove(file)
+            continue
+
         tag = 1
         while tag:
             if os.path.exists(MERGED):
@@ -186,10 +199,19 @@ for run in UniqRuns:
             shutil.copy(TMP_FILES[0],TMP_MERGED)
         else:
             LOGFILE.write(os.popen('visDQMMergeFile '+TMP_MERGED+' '+TMPDIR+'/DQM_*_R'+run+'.root').read())
-        print 'moving merged file to the master directory'
-        os.system('scp '+TMP_MERGED+' '+CMSMON+':'+MERGED)
-        os.remove(TMP_MERGED)
-        
+
+        if os.path.exists(TMP_MERGED):
+            print 'moving merged file to the master directory'
+            os.system('scp '+TMP_MERGED+' '+CMSMON+':'+MERGED)
+            os.remove(TMP_MERGED)
+        else:
+            LOGFILE.write('Failed merging files for run '+run)
+            LOGFILE.close()
+            shutil.move(TMP_LOG,LOGDIR)
+            for file in TMP_FILES:
+                os.remove(file)
+            continue
+                
         tag = 1
         while tag:
             if os.path.exists(MERGED):
