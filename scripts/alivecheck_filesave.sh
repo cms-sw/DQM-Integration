4b5e5361398656ee8aa4a9a5418ab4abbc2272d4
Hyunkwan Seo <sha1-885d53b681c72a58fe6bb49e412be85d8087d0ef@cern.ch>
2008-07-15 09:17:19 +0000

    added host name to the name of logfile

--- scripts/alivecheck_filesave.sh
+++ scripts/alivecheck_filesave.sh
@@ -1,13 +1,14 @@
 #! /bin/csh -f
 
-set EXE=/cmsnfshome0/nfshome0/cmsmon/filereg_test/filesave_online.py
+source /cms/mon/data/dqm/filereg/setupForFileReg.csh
+set EXE=/cms/mon/data/dqm/filereg/filesave_online.py
 set RUN_STAT = `ps -ef | grep filesave_online.py | grep -v grep | wc | awk '{print $1}'`
 
 if ($RUN_STAT != 0) then
 echo filesave_online.py is running at $HOST.
 else
 echo filesave_online.py stopped by unknown reason and restarted now.
-set LOG=/cmsnfshome0/nfshome0/cmsmon/filereg_test/log/LOG.filesave.$$
+set LOG=/cms/mon/data/dqm/filereg/log/LOG.filesave.$HOST.$$
 $EXE >& $LOG &
 date >> $LOG
 echo filesave_online.py stopped by unknown reason and restarted at $HOST. >> $LOG
