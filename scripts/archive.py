e894c5b754b82b73aca1d7a063e2976a338efb75
Sho Maruyama <sha1-b47564b910de3cc3cd657a4905f52d93b10eea83@cern.ch>
2008-07-17 03:22:07 +0000

    Mock Up File Transfer System, plus New DB Format

--- scripts/archive.py
+++ scripts/archive.py
@@ -5,11 +5,17 @@ import os, time
 LOGFILE = open('archival_log.txt', 'a')
 
 #local testing area
-dir = "/afs/cern.ch/user/s/smaruyam/CMSSW_2_0_6/src/DQM/Integration/scripts/tmp/" # Execution Directory, Local now
-db = "/afs/cern.ch/user/s/smaruyam/CMSSW_2_0_6/src/DQM/Integration/scripts/tmp/tmp.db" # master db
-adb = "/afs/cern.ch/user/s/smaruyam/CMSSW_2_0_6/src/DQM/Integration/scripts/tmp/archival.db" # archival db
+dir = "/nfshome0/smaruyam/CMSSW_2_0_10/src/DQM/Integration/scripts/tmp/" # Execution Directory, Local now
+db =  "/nfshome0/smaruyam/CMSSW_2_0_10/src/DQM/Integration/scripts/tmp/tmp.db" # master db
+adb = "/nfshome0/smaruyam/CMSSW_2_0_10/src/DQM/Integration/scripts/tmp/archival.db" # archival db
+transfer_script = "/nfshome0/tier0/scripts/injectFileIntoTransferSystem.pl --type dqm --hostname srv-c2c06-02.cms --lumisection 1 --appname CMSSW --appversion CMSSW_2_0_10 "
+bare_script = "/nfshome0/tier0/scripts/injectFileIntoTransferSystem.pl "
+test_mode = "--destination TransferTest "
+logic_test = "--test "
+mock_up_mode = True
 targetdir = "/castor/cern.ch/user/s/smaruyam/Archive/" # Command for Archival, Local now
 #pnfsdir = "/pnfs/cms/WAX/2/mayaruma/Archive/" # Directory on Tape, dCache now
+Null = "null"
 disk_threshold = 80.0  # disk usage threshold, 80% now
 
 """
@@ -31,7 +37,7 @@ def fileunreg(db,file,logfile):
 Archiving Files
 """
 def Archive() :
-    print "Starting Archiver "
+    print " *** Starting Archiver ***"
     if os.path.exists(adb) is False : CreatePrivateDBForArchival()
 #    count = 0
 #    DiskUsage()   # disabeled for now
@@ -51,7 +57,7 @@ cmsnfshome0:/nfshome0
 # cmsmon is 5th line from top.
 """
 def DiskUsage() :
-    print "Checking Disk Usage"
+    print " *** Checking Disk Usage ***"
     df_file=os.popen('df')
     usage = False
     lines = df_file.readlines()
@@ -73,17 +79,17 @@ check copied file and its size
 rfdir out put is assumed as follows.
 -rw-r--r--   1 smaruyam zh                         10 May 08 22:35 /castor/cern.ch/user/s/smaruyam/Archive/test.txt
 """
-def ConfirmSize(filename, size) :
-    print "Confirming Copied File Size for " , filename
+def ConfirmSize(filename, size) : # Disabled NOW
+    print " *** Confirming Copied File Size for *** " , filename
     time.sleep(10)
  #   ls = "ls -l " + pnfsdir + filename
     ls = "rfdir " + targetdir + filename
 #    print ls
     result = os.popen('%s' %ls)
 #    print result
-    for line in result :
+    for line in result.readlines() :
         error_check = "%s%s: No such file or directory" %(targetdir, filename)
-        if line == error_check :
+        if cmp(line, error_check) == 0 :
             return False
         else :
 #            print line
@@ -100,7 +106,7 @@ def ConfirmSize(filename, size) :
 File Cleaner
 """
 def Cleaner() :
-    print "Cleaning File"
+    print " *** Cleaning File ***"
     flag = True
     archivedfiles = GetListFromMasterDB(flag).readlines()
     if len(archivedfiles) != 0 :
@@ -115,7 +121,7 @@ def Cleaner() :
 Master DataBase Reference
 """
 def GetListFromMasterDB(flag) :
-    print "Getting Merged File List from Master DB"
+    print " *** Getting Merged File List from Master DB ***"
     if flag is False : # Retrive Merged File Info Only
         string = "'" + dir + "DQM_V%_R%.root" + "'"
 #        print string
@@ -135,65 +141,79 @@ def GetListFromMasterDB(flag) :
 Private DataBase Check
 Private DB is asuumed as follows.
 CREATE TABLE t_files (id integer primary key autoincrement, name text unique
-not null, size integer not null, mtime integer not null, atime text not null);
+not null, size integer not null, mtime integer not null, atime text not null, status text null, path text null);
 """
 def CheckPrivateDB(filepath) :
     string = filepath.split('|')
-    print "Checking Archival DB for " , string[0]
+    print " *** Checking Archival DB ***"
     search = "'" + string[0] + "'"
-    sqlite = "sqlite3 %s \"select name, size, mtime, atime from t_files where name like %s \""  %(adb, search)
+    sqlite = "sqlite3 %s \"select status from t_files where name like %s \""  %(adb, search)
     result = os.popen(sqlite)
 #    print result
     check_result = result.readlines()
 #    print " Read Length is " , len(check_result)
     if len(check_result) == 0 :
-        return False # no entry
+        return Null # no entry
     else :
-        return True # entry exist
+        return check_result[0].rstrip() # entry exist
 
 
 """
 Tranfer Merged Files
 """
 def Transfer() :
-    print "Starting File Transfer"
+    print " *** Starting File Transfer ***"
     flag = False
     mergedfiles = GetListFromMasterDB(flag) # Retrieve Merged File List 
-    archived = True
     for file in mergedfiles :
 #        print file
         string = file.split('|')
-        archived = CheckPrivateDB(string[0])
-        if archived is False :
-#        print "Here is Root File List ", mergedfiles
-            SendFile(string[0])
-        else : continue
+	archived = CheckPrivateDB(string[0])
+        if archived is Null :
+            print "Brand New File "
+            SendFile(string[0], archived)
+        elif cmp(archived, "queued") == 0 :
+            print "Queued File "
+            SendFile(string[0], archived)
+        elif cmp(archived, "archived") == 0 : print " Already Archived! "
 
 
 """
 Update Private DB for Archival Information
 """
-def UpdatePrivateDBForArchival(filepath) :
-    print "Updating Archival DB for " , filepath
+def UpdatePrivateDBForArchival(filepath, status, path) :
+    print " *** Updating Archival DB ***"
     name = filepath
     size = os.path.getsize(filepath) 
     mtime = os.path.getmtime(filepath)
     tm = time.localtime(time.time())
     atime = time.strftime("%Y/%m/%d", tm) 
-    sqlite = "sqlite3 %s \"insert into t_files(name, size, mtime, atime) values('%s', '%d', '%d', '%s')\""  %(adb, name, size, mtime, atime)
-    result = os.popen(sqlite)
-    check_result = result.readlines()
+
+    if cmp(status,"queued") == 0 :
+	    sqlite = "sqlite3 %s \"insert into t_files(name, size, mtime, atime, status, path) values('%s', '%d', '%d', '%s', '%s', '%s')\""  %(adb, name, size, mtime, atime, status, path)
+	    result = os.popen(sqlite)
+	    check_result = result.readlines()
 #    print " Read Length is " , len(check_result)
-    if len(check_result) == 0 :
-        return True
-    else : return False
+	    if len(check_result) == 0 :
+	        return True
+	    else : return False
+    if cmp(status,"archived") == 0 :
+	    sqlite = "sqlite3 %s \"update t_files set status = '%s' where name = '%s';\""  %(adb, status, filepath)
+	    result = os.popen(sqlite)
+	    check_result = result.readlines()
+#    print " Read Length is " , len(check_result)
+	    if len(check_result) == 0 :
+	        return True
+	    else : return False
+    if cmp(status,"archived") != 0 and cmp(status,"queued") != 0 : print " ***** Logic Flaw !!!! ****"
+
 
 """
 Create a DB if not present
 """
 def CreatePrivateDBForArchival() :
-    print "Creating Archival DB"
-    sqlite = "sqlite3 %s \"CREATE TABLE t_files (id integer primary key autoincrement, name text unique not null, size integer not null, mtime integer not null, atime text not null); \" " %adb
+    print " *** Creating Archival DB ***"
+    sqlite = "sqlite3 %s \"CREATE TABLE t_files (id integer primary key autoincrement, name text unique not null, size integer not null, mtime integer not null, atime text not null, status text null, path text null); \" " %adb
     result = os.popen(sqlite)
     check_result = result.readlines()
 #    print " Read Length is " , len(check_result)
@@ -203,14 +223,46 @@ def CreatePrivateDBForArchival() :
         print result
         return False
 
+"""
+Mock Up Transfer System
+to avoid creating null entries in db
+"""
+def MockUp(filepath) :
+    print " *** Mock-Up Transfer Mode ***"
+    success_string = "Warning: No filesize supplied (or filesize=0 chosen), but hostname = this machine: using size of file on disk\nDB inserts completed, running Tier 0 notification script\nFile sucessfully submitted for transfer.\n"
+    fail_string = "Warning: No filesize supplied (or filesize=0 chosen), but hostname = this machine: using size of file on disk\nError: Found file in DB. To see its status please execute:\n /nfshome0/tier0/scripts/injectFileIntoTransferSystem.pl --check --filename test_transfer_test.root\n"
+    check = CheckPrivateDB(filepath)
+    if cmp(check, Null) == 0 : return success_string.splitlines()
+    else : return fail_string.splitlines()
+
+""" 
+Mock Up Transfer System 
+to avoid creating null entries in db 
+""" 
+def MockUpStatus(filepath) : 
+    print " *** Mock-Up Status Mode ***"
+    success_string = "FILES_TRANS_NEW: File found in database and being processed by T0 system.\n"
+    fail_string = "File not found in database.\n"
+    check = CheckPrivateDB(filepath) 
+    if cmp(check,Null) == 0 : return fail_string.splitlines() 
+    else : return success_string.splitlines()
+
 
 """
 copy file to dcache or castor
 """
-def SendFile(filepath) :
-    print "Shipping %s to %s" %(filepath, targetdir)
+def SendFile(filepath, status) :
+#    print " Status is " , status
+    path = "null"
+#    print "Shipping %s to %s" %(filepath, targetdir)
 #   Assumed root name =  DQM_Vxx_Ryyyyyyyyy-Rzzzzzzzzz.root
     filename = filepath[-34:] # file name only
+    if status is Null :
+    	print " *** Shipping %s to Castor ***" %filepath
+	nrun = filename[9:-16]
+#    print nrun
+	pathtofile = filepath[:-34]
+#    print pathtofile
 #    print filename
 
 #    cdir = os.popen('pwd')
@@ -219,38 +271,67 @@ def SendFile(filepath) :
 #    print "srmcp -debug==true file:////%s/%s \"%s%s\"" %(pwd, filepath, targetdir, filename)
 
 #    result = os.popen('srmcp -debug==true file:////%s/%s "%s%s"' %(pwd, filepath, targetdir, filename) )
-    result = os.popen('rfcp %s %s' %(filepath, targetdir) )
-#    print result
+	if mock_up_mode == True :
+		result = MockUp(filepath)
+	else : 
+		transfer_string = transfer_script + "--runnumber " + nrun + "--filepath " + pathtofile + "--filename " + filename
+		result = os.popen('%s' %transfer_string ) 
+#	print result
+	check = False
+	for line in result : # ONLY good for mock up mode .readlines() :
+		if cmp(line,"File sucessfully submitted for transfer.") == 0:
+			check = True
+			status = "queued"
+        		print "%s is queued " %filepath
+	        	update = UpdatePrivateDBForArchival(filepath, status, path)
+        		if update is False : print "Updating Private DB Failed !"
+	        	time.sleep(10)
+			break
+		elif cmp(line, "Error: Found file in DB. To see its status please execute:") == 0:
+			print " Transfer Failed, because the file already exists on Tape! If not, you need rename the file name. " , result 
+        	        break
+		else : continue
+	if check == False :
+		print " Transfer Failed, because "  , result
+		return 0
 
+    if cmp(status, "queued") == 0 : 
+    	print " *** Checking File Status ***", filepath
 #    ls = "ls -l " + filename
-    ls = "rfdir %s%s" %(targetdir, filename)
-#    print ls 
-    time.sleep(10)
-    result = os.popen('%s' %ls)
-#    print result 
-    size = ""
-    for line in result.readlines() :
+	if mock_up_mode == True : result = MockUpStatus(filepath)
+	else :    
+		check_status = bare_script + "--check --filename " + filename
+		time.sleep(10)
+		result = os.popen('%s' %check_status)
+#	print result 
+#    size = ""
+	success = False
+	for line in result : # ONLY good for mock up mode  .readlines() :
 #        print line
-        string = line.split()
+		if cmp(line, "FILES_TRANS_NEW: File found in database and being processed by T0 system.") == 0:
+			success = True
+			status = "archived"
+			break
+#        string = line.split()
 #        print string
-        if len(string) == 9 :
+#        if len(string) == 9 :
 #            print string[4]
-            size = string[4]
-    success = False
-    counter = 0
-    while success == False and counter < 5 : # repeat up to five times
-        counter += 1
-        success = ConfirmSize(filename, size)
-        if success is False :  ## now error check srmcp 
-            print "File Transfer Failed. This is %d time(s) Failure: "  %counter 
-        else : continue
-    if success is True :
-            print "%s is copyied " %filepath
-            update = UpdatePrivateDBForArchival(filepath)
-            if update is False : print "Updating Private DB Failed !"
-            time.sleep(10)
-    if success is False : print "Copying File was Failed! "
-
+#            size = string[4]
+#    success = False
+#    counter = 0
+#    while success == False and counter < 5 : # repeat up to five times
+#        counter += 1
+#        success = ConfirmSize(filename, size)
+#        if success is False :  ## now error check srmcp 
+#            print "File Transfer Failed. This is %d time(s) Failure: "  %counter 
+#        else : continue
+	if success is True :
+        	print "%s is copied " %filepath
+	        update = UpdatePrivateDBForArchival(filepath, status, path)
+        	if update is False : print "Updating Private DB Failed !"
+	        time.sleep(10)
+	elif success is False : print "Copying File was Failed! "
+    if cmp(status,Null) != 0 and cmp(status,"queued") != 0 and cmp(status,"archived") is not 0: print " ********** Logic Flaw *** Unreachable !!!! "
 
 """
 Remove file from disk
