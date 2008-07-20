c549fa4c342cdb1a6e0807af1042c56b5119ac73
David Lomidze <sha1-caee96601a9b34acf0a8be2f22a26eb4dd6cd64d@cern.ch>
2008-07-20 14:18:18 +0000

    updated RPC Layouts

--- config/rpc-layouts.py
+++ config/rpc-layouts.py
@@ -1,7 +1,6 @@
 def rpclayout(i, p, *rows): i["RPC/Layouts/" + p] = DQMItem(layout=rows)
 
 rpclayout(dqmitems, "RPC_Summary",
-<<<<<<< rpc-layouts.py
           ["RPC/RecHits/SummaryHistograms/NumberOfClusters_for_Barrel",
            "RPC/RecHits/SummaryHistograms/NumberOfDigi_for_Barrel"],
           
@@ -128,105 +127,4 @@ rpclayout(dqmitems, "SectorOccupancy_WWheel_-2",
           ["RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_10",
            "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_11",
            "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_12"])
-=======
-  ["RPC/RecHits/SummaryHistograms/NumberOfClusters_for_Barrel",
-   "RPC/RecHits/SummaryHistograms/NumberOfDigi_for_Barrel",
-   "RPC/RecHits/SummaryHistograms/SameBXDigis"],
-
-  ["RPC/RecHits/SummaryHistograms/ClusterSize_for_Barrel",
-   "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_0",
-   "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_1",
-   "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_2"])
-
-rpclayout(dqmitems, "RPC Cluster size by Wheels",
-   ["RPC/RecHits/SummaryHistograms/ClusterSize_for_Wheel_0",
-   "RPC/RecHits/SummaryHistograms/ClusterSize_for_Wheel_1",
-   "RPC/RecHits/SummaryHistograms/ClusterSize_for_Wheel_2"])
-
-rpclayout(dqmitems, "SectorOccupancy_WWheel_2",
-  ["RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_1",
-   "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_2",
-   "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_3"],
-
-  ["RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_4",
-   "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_5",
-   "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_6"],
-
-  ["RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_7",
-   "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_8",
-   "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_9"],
-
-  ["RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_10",
-   "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_11",
-   "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_12"])
-
-rpclayout(dqmitems, "SectorOccupancy_WWheel_1",
-  ["RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_1",
-   "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_2",
-   "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_3"],
-
-  ["RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_4",
-   "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_5",
-   "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_6"],
-
-  ["RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_7",
-   "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_8",
-   "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_9"],
-
-  ["RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_10",
-   "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_11",
-   "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_12"])
-
-
-rpclayout(dqmitems, "SectorOccupancy_WWheel_0",
-  ["RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_1",
-   "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_2",
-   "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_3"],
-
-  ["RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_4",
-   "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_5",
-   "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_6"],
-
-  ["RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_7",
-   "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_8",
-   "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_9"],
-
-  ["RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_10",
-   "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_11",
-   "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_12"])
-
-rpclayout(dqmitems, "SectorOccupancy_WWheel_-1",
-  ["RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_1",
-   "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_2",
-   "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_3"],
-
-  ["RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_4",
-   "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_5",
-   "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_6"],
-
-  ["RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_7",
-   "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_8",
-   "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_9"],
-
-  ["RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_10",
-   "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_11",
-   "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_12"])
-
-rpclayout(dqmitems, "SectorOccupancy_WWheel_-2",
-  ["RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_1",
-   "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_2",
-   "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_3"],
-
-  ["RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_4",
-   "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_5",
-   "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_6"],
-
-  ["RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_7",
-   "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_8",
-   "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_9"],
-
-  ["RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_10",
-   "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_11",
-   "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_12"])
->>>>>>> 1.13
 
--- config/shift_rpc_layout.py
+++ config/shift_rpc_layout.py
@@ -2,25 +2,3 @@ def shiftrpclayout(i, p, *rows): i["00 Shift/RPC/" + p] = DQMItem(layout=rows)
 
 shiftrpclayout(dqmitems, "00-BarrelOccupancy",
   [{ 'path': "RPC/RecHits/SummaryHistograms/BarrelOccupancy", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
-
-shiftrpclayout(dqmitems, "01-BarrelClusterSize",
-  [{ 'path': "RPC/RECHits/SummaryHistograms/ClusterSize_for_Barrel" , 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
-
-shiftrpclayout(dqmitems, "02-BarrelNumberOfDigis",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/NumberOfDigi_for_Barrel", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
-
-shiftrpclayout(dqmitems, "Wheel_+2_Occupancy",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_2", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
-
-shiftrpclayout(dqmitems, "Wheel_+1_Occupancy",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_1", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
-
-shiftrpclayout(dqmitems, "Wheel_0_Occupancy",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_0", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
-
-
-shiftrpclayout(dqmitems, "Wheel_-1_Occupancy",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_-1", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
-
-shiftrpclayout(dqmitems, "Wheel_-2_Occupancy",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/1DOccupancy_Wheel_-2", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
