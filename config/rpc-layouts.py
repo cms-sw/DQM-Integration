ab8747efe7fd7986e02c3a18009d86256c59c099
David Lomidze <sha1-caee96601a9b34acf0a8be2f22a26eb4dd6cd64d@cern.ch>
2008-07-23 14:49:57 +0000

    updater RPC Layouts

--- config/rpc-layouts.py
+++ config/rpc-layouts.py
@@ -42,7 +42,7 @@ rpclayout(dqmitems, "RPC_Barrel_XY_Occupancy",
            "RPC/RecHits/SummaryHistograms/OccupancyXY_Wheel_-1",
            "RPC/RecHits/SummaryHistograms/OccupancyXY_Wheel_-2"])
 
-rpclayout(dqmitems, "SectorOccupancy_WWheel_2",
+rpclayout(dqmitems, "SectorOccupancy_Wheel_2",
           ["RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_1",
            "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_2",
            "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_3"],
@@ -59,7 +59,7 @@ rpclayout(dqmitems, "SectorOccupancy_WWheel_2",
            "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_11",
            "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/Occupancy_Wheel_2_Sector_12"])
 
-rpclayout(dqmitems, "SectorOccupancy_WWheel_1",
+rpclayout(dqmitems, "SectorOccupancy_Wheel_1",
           ["RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_1",
            "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_2",
            "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_3"],
@@ -77,7 +77,7 @@ rpclayout(dqmitems, "SectorOccupancy_WWheel_1",
            "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/Occupancy_Wheel_1_Sector_12"])
 
 
-rpclayout(dqmitems, "SectorOccupancy_WWheel_0",
+rpclayout(dqmitems, "SectorOccupancy_Wheel_0",
           ["RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_1",
            "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_2",
            "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_3"],
@@ -94,7 +94,7 @@ rpclayout(dqmitems, "SectorOccupancy_WWheel_0",
            "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_11",
            "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/Occupancy_Wheel_0_Sector_12"])
 
-rpclayout(dqmitems, "SectorOccupancy_WWheel_-1",
+rpclayout(dqmitems, "SectorOccupancy_Wheel_-1",
           ["RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_1",
            "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_2",
            "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_3"],
@@ -111,7 +111,7 @@ rpclayout(dqmitems, "SectorOccupancy_WWheel_-1",
            "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_11",
            "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/Occupancy_Wheel_-1_Sector_12"])
 
-rpclayout(dqmitems, "SectorOccupancy_WWheel_-2",
+rpclayout(dqmitems, "SectorOccupancy_Wheel_-2",
           ["RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_1",
            "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_2",
            "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_3"],
@@ -128,3 +128,89 @@ rpclayout(dqmitems, "SectorOccupancy_WWheel_-2",
            "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_11",
            "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/Occupancy_Wheel_-2_Sector_12"])
 
+rpclayout(dqmitems, "SectorClusterSize_Wheel_2",
+          ["RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/ClusterSize_Wheel_2_Sector_1",
+           "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/ClusterSize_Wheel_2_Sector_2",
+           "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/ClusterSize_Wheel_2_Sector_3"],
+          
+          ["RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/ClusterSize_Wheel_2_Sector_4",
+           "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/ClusterSize_Wheel_2_Sector_5",
+           "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/ClusterSize_Wheel_2_Sector_6"],
+          
+          ["RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/ClusterSize_Wheel_2_Sector_7",
+           "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/ClusterSize_Wheel_2_Sector_8",
+           "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/ClusterSize_Wheel_2_Sector_9"],
+          
+          ["RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/ClusterSize_Wheel_2_Sector_10",
+           "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/ClusterSize_Wheel_2_Sector_11",
+           "RPC/RecHits/Barrel/Wheel_2/SummaryBySectors/ClusterSize_Wheel_2_Sector_12"])
+
+rpclayout(dqmitems, "SectorClusterSize_Wheel_1",
+          ["RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/ClusterSize_Wheel_1_Sector_1",
+           "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/ClusterSize_Wheel_1_Sector_2",
+           "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/ClusterSize_Wheel_1_Sector_3"],
+          
+          ["RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/ClusterSize_Wheel_1_Sector_4",
+           "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/ClusterSize_Wheel_1_Sector_5",
+           "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/ClusterSize_Wheel_1_Sector_6"],
+          
+          ["RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/ClusterSize_Wheel_1_Sector_7",
+           "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/ClusterSize_Wheel_1_Sector_8",
+           "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/ClusterSize_Wheel_1_Sector_9"],
+          
+          ["RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/ClusterSize_Wheel_1_Sector_10",
+           "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/ClusterSize_Wheel_1_Sector_11",
+           "RPC/RecHits/Barrel/Wheel_1/SummaryBySectors/ClusterSize_Wheel_1_Sector_12"])
+
+rpclayout(dqmitems, "SectorClusterSize_Wheel_0",
+          ["RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/ClusterSize_Wheel_0_Sector_1",
+           "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/ClusterSize_Wheel_0_Sector_2",
+           "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/ClusterSize_Wheel_0_Sector_3"],
+          
+          ["RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/ClusterSize_Wheel_0_Sector_4",
+           "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/ClusterSize_Wheel_0_Sector_5",
+           "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/ClusterSize_Wheel_0_Sector_6"],
+          
+          ["RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/ClusterSize_Wheel_0_Sector_7",
+           "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/ClusterSize_Wheel_0_Sector_8",
+           "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/ClusterSize_Wheel_0_Sector_9"],
+          
+          ["RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/ClusterSize_Wheel_0_Sector_10",
+           "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/ClusterSize_Wheel_0_Sector_11",
+           "RPC/RecHits/Barrel/Wheel_0/SummaryBySectors/ClusterSize_Wheel_0_Sector_12"])
+
+
+
+rpclayout(dqmitems, "SectorClusterSize_Wheel_-1",
+          ["RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/ClusterSize_Wheel_-1_Sector_1",
+           "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/ClusterSize_Wheel_-1_Sector_2",
+           "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/ClusterSize_Wheel_-1_Sector_3"],
+          
+          ["RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/ClusterSize_Wheel_-1_Sector_4",
+           "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/ClusterSize_Wheel_-1_Sector_5",
+           "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/ClusterSize_Wheel_-1_Sector_6"],
+          
+          ["RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/ClusterSize_Wheel_-1_Sector_7",
+           "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/ClusterSize_Wheel_-1_Sector_8",
+           "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/ClusterSize_Wheel_-1_Sector_9"],
+          
+          ["RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/ClusterSize_Wheel_-1_Sector_10",
+           "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/ClusterSize_Wheel_-1_Sector_11",
+           "RPC/RecHits/Barrel/Wheel_-1/SummaryBySectors/ClusterSize_Wheel_-1_Sector_12"])
+
+rpclayout(dqmitems, "SectorClusterSize_Wheel_-2",
+          ["RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/ClusterSize_Wheel_-2_Sector_1",
+           "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/ClusterSize_Wheel_-2_Sector_2",
+           "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/ClusterSize_Wheel_-2_Sector_3"],
+          
+          ["RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/ClusterSize_Wheel_-2_Sector_4",
+           "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/ClusterSize_Wheel_-2_Sector_5",
+           "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/ClusterSize_Wheel_-2_Sector_6"],
+          
+          ["RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/ClusterSize_Wheel_-2_Sector_7",
+           "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/ClusterSize_Wheel_-2_Sector_8",
+           "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/ClusterSize_Wheel_-2_Sector_9"],
+          
+          ["RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/ClusterSize_Wheel_-2_Sector_10",
+           "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/ClusterSize_Wheel_-2_Sector_11",
+           "RPC/RecHits/Barrel/Wheel_-2/SummaryBySectors/ClusterSize_Wheel_-2_Sector_12"])
