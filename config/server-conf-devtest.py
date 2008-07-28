dfc86833eb6ff2e96b08f67ffb9b00d7d217d1ba
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2008-07-28 14:41:12 +0000

    Update for V4 server.

--- config/server-conf-devtest.py
+++ config/server-conf-devtest.py
@@ -2,15 +2,19 @@ import pwd, os, os.path
 global CONFIGDIR
 CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
 LAYOUTS = ["%s/%s-layouts.py" % (CONFIGDIR, x) for x in
-	   ("csc", "dt", "eb", "ecal", "ee", "hcal", "l1t", "rpc")]
+	   ("csc", "dt", "eb", "hcal", "hlt", "l1t", "l1temulator", "rpc", "pixel", "sistrip")]
 LAYOUTS += ["%s/shift_%s_layout.py" % (CONFIGDIR, x) for x in
-            ("dt", "eb", "hcal", "l1t", "l1temulator", "hlt", "rpc")]
+            ("csc", "dt", "eb", "hcal", "hlt", "l1t", "l1temulator", "rpc", "pixel", "sistrip")]
 
-modules = ("GuiDQM",)
+modules = ("GuiDQM", "GuiEventDisplay")
 basedir = "/tmp/$USER/dqmgui".replace("$USER", pwd.getpwuid(os.geteuid())[0])
 envsetup = """
  source $BASE/rpms/cmsset_default.sh
  source $BASE/rpms/slc4_ia32_gcc345/cms/webtools/1.3.0/etc/profile.d/dependencies-setup.sh
+ source $BASE/rpms/slc4_ia32_gcc345/external/py2-pil/1.1.6/etc/profile.d/init.sh
+ source $BASE/rpms/slc4_ia32_gcc345/external/libpng/1.2.10/etc/profile.d/init.sh
+ source $BASE/rpms/slc4_ia32_gcc345/external/libjpg/6b/etc/profile.d/init.sh
+ source $BASE/rpms/slc4_ia32_gcc345/external/libtiff/3.8.2/etc/profile.d/init.sh
  export QUIET_ASSERT=a
 """.replace("$BASE", basedir)
 
@@ -20,9 +24,9 @@ server.baseUrl     = '/dqm/devtest'
 server.title       = 'CMS data quality'
 server.serviceName = 'GUI test'
 
-server.source('dqm', 'DQMLive', '--listen 9191', '--collector localhost:9190')
-server.source('file', 'DQMArchive', basedir + "/dqm.db", '--listen 9192')
-server.source('layouts', 'DQMLayout', *LAYOUTS)
+server.source('DQMLive', 'dqm', '--listen 9191', '--collector localhost:9190')
+server.source('DQMArchive', 'file', basedir + "/dqm.db", '--listen 9192')
+server.source('DQMLayout', 'layout', *LAYOUTS)
 
 execfile(CONFIGDIR + "/dqm-services.py")
 execfile(CONFIGDIR + "/workspaces-online.py")
