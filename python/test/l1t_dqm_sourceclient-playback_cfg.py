92e6123f15be029fda845a59f8a09bc6f85aaf90
Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch>
2008-08-08 11:44:51 +0000

    updated url config

--- python/test/l1t_dqm_sourceclient-playback_cfg.py
+++ python/test/l1t_dqm_sourceclient-playback_cfg.py
@@ -24,28 +24,20 @@ process.load("DQM.Integration.test.environment_playback_cfi")
 #-----------------------------
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
-process.load("Geometry.CSCGeometry.cscGeometry_cfi")
+process.load("Configuration.StandardSequences.Geometry_cff")
 
 process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
 
-process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
-
-process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
-
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-#process.GlobalTag.connect="oracle://cms_orcon_prod/CMS_COND_21X_GLOBALTAG"
-#process.GlobalTag.DBParameters.authenticationPath="/nfshome0/xiezhen/conddb"
 process.GlobalTag.connect = "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_20X_GLOBALTAG"   
 
-#process.GlobalTag.globaltag = 'IDEAL_V5::All' # or any other appropriate
 process.GlobalTag.globaltag = 'CRUZET3_V6H2::All' # or any other appropriate
 process.prefer("GlobalTag")
 
-
 process.load("DQM.L1TMonitor.L1TMonitor_cff")
-
 process.load("DQM.L1TMonitorClient.L1TMonitorClient_cff")
 
 process.EventStreamHttpReader.consumerName = 'L1T DQM Consumer'
 process.dqmEnv.subSystemFolder = 'L1T'
 
+process.source.sourceURL  = cms.string("http://srv-c2c05-27:50082/urn:xdaq-application:lid=29") 
