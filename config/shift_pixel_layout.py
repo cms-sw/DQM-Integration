e707d060f2ae7818acebc15f9a83cb8ab96cd0a0
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2008-08-12 11:15:14 +0000

    new layouts for CRUZET4

--- config/shift_pixel_layout.py
+++ config/shift_pixel_layout.py
@@ -1,13 +1,11 @@
 def pixellayout(i, p, *rows): i["00 Shift/Pixel/" + p] = DQMItem(layout=rows)
-pixellayout(dqmitems, "Pixel_DIGI_Summary",
-  ["Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMDIG_adc_Panel_2",
-   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMDIG_ndigis_Panel_2"])
 pixellayout(dqmitems, "Pixel_FEDError_Summary",   
-  ["Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_NErrors_Panel_2",
-   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_errorType_Panel_2",
-   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_TBMType_Panel_2"],
-  ["Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_EvtNbr_Panel_2",
-   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_ROCNmbr_Panel_2"],
-  ["Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_ROCId_Panel_2",
-   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_DCOLId_Panel_2",
-   "Pixel/Endcap/HalfCylinder_pO/Disk_1/Blade_09/Panel_2/SUMRAW_PXId_Panel_2"])
+  ["Pixel/Endcap/SUMRAW_NErrors_Endcap",
+   "Pixel/Endcap/SUMRAW_errorType_Endcap"])
+pixellayout(dqmitems, "Pixel_DIGI_Summary",
+  ["Pixel/Endcap/SUMDIG_adc_Endcap",
+   "Pixel/Endcap/SUMDIG_ndigis_Endcap"])
+pixellayout(dqmitems, "Pixel_CLUSTER_Summary",
+  ["Pixel/Endcap/SUMCLU_charge_Endcap",
+   "Pixel/Endcap/SUMCLU_nclusters_Endcap",
+   "Pixel/Endcap/SUMCLU_size_Endcap"])
