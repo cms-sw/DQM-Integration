a31115c7cd00b55559cc0446a139b9fc75f1f4d5
Wee Don Teo <sha1-14512a3b4364601904b1c582c7b9ac2ded235b1a@cern.ch>
2008-08-12 07:14:21 +0000

    updated global tag

--- python/test/l1t_dqm_sourceclient-file_cfg.py
+++ python/test/l1t_dqm_sourceclient-file_cfg.py
@@ -16,7 +16,7 @@ process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.connect = "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_20X_GLOBALTAG" 	
 #process.GlobalTag.globaltag = 'CRUZET3_V6H::All' # or any other appropriate
-process.GlobalTag.globaltag = 'CRUZET3_V6H2::All' # or any other appropriate
+process.GlobalTag.globaltag = 'CRUZET3_V6H7::All' # or any other appropriate
 #process.GlobalTag.globaltag = 'IDEAL_V5::All' # or any other appropriate
 #process.es_prefer = cms.ESPrefer("GlobalTag")
 process.prefer("GlobalTag")
