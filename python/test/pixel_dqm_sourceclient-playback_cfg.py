42079c994855bf8a90f458f336cfd1d10bf9ce68
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2008-08-12 10:32:33 +0000

    fix of DB

--- python/test/pixel_dqm_sourceclient-playback_cfg.py
+++ python/test/pixel_dqm_sourceclient-playback_cfg.py
@@ -44,8 +44,12 @@ process.load("Configuration.GlobalRuns.ForceZeroTeslaField_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_PIXEL"
-process.GlobalTag.globaltag = "CRZT210_V1C::All"
+
+process.GlobalTag.connect = "sqlite_file:/nfshome0/malgeri/public/globtag/CRZT210_V1H.db"
+process.GlobalTag.globaltag = "CRZT210_V1CH::All"
+
+#process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_PIXEL"
+#process.GlobalTag.globaltag = "CRZT210_V1C::All"
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 #If Frontier is used in xdaq environment use the following service
