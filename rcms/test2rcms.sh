09afac0808f8e3c836bbf087ed5529e290605c6b
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2008-08-13 10:41:39 +0000

    fix sed statement to work multiple times per line

--- rcms/test2rcms.sh
+++ rcms/test2rcms.sh
@@ -6,15 +6,14 @@ for i in ecal hcal sistrip pixel hlt l1t l1temulator rpc dt csc hlx ; do
 cfgfile=$i\_$name.cfg
 pyfile=$i\_$name\_cfg.py
 echo "==================="
-echo removing $cfgfile in 2 seconds
-sleep 2
+
+if [ -e $pyfile ] ; then
+echo removing $cfgfile
 rm $cfgfile
 echo recreating $cfgfile from $pyfile
-if [ -e $pyfile ] ; then
 cat >> $pyfile << EOF
 print process.dumpConfig()
 EOF
-
 python $pyfile > $cfgfile
 
 sed -i 's/srv-c2d05-18/srv-c2d05-19/' $cfgfile
@@ -24,8 +23,8 @@ sed -i 's/DQM\/Integration\/test\/inputsource.cfi/DQM\/Integration\/rcms\/inputs
 sed -i 's/DQM\/Integration\/test\/environment.cfi/DQM\/Integration\/rcms\/environment.cfi/' $cfgfile 
 
 #fix for sequenceplaceholder
-sed -i 's/cms.SequencePlaceholder("//' $cfgfile
-sed -i 's/")//' $cfgfile
+sed -i 's/cms.SequencePlaceholder("//g' $cfgfile
+sed -i 's/")//g' $cfgfile
 
 
 echo removing $pyfile
