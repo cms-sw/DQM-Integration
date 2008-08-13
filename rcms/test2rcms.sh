b0bb92a84da433595881efb6846e6d6e11d4d23a
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2008-08-13 03:13:36 +0000

    fix for bug in parameterSet for python to cfg conversion

--- rcms/test2rcms.sh
+++ rcms/test2rcms.sh
@@ -6,7 +6,10 @@ for i in ecal hcal sistrip pixel hlt l1t l1temulator rpc dt csc hlx ; do
 cfgfile=$i\_$name.cfg
 pyfile=$i\_$name\_cfg.py
 echo "==================="
-echo creating $cfgfile from $pyfile
+echo removing $cfgfile in 2 seconds
+sleep 2
+rm $cfgfile
+echo recreating $cfgfile from $pyfile
 if [ -e $pyfile ] ; then
 cat >> $pyfile << EOF
 print process.dumpConfig()
@@ -19,6 +22,11 @@ sed -i 's/dqmdev/dqmpro/' $cfgfile
 sed -i 's/cmsmon:50082\/urn:xdaq-application:lid=29/srv-c2d05-14.cms:22100\/urn:xdaq-application:lid=30/' $cfgfile 
 sed -i 's/DQM\/Integration\/test\/inputsource.cfi/DQM\/Integration\/rcms\/inputsource.cfi/' $cfgfile
 sed -i 's/DQM\/Integration\/test\/environment.cfi/DQM\/Integration\/rcms\/environment.cfi/' $cfgfile 
+
+#fix for sequenceplaceholder
+sed -i 's/cms.SequencePlaceholder("//' $cfgfile
+sed -i 's/")//' $cfgfile
+
 
 echo removing $pyfile
 rm $pyfile
