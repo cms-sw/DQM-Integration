c4e9bf4a64dc72362df6e9ea338304465fd1dcce
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2008-08-13 01:20:25 +0000

    scripts for installaton and python conversion

---
+++ rcms/installrcms.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ln -s  daq_dqm_sourceclient-live.cfg           dqmfu10-1.cfg 
+ln -s  sistrip_dqm_sourceclient-live.cfg       dqmfu15-1.cfg 
+ln -s  pixel_dqm_sourceclient-live.cfg         dqmfu15-2.cfg 
+ln -s  shipper_dqm_sourceclient-live.cfg       dqmfu15-3.cfg 
+ln -s  hlx_dqm_sourceclient-live.cfg           dqmfu15-4.cfg 
+ln -s  ecal_dqm_sourceclient-live.cfg          dqmfu16-1.cfg 
+ln -s  hcal_dqm_sourceclient-live.cfg          dqmfu16-2.cfg 
+ln -s  dt_dqm_sourceclient-live.cfg            dqmfu16-3.cfg 
+ln -s  hlt_dqm_sourceclient-live.cfg           dqmfu16-4.cfg 
+ln -s  csc_dqm_sourceclient-live.cfg           dqmfu17-1.cfg 
+ln -s  l1t_dqm_sourceclient-live.cfg           dqmfu17-2.cfg 
+ln -s  l1temulator_dqm_sourceclient-live.cfg   dqmfu17-4.cfg 
+
--- rcms/test2rcms.sh
+++ rcms/test2rcms.sh
@@ -1,10 +1,30 @@
 #/bin/bash
 
-for i in *live.cfg ;  do
-ls $i
-sed -i 's/srv-c2d05-18/srv-c2d05-19/' $i 
-sed -i 's/dqmdev/dqmpro/' $i  
-sed -i 's/cmsmon:50082\/urn:xdaq-application:lid=29/srv-c2d05-14.cms:22100\/urn:xdaq-application:lid=30/' $i 
-sed -i 's/DQM\/Integration\/test\/inputsource.cfi/DQM\/Integration\/rcms\/inputsource.cfi/' $i 
-sed -i 's/DQM\/Integration\/test\/environment.cfi/DQM\/Integration\/rcms\/environment.cfi/' $i 
+name=dqm_sourceclient-live
+
+for i in ecal hcal sistrip pixel hlt l1t l1temulator rpc dt csc hlx ; do
+cfgfile=$i\_$name.cfg
+pyfile=$i\_$name\_cfg.py
+echo "==================="
+echo creating $cfgfile from $pyfile
+if [ -e $pyfile ] ; then
+cat >> $pyfile << EOF
+print process.dumpConfig()
+EOF
+
+python $pyfile > $cfgfile
+
+sed -i 's/srv-c2d05-18/srv-c2d05-19/' $cfgfile
+sed -i 's/dqmdev/dqmpro/' $cfgfile
+sed -i 's/cmsmon:50082\/urn:xdaq-application:lid=29/srv-c2d05-14.cms:22100\/urn:xdaq-application:lid=30/' $cfgfile 
+sed -i 's/DQM\/Integration\/test\/inputsource.cfi/DQM\/Integration\/rcms\/inputsource.cfi/' $cfgfile
+sed -i 's/DQM\/Integration\/test\/environment.cfi/DQM\/Integration\/rcms\/environment.cfi/' $cfgfile 
+
+echo removing $pyfile
+rm $pyfile
+
+else
+echo $pyfile does not exist
+fi
+
 done
