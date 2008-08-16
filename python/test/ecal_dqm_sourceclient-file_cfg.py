7547f8917a4ea3a638f3e7cd9dc21e2345c3cf22
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-08-16 13:20:58 +0000

    - cosmetics

--- python/test/ecal_dqm_sourceclient-file_cfg.py
+++ python/test/ecal_dqm_sourceclient-file_cfg.py
@@ -94,7 +94,7 @@ process.source = cms.Source("NewEventStreamFileReader",
 )
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG"
+process.GlobalTag.connect = "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG"
 process.GlobalTag.globaltag = "CRZT210_V3H::All"
 process.prefer("GlobalTag")
 
