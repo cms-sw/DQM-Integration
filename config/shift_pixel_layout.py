7f6101933391ddecd3c916e6c12470d25d9cd5e7
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2008-08-19 01:18:21 +0000

    added link to PIxel shift instructions

--- config/shift_pixel_layout.py
+++ config/shift_pixel_layout.py
@@ -1,9 +1,9 @@
 def pixellayout(i, p, *rows): i["00 Shift/Pixel/" + p] = DQMItem(layout=rows)
 pixellayout(dqmitems, "Pixel_FEDError_Summary",   
-  ["Pixel/Endcap/SUMRAW_NErrors_Endcap",
-   "Pixel/Endcap/SUMRAW_errorType_Endcap"],
-  ["Pixel/AdditionalPixelErrors/SUMRAW_NErrors_FEDErrors",
-   "Pixel/AdditionalPixelErrors/SUMRAW_errorType_FEDErrors"])
+  [{ 'path': "Pixel/Endcap/SUMRAW_NErrors_Endcap", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"},
+   { 'path': "Pixel/Endcap/SUMRAW_errorType_Endcap", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"}],
+  [{ 'path': "Pixel/AdditionalPixelErrors/SUMRAW_NErrors_FEDErrors", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"},
+   { 'path': "Pixel/AdditionalPixelErrors/SUMRAW_errorType_FEDErrors", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"}])
 pixellayout(dqmitems, "Pixel_DIGI_Summary",
-  ["Pixel/Endcap/SUMDIG_adc_Endcap",
-   "Pixel/Endcap/SUMDIG_ndigis_Endcap"])
+  [{ 'path': "Pixel/Endcap/SUMDIG_adc_Endcap", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"},
+   { 'path': "Pixel/Endcap/SUMDIG_ndigis_Endcap", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"}])
