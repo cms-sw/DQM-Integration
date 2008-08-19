2585584007075f681bf75a78a18fa88abfccfb5b
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2008-08-19 12:23:00 +0000

    Add event display upload facility.

--- config/server-conf-online.py
+++ config/server-conf-online.py
@@ -22,6 +22,8 @@ server.baseUrl     = '/dqm/online'
 server.title       = 'CMS data quality'
 server.serviceName = 'Online'
 
+server.extend('EVDSnapshotUpload', '/home/dqm/iguana-snapshots')
+server.source('EVDSnapshot', 'evd', '/home/dqm/iguana-snapshots')
 server.source('DQMLive', 'dqm', '--listen 9091', '--collector localhost:9090')
 server.source('DQMArchive', 'file', '/home/dqm/dqm.db', '--listen 9097')
 server.source('DQMLayout', 'layouts', *LAYOUTS)
