3dded5f6f7aa01635c6c4810d2e9e9c6176d609f
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2008-08-20 11:34:51 +0000

    new GlobalTag

--- python/test/pixel_dqm_sourceclient-file_cfg.py
+++ python/test/pixel_dqm_sourceclient-file_cfg.py
@@ -53,7 +53,7 @@ process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG"
 #process.GlobalTag.globaltag = "CRZT210_V1C::All"
 #process.GlobalTag.globaltag = "CRZT210_V1H::All"
-process.GlobalTag.globaltag = "CRUZET4_V1H::All"
+process.GlobalTag.globaltag = "CRUZET4_V2H::All"
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 #If Frontier is used in xdaq environment use the following service
