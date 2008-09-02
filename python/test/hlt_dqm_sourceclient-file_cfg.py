466c0811b7bab6c33ddaf6950eb46a0b76b93264
Peter Wittich <sha1-1c129fc6f1e49c3fc8fb03accebf4e9c0677cc9f@cern.ch>
2008-09-02 13:24:54 +0000

    add scaler client to test file

--- python/test/hlt_dqm_sourceclient-file_cfg.py
+++ python/test/hlt_dqm_sourceclient-file_cfg.py
@@ -8,11 +8,27 @@ process.load("DQM.HLTEvF.HLTMonitor_cff")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 
 process.maxEvents = cms.untracked.PSet(
+#    input = cms.untracked.int32(100)
     input = cms.untracked.int32(-1)
 )
-process.source = cms.Source("NewEventStreamFileReader",
-    fileNames = cms.untracked.vstring('file:/data1/lookarea/GlobalAug07.00017220.0001.A.storageManager.0.0000.dat')
+process.source = cms.Source("PoolSource",
+    debugVerbosity = cms.untracked.uint32(1),
+    debugVebosity = cms.untracked.bool(True),
+    fileNames = cms.untracked.vstring('/store/data/Commissioning08/HLTDebug/RAW/CRUZET4_v1/000/058/042/0ECC6BE3-5F6F-DD11-A328-0019DB29C614.root')
 )
+#process.source = cms.Source("NewEventStreamFileReader",
+#    fileNames = cms.untracked.vstring('file:/data1/lookarea/GlobalAug07.00017220.0001.A.storageManager.0.0000.dat')
+#)
+
+process.tsumm = cms.EDAnalyzer("TriggerSummaryAnalyzerAOD")
+process.tsumm.inputTag = cms.InputTag("hltTriggerSummaryAOD","","HLT")
+
+process.hltrep = cms.EDAnalyzer("HLTrigReport")
+process.hltrep.HLTriggerResults = cms.InputTag("TriggerResults","","HLT")
+process.ptsum = cms.Path(process.tsumm+process.hltrep)
+
+process.p = cms.EndPath(process.hlts+process.hltsClient)
+
 
 process.pp = cms.Path(process.dqmEnv+process.dqmSaver)
 process.DQMStore.verbose = 0
@@ -26,3 +42,37 @@ process.dqmEnv.subSystemFolder = 'HLT'
 process.dqmSaver.saveByRun = 1
 process.dqmSaver.saveAtJobEnd = True
 
+# # Message Logger
+# process.load("FWCore.MessageService.MessageLogger_cfi")
+# #process.MessageLogger.categories = ['hltResults']
+# process.MessageLogger.destinations = ['cout', 'detailedInfo', 'critical']
+# process.MessageLogger.cout = cms.untracked.PSet(
+#     #threshold = cms.untracked.string('ERROR'),
+#     #threshold = cms.untracked.string('INFO'),
+#     #INFO = cms.untracked.PSet(
+#     # limit = cms.untracked.int32(-1)
+#     #)#,
+#     threshold = cms.untracked.string('DEBUG'),
+#     DEBUG = cms.untracked.PSet(
+#     limit = cms.untracked.int32(-1) ## DEBUG, all messages
+#     )
+#     )
+# process.MessageLogger.categories = ['Status', 'Parameter']
+# # copy stdout to a file
+# process.MessageLogger.detailedInfo = process.MessageLogger.cout
+# process.MessageLogger.debugModules = ['hltResults']
+# process.MessageLogger.critical = cms.untracked.PSet(
+#     threshold = cms.untracked.string('ERROR'),
+#     #threshold = cms.untracked.string('INFO'),
+#     #INFO = cms.untracked.PSet(
+#     # limit = cms.untracked.int32(-1)
+#     #)#,
+#     #threshold = cms.untracked.string('DEBUG'),
+#     ERROR = cms.untracked.PSet(
+#     limit = cms.untracked.int32(-1) ## all messages
+#     )
+#     )
+# # summary
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+    )
