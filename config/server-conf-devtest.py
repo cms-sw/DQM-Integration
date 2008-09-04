a4d024089ac9f3e2a2372fb5d5d5e1ec7147dc96
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2008-09-04 14:57:16 +0000

    Update configuration for RPM-based installation.

--- config/server-conf-devtest.py
+++ config/server-conf-devtest.py
@@ -1,22 +1,15 @@
 import pwd, os, os.path
 global CONFIGDIR
+BASEDIR   = "/tmp/$USER/dqmgui".replace("$USER", pwd.getpwuid(os.geteuid())[0])
 CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
+
 LAYOUTS = ["%s/%s-layouts.py" % (CONFIGDIR, x) for x in
 	   ("csc", "dt", "eb", "hcal", "hlt", "l1t", "l1temulator", "rpc", "pixel", "sistrip")]
 LAYOUTS += ["%s/shift_%s_layout.py" % (CONFIGDIR, x) for x in
             ("csc", "dt", "eb", "hcal", "hlt", "l1t", "l1temulator", "rpc", "pixel", "sistrip")]
 
 modules = ("GuiDQM", "GuiEventDisplay")
-basedir = "/tmp/$USER/dqmgui".replace("$USER", pwd.getpwuid(os.geteuid())[0])
-envsetup = """
- source $BASE/rpms/cmsset_default.sh
- source $BASE/rpms/slc4_ia32_gcc345/cms/webtools/1.3.0/etc/profile.d/dependencies-setup.sh
- source $BASE/rpms/slc4_ia32_gcc345/external/py2-pil/1.1.6/etc/profile.d/init.sh
- source $BASE/rpms/slc4_ia32_gcc345/external/libpng/1.2.10/etc/profile.d/init.sh
- source $BASE/rpms/slc4_ia32_gcc345/external/libjpg/6b/etc/profile.d/init.sh
- source $BASE/rpms/slc4_ia32_gcc345/external/libtiff/3.8.2/etc/profile.d/init.sh
- export QUIET_ASSERT=a
-""".replace("$BASE", basedir)
+envsetup = "export QUIET_ASSERT=a"
 
 server.port        = 8888
 server.serverDir   = basedir + "/gui"
