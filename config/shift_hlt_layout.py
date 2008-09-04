395b6b7027680425bb0bac4f83e372cc613978ff
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-09-04 15:35:05 +0000

    updated with hlt scalers histos

--- config/shift_hlt_layout.py
+++ config/shift_hlt_layout.py
@@ -1,26 +1,13 @@
 def hltlayout(i, p, *rows): i["00 Shift/HLT/" + p] = DQMItem(layout=rows)
   
-hltlayout(dqmitems,"00 Filtered ET Muon",
-  	[{'path': "HLT/HLTMonhltMonMu/hltZMML2Filteredet", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+hltlayout(dqmitems,"00 Rates per HLT path",
+  	[{'path': "HLT/HLTScalers_EvF/cur_rate", 'description': "Trigger Rate per path. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
 
-hltlayout(dqmitems,"01 Filtered Eta Muon",
-  	[{'path': "HLT/HLTMonhltMonMu/hltZMML2Filteredeta", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+hltlayout(dqmitems,"01 Rates per HLT path",
+  	[{'path': "HLT/HLTScalers_EvF/detailedHltScalers", 'description': "Trigger Rate rejection per filter (Y) and path (X). For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
 
-hltlayout(dqmitems,"02 Total Efficiency Muon",
-  	[{'path': "HLT/HLTMonhltMonMu/total eff", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+hltlayout(dqmitems,"02 Events per HLT path",
+  	[{'path': "HLT/HLTScalers_EvF/hltScalers", 'description': "Events per HLT path. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
 
-hltlayout(dqmitems,"03 Occupancy Muon",
-  	[{'path': "HLT/HLTMonhltMonMu/hltZMML2Filteredeta_phi", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
-
-
-hltlayout(dqmitems,"00 Filtered ET Electron",
-  	[{'path': "HLT/HLTMonhltMonE/hltL1seedRelaxedSingleEt8et", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
-
-hltlayout(dqmitems,"01 Filtered Eta Electron",
-  	[{'path': "HLT/HLTMonhltMonE/hltL1seedRelaxedSingleEt8eta", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
-
-hltlayout(dqmitems,"02 Total Efficiency Electron",
-  	[{'path': "HLT/HLTMonhltMonE/total eff", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
-
-hltlayout(dqmitems,"03 Occupancy Electron",
-  	[{'path': "HLT/HLTMonhltMonE/hltL1seedRelaxedSingleEt8eta_phi", 'description': "Information missing. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+hltlayout(dqmitems,"03 Exceptions per HLT path",
+  	[{'path': "HLT/HLTScalers_EvF/hltExceptions", 'description': "Exceptions per HLT path. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
