5ef7744dccf90d5cf4c7c6e081e6f835a7ba9e2b
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-09-07 21:45:22 +0000

    Fix verbosity

--- python/test/dt_dqm_sourceclient_common_cff.py
+++ python/test/dt_dqm_sourceclient_common_cff.py
@@ -76,7 +76,7 @@ from DQM.DTMonitorClient.dtNoiseAnalysis_cfi import *
 from DQM.DTMonitorClient.dtSummaryClients_cfi import *
 
 dtqTester = cms.EDFilter("QualityTester",
-                         reportThreshold = cms.untracked.string('red'),
+                         #reportThreshold = cms.untracked.string('red'),
                          prescaleFactor = cms.untracked.int32(1),
                          qtList = cms.untracked.FileInPath('DQM/DTMonitorClient/test/QualityTests.xml'),
                          getQualityTestsFromFile = cms.untracked.bool(True)
