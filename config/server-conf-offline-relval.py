f8bc9c36080b08e26afbbd6e87e2c198e0d5279e
Cms Dqm <sha1-ab433251379c0653a4d5fcb697bb9f400ca219d0@cern.ch>
2008-09-08 16:26:55 +0000

    Upgrade to DQM GUI based on full RPM distribution.

--- config/server-conf-offline-relval.py
+++ config/server-conf-offline-relval.py
@@ -3,15 +3,7 @@ global CONFIGDIR
 CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
 
 modules = ("GuiDQM",)
-envsetup = """
- source /data/sw/cmsset_default.sh
- source /data/sw/slc4_ia32_gcc345/cms/webtools/1.3.0/etc/profile.d/dependencies-setup.sh
- source /data/sw/slc4_ia32_gcc345/external/py2-pil/1.1.6/etc/profile.d/init.sh
- source /data/sw/slc4_ia32_gcc345/external/libpng/1.2.10/etc/profile.d/init.sh
- source /data/sw/slc4_ia32_gcc345/external/libjpg/6b/etc/profile.d/init.sh
- source /data/sw/slc4_ia32_gcc345/external/libtiff/3.8.2/etc/profile.d/init.sh
- export QUIET_ASSERT=a
-"""
+envsetup = "export QUIET_ASSERT=a"
 
 server.port        = 8050
 server.serverDir   = '/data/dqm/relval/gui'
