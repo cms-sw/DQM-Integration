3e59731d28c313bcd553a9ec6f4e6e93b0f8bf67
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2008-09-18 21:48:34 +0000

    *** empty log message ***

--- rcms/installrcms.sh
+++ rcms/installrcms.sh
@@ -3,9 +3,7 @@
 ln -s  daq_dqm_sourceclient-live.cfg           dqmfu10-1.cfg 
 
 ln -s  sistrip_dqm_sourceclient-live.cfg       dqmfu15-1.cfg 
-ln -s  pixel_dqm_sourceclient-live.cfg         dqmfu15-2.cfg 
-ln -s  shipper_dqm_sourceclient-live.cfg       dqmfu15-3.cfg 
-ln -s  csc_dqm_sourceclient-live.cfg           dqmfu15-4.cfg 
+ln -s  hlx_dqm_sourceclient-live.cfg           dqmfu15-2.cfg 
 
 ln -s  ee_dqm_sourceclient-live.cfg            dqmfu16-1.cfg 
 ln -s  eb_dqm_sourceclient-live.cfg            dqmfu16-2.cfg 
@@ -17,5 +15,7 @@ ln -s  l1t_dqm_sourceclient-live.cfg           dqmfu17-2.cfg
 ln -s  l1temulator_dqm_sourceclient-live.cfg   dqmfu17-3.cfg 
 ln -s  rpc_dqm_sourceclient-live.cfg           dqmfu17-4.cfg 
 
-ln -s  hlx_dqm_sourceclient-live.cfg           dqmfu18-1.cfg 
+ln -s  pixel_dqm_sourceclient-live.cfg         dqmfu18-1.cfg 
+ln -s  csc_dqm_sourceclient-live.cfg           dqmfu18-2.cfg 
+ln -s  shipper_dqm_sourceclient-live.cfg       dqmfu18-3.cfg 
 
