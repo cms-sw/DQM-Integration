5eb8c7c4887fd150ae5560a34642039bbb6c1d43
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2008-09-29 16:36:02 +0000

    fix playback server location (commented out)

--- python/test/inputsource_cfi.py
+++ python/test/inputsource_cfi.py
@@ -5,7 +5,7 @@ maxEvents = cms.untracked.PSet(
 )
 source = cms.Source("EventStreamHttpReader",
     sourceURL = cms.string('http://srv-c2d05-14.cms:22100/urn:xdaq-application:lid=30'),
-#    sourceURL = cms.string('http://srv-c2c05-27:50082/urn:xdaq-application:lid=29'),
+#    sourceURL = cms.string('http://srv-c2d05-05:50082/urn:xdaq-application:lid=29'),
     consumerPriority = cms.untracked.string('normal'),
     max_event_size = cms.int32(7000000),
     consumerName = cms.untracked.string('DQM Source'),
