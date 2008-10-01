6b268d9c1e5af46da8c7814ad0375107255c57a3
Hyunkwan Seo <sha1-885d53b681c72a58fe6bb49e412be85d8087d0ef@cern.ch>
2008-10-01 12:54:26 +0000

    defined some variables used in mergeAndRegister.py

--- scripts/dqmPostProcessing_online.py
+++ scripts/dqmPostProcessing_online.py
@@ -2,15 +2,30 @@
 
 import os,time,sys,shutil
 
-DIR = '/cms/mon/data/dropbox'  # directory to search new files
-TimeTag = '/cms/mon/data/dropbox/timetag' #file for time tag for searching new file
-
-#### Directories and files for test and development
-#DIR = '/cms/mon/data/dropbox_test'  # directory to search new files
-#TimeTag = '/cms/mon/data/dropbox_test/timetag' #file for time tag for searching new file
-
+### set environments
+DIR = '/data/dqm/dropbox'  # directory to search new files
+DB = '/home/dqm/dqm.db' #master db
+BAKDB = '/data/dqm/filereg/backups/dqm.db' #bakcup db (timestamp will be attatched)
+TMPDB = '/data/dqm/dropbox/dqm-tmp.db' # temporal db
+FILEDIR = '/data/dqm/results' # directory, to which merged file is stored
+TMPDIR = '/data/dqm/.dropbox_tmp' # directory, in which merged file is created
+TimeTag = '/data/dqm/dropbox/timetag' #file for time tag for searching new file
+LOGDIR = '/data/dqm/filereg/log'
 WAITTIME = 120 # waiting time for new files (sec)
-MERGE_EXE = '/cms/mon/data/dqm/filereg/mergeAndRegister.py'
+MERGE_EXE = '/data/dqm/filereg/mergeAndRegister.py'
+
+## For test and development
+#DIR = '/data/dqm/dropbox_test'  # directory to search new files
+#DB = '/data/dqm/filereg_test/dqm.db' #master db
+#BAKDB = '/data/dqm/filereg_test/backups/dqm.db' #bakcup db (timestamp will be attatched)
+#TMPDB = '/data/dqm/dropbox/dqm-tmp.db' # temporal db
+#FILEDIR = '/data/dqm/results_test' # directory, to which merged file is stored
+#TMPDIR = '/data/dqm/.dropbox_tmp' # directory, in which merged file is created
+#TimeTag = '/data/dqm/dropbox_test/timetag' #file for time tag for searching new file
+#LOGDIR = '/data/dqm/filereg_test/log'
+#WAITTIME = 120 # waiting time for new files (sec)
+#MERGE_EXE = '/data/dqm/filereg_test/mergeAndRegister.py'
+
 
 if not os.path.exists(TimeTag):
         os.system('touch -t 01010000 '+ TimeTag)
@@ -37,5 +52,6 @@ while 1:
 	    run = filename[-14:-5] #extract run number
 	    DqmSummaryLog = dir + '/GetAndWriteDQMSummaryIntoOMDS_R' + run + '.log'
 	    OUT_ = os.popen('GetAndWriteDQMSummaryIntoOMDS '+  filename + ' ' + DqmSummaryLog).read()
+
 	    print OUT_
 	    
