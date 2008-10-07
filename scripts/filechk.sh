fafba28b7dbab8f04e821de74de9f70194ee0ae4
Hyunkwan Seo <sha1-885d53b681c72a58fe6bb49e412be85d8087d0ef@cern.ch>
2008-10-07 19:42:13 +0000

    some typo was corrected. (However this typo didn't cause problem but just gave warning)

--- scripts/filechk.sh
+++ scripts/filechk.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -o nounset
 
-#WorkDir='/home/dqmprolocal/filecopy' #comment out because it is defined in alivecheck_filesave
-.sh
+#WorkDir='/home/dqmprolocal/filecopy' #comment out because it is defined in alivecheck_filesave.sh
 
 inputFile=$1
 
