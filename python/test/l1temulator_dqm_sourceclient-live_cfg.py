9d284a8bb9f724bf4f59fabd858cb5fd2daf9a83
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2008-11-03 10:25:17 +0000

    updates (global tag specification, subsystem masking in summary map)

--- python/test/l1temulator_dqm_sourceclient-live_cfg.py
+++ python/test/l1temulator_dqm_sourceclient-live_cfg.py
@@ -7,20 +7,18 @@ process.load("DQMServices.Components.DQMEnvironment_cfi")
 process.load("DQM.Integration.test.inputsource_cfi")
 process.load("DQM.Integration.test.environment_cfi")
 
-#--------------------------
-# Calibration
-#--------------------------
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG"
-process.GlobalTag.globaltag = "CRAFT_V2H::All"
-process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
-
-
 process.load("DQM.L1TMonitor.L1TEmulatorMonitor_cff")    
 process.load("DQM.L1TMonitorClient.L1TEMUMonitorClient_cff")    
 
-#replace DQMStore.referenceFileName = "L1TEMU_reference.root"
+#specify subsystems with qt's to be temporarily masked in summary map
+#sequence: dtf,dtp,ctf,ctp,rpc,gmt, etp,htp,rct,gct,gt
+#process.l1temuEventInfoClient.maskedSystems = [0,1,1,0,0,0, 1,1,1,0,1]
 
+#Updated GLOBAL TAG to be specified by central OPERATIONS here!
+process.GlobalTag.globaltag = 'CRAFT_V2H::All'
+process.GlobalTag.connect = 'frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG'
+
+#replace DQMStore.referenceFileName = "L1TEMU_reference.root"
 process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(
     SelectEvents = cms.vstring("*")
 )
--- python/test/l1temulator_dqm_sourceclient-playback_cfg.py
+++ python/test/l1temulator_dqm_sourceclient-playback_cfg.py
@@ -1,42 +1,24 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("DQM")
-#----------------------------
-# Event Source
-#-----------------------------
+
 process.load("DQM.Integration.test.inputsource_playback_cfi")
-
-#----------------------------
-# DQM Environment
-#-----------------------------
 process.load("DQMServices.Core.DQM_cfg")
-
-#replace DQMStore.referenceFileName = "L1TEMU_reference.root"
 process.load("DQMServices.Components.DQMEnvironment_cfi")
-
-#----------------------------
-# DQM Playback Environment
-#-----------------------------
 process.load("DQM.Integration.test.environment_playback_cfi")
-
-#-----------------------------
-#  DQM SOURCES
-#-----------------------------
-process.load("CondCore.DBCommon.CondDBSetup_cfi")
-
-process.load("Configuration.StandardSequences.Geometry_cff")
-
-process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
-
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.connect = "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG"
-process.GlobalTag.globaltag = 'CRZT210_V1H::All' # or any other appropriate
-process.prefer("GlobalTag")
 
 process.load("DQM.L1TMonitor.L1TEmulatorMonitor_cff")    
 process.load("DQM.L1TMonitorClient.L1TEMUMonitorClient_cff")    
 
-#replace DQMStore.referenceFileName = "L1TEMU_reference.root"
+#masking sequence: 
 
+#off-line
+#process.GlobalTag.globaltag = 'CRAFT_V2P::All'
+#process.GlobalTag.connect = 'frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'
+#on-line
+process.GlobalTag.globaltag = 'CRAFT_V2H::All'
+process.GlobalTag.connect = 'frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG'
+
+#replace DQMStore.referenceFileName = "L1TEMU_reference.root"
 process.EventStreamHttpReader.consumerName = 'L1TEMU DQM Consumer'
 process.dqmEnv.subSystemFolder = 'L1TEMU'
