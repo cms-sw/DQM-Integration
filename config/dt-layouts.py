6da7bd42b2bae0104481a5415dab3a36477044bc
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-11-03 13:50:03 +0000

    Add summary of synch noise and TP occup. and layout event size

--- config/dt-layouts.py
+++ config/dt-layouts.py
@@ -27,6 +27,13 @@ dtlayout(dqmitems, "00-Summary/07-NoiseChannelsSummary",
   [{ 'path': "DT/04-Noise/NoiseSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description for the <b>Central DQM shifter</b></a>  -  <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DTDQMPlots>Instructions for the <b>DT shifter</b></a>" }])
 
 
+dtlayout(dqmitems, "00-Summary/08-SynchNoiseSummary",
+         [{ 'path': "DT/04-Noise/SynchNoise/SynchNoiseSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description for the <b>Central DQM shifter</b></a>  -  <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DTDQMPlots>Instructions for the <b>DT shifter</b></a>" }])
+
+dtlayout(dqmitems, "00-Summary/09-TestPulseOccupancy",
+         [{ 'path': "DT/99-TestPulses/OccupancySummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description for the <b>Central DQM shifter</b></a>  -  <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DTDQMPlots>Instructions for the <b>DT shifter</b></a>" }])
+
+
 #### OCCUPANCIES #################################################################################
 
 for wheel in range(-2, 3):
@@ -38,7 +45,7 @@ for wheel in range(-2, 3):
             histoname = "DT/01-Digi/Wheel" + str(wheel) + "/Station" + str(station) + "/Sector" + str(sector) + "/OccupancyAllHits_perCh_W" + str(wheel) + "_St" + str(station) + "_Sec" +  str(sector)
             dtlayout(dqmitems, name,[{ 'path': histoname}])
 
-
+            
 #### TIME BOXES #################################################################################
 
 for wheel in range(-2, 3):
@@ -58,6 +65,18 @@ for wheel in range(-2, 3):
             else:
                 dtlayout(dqmitems, name,[{ 'path': histoname_SL1}],
                          [{ 'path': histoname_SL3}])
-
-
+                
+                
+#### EVENT SIZE #################################################################################
+for fed in range(770, 775):
+    for ros in range(1, 13):
+        name = "03-ROSEventSize/FED" + str(fed) + "/ROS" + str(ros)
+        histoname = "DT/00-DataIntegrity/FED" + str(fed) + "/ROS" + str(ros) + "/FED" + str(fed) + "_ROS" + str(ros) + "_ROSEventLenght"
+        dtlayout(dqmitems, name,[{ 'path': histoname}])
+#    
+#         
+#         
+#         
+#         
+        
 
