3cf70d2be68011a7fcb0c61bb9679f076b85ad8e
Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch>
2008-11-07 19:10:04 +0000

    updating sistrip live config file with latest global tag for condDB

--- python/test/sistrip_dqm_sourceclient-live_cfg.py
+++ python/test/sistrip_dqm_sourceclient-live_cfg.py
@@ -55,7 +55,7 @@ process.load("Configuration.StandardSequences.Geometry_cff")
 #--------------------------
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG"
-process.GlobalTag.globaltag = "CRAFT_V3H::All"
+process.GlobalTag.globaltag = "CRAFT_V4H::All"
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 #-----------------------
