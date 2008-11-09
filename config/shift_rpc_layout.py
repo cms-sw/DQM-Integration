e89bbf41bc8f8920f823b6982d1c5a16e71cd3de
David Lomidze <sha1-caee96601a9b34acf0a8be2f22a26eb4dd6cd64d@cern.ch>
2008-11-09 20:36:41 +0000

    bug fixed

--- config/shift_rpc_layout.py
+++ config/shift_rpc_layout.py
@@ -1,4 +1,4 @@
 def shiftrpclayout(i, p, *rows): i["00 Shift/RPC/" + p] = DQMItem(layout=rows)
 
 shiftrpclayout(dqmitems, "00-BarrelOccupancy",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/BarrelOccupancy", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
+  [{ 'path': "RPC/RecHits/SummaryHistograms/Occupancy_for_Barrel", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
