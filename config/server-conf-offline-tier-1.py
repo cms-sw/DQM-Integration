2d59fb9eda5de630d3aa01e8403886dc66f64da5
Cms Dqm <sha1-ab433251379c0653a4d5fcb697bb9f400ca219d0@cern.ch>
2008-11-25 07:56:04 +0000

    Add Tier-1 configuration.

---
+++ config/server-conf-offline-tier-1.py
@@ -0,0 +1,24 @@
+import os.path
+global CONFIGDIR
+CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
+
+modules = ("GuiDQM",)
+envsetup = """
+  export YUI_ROOT=/data/sw/$SCRAM_ARCH/external/yui/2.4.1
+  export QUIET_ASSERT=a
+"""
+
+LAYOUTS = ["%s/shift_%s_T0_layout.py" % (CONFIGDIR, x) for x in ("eb", "ee", "hcal", "l1t", "l1temulator", "hlt", "pixel", "sistrip", "dt", "muons", "jetmet", "egamma")]
+
+server.port        = 8070
+server.serverDir   = '/data/dqm/tier-1/gui'
+server.baseUrl     = '/dqm/tier-1'
+server.title       = 'CMS data quality'
+server.serviceName = 'Tier-1'
+
+server.source('DQMArchive', 'file', '/data/dqm/tier-1/dqm.db', '--listen 8071')
+server.extend('DQMFileAccess', '/dev/null', '/data/dqm/tier-1/upload')
+server.source('DQMLayout', 'layouts', *LAYOUTS)
+
+execfile(CONFIGDIR + "/dqm-services.py")
+execfile(CONFIGDIR + "/workspaces-tier-0.py")
