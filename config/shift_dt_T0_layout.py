7e18db9bbb937ad0c6d67432bf70b699720b7634
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2008-12-03 17:04:11 +0000

    Update to use segment summary histos

--- config/shift_dt_T0_layout.py
+++ config/shift_dt_T0_layout.py
@@ -1,40 +1,19 @@
 def shiftdtlayout(i, p, *rows): i["00 Shift/DT/" + p] = DQMItem(layout=rows)
 
-shiftdtlayout(dqmitems, "00-DT Shift Histogram",
-  [{ 'path': "DT/DTSegmentsTask/02-Segments/segmentSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description for the <b>Central DQM shifter</b></a>" }])
+shiftdtlayout(dqmitems, "00-SegmentQualitySummary",
+  [{ 'path': "DT/02-Segments/segmentSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineDT>Description for the <b>Central DQM Offline shifter</b></a>" }])
 
-#shiftdtlayout(dqmitems, "00-DataIntegritySummary",
-#  [{ 'path': "DT/00-DataIntegrity/DataIntegritySummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description for the <b>Central DQM shifter</b></a>" }])
+shiftdtlayout(dqmitems, "01-SegmentOccupancy_W-2",
+  [{ 'path': "DT/02-Segments/Wheel-2/numberOfSegments_W-2", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineDT>Description for the <b>Central DQM Offline shifter</b></a>" }])
 
-#shiftdtlayout(dqmitems, "01-OccupancySummary",
-#  [{ 'path': "DT/01-Digi/OccupancySummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description for the <b>Central DQM shifter</b></a>" }])
+shiftdtlayout(dqmitems, "02-SegmentOccupancy_W-1",
+  [{ 'path': "DT/02-Segments/Wheel-1/numberOfSegments_W-1", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineDT>Description for the <b>Central DQM Offline shifter</b></a>" }])
 
-#shiftdtlayout(dqmitems, "02-SegmentSummary",
-#  [{ 'path': "DT/02-Segments/segmentSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description for the <b>Central DQM shifter</b></a>" }])
+shiftdtlayout(dqmitems, "03-SegmentOccupancy_W0",
+  [{ 'path': "DT/02-Segments/Wheel0/numberOfSegments_W0", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineDT>Description for the <b>Central DQM Offline shifter</b></a>" }])
 
-#shiftdtlayout(dqmitems, "03-DDU_TriggerCorrFactionSummary",
-#  [{ 'path': "DT/03-LocalTrigger/DDU_CorrFractionSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description for the <b>Central DQM shifter</b></a>" }])
+shiftdtlayout(dqmitems, "04-SegmentOccupancy_W1",
+  [{ 'path': "DT/02-Segments/Wheel1/numberOfSegments_W1", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineDT>Description for the <b>Central DQM Offline shifter</b></a>" }])
 
-# shiftdtlayout(dqmitems, "04-DCC-TriggerCorrFactionSummary",
-#   [{ 'path': "DT/03-LocalTrigger/DCC/DCC_CorrFractionSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description for the <b>Central DQM shifter</b></a>" }])
-
-#shiftdtlayout(dqmitems, "05-DDU_Trigger2ndFactionSummary",
-#  [{ 'path': "DT/03-LocalTrigger/DDU_2ndFractionSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description for the <b>Central DQM shifter</b></a>" }])
-
-# shiftdtlayout(dqmitems, "06-DCC-Trigger2ndFactionSummary",
-#   [{ 'path': "DT/03-LocalTrigger/DCC/DCC_2ndFractionSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description for the <b>Central DQM shifter</b></a>" }])
-
-###########
-
-# shiftdtlayout(dqmitems, "00-DataIntegritySummary",
-#   [{ 'path': "DT/DataIntegrity/DataIntegritySummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description</a>" }])
-
-# shiftdtlayout(dqmitems, "01-OccupancySummary",
-#   [{ 'path': "DT/Digi/OccupancySummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description</a>" }])
-
-# shiftdtlayout(dqmitems, "02-SegmentSummary",
-#   [{ 'path': "DT/Segments/segmentSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description</a>" }])
-
-# shiftdtlayout(dqmitems, "03-LocalTriggerSummary",
-#   [{ 'path': "DT/LocalTrigger/DDU_CorrFractionSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftDT>Description</a>" }])
-
+shiftdtlayout(dqmitems, "05-SegmentOccupancy_W2",
+  [{ 'path': "DT/02-Segments/Wheel2/numberOfSegments_W2", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineDT>Description for the <b>Central DQM Offline shifter</b></a>" }])
