b673c9d8fbb143efe051808d6571460fd8836987
giorgia <sha1-b060207c7684f589ccb32410d310a32eecc39a52@cern.ch>
2009-02-26 14:18:38 +0000

    new reference summary histos

--- config/shift_muons_T0_layout.py
+++ config/shift_muons_T0_layout.py
@@ -1,70 +1,28 @@
 def shiftmuonlayout(i, p, *rows): i["00 Shift/Muons/" + p] = DQMItem(layout=rows)
 
-shiftmuonlayout(dqmitems, "01-staMuonPhiDirection",
-                [{ 'path': "Muons/MuonRecoAnalyzer/StaMuon_phi", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
+shiftmuonlayout(dqmitems, "00-reportSummary",
+                [{ 'path': "Muons/EventInfo/reportSummary", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
 
 
-shiftmuonlayout(dqmitems, "02-staMuonEtaDirection",
-                [{ 'path': "Muons/MuonRecoAnalyzer/StaMuon_eta", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
+shiftmuonlayout(dqmitems, "01-kinematicsSummary",
+                [{ 'path': "Muons/TestSummary/kinematicsSummaryMap", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
 
 
-shiftmuonlayout(dqmitems, "03-staMuonPt",
-                [{ 'path': "Muons/MuonRecoAnalyzer/StaMuon_pt", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
+shiftmuonlayout(dqmitems, "02-residualsSummary",
+                [{ 'path': "Muons/TestSummary/residualsSummaryMap", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
 
 
-shiftmuonlayout(dqmitems, "04-staMuonNumRecHitPerTrack",
-                [{ 'path': "Muons/cosmicMuons/NumberOfRecHitsPerTrack_sta", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
+shiftmuonlayout(dqmitems, "03-energySummary",
+                [{ 'path': "Muons/TestSummary/energySummaryMap", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
 
 
-shiftmuonlayout(dqmitems, "05-staMuonHitStaProvenance",
-                [{ 'path': "Muons/SegmentTrackAnalyzer/trackHitStaProvenance_cosmicMuons", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
+shiftmuonlayout(dqmitems, "04-muonIdSummary",
+                [{ 'path': "Muons/TestSummary/muonIdSummaryMap", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
 
 
-shiftmuonlayout(dqmitems, "06-glbMuonPhiDirection",
-                [{ 'path': "Muons/MuonRecoAnalyzer/GlbMuon_Glb_phi", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
+shiftmuonlayout(dqmitems, "05-molteplicitySummary",
+                [{ 'path': "Muons/TestSummary/molteplicitySummaryMap", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
 
 
-shiftmuonlayout(dqmitems, "07-glbMuonEtaDirection",
-                [{ 'path': "Muons/MuonRecoAnalyzer/GlbMuon_Glb_eta", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
-
-
-shiftmuonlayout(dqmitems, "08-glbMuonPt",
-                [{ 'path': "Muons/MuonRecoAnalyzer/GlbMuon_Glb_pt", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
-
-
-shiftmuonlayout(dqmitems, "09-glbMuonNumRecHitPerTrack",
-                [{ 'path': "Muons/globalCosmicMuons/NumberOfRecHitsPerTrack_glb", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
-
-
-shiftmuonlayout(dqmitems, "10-glbMuonHitStaProvenance",
-                [{ 'path': "Muons/SegmentTrackAnalyzer/trackHitStaProvenance_globalCosmicMuons", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
-
-
-shiftmuonlayout(dqmitems, "11-glbMuonHitTkProvenance",
-                [{ 'path': "Muons/SegmentTrackAnalyzer/trackHitTkProvenance_globalCosmicMuons", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
-
-
-shiftmuonlayout(dqmitems, "12-glbMuonPhiResiduals_TkGlb",
-                [{ 'path': "Muons/MuonRecoAnalyzer/Res_TkGlb_phi", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
-
-
-shiftmuonlayout(dqmitems, "13-glbMuonEtaResiduals_TkGlb",
-                [{ 'path': "Muons/MuonRecoAnalyzer/Res_TkGlb_eta", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
-
-
-shiftmuonlayout(dqmitems, "14-glbMuonPtResiduals_TkGlb",
-                [{ 'path': "Muons/MuonRecoAnalyzer/Res_TkGlb_pt", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
-
-
-shiftmuonlayout(dqmitems, "15-glbMuonPhiResiduals_GlbSta",
-                [{ 'path': "Muons/MuonRecoAnalyzer/Res_GlbSta_phi", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
-
-
-shiftmuonlayout(dqmitems, "16-glbMuonEtaResiduals_GlbSta",
-                [{ 'path': "Muons/MuonRecoAnalyzer/Res_GlbSta_eta", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
-
-
-shiftmuonlayout(dqmitems, "17-glbMuonPtResiduals_GlbSta",
-                [{ 'path': "Muons/MuonRecoAnalyzer/Res_GlbSta_pt", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineMuon>Description</a>" }])
 
 
