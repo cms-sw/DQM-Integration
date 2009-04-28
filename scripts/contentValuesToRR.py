a6e412591d133778d79088d8b16de13bbf6805ae
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2009-04-28 10:51:26 +0000

    scripts/contentValuesToRR.py updated to split Online and Offline DQM values

--- scripts/contentValuesToRR.py
+++ scripts/contentValuesToRR.py
@@ -3,7 +3,12 @@
 import sys, os, optparse, re
 import ROOT, xmlrpclib
  
-FOLDERS     = [ 'reportSummaryContents', 'CertificationContents', 'DAQContents', 'DCSContents' ]
+FOLDERS     = { 
+  'reportSummaryContents': 'DQM', 
+  'CertificationContents': 'Cert', 
+  'DAQContents': 'DAQ', 
+  'DCSContents': 'DCS' 
+}
 SERVER_URL = "http://cmswbm.cms/runregistry/xmlrpc"
 SUBSYSTEMS  = {
   'CSC' :        'CSC',
@@ -22,9 +27,11 @@ class OptionParser(optparse.OptionParser):
   """ Option parser class """
   def __init__(self):
     optparse.OptionParser.__init__(self, usage="%prog [options] root_file ...", version="%prog 0.0.1", conflict_handler="resolve")
-    self.add_option("--url=", action="store", type="string", dest="url", default=SERVER_URL, help="specify RR XML-RPC server URL. Default is " + SERVER_URL)
+    self.add_option("--shift", "-s", action="store", type="choice", dest="shift", choices=("online","offline"), help="specify shift type: online or offline values allowed")
+    self.add_option("--url", action="store", type="string", dest="url", default=SERVER_URL, help="specify RR XML-RPC server URL. Default is " + SERVER_URL)
+    self.add_option("--debug", "-d", action="store_true", dest="debug", default=False, help="print values and exit. Do not write to RR")
 
-def getSummaryValues(file_name):
+def getSummaryValues(file_name, shift_type):
   """ Method to extract keys from root file and return dict """
   ROOT.gROOT.Reset()
 
@@ -59,19 +66,23 @@ def getSummaryValues(file_name):
     evInfo = sub.ReadObj().GetDirectory("Run summary/EventInfo")
     if evInfo == None: continue
 
-    for folder_name in FOLDERS:
+    for folder_name in FOLDERS.keys():
 
       folder = evInfo.GetDirectory(folder_name)
       if folder == None: continue
       
-      result['subs'][sub_key][folder_name] = {}
+      folder_id = FOLDERS[folder_name]
+      if folder_id == 'DQM':
+        folder_id = 'DQM ' + shift_type.upper()
+
+      result['subs'][sub_key][folder_id] = {}
 
       for value in folder.GetListOfKeys():
         full_name = value.ReadObj().GetName()
         if not value.IsFolder() and re.match("^<.+>f=-{,1}[0-9\.]+</.+>$", full_name):
           value_name = re.sub("<(?P<n>[^>]+)>.+", "\g<n>", full_name)
           value_numb = float(re.sub("<.+>f=(?P<n>-{,1}[0-9\.]+)</.+>", "\g<n>", full_name))
-          result['subs'][sub_key][folder_name][value_name] = value_numb
+          result['subs'][sub_key][folder_id][value_name] = value_numb
 
   f.Close()
 
@@ -107,6 +118,11 @@ if __name__ == "__main__":
     print "At least one ROOT file must be priovided, use --help for hit"
     sys.exit(1)
 
+  # Check if shift type is provided (mandatory!)
+  if not opts['shift']:
+    print "Shift type must be provided, use --help for hit"
+    sys.exit(1)
+
   # Check if all files exists and are accessible
   for rfile in args:
     try:
@@ -121,11 +137,15 @@ if __name__ == "__main__":
   # defined XML-RPC url 
   for rfile in args:
 
-    values = getSummaryValues(file_name = rfile)
+    values = getSummaryValues(file_name = rfile, shift_type = opts['shift'])
 
     try:
-      result = server.SummaryValuesWriter.write(dict2json(values))
-      print "RR: %d rows modified for file %s" % (result, rfile)
+      json = dict2json(values)
+      if opts['debug']:
+        print json
+      else:
+        result = server.SummaryValuesWriter.write(json)
+        print "RR: %d rows modified for file %s" % (result, rfile)
     except xmlrpclib.Error, errstring:
       print "ERROR", errstring
       sys.exit(3)
