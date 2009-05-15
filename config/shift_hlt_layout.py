78106f95596db14017c3d7110c08ca6025f3ac03
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2009-05-15 08:43:09 +0000

    new layout for MWGR

--- config/shift_hlt_layout.py
+++ config/shift_hlt_layout.py
@@ -1,13 +1,19 @@
 def hltlayout(i, p, *rows): i["00 Shift/HLT/" + p] = DQMItem(layout=rows)
   
-hltlayout(dqmitems,"00 Rates per HLT path",
-  	[{'path': "HLT/HLTScalers_EvF/cur_rate", 'description': "Trigger Rate per path. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+hltlayout(dqmitems,"00 Counts per muon HLT path",
+  	[{'path': "HLT/HLTMonMuon/EfficiencySummary/CountHistos/hCountSummary", 'description': "event count per muon path. For more information please click 
+<a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
-hltlayout(dqmitems,"01 Rates per HLT path",
-  	[{'path': "HLT/HLTScalers_EvF/detailedHltScalers", 'description': "Trigger Rate rejection per filter (Y) and path (X). For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+hltlayout(dqmitems,"01 Path efficiencies wrt L1MuOpen",
+  	[{'path': "HLT/HLTMonMuon/EfficiencySummary/hEffSummary", 'description': "Muon path efficiency with respect to the L1MuOpen. For more information 
+please click <a 
+href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
-hltlayout(dqmitems,"02 Events per HLT path",
-  	[{'path': "HLT/HLTScalers_EvF/hltScalers", 'description': "Events per HLT path. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+hltlayout(dqmitems,"02 HLT Muon Eta",
+  	[{'path': "HLT/HLTMonMuon/L1PassThrough/Level1/HLTMuonL1_eta", 'description': "Eta distribution for muon candidates passing any L1Muon path. For more 
+information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
-hltlayout(dqmitems,"03 Exceptions per HLT path",
-  	[{'path': "HLT/HLTScalers_EvF/hltExceptions", 'description': "Exceptions per HLT path. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftTrigger\">here</a>."}])
+hltlayout(dqmitems,"03 HLT Muon Phi",
+  	[{'path': "HLT/HLTMonMuon/L1PassThrough/Level1/HLTMuonL1_phi", 'description': "Phi distribution for muon candidates passing any L1Muon path. For more 
+information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+
