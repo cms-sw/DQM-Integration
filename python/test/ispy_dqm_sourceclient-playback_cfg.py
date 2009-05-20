332ba2ec3fa4e7fc0d82babd84addaca195a4fe4
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2009-05-20 13:33:51 +0000

    update config files for ispy Event Display remove old iggi files

--- python/test/ispy_dqm_sourceclient-playback_cfg.py
+++ python/test/ispy_dqm_sourceclient-playback_cfg.py
@@ -12,21 +12,21 @@ process.GlobalTag.globaltag = 'CRAFT_V2H::All'
 process.GlobalTag.connect = "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG"
 
 
-process.load("DQM.Integration.test.inputsource_cfi")
-process.EventStreamHttpReader.consumerName = 'Event Display'
+process.load("DQM.Integration.test.inputsource_playback_cfi")
+process.EventStreamHttpReader.consumerName = 'iSpy Event Display'
 #process.maxEvents = cms.untracked.PSet(
 #    input = cms.untracked.int32(-1)
 #)
 #process.source = cms.Source("EventStreamHttpReader",
 #	sourceURL = cms.string("http://localhost:5000/urn:xdaq-application:lid=29"),
-#	sourceURL = cms.string('http://srv-c2d05-05.cms:50082/urn:xdaq-application:lid=29'),
 #	sourceURL = cms.string('http://srv-c2d05-14.cms:22100/urn:xdaq-application:lid=30'),
+#	sourceURL = cms.string('http://srv-c2d05-05.cms:50082/urn:xdaq-application:lid=29'),
 #	consumerPriority = cms.untracked.string('normal'),
 #	max_event_size = cms.int32(7000000),
 #	consumerName = cms.untracked.string('Event Display'),
 #	max_queue_depth = cms.int32(5),
-#	SelectHLTOutput = cms.untracked.string("hltOutputDQM"),
-#	maxEventRequestRate = cms.untracked.double(35.0),
+#   SelectHLTOutput = cms.untracked.string("hltOutputDQM"),
+#	maxEventRequestRate = cms.untracked.double(12.0),
 #	SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('*')),
 #	headerRetryInterval = cms.untracked.int32(3)
 #)
@@ -35,21 +35,22 @@ from FWCore.MessageLogger.MessageLogger_cfi import *
 
 process.add_(
     cms.Service("IguanaService",
-    outputFileName = cms.untracked.string('/home/dqmprolocal/output/Higgs200ChargedTaus.ig'),
-    outputESFileName = cms.untracked.string('/home/dqmprolocal/output/iggi_FrontierConditions_GlobalTag_STARTUP_V5.ig'),
+    outputFileName = cms.untracked.string('/home/dqmdevlocal/output/Higgs200ChargedTaus.ig'),
+    outputESFileName = cms.untracked.string('/home/dqmdevlocal/output/ispy_FrontierConditions_GlobalTag_STARTUP_V5.ig'),
     outputMaxEvents = cms.untracked.int32(3000),
     outputHost = cms.untracked.string('localhost'),
     outputPort = cms.untracked.uint32(9000),
-    online = cms.untracked.bool(True),
+    online = cms.untracked.bool(False),
     debug = cms.untracked.bool(False)
     )
 )
 
 
-
 process.load("VisReco.Analyzer.VisBasicCluster_cfi")
 process.load("VisReco.Analyzer.VisCaloTower_cfi")
 process.load("VisReco.Analyzer.VisCSCSegment_cfi")
+process.load("VisReco.Analyzer.VisDTDigi_cfi")
+process.load("VisReco.Analyzer.VisDTRecHit_cfi")
 process.load("VisReco.Analyzer.VisDTRecSegment4D_cfi")
 process.load("VisReco.Analyzer.VisEcalRecHit_cfi")
 process.load("VisReco.Analyzer.VisEvent_cfi")
@@ -61,10 +62,8 @@ process.load("VisReco.Analyzer.VisHORecHit_cfi")
 process.load("VisReco.Analyzer.VisJet_cfi")
 process.load("VisReco.Analyzer.VisMET_cfi")
 process.load("VisReco.Analyzer.VisMuon_cfi")
-#process.load("VisReco.Analyzer.VisPFCluster_cfi")
-#process.load("VisReco.Analyzer.VisPFRecHit_cfi")
-#process.load("VisReco.Analyzer.VisPFRecTrack_cfi")
 process.load("VisReco.Analyzer.VisPixelDigi_cfi")
+process.load("VisReco.Analyzer.VisRPCRecHit_cfi")
 process.load("VisReco.Analyzer.VisSiPixelCluster_cfi")
 process.load("VisReco.Analyzer.VisSiPixelRecHit_cfi")
 process.load("VisReco.Analyzer.VisSiStripCluster_cfi")
@@ -75,14 +74,9 @@ process.load("VisReco.Analyzer.VisTrackingRecHit_cfi")
 process.VisTrack.visTrackTag = cms.InputTag("globalCosmicMuons")
 process.VisTrackingRecHit.visTrackingRecHitTag = cms.InputTag("globalCosmicMuons")
 process.VisSiStripDigi.visSiStripDigiTag = cms.InputTag("siStripDigis","ZeroSuppressed")
-process.VisEcalRecHit.visEcalRecHitTag = cms.InputTag("ecalRecHit", "EcalRecHitsEB")
 process.VisMuon.visMuonTag = cms.InputTag("muons")
 process.VisBasicCluster.visBasicClusterTag = cms.InputTag("cosmicBasicClusters","CosmicBarrelBasicClusters")
 process.VisPixelDigi.VisPixelDigiTag = cms.InputTag("siPixelDigis","")
-
-#process.VisPFCluster.visPFClusterTag = cms.InputTag("particleFlowClusterECAL")
-#process.VisPFRecHit.visPFRecHitTag = cms.InputTag("particleFlowRecHitECAL")
-#process.VisPFRecTrack.visPFRecTrackTag = cms.InputTag("elecpreid")
 
 process.p1 = cms.Path(process.RawToDigi_woGCT*
                       process.reconstructionCosmics*
@@ -90,10 +84,12 @@ process.p1 = cms.Path(process.RawToDigi_woGCT*
                       process.VisEventSetup*
                       process.VisTrack*
                       process.VisTrackingRecHit*
-                      process.VisBasicCluster*
                       process.VisCSCSegment*
                       process.VisCaloTower*
                       process.VisDTRecSegment4D*
+                      process.VisDTRecHit*
+                      process.VisDTDigi*
+                      process.VisRPCRecHit*
                       process.VisEcalRecHit*
                       process.VisHBRecHit*
                       process.VisHERecHit*
@@ -101,9 +97,6 @@ process.p1 = cms.Path(process.RawToDigi_woGCT*
                       process.VisHORecHit*
                       process.VisMET*
                       process.VisMuon*
-#                      process.VisPFCluster*
-#                      process.VisPFRecHit*
-#                      process.VisPFRecTrack*
                       process.VisPixelDigi*
                       process.VisSiPixelCluster*
                       process.VisSiPixelRecHit*
