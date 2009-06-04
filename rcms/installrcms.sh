1476b1aa0d482f1bfcd9f327c152d49c0af76f36
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2009-06-04 13:27:28 +0000

    *** empty log message ***

--- rcms/installrcms.sh
+++ rcms/installrcms.sh
@@ -4,29 +4,18 @@ if [ "$1" = "live" ] ; then
   echo live
 
   ln -s daq_dqm_sourceclient-live_cfg.py              dqmfu10-1_cfg.py
-<<<<<<< installrcms.sh
 
   ln -s eb_dqm_sourceclient-live_cfg.py               dqmfu12-2_cfg.py
 
   ln -s ee_dqm_sourceclient-live_cfg.py               dqmfu13-1_cfg.py
 
-=======
-  
->>>>>>> 1.11
   ln -s sistrip_dqm_sourceclient-live_cfg.py          dqmfu15-1_cfg.py 
   ln -s hlx_dqm_sourceclient-live_cfg.py              dqmfu15-2_cfg.py
-<<<<<<< installrcms.sh
-  ln -s hcaltiming_dqm_sourceclient-live_cfg.py       dqmfu15-3_cfg.py 
-  ln -s iggi_2XX_dqm_sourceclient-live_cfg.py         dqmfu15-4_cfg.py
-   
-  ln -s es_dqm_sourceclient-live_cfg.py               dqmfu16-1_cfg.py 
-=======
   ln -s hcaltiming_dqm_sourceclient-live_cfg.py       dqmfu15-3_cfg.py
   ln -s ispy_dqm_sourceclient-live_cfg.py             dqmfu15-4_cfg.py
-  
-  ln -s ee_dqm_sourceclient-live_cfg.py               dqmfu16-1_cfg.py
-  ln -s eb_dqm_sourceclient-live_cfg.py               dqmfu16-2_cfg.py 
->>>>>>> 1.11
+   
+  ln -s es_dqm_sourceclient-live_cfg.py               dqmfu16-1_cfg.py 
+
   ln -s hcal_dqm_sourceclient-live_cfg.py             dqmfu16-3_cfg.py
   ln -s dt_dqm_sourceclient-live_cfg.py               dqmfu16-4_cfg.py
   
@@ -39,16 +28,10 @@ if [ "$1" = "live" ] ; then
   ln -s csc_dqm_sourceclient-live_cfg.py              dqmfu18-2_cfg.py
   ln -s fed_dqm_sourceclient-live_cfg.py              dqmfu18-3_cfg.py
   ln -s fedtest_dqm_sourceclient-live_cfg.py          dqmfu18-4_cfg.py
-<<<<<<< installrcms.sh
 
-
-=======
   
->>>>>>> 1.11
-
   cp ../python/test/*live_cfg.py .
 
-#
 elif [ "$1" = "playback" ] ; then
   echo playback
   ln -s ee_dqm_sourceclient-playback_cfg.py           dqmfu09-1_cfg.py  
