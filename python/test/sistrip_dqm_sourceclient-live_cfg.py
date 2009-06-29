1db705184d0d7343acb678e4a6d8f1c75d2308de
Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch>
2009-06-29 07:01:37 +0000

    updating SiStrip config files to change the Global Tag from CRAFT_V17H::All to CRAFT_V18H::All

--- python/test/sistrip_dqm_sourceclient-live_cfg.py
+++ python/test/sistrip_dqm_sourceclient-live_cfg.py
@@ -59,7 +59,7 @@ process.load("Configuration.StandardSequences.Geometry_cff")
 #--------------------------
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG"
-process.GlobalTag.globaltag = "CRAFT_V17H::All"
+process.GlobalTag.globaltag = "CRAFT_V18H::All"
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 #-----------------------
