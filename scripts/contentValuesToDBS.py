ef11e1d74e53b9d17dda38a5e1d1bbf8f4503a71
Valdas Rapsevicius <sha1-696e89da10bf3b1819e4a75ea8d9d57d29d646f7@cern.ch>
2009-07-06 14:21:29 +0000

    RR/DBS scripts modified/added

--- scripts/contentValuesToDBS.py
+++ scripts/contentValuesToDBS.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
 from contentValuesLib import *
- 
+
 SERVER_URL = "http://cmswbm.cms/runregistry/xmlrpc"
 
 class OptionParser(optparse.OptionParser):
   """ Option parser class """
   def __init__(self):
     optparse.OptionParser.__init__(self, usage="%prog [options] root_file ...", version="%prog 0.0.1", conflict_handler="resolve")
-    self.add_option("--shift", "-s", action="store", type="choice", dest="shift", choices=("online","offline"), help="specify shift type: online or offline values allowed")
-    self.add_option("--url", action="store", type="string", dest="url", default=SERVER_URL, help="specify RR XML-RPC server URL. Default is " + SERVER_URL)
-    self.add_option("--debug", "-d", action="store_true", dest="debug", default=False, help="print values and exit. Do not write to RR")
+    self.add_option("--url", action="store", type="string", dest="url", default=SERVER_URL, help="specify DBS DQM XML-RPC server URL. Default is " + SERVER_URL)
+    self.add_option("--debug", "-d", action="store_true", dest="debug", default=False, help="print values and exit. Do not write to DBS")
 
 if __name__ == "__main__":
   
@@ -22,11 +21,6 @@ if __name__ == "__main__":
   # Check if at least one root file defined (can be many!)
   if len(args) == 0:
     print "At least one ROOT file must be priovided, use --help for hit"
-    sys.exit(1)
-
-  # Check if shift type is provided (mandatory!)
-  if not opts['shift']:
-    print "Shift type must be provided, use --help for hit"
     sys.exit(1)
 
   # Check if all files exists and are accessible
@@ -43,15 +37,14 @@ if __name__ == "__main__":
   # defined XML-RPC url 
   for rfile in args:
 
-    values = getSummaryValues(file_name = rfile, shift_type = opts['shift'])
+    values = getSummaryValues(file_name = rfile, shift_type = None)
 
     try:
-      json = dict2json(values)
       if opts['debug']:
-        print json
+        print values
       else:
-        result = server.SummaryValuesWriter.write(json)
-        print "RR: %d rows modified for file %s" % (result, rfile)
+        result = server.insertdq_auto(values)
+        print "DBS DQM: %d rows modified for file %s" % (result, rfile)
     except xmlrpclib.Error, errstring:
       print "ERROR", errstring
       sys.exit(3)
