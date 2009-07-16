69044b0751a7312fdd32502fa158fefe7f926ede
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2009-07-16 09:46:14 +0000

    fixed histo name

--- config/workspaces-online.py
+++ config/workspaces-online.py
@@ -100,7 +100,7 @@ server.workspace('DQMContent', 30, 'Trigger/Lumi', 'L1T', '^L1T/',
                  'L1T/L1TDTTF/DTTF_TRACKS/INTEG/Occupancy Summary',
                  'L1T/L1TCSCTF/CSCTF_Chamber_Occupancies',
                  'L1T/L1TCSCTF/CSCTF_occupancies',
-                 'L1T/L1TRPCTF/RPCTF_muons_tower_phipacked_bx0')
+                 'L1T/L1TRPCTF/RPCTF_muons_eta_phi_bx0')
 
 server.workspace('DQMContent', 30, 'Trigger/Lumi', 'L1TEMU', '^L1TEMU/',
                  'L1TEMU/common/sysrates',
