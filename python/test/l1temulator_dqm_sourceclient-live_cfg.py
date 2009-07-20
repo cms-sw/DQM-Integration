9050eb4c85c6c20f8bef633584afcb0302c368a3
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2009-07-20 14:52:15 +0000

    updated global tag and unmasked systems

--- python/test/l1temulator_dqm_sourceclient-live_cfg.py
+++ python/test/l1temulator_dqm_sourceclient-live_cfg.py
@@ -8,12 +8,9 @@ process.load("DQM.Integration.test.inputsource_cfi")
 process.load("DQM.Integration.test.environment_cfi")
 
 #Please SPECIFY GLOBAL TAG for central OPERATIONS here!
-#process.GlobalTag.globaltag = 'CRAFT_V2H::All'
-#process.GlobalTag.connect = 'frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_21X_GLOBALTAG'
-
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"   
-process.GlobalTag.globaltag = 'GR09_31X_V1H::All' # or any other appropriate
+process.GlobalTag.globaltag = 'GR09_31X_V3H::All' # or any other appropriate
 #process.prefer("GlobalTag")
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
@@ -26,7 +23,7 @@ process.l1GtParameters.BstLengthBytes = 52
 
 #specify subsystems with qt's to be temporarily masked in summary map
 #sequence: dtf,dtp,ctf,ctp,rpc,gmt, etp,htp,rct,gct,gt
-process.l1temuEventInfoClient.maskedSystems = [0,1,1,0,0,0, 0,1,0,0,1]
+#process.l1temuEventInfoClient.maskedSystems = [0,1,1,0,0,0, 0,1,0,0,1]
 
 #replace DQMStore.referenceFileName = "L1TEMU_reference.root"
 process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(
