9c4c267a4a1874517dbdd469a2965663cabf7705
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2009-07-22 20:51:11 +0000

    Fix various things

--- python/test/inputsource_playback_cfi.py
+++ python/test/inputsource_playback_cfi.py
@@ -8,8 +8,9 @@ source = cms.Source("EventStreamHttpReader",
     consumerPriority = cms.untracked.string('normal'),
     max_event_size = cms.int32(7000000),
     consumerName = cms.untracked.string('Playback Source'),
+    SelectHLTOutput = cms.untracked.string('hltOutputDQM'),
     max_queue_depth = cms.int32(5),
-    maxEventRequestRate = cms.untracked.double(12.0),
+    maxEventRequestRate = cms.untracked.double(60.0),
     SelectEvents = cms.untracked.PSet(
         SelectEvents = cms.vstring('*')
     ),
--- python/test/ispy_dqm_sourceclient-playback_cfg.py
+++ python/test/ispy_dqm_sourceclient-playback_cfg.py
@@ -3,61 +3,66 @@ import datetime as dt
 process = cms.Process("IGUANA")
 
 process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("Configuration.StandardSequences.Reconstruction_cff")
-process.load("Configuration.StandardSequences.DigiToRaw_cff")
-process.load("Configuration.StandardSequences.RawToDigi_cff")
-process.load('Configuration.StandardSequences.VtxSmearedEarly10TeVCollision_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('Configuration.StandardSequences.ReconstructionCosmics_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'GR09_31X_V4H::All'
+process.GlobalTag.connect = "frontier://(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"
 
 process.load("DQM.Integration.test.inputsource_playback_cfi")
 process.EventStreamHttpReader.consumerName = 'iSpy Event Display'
 
-process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = "GR09_31X_V3H::ALL"
-process.GlobalTag.connect = "frontier://(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"
 
 from FWCore.MessageLogger.MessageLogger_cfi import *
 
 process.add_(
     cms.Service("IguanaService",
     outputFileName = cms.untracked.string('/home/dqmdevlocal/output/iSpy_%d_%s.ig' % (int(dt.date.today().strftime("%W"))+1,dt.date.today().strftime("%Y%m%d"))),
-    outputMaxEvents = cms.untracked.int32(3000),
+    outputESFileName=cms.untracked.string('/tmp/iSpy_ES.ig'),
     outputHost = cms.untracked.string('localhost'),
     outputPort = cms.untracked.uint32(9000),
-    online = cms.untracked.bool(False),
-    debug = cms.untracked.bool(True)
+    outputMaxEvents = cms.untracked.int32(100),
+    online = cms.untracked.bool(True),
+    debug = cms.untracked.bool(False)
     )
 )
 
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
 
-process.load("VisReco.Analyzer.VisCaloTower_cfi")
+process.load("VisReco.Analyzer.VisEventSetup_cfi")
+process.load("VisReco.Analyzer.VisEvent_cfi")
+process.load("VisReco.Analyzer.VisBasicCluster_cfi")
 process.load("VisReco.Analyzer.VisCSCSegment_cfi")
-process.load('VisReco.Analyzer.VisCSCStripDigi_cfi')
-process.load('VisReco.Analyzer.VisCSCWireDigi_cfi')
-process.load("VisReco.Analyzer.VisDTRecSegment4D_cfi")
+process.load("VisReco.Analyzer.VisCSCStripDigi_cfi")
+process.load("VisReco.Analyzer.VisCSCWireDigi_cfi")
+process.load("VisReco.Analyzer.VisCaloTower_cfi")
 process.load('VisReco.Analyzer.VisDTDigi_cfi')
 process.load('VisReco.Analyzer.VisDTRecHit_cfi')
-process.load('VisReco.Analyzer.VisRPCRecHit_cfi')
-process.load("VisReco.Analyzer.VisEcalRecHit_cfi")
-process.load("VisReco.Analyzer.VisEvent_cfi")
+process.load("VisReco.Analyzer.VisDTRecSegment4D_cfi")
+process.load("VisReco.Analyzer.VisEBRecHit_cfi")
+process.load("VisReco.Analyzer.VisEERecHit_cfi")
+process.load("VisReco.Analyzer.VisESRecHit_cfi")
+#process.load("VisReco.Analyzer.VisEcalRecHit_cfi")
 process.load("VisReco.Analyzer.VisHBRecHit_cfi")
 process.load("VisReco.Analyzer.VisHERecHit_cfi")
 process.load("VisReco.Analyzer.VisHFRecHit_cfi")
 process.load("VisReco.Analyzer.VisHORecHit_cfi")
 process.load("VisReco.Analyzer.VisJet_cfi")
+process.load("VisReco.Analyzer.VisL1GlobalTriggerReadoutRecord_cfi")
 process.load("VisReco.Analyzer.VisMET_cfi")
 process.load("VisReco.Analyzer.VisMuon_cfi")
 process.load("VisReco.Analyzer.VisPixelDigi_cfi")
+process.load('VisReco.Analyzer.VisRPCRecHit_cfi')
 process.load("VisReco.Analyzer.VisSiPixelCluster_cfi")
 process.load("VisReco.Analyzer.VisSiPixelRecHit_cfi")
 process.load("VisReco.Analyzer.VisSiStripCluster_cfi")
 process.load("VisReco.Analyzer.VisSiStripDigi_cfi")
 process.load("VisReco.Analyzer.VisTrack_cfi")
 process.load("VisReco.Analyzer.VisTrackingRecHit_cfi")
-process.load('VisReco.Analyzer.VisSimTrack_cfi')
-process.load('VisReco.Analyzer.VisTrackingParticle_cfi')
-process.load('VisReco.Analyzer.VisTriggerEvent_cfi')
-process.load('VisReco.Analyzer.VisL1GlobalTriggerReadoutRecord_cfi')
+process.load("VisReco.Analyzer.VisTriggerEvent_cfi")
 
 process.VisCSCSegment.visCSCSegmentTag = cms.InputTag("muonCSCSegments")
 process.VisDTRecHit.visDTRecHitTag = cms.InputTag("dt1DRecHits")
@@ -65,37 +70,41 @@ process.VisRPCRecHit.visRPCRecHitTag = cms.InputTag("rpcRecHits")
 process.VisMET.visMETTag = cms.InputTag('genMetIC5GenJets')
 process.VisMuon.visMuonTag = cms.InputTag('muons')
 process.VisSiStripDigi.visSiStripDigiTag = cms.InputTag('siStripDigis:ZeroSuppressed')
-process.VisTrack.visTrackTag = cms.InputTag('generalTracks')
-process.VisTrackingRecHit.visTrackingRecHitTag = cms.InputTag('generalTracks')
+process.VisTrack.visTrackTag = cms.InputTag('cosmicMuons')
+process.VisTrackingRecHit.visTrackingRecHitTag = cms.InputTag('cosmicMuons')
 
 process.vis = cms.Path(process.VisEvent*
-                       process.VisTrack*
-                       process.VisSimTrack*
-                       process.VisTrackingRecHit*
+                       process.VisEventSetup*
+                       process.VisBasicCluster*
                        process.VisCSCSegment*
-                       process.VisCSCWireDigi*
                        process.VisCSCStripDigi*
+                       process.VisCSCWireDigi*
                        process.VisCaloTower*
+                       process.VisTrack*
+                       process.VisTrackingRecHit*
                        process.VisDTRecSegment4D*
                        process.VisDTDigi*
                        process.VisDTRecHit*
                        process.VisRPCRecHit*
-                       process.VisEcalRecHit*
+                       process.VisEBRecHit*
+                       process.VisEERecHit*
+                       process.VisESRecHit*
+                       #process.VisEcalRecHit*
                        process.VisHBRecHit*
                        process.VisHERecHit*
                        process.VisHFRecHit*
                        process.VisHORecHit*
+                       process.VisJet*
                        process.VisMET*
-                       process.VisMuon*
+                       #process.VisMuon*
+                       process.VisPixelDigi*
                        process.VisSiPixelCluster*
                        process.VisSiPixelRecHit*
                        process.VisSiStripCluster*
                        process.VisSiStripDigi*
-                       process.VisJet*
-                       process.VisTrackingParticle*
-                       process.VisTriggerEvent*
-                       process.VisL1GlobalTriggerReadoutRecord)
+                       process.VisL1GlobalTriggerReadoutRecord*
+                       process.VisTriggerEvent)
 
 process.p3= cms.Path(process.RawToDigi)
-process.p4= cms.Path(process.reconstruction)
+process.p4= cms.Path(process.reconstructionCosmics)
 process.schedule = cms.Schedule(process.p3,process.p4,process.vis)
