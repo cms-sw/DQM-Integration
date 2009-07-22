48dc5688c1e6de7a351a083e2b9c8ff6401ee1c0
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2009-07-22 18:53:08 +0000

    *** empty log message ***

--- config/es-layouts.py
+++ config/es-layouts.py
@@ -1,7 +1,5 @@
 def ecalpreshowerlayout(i, p, *rows): i["EcalPreshower/Layouts/" + p] = DQMItem(layout=rows)
-def esshifterlayout(i, p, *rows): i["EcalPreshower/00 Shift/" + p] = DQMItem(layout=rows)
-#def eeshifterlayout(i, p, *rows): i["EcalEndcap/00 Shift/" + p] = DQMItem(layout=rows)
-#def eelayout(i, p, *rows): i["EcalEndcap/02 ECAL Expert Shift/" + p] = DQMItem(layout=rows)
+
 
 # Quick Collections
 ecalpreshowerlayout(dqmitems, "01-IntegritySummary-EcalPreshower",
@@ -17,24 +15,6 @@ ecalpreshowerlayout(dqmitems, "02-OccupancySummary-EcalPreshower",
    { 'path': "EcalPreshower/ESOccupancyTask/ES RecHit 2D Occupancy Z -1 P 2", 'description': "ES RecHit 2D Occupancy Z -1 P 2 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" }])
 
 ecalpreshowerlayout(dqmitems, "03-RechitEnergySummary-EcalPreshower",
-  [{ 'path': "EcalPreshower/ESOccupancyTask/ES RecHit Energy Z 1 P 1", 'description': "ES RecHit Energy Z 1 P 1 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" },
-   { 'path': "EcalPreshower/ESOccupancyTask/ES RecHit Energy Z -1 P 1", 'description': "ES RecHit Energy Z -1 P 1 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" }],
-  [{ 'path': "EcalPreshower/ESOccupancyTask/ES RecHit Energy Z 1 P 2", 'description': "ES RecHit Energy Z 1 P 2 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" },
-   { 'path': "EcalPreshower/ESOccupancyTask/ES RecHit Energy Z -1 P 2", 'description': "ES RecHit Energy Z -1 P 2 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" }])
-
-esshifterlayout(dqmitems, "01-IntegritySummary",
-  [{ 'path': "EcalPreshower/ESIntegrityClient/ES Integrity Summary 1 Z 1 P 1", 'description': "ES+ Front Integrity Summary 1 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" },
-   { 'path': "EcalPreshower/ESIntegrityClient/ES Integrity Summary 1 Z -1 P 1", 'description': "ES- Front Integrity Summary 1 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" }],
-  [{ 'path': "EcalPreshower/ESIntegrityClient/ES Integrity Summary 1 Z 1 P 2", 'description': "ES+ Rear Integrity Summary 1 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" },
-   { 'path': "EcalPreshower/ESIntegrityClient/ES Integrity Summary 1 Z -1 P 2", 'description': "ES- Rear Integrity Summary 1 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" }])
-
-esshifterlayout(dqmitems, "02-OccupancySummary",
-  [{ 'path': "EcalPreshower/ESOccupancyTask/ES RecHit 2D Occupancy Z 1 P 1", 'description': "ES RecHit 2D Occupancy Z 1 P 1 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" },
-   { 'path': "EcalPreshower/ESOccupancyTask/ES RecHit 2D Occupancy Z -1 P 1", 'description': "ES RecHit 2D Occupancy Z -1 P 1 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" }],
-  [{ 'path': "EcalPreshower/ESOccupancyTask/ES RecHit 2D Occupancy Z 1 P 2", 'description': "ES RecHit 2D Occupancy Z 1 P 2 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" },
-   { 'path': "EcalPreshower/ESOccupancyTask/ES RecHit 2D Occupancy Z -1 P 2", 'description': "ES RecHit 2D Occupancy Z -1 P 2 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" }])
-
-esshifterlayout(dqmitems, "03-RechitEnergySummary",
   [{ 'path': "EcalPreshower/ESOccupancyTask/ES RecHit Energy Z 1 P 1", 'description': "ES RecHit Energy Z 1 P 1 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" },
    { 'path': "EcalPreshower/ESOccupancyTask/ES RecHit Energy Z -1 P 1", 'description': "ES RecHit Energy Z -1 P 1 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" }],
   [{ 'path': "EcalPreshower/ESOccupancyTask/ES RecHit Energy Z 1 P 2", 'description': "ES RecHit Energy Z 1 P 2 - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftPreshower>DQMShiftPreshower</a>" },
--- config/workspaces-online.py
+++ config/workspaces-online.py
@@ -1,33 +1,57 @@
 server.workspace('DQMQuality', 0, 'Summaries', 'Summary')
 server.workspace('DQMSummary', 1, 'Summaries', 'Reports')
 server.workspace('DQMShift',   2, 'Summaries', 'Shift')
+server.workspace('DQMContent', 3, 'Summaries', 'Everything', '^')
 
-server.workspace('DQMContent', 10, 'Tracker', 'Pixel', '^Pixel/',
-                 'Pixel/Layouts/00 - Pixel_RawData_FED_Summary',
-                 'Pixel/Layouts/01 - Pixel_RawData_Summary',
-                 'Pixel/Layouts/02 - Pixel_Digi_Occupancy',
-                 'Pixel/Layouts/03 - Pixel_Digi_Summary',
-                 'Pixel/Layouts/04 - Pixel_Cluster_Summary')
 
-server.workspace('DQMContent', 10, 'Tracker', 'SiStrip', '^SiStrip/',
-                 'SiStrip/Layouts/00 - Tracks',
-                 'SiStrip/Layouts/01 - OnTrackClusters(Total Number)',
-                 'SiStrip/Layouts/02 - OnTrackCluster (StoN)', 
-                 'SiStrip/Layouts/03 - OffTrackCluster (Total Number)',
-                 'SiStrip/Layouts/04 - OffTrackCluster (Charge)',
-                 'SiStrip/Layouts/05 - TIBSummary',
-                 'SiStrip/Layouts/06 - TIDFSummary',
-                 'SiStrip/Layouts/07 - TIDBSummary',
-                 'SiStrip/Layouts/08 - TOBSummary',
-                 'SiStrip/Layouts/09 - TECFSummary',
-                 'SiStrip/Layouts/10 - TECBSummary',
-                 'SiStrip/Layouts/11 - FedMonitoringSummary')
+server.workspace('DQMContent', 10, 'Tracker/Muons', 'Pixel', '^Pixel/',
+                 'Pixel/Layouts/00 - Pixel_Error_Summary',
+                 'Pixel/Layouts/01 - Pixel_Digi_Occupancy',
+                 'Pixel/Layouts/02 - Pixel_Noise_Summary',
+                 'Pixel/Layouts/03 - Pixel_Charge_Summary')
+
+server.workspace('DQMContent', 10, 'Tracker/Muons', 'SiStrip', '^(SiStrip|Tracking)/',
+                 'SiStrip/Layouts/00 - ReportSummary',
+                 'SiStrip/Layouts/01 - FED-Detected Errors',
+                 'SiStrip/Layouts/02 - # of Digi Trend',
+                 'SiStrip/Layouts/03 - # of Cluster Trend',
+                 'SiStrip/Layouts/04 - OffTrackCluster (Total Number)',
+                 'SiStrip/Layouts/05 - Tracks')
+               
+server.workspace('DQMContent', 10, 'Tracker/Muons', 'CSC', '^CSC/',
+                 'CSC/Layouts/00 Top Physics Efficiency',
+                 'CSC/Layouts/01 Station Physics Efficiency',
+                 'CSC/Layouts/02 EMU Summary/EMU Test01 - DDUs in Readout',
+                 'CSC/Layouts/02 EMU Summary/EMU Test03 - DDU Reported Errors',
+                 'CSC/Layouts/02 EMU Summary/EMU Test04 - DDU Format Errors',
+                 'CSC/Layouts/02 EMU Summary/EMU Test05 - DDU Inputs Status',
+                 'CSC/Layouts/02 EMU Summary/EMU Test06 - DDU Inputs in ERROR-WARNING State',
+                 'CSC/Layouts/02 EMU Summary/EMU Test08 - CSCs Reporting Data and Unpacked',
+                 'CSC/Layouts/02 EMU Summary/EMU Test10 - CSCs with Errors and Warnings (Fractions)',
+                 'CSC/Layouts/02 EMU Summary/EMU Test11 - CSCs without Data Blocks')
+
+server.workspace('DQMContent', 10, 'Tracker/Muons', 'DT', '^DT/',
+                 'DT/Layouts/00-Summary/00-DataIntegritySummary',
+                 'DT/Layouts/00-Summary/00-ROChannelSummary',
+                 'DT/Layouts/00-Summary/01-OccupancySummary',
+                 'DT/Layouts/00-Summary/02-SegmentSummary',
+                 'DT/Layouts/00-Summary/03-DDU_TriggerCorrFractionSummary',
+                 'DT/Layouts/00-Summary/04-DDU_Trigger2ndFractionSummary',
+                 'DT/Layouts/00-Summary/05-DCC_TriggerCorrFractionSummary',
+                 'DT/Layouts/00-Summary/06-DCC_Trigger2ndFractionSummary',
+                 'DT/Layouts/00-Summary/07-NoiseChannelsSummary',
+                 'DT/Layouts/00-Summary/08-SynchNoiseSummary')
+
+server.workspace('DQMContent', 10, 'Tracker/Muons', 'RPC', '^RPC/',
+                 'RPC/Layouts/RPC_Summary/TOP_Summary_ChamberQuality',
+                 'RPC/Layouts/Occupancy_Summary/Wheels/RPC_BarrelOccupancy',      
+                 'RPC/Layouts/Occupancy_Summary/Wheels/RPC_EndCapOccupancy') 
 
 server.workspace('DQMContent', 20, 'Calorimeter', 'EcalPreshower', '^EcalPreshower',
-#                 'Ecal/Layouts/00-Global-Summary',
-                 'Ecal/Layouts/01-IntegritySummary-EcalPreshower'
-#                 'Ecal/Layouts/02-Cluster-Summary')
-)
+                'EcalPreshower/Layouts/01-IntegritySummary-EcalPreshower',
+                'EcalPreshower/Layouts/02-OccupancySummary-EcalPreshower',
+                'EcalPreshower/Layouts/03-RechitEnergySummary-EcalPreshower') 
+                
 server.workspace('DQMContent', 20, 'Calorimeter', 'EcalBarrel', '^EcalBarrel/',
                  'EcalBarrel/Layouts/00-Global-Summary-EcalBarrel',
                  'EcalBarrel/Layouts/01-Occupancy-Summary-EcalBarrel',
@@ -37,13 +61,6 @@ server.workspace('DQMContent', 20, 'Calorimeter', 'EcalEndcap', '^EcalEndcap/',
                  'EcalEndcap/Layouts/00-Global-Summary-EcalEndcap',
                  'EcalEndcap/Layouts/01-Occupancy-Summary-EcalEndcap',
                  'EcalEndcap/Layouts/02-Cluster-Summary-EcalEndcap')
-
-server.workspace('DQMContent', 20, 'Luminosity', 'HLX', '^HLX',
-                 'HLX/Layouts/HF-Comparison',
-                 'HLX/Layouts/HLX-Averages',
-                 'HLX/Layouts/HLX-Luminosity',
-                 'HLX/Layouts/HLX-Occupancy-Check-Sums',
-                 'HLX/Layouts/HLX-EtSumAndLumi-History-Plots')
 
 
 server.workspace('DQMContent', 20, 'Calorimeter', 'HCAL', '^Hcal/',
@@ -59,33 +76,15 @@ server.workspace('DQMContent', 20, 'Calorimeter', 'HCAL', '^Hcal/',
                  "Hcal/Layouts/03 HCAL Shifter Checklist Plots - Summaries"
                  )
 
-server.workspace('DQMContent', 30, 'Muon', 'CSC', '^CSC/',
-                 'CSC/Layouts/00 Top Physics Efficiency',
-                 'CSC/Layouts/01 Station Physics Efficiency',
-		 'CSC/Layouts/02 EMU Summary/EMU Test01 - DDUs in Readout',
-		 'CSC/Layouts/02 EMU Summary/EMU Test03 - DDU Reported Errors',
-		 'CSC/Layouts/02 EMU Summary/EMU Test04 - DDU Format Errors',
-		 'CSC/Layouts/02 EMU Summary/EMU Test05 - DDU Inputs Status',
-		 'CSC/Layouts/02 EMU Summary/EMU Test06 - DDU Inputs in ERROR-WARNING State',
-		 'CSC/Layouts/02 EMU Summary/EMU Test08 - CSCs Reporting Data and Unpacked',
-		 'CSC/Layouts/02 EMU Summary/EMU Test10 - CSCs with Errors and Warnings (Fractions)',
-		 'CSC/Layouts/02 EMU Summary/EMU Test11 - CSCs without Data Blocks')
+server.workspace('DQMContent', 30, 'Trigger/Lumi', 'HLX', '^HLX',
+                 'HLX/Layouts/HF-Comparison',
+                 'HLX/Layouts/HLX-Averages',
+                 'HLX/Layouts/HLX-Luminosity',
+                 'HLX/Layouts/HLX-Occupancy-Check-Sums',
+                 'HLX/Layouts/HLX-EtSumAndLumi-History-Plots')
 
-server.workspace('DQMContent', 30, 'Muon', 'DT', '^DT/',
-                 'DT/Layouts/00-Summary/00-DataIntegritySummary',
-                 'DT/Layouts/00-Summary/01-OccupancySummary',
-                 'DT/Layouts/00-Summary/02-SegmentSummary',
-                 'DT/Layouts/00-Summary/03-DDU_TriggerCorrFactionSummary',
-                 'DT/Layouts/00-Summary/04-DDU_Trigger2ndFactionSummary',
-                 'DT/Layouts/00-Summary/05-DCC_TriggerCorrFactionSummary',
-                 'DT/Layouts/00-Summary/06-DCC_Trigger2ndFactionSummary',
-                 'DT/Layouts/00-Summary/07-NoiseChannelsSummary')
 
-server.workspace('DQMContent', 30, 'Muon', 'RPC', '^RPC/',
-                 'RPC/Layouts/RPC_BarrelOccupancy',      
-                 'RPC/Layouts/RPC_EndcapOccupancy') 
-
-server.workspace('DQMContent', 40, 'Trigger/DAQ', 'L1T', '^L1T/',
+server.workspace('DQMContent', 30, 'Trigger/Lumi', 'L1T', '^L1T/',
                  'L1T/L1TGT/algo_bits',
                  'L1T/L1TGT/tt_bits',
                  'L1T/L1TGMT/Regional_trigger',
@@ -98,12 +97,12 @@ server.workspace('DQMContent', 40, 'Trigger/DAQ', 'L1T', '^L1T/',
                  'L1T/Layouts/03-RCT-Summary/RctEmNonIsoEmEtEtaPhi',
                  'L1T/Layouts/03-RCT-Summary/RctEmIsoEmEtEtaPhi',
                  'L1T/Layouts/03-RCT-Summary/RctRegionsEtEtaPhi',
-		 'L1T/L1TDTTF/DTTF_TRACKS/INTEG/Occupancy Summary',
-		 'L1T/L1TCSCTF/CSCTF_Chamber_Occupancies',
-		 'L1T/L1TCSCTF/CSCTF_occupancies',
-		 'L1T/L1TRPCTF/RPCTF_muons_eta_phi_bx0')
+                 'L1T/L1TDTTF/DTTF_TRACKS/INTEG/Occupancy Summary',
+                 'L1T/L1TCSCTF/CSCTF_Chamber_Occupancies',
+                 'L1T/L1TCSCTF/CSCTF_occupancies',
+                 'L1T/L1TRPCTF/RPCTF_muons_eta_phi_bx0')
 
-server.workspace('DQMContent', 40, 'Trigger/DAQ', 'L1TEMU', '^L1TEMU/',
+server.workspace('DQMContent', 30, 'Trigger/Lumi', 'L1TEMU', '^L1TEMU/',
                  'L1TEMU/common/sysrates',
                  'L1TEMU/common/errorflag',
                  'L1TEMU/common/sysncandData',
@@ -118,19 +117,14 @@ server.workspace('DQMContent', 40, 'Trigger/DAQ', 'L1TEMU', '^L1TEMU/',
                  'L1TEMU/DTTPG/DTPErrorFlag',
                  'L1TEMU/RPC/RPCErrorFlag',
                  'L1TEMU/ECAL/ETPErrorFlag',
-                 'L1TEMU/HCAL/HTPErrorFlag',
-                 'L1TEMU/Layouts/03-L1TdeRCT-Summary/rctIsoEmEff1',
-                 'L1TEMU/Layouts/03-L1TdeRCT-Summary/rctNisoEmEff1',
-                 'L1TEMU/Layouts/03-L1TdeRCT-Summary/rctRegEff2D')
+                 'L1TEMU/HCAL/HTPErrorFlag')
 
-
-server.workspace('DQMContent', 41, 'Trigger/DAQ', 'HLT', '^HLT/',
+server.workspace('DQMContent', 30, 'Trigger/Lumi', 'HLT', '^HLT/',
                  'HLT/HLTMonMuon/Summary/PassingBits_Summary',
                  'HLT/HLTMonMuon/Summary/Ratio_HLT_L1MuOpen',
                  'HLT/HLTMonMuon/L1PassThrough/Level1/HLTMuonL1_etaphi',
                  'HLT/HLTMonMuon/L1PassThrough/Level1/HLTMuonL1_eta',
                  'HLT/HLTMonMuon/L1PassThrough/Level1/HLTMuonL1_phi')
 
-
-server.workspace('DQMContent', 90, 'Other', 'Everything', '^')
 server.workspace('EVDSnapshot', 99, 'Other', 'Event display', '/home/dqm/iguana-snapshots')
+
