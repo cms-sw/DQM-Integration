3b9ffbd8ea8f6384f564d728c99d4a13b8fb5988
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2009-07-22 08:38:40 +0000

    updated for CRAFT09

--- python/test/pixel_dqm_sourceclient-live_cfg.py
+++ python/test/pixel_dqm_sourceclient-live_cfg.py
@@ -39,9 +39,9 @@ process.dqmEnv.subSystemFolder    = "Pixel"
 # Magnetic Field
 #-----------------------------
 # 0T field
-process.load("Configuration.StandardSequences.MagneticField_0T_cff")
+#process.load("Configuration.StandardSequences.MagneticField_0T_cff")
 # 3.8T field
-#process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 #process.prefer("VolumeBasedMagneticFieldESProducer")
 
 #-------------------------------------------------
@@ -55,7 +55,7 @@ process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 #process.GlobalTag.connect = "sqlite_file:/nfshome0/malgeri/public/globtag/CRZT210_V1H.db"
 process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"
-process.GlobalTag.globaltag = "GR09_31X_V2H::All"
+process.GlobalTag.globaltag = "GR09_31X_V4H::All"
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 #If Frontier is used in xdaq environment use the following service
@@ -64,14 +64,31 @@ process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 #-----------------------
 #  Reconstruction Modules
 #-----------------------
-process.load("Configuration.StandardSequences.ReconstructionCosmics_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+#process.load("Configuration.StandardSequences.ReconstructionCosmics_cff")
 # Real data raw to digi
-process.load("EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi")
+#process.load("EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi")
+process.load("Configuration.StandardSequences.RawToDigi_cff")
 process.siPixelDigis.InputLabel = 'source'
 process.siPixelDigis.IncludeErrors = True
 
 # Local Reconstruction
-#process.load("RecoLocalTracker.SiPixelClusterizer.SiPixelClusterizer_cfi")
+process.load("RecoLocalTracker.SiPixelClusterizer.SiPixelClusterizer_cfi")
+
+process.load("EventFilter.SiStripRawToDigi.SiStripRawToDigis_standard_cff")
+process.siStripDigis.ProductLabel = 'source'
+
+process.load("RecoLocalTracker.SiStripClusterizer.SiStripClusterizer_cfi")
+process.load("RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi")
+process.load("RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitMatcher_cfi")
+process.load("RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi")
+process.load("RecoLocalTracker.SiStripZeroSuppression.SiStripZeroSuppression_cfi")
+
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cff")
+process.load("RecoPixelVertexing.Configuration.RecoPixelVertexing_cff")
+##process.load("RecoLocalTracker.Configuration.RecoLocalTracker_Cosmics_cff")
+process.load("RecoTracker.Configuration.RecoTrackerP5_cff")
+
 
 #--------------------------
 # Pixel DQM Source and Client
@@ -96,6 +113,13 @@ process.AdaptorConfig = cms.Service("AdaptorConfig")
 # Scheduling
 #--------------------------
 #process.Reco = cms.Sequence(process.siPixelDigis)
-process.Reco = cms.Sequence(process.siPixelDigis*process.siPixelClusters)
+process.Reco = cms.Sequence(process.siPixelDigis*process.siPixelClusters*process.siPixelRecHits)
+process.RecoStrips = cms.Sequence(process.siStripDigis*process.siStripClusters)
+process.siPixelLocalReco = cms.Sequence(process.siPixelRecHits) 
+process.siStripLocalReco = cms.Sequence(process.siStripMatchedRecHits)
+process.trackerLocalReco = cms.Sequence(process.siPixelLocalReco*process.siStripLocalReco)
+process.trackReco = cms.Sequence(process.trackerLocalReco*process.offlineBeamSpot*process.recopixelvertexing*process.tracksP5) #*process.rstracks 
 process.DQMmodules = cms.Sequence(process.dqmEnv*process.qTester*process.dqmSaver)
-process.p = cms.Path(process.Reco*process.dqmEnv*process.siPixelP5DQM_source_woTrack*process.PixelP5DQMClientWithDataCertification*process.dqmSaver)
+
+#process.p = cms.Path(process.Reco*process.dqmEnv*process.siPixelP5DQM_source_woTrack*process.PixelP5DQMClientWithDataCertification*process.dqmSaver)
+process.p = cms.Path(process.Reco*process.RecoStrips*process.trackReco*process.dqmEnv*process.siPixelP5DQM_cosmics_source*process.qTester*process.PixelP5DQMClientWithDataCertification*process.dqmSaver)
