250c4e49155ca04772367571440ae43eeba1a33f
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2009-07-29 12:03:27 +0000

    update GT

--- python/test/ee_dqm_sourceclient-file_cfg.py
+++ python/test/ee_dqm_sourceclient-file_cfg.py
@@ -91,7 +91,7 @@ process.source = cms.Source("NewEventStreamFileReader",
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.connect = "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"
-process.GlobalTag.globaltag = "GR09_31X_V3H::All"
+process.GlobalTag.globaltag = "GR09_31X_V4H::All"
 process.prefer("GlobalTag")
 
 process.MessageLogger = cms.Service("MessageLogger",
