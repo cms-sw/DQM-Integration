5f7936719304146fa4f71e2b1f3f295fe680eb5f
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2009-07-29 09:42:52 +0000

    Adjust for V5 DQM GUI.

--- config/server-conf-gui-test.py
+++ config/server-conf-gui-test.py
@@ -4,27 +4,29 @@ BASEDIR   = os.path.dirname(os.path.dirname(__file__))
 CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
 
 LAYOUTS = ["%s/%s-layouts.py" % (CONFIGDIR, x) for x in
-	   ("csc", "dt", "eb", "ee", "hcal", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip")]
+	   ("csc", "dt", "eb", "ee", "es","hcal", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip")]
 LAYOUTS += ["%s/shift_%s_layout.py" % (CONFIGDIR, x) for x in
-            ("csc", "dt", "eb", "ee", "hcal", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip" , "fed" )]
+            ("csc", "dt", "eb", "ee", "es","hcal", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip" , "fed" )]
 
 modules = ("GuiDQM", "GuiEventDisplay")
 envsetup = "export QUIET_ASSERT=a"
 
+#server.instrument  = 'valgrind --num-callers=999 `cmsvgsupp` --error-limit=no'
+#server.instrument  = 'valgrind --tool=helgrind --num-callers=999 --error-limit=no'
+#server.instrument  = 'igprof -t python -pp'
 server.serverDir   = '/home/dqm/gui'
 server.baseUrl     = '/dqm/gui-test'
 server.title       = 'CMS data quality'
 server.serviceName = 'GUI test'
 
 server.plugin('render', BASEDIR + "/style/*.cc")
-server.extend('EVDSnapshotUpload', '/home/dqm/iguana-snapshots')
-server.source('EVDSnapshot', 'evd', '/home/dqm/iguana-snapshots')
-server.source('DQMUnknown', 'unknown', 'DQMArchive', 9097)
-server.source('DQMLive', 'dqm',
-              '--listen 9091', '--collector srv-c2d05-18:9090',
-              '--load ' + server.pathOfPlugin('render'))
+#server.extend('EVDSnapshotUpload', '/home/dqm/iguana-snapshots')
+#server.source('EVDSnapshot', 'evd', '/home/dqm/iguana-snapshots')
+server.source('DQMUnknown', 'unknown', 9091)
+server.source('DQMLive', 'dqm', 'srv-c2d05-19:9090', '--listen 9091',
+	      '--load ' + server.pathOfPlugin('render'))
 server.source('DQMArchive', 'file',
-              '/home/dqm/dqm.db', '--listen 9097',
+              '/home/dqm/idx', '^/Global/', '--listen 9091',
               '--load ' + server.pathOfPlugin('render'))
 server.source('DQMLayout', 'layouts', *LAYOUTS)
 
