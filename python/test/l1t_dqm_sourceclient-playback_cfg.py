5e6d483ec79b2b07210ee683b7caaa79029c4879
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2009-07-30 16:12:17 +0000

    changed globaltag

--- python/test/l1t_dqm_sourceclient-playback_cfg.py
+++ python/test/l1t_dqm_sourceclient-playback_cfg.py
@@ -33,7 +33,7 @@ process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"   
-process.GlobalTag.globaltag = 'GR09_31X_V3H::All' # or any other appropriate
+process.GlobalTag.globaltag = 'GR09_31X_V4H::All' # or any other appropriate
 #process.prefer("GlobalTag")
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
