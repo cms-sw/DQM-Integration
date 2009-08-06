b220aa873414d56d7a2fcafdf814381a9d684e80
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2009-08-06 12:12:17 +0000

    *** empty log message ***

--- python/test/inputsource_cfi.py
+++ python/test/inputsource_cfi.py
@@ -4,6 +4,7 @@ maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
 source = cms.Source("EventStreamHttpReader",
+    #sourceURL = cms.string('http://srv-c2d05-05:50082/urn:xdaq-application:lid=29'),
     #sourceURL = cms.string('http://srv-c2d05-14.cms:22100/urn:xdaq-application:lid=30'),
     #sourceURL = cms.string('http://srv-c2d05-05:50082/urn:xdaq-application:lid=29'),
     sourceURL = cms.string('http://%s:22100/urn:xdaq-application:lid=30' % socket.gethostname()),
