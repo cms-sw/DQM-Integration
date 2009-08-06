f8cd008146a026f1da24ef1641204fb3d60eb1ef
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2009-08-06 21:46:03 +0000

    *** empty log message ***

--- python/test/inputsource_cfi.py
+++ python/test/inputsource_cfi.py
@@ -1,13 +1,12 @@
 import FWCore.ParameterSet.Config as cms
-import socket
+
 maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
 source = cms.Source("EventStreamHttpReader",
+   sourceURL = cms.string('http://srv-c2d05-14.cms:22100/urn:xdaq-application:lid=30'),
     #sourceURL = cms.string('http://srv-c2d05-05:50082/urn:xdaq-application:lid=29'),
-    #sourceURL = cms.string('http://srv-c2d05-14.cms:22100/urn:xdaq-application:lid=30'),
-    #sourceURL = cms.string('http://srv-c2d05-05:50082/urn:xdaq-application:lid=29'),
-    sourceURL = cms.string('http://%s:22100/urn:xdaq-application:lid=30' % socket.gethostname()),
+    #sourceURL = cms.string('http://%s:22100/urn:xdaq-application:lid=30' % socket.gethostname()),
     consumerPriority = cms.untracked.string('normal'),
     max_event_size = cms.int32(7000000),
     consumerName = cms.untracked.string('DQM Source'),
@@ -19,5 +18,5 @@ source = cms.Source("EventStreamHttpReader",
     ),
     headerRetryInterval = cms.untracked.int32(3)
 )
-print source.sourceURL
+
 
