ab4f2db1158933aa73e636f652f9e70a230cd60a
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2009-08-06 20:30:33 +0000

    updated global tag

--- python/test/l1t_dqm_sourceclient-live_cfg.py
+++ python/test/l1t_dqm_sourceclient-live_cfg.py
@@ -49,7 +49,7 @@ process.l1tEventInfoClient.emulatorMaskedSystems = cms.untracked.vstring("All")
 
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"   
-process.GlobalTag.globaltag = 'GR09_31X_V4H::All' # or any other appropriate
+process.GlobalTag.globaltag = 'GR09_31X_V6H::All' # or any other appropriate
 #process.prefer("GlobalTag")
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
