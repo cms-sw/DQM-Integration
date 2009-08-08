41ffeea9c40057dd2bd1d0f189027945a0db8236
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2009-08-08 17:29:25 +0000

    moved to global tag

--- python/test/rpc_dqm_sourceclient-live_cfg.py
+++ python/test/rpc_dqm_sourceclient-live_cfg.py
@@ -17,19 +17,27 @@ process.rpcunpacker = cms.EDFilter("RPCUnpackingModule",
     doSynchro = cms.bool(False)
 )
 
-process.RPCCabling = cms.ESSource("PoolDBESSource",
-    DBParameters = cms.PSet(
-        messageLevel = cms.untracked.int32(0),
-        authenticationPath = cms.untracked.string('/nfshome0/hltpro/cmssw/cfg/')
-    ),
-    timetype = cms.string('runnumber'),
-    toGet = cms.VPSet(cms.PSet(
-        record = cms.string('RPCEMapRcd'),
-        tag = cms.string('RPCEMap_v2')
-    )),
-    connect = cms.string('frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_RPC'),
-    siteLocalConfig = cms.untracked.bool(False)
-)
+
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"
+process.GlobalTag.globaltag = "GR09_31X_V6H::All"
+process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+
+
+## process.RPCCabling = cms.ESSource("PoolDBESSource",
+##     DBParameters = cms.PSet(
+##         messageLevel = cms.untracked.int32(0),
+##         authenticationPath = cms.untracked.string('/nfshome0/hltpro/cmssw/cfg/')
+##     ),
+##     timetype = cms.string('runnumber'),
+##     toGet = cms.VPSet(cms.PSet(
+##         record = cms.string('RPCEMapRcd'),
+##         tag = cms.string('RPCEMap_v2')
+##     )),
+##     connect = cms.string('frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_RPC'),
+##     siteLocalConfig = cms.untracked.bool(False)
+## )
 
 ################# RPC Rec Hits  ######################
 process.load("RecoLocalMuon.RPCRecHit.rpcRecHits_cfi")
@@ -89,7 +97,7 @@ process.rpcChamberQuality = cms.EDAnalyzer("RPCChamberQuality",
                                            )
 
 ################  Sequences ############################
-process.rpcDigi = cms.Sequence(process.rpcunpacker*process.rpcRecHits*process.rpcdigidqm*process.rpcMonitorRaw)
+process.rpcDigi = cms.Sequence(process.rpcunpacker*process.rpcRecHits*process.rpcdigidqm*process.rpcMonitorRaw*process.rpcAfterPulse)
 process.rpcClient = cms.Sequence(process.qTesterRPC*process.rpcdqmclient*process.rpcChamberQuality*process.rpcEventSummary*process.dqmEnv*process.dqmSaver)
 process.p = cms.Path(process.rpcDigi*process.rpcClient)
 
