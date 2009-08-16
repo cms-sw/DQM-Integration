5abf3dd8289f62710715183c49312d055dd9b587
Cms Dqm <sha1-ab433251379c0653a4d5fcb697bb9f400ca219d0@cern.ch>
2009-08-16 11:58:08 +0000

    added relval layouts for HLT

--- config/server-conf-offline.py
+++ config/server-conf-offline.py
@@ -7,6 +7,8 @@ LAYOUTS = ["%s/shift_%s_T0_layout.py" % (CONFIGDIR, x) for x in
            ("eb", "ee", "csc", "rpc", "hcal", "l1t", "l1temulator", "hlt", "pixel", "sistrip", "dt", "muons", "jetmet", "egamma")]
 LAYOUTS += ["%s/%s_T0_layouts.py" % (CONFIGDIR, x) for x in
            ("pixel","sistrip","hcal")]
+LAYOUTS += [CONFIGDIR + "/shift_hlt_relval_layout.py"]
+LAYOUTS += [CONFIGDIR + "/hlt_relval-layouts.py"]
 
 modules = ("GuiDQM",)
 envsetup = "export QUIET_ASSERT=a"
