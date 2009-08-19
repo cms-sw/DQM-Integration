ea023ed56c32166cab10863d6b86a95de628465f
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2009-08-19 08:22:41 +0000

    *** empty log message ***

--- python/test/hcalcalib_dqm_sourceclient-live_cfg.py
+++ python/test/hcalcalib_dqm_sourceclient-live_cfg.py
@@ -8,6 +8,7 @@ process = cms.Process("HCALDQM")
 #-----------------------------
 process.load("DQM.Integration.test.inputsource_cfi")
 #process.EventStreamHttpReader.consumerName = 'Hcal DQM Consumer'
+process.EventStreamHttpReader.sourceURL = cms.string('http://srv-c2c05-09.cms:23100/urn:xdaq-application:lid=30')
 process.EventStreamHttpReader.consumerName = 'Hcal Orbit Gap DQM Consumer'
 process.EventStreamHttpReader.SelectEvents =  cms.untracked.PSet(SelectEvents = cms.vstring('HLT_HcalCalibration'))
 
