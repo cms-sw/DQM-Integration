ad8c63924384bb86ed99b6e929d1afebc85117ec
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2009-08-23 20:24:03 +0000

    *** empty log message ***

---
+++ rcms/hcalcalib_dqm_sourceclient-live_cfg.py
@@ -0,0 +1,162 @@
+import FWCore.ParameterSet.Config as cms
+from DQM.HcalMonitorModule.HcalMonitorModule_cfi import * # Can this be done better?
+from DQM.HcalMonitorClient.HcalMonitorClient_cfi import *
+
+process = cms.Process("HCALDQM")
+#----------------------------
+# Event Source
+#-----------------------------
+process.load("DQM.Integration.test.inputsource_cfi")
+#process.EventStreamHttpReader.consumerName = 'Hcal DQM Consumer'
+process.EventStreamHttpReader.sourceURL = cms.string('http://srv-c2c05-09.cms:23100/urn:xdaq-application:lid=30')
+process.EventStreamHttpReader.consumerName = 'Hcal Orbit Gap DQM Consumer'
+process.EventStreamHttpReader.SelectEvents =  cms.untracked.PSet(SelectEvents = cms.vstring('HLT_HcalCalibration'))
+
+
+#----------------------------
+# DQM Environment
+#-----------------------------
+process.load("DQMServices.Core.DQM_cfg")
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+
+process.load("DQM.Integration.test.environment_cfi")
+process.dqmEnv.subSystemFolder = "HcalCalib"
+process.DQMStore.referenceFileName = '/dqmdata/dqm/reference/hcal_reference.root'
+
+#-----------------------------
+# Hcal Conditions: from Global Conditions Tag 
+#-----------------------------
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.connect = "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"
+process.GlobalTag.globaltag = 'GR09_31X_V6H::All' # or any other appropriate
+process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+
+#-----------------------------
+# Hcal DQM Source, including SimpleReconstrctor
+#-----------------------------
+process.load("DQM.HcalMonitorModule.HcalMonitorModule_cfi")
+process.load("EventFilter.HcalRawToDigi.HcalRawToDigi_cfi")
+process.load("RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_hbhe_cfi")
+process.load("RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_ho_cfi")
+process.load("RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_hf_cfi")
+process.load("RecoLocalCalo.HcalRecProducers.HcalHitReconstructor_zdc_cfi")
+
+# Turn off default blocking of dead channels from rechit reconstructor
+process.essourceSev =  cms.ESSource("EmptyESSource",
+                                               recordName = cms.string("HcalSeverityLevelComputerRcd"),
+                                               firstValid = cms.vuint32(1),
+                                               iovIsRunNotTime = cms.bool(True)
+                            )
+
+
+process.hcalRecAlgos = cms.ESProducer("HcalRecAlgoESProducer",
+                                      SeverityLevels = cms.VPSet(
+    cms.PSet( Level = cms.int32(0),
+              RecHitFlags = cms.vstring(''),
+              ChannelStatus = cms.vstring('')
+              ),
+    cms.PSet( Level = cms.int32(5),
+              RecHitFlags = cms.vstring('HSCP_R1R2','HSCP_FracLeader','HSCP_OuterEnergy',
+                                        'HSCP_ExpFit','ADCSaturationBit'),
+              ChannelStatus = cms.vstring('')
+              ),
+    cms.PSet( Level = cms.int32(8),
+              RecHitFlags = cms.vstring('HBHEHpdHitMultiplicity', 'HBHEPulseShape', 'HOBit',
+                                        'HFDigiTime', 'HFLongShort', 'ZDCBit', 'CalibrationBit',
+                                        'TimingErrorBit'),
+              ChannelStatus = cms.vstring('')
+              ),
+    cms.PSet( Level = cms.int32(10),
+              RecHitFlags = cms.vstring(''),
+              ChannelStatus = cms.vstring('HcalCellHot')
+              ),
+    cms.PSet( Level = cms.int32(20),
+              RecHitFlags = cms.vstring(''),
+              ChannelStatus = cms.vstring('HcalCellOff', 'HcalCellDead')
+              )
+    ),
+                                      RecoveredRecHitBits = cms.vstring('TimingAddedBit','TimingSubtractedBit'),
+                                      DropChannelStatusBits = cms.vstring('HcalCellOff',) #'HcalCellDead' had also been present
+                                      )
+
+# hcalMonitor configurable values -----------------------
+process.hcalMonitor.debug = 0
+#process.hcalMonitor.DigiOccThresh = -999999999 ##Temporary measure while DigiOcc is reworked.
+process.hcalMonitor.pedestalsInFC = True
+process.hcalMonitor.showTiming = False
+process.hcalMonitor.checkNevents=1000
+process.hcalMonitor.dump2database = False
+
+process.hcalMonitor.subSystemFolder = cms.untracked.string("HcalCalib")
+
+# Turn on/off individual hcalMonitor modules ------------
+process.hcalMonitor.DetDiagPedestalMonitor = True
+process.hcalMonitor.DetDiagLaserMonitor    = True
+
+process.hcalMonitor.DataFormatMonitor   = False
+process.hcalMonitor.DataIntegrityTask   = False
+process.hcalMonitor.DigiMonitor         = False
+process.hcalMonitor.RecHitMonitor       = False
+process.hcalMonitor.TrigPrimMonitor     = False
+process.hcalMonitor.DeadCellMonitor     = False
+process.hcalMonitor.HotCellMonitor      = False
+process.hcalMonitor.BeamMonitor         = False
+process.hcalMonitor.PedestalMonitor     = False
+process.hcalMonitor.DetDiagNoiseMonitor = False
+process.hcalMonitor.LEDMonitor          = False
+process.hcalMonitor.CaloTowerMonitor    = False
+process.hcalMonitor.MTCCMonitor         = False
+process.hcalMonitor.HcalAnalysis        = False
+
+# This takes the default cfg values from the hcalMonitor base class and applies them to the subtasks.
+setHcalTaskValues(process.hcalMonitor)
+
+# Set individual Task values here (otherwise they will remain set to the values specified for the hcalMonitor.)
+
+process.hcalMonitor.HotCellMonitor_makeDiagnosticPlots  = False
+process.hcalMonitor.HotCellMonitor_test_neighbor        = False
+
+#-----------------------------
+# Hcal DQM Client
+#-----------------------------
+process.load("DQM.HcalMonitorClient.HcalMonitorClient_cfi")
+
+# hcalClient configurable values ------------------------
+# suppresses html output from HCalClient  
+process.hcalClient.baseHtmlDir = ''  # set to '' to prevent html output
+
+# Set client settings to the same as monitor.  At the moment, this doesn't affect client minErrorFlag
+# Summary Client is also unaffected
+setHcalClientValuesFromMonitor(process.hcalClient,process.hcalMonitor, debug=False)  # turn debug to True to dump out client settings
+
+process.hcalClient.SummaryClient        = True
+#process.hcalClient.HotCellClient        = False
+#process.hcalClient.DeadCellClient       = False
+#process.hcalClient.PedestalClient       = False
+
+
+#-----------------------------
+# Scheduling
+#-----------------------------
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound', 
+        'TooManyProducts', 
+        'TooFewProducts')
+)
+
+process.p = cms.Path(process.hcalDigis*process.hcalMonitor*process.hcalClient*process.dqmEnv*process.dqmSaver)
+
+
+#-----------------------------
+# Quality Tester 
+# will add switch to select histograms to be saved soon
+#-----------------------------
+process.qTester = cms.EDFilter("QualityTester",
+    prescaleFactor = cms.untracked.int32(1),
+    qtList = cms.untracked.FileInPath('DQM/HcalMonitorClient/data/hcal_qualitytest_config.xml'),
+    getQualityTestsFromFile = cms.untracked.bool(True)
+)
+
