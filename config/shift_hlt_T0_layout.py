153f2709503926025b4ff3b3690ff4690faf1f4e
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2009-08-28 19:22:21 +0000

    removing some HLT_L1Mupen, adding HLT_L2Mu9, HLT_Jet30U

--- config/shift_hlt_T0_layout.py
+++ config/shift_hlt_T0_layout.py
@@ -47,25 +47,33 @@ trigvalFVPho(dqmitems,"Photon Eff HLT to RECO",
 
 def hltlayout(i, p, *rows): i["00 Shift/HLT/Cosmics/" + p] = DQMItem(layout=rows)
   
-hltlayout(dqmitems,"00-HLT Shift Histogram",
+hltlayout(dqmitems,"N of HLT_L1MuOpen muons" ,
   	[{'path': "HLT/FourVector/source/HLT_L1MuOpen/HLT_L1MuOpen_wrt__NOn", 'description': "Multiplicity of HLT muons passing HLT_L1Mu path.  For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineHLT\">here</a>."}])
-hltlayout(dqmitems,"01-HLT Shift Histogram",
-  	[{'path': "HLT/FourVector/source/HLT_L1MuOpen/HLT_L1MuOpen_wrt__onEtOn", 'description': "Et of the HLT muons. (never mind that the histogram title reads offline.  For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineHLT\">here</a>."}])
   
-hltlayout(dqmitems,"02-HLT Shift Histogram",
-  	[{'path': "HLT/FourVector/source/HLT_L1MuOpen/HLT_L1MuOpen_wrt__offEtOnOff", 'description': "Et of the RECO muons matched (eta-phi) with the triggering HLT muons.   For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineHLT\">here</a>."}])
+hltlayout(dqmitems,"RECO muons Et",
+  	[{'path': "HLT/FourVector/source/HLT_L1MuOpen/HLT_L1MuOpen_wrt__offEtOnOff", 'description': "Et of the RECO muons matched (eta-phi) with the triggering HLT_L1MuOpen muons.   For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineHLT\">here</a>."}])
   
-hltlayout(dqmitems,"04-HLT Shift Histogram",
-  	[{'path': "HLT/FourVector/source/HLT_L1MuOpen/HLT_L1MuOpen_wrt__onEtaonPhiOn", 'description': "X=eta and Y=phi for HLT muons triggering this path.  For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineHLT\">here</a>."}])
-  
-hltlayout(dqmitems,"05-HLT Shift Histogram",
+hltlayout(dqmitems,"RECO muons eta-phi",
   	[{'path': "HLT/FourVector/source/HLT_L1MuOpen/HLT_L1MuOpen_wrt__offEtaoffPhiOff", 'description': "X=eta and Y=phi for RECO muons that are matched (eta-phi) with HLT muons triggering this path.  For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineHLT\">here</a>."}])
   
-hltlayout(dqmitems,"06-HLT Shift Histogram",
+hltlayout(dqmitems,"HLT_L1MuOpen & L1 muons eta-phi",
   	[{'path': "HLT/FourVector/source/HLT_L1MuOpen/HLT_L1MuOpen_wrt__l1Etal1PhiL1On", 'description': "X=eta and Y=phi for L1 muons that are matched (eta-phi) with HLT muons triggering this path.  For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineHLT\">here</a>."}])
   
-hltlayout(dqmitems,"07-HLT Shift Histogram",
+hltlayout(dqmitems,"Eff HLT_L1MuOpen to its L1 vs Pt",
   	[{'path': "HLT/FourVector/client/HLT_L1MuOpen/custom-eff/HLT_L1MuOpen_wrt__l1Et_Eff_OnToL1", 'description': "Fraction of L1 muons that triggered this HLT paht as function of Et.  For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineHLT\">here</a>."}])
   
-hltlayout(dqmitems,"08-HLT Shift Histogram",
+hltlayout(dqmitems,"Eff HLT_L1MuOpen to its L1 vs eta-phi",
   	[{'path': "HLT/FourVector/client/HLT_L1MuOpen/custom-eff/HLT_L1MuOpen_wrt__l1Etal1Phi_Eff_OnToL1", 'description': "Fraction of L1 muons that triggered this HLT paht as function of eta-phi.  For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineHLT\">here</a>."}])
+
+hltlayout(dqmitems,"Eff HLT_L1MuOpen to its L1 vs Pt",
+  	[{'path': "HLT/FourVector/client/HLT_L2Mu9/custom-eff/HLT_L2Mu9_wrt__l1Et_Eff_OnToL1", 'description': "Fraction of L1 muons that triggered this HLT paht as function of Et.  For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineHLT\">here</a>."}])
+  
+hltlayout(dqmitems,"Eff HLT_L2Mu9 to its L1 vs eta-phi",
+  	[{'path': "HLT/FourVector/client/HLT_L2Mu9/custom-eff/HLT_L2Mu9_wrt__l1Etal1Phi_Eff_OnToL1", 'description': "Fraction of L1 muons that triggered this HLT paht as function of eta-phi.  For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineHLT\">here</a>."}])
+
+hltlayout(dqmitems,"Jet Eff HLT to L1",
+[{'path': "HLT/FourVector/client/HLT_Jet30U/custom-eff/HLT_Jet30U_wrt__l1Et_Eff_OnToL1", 'description':"Efficiency of HLT to L1 for path HLT_Jet50"}])
+
+hltlayout(dqmitems,"Jet Eff HLT to RECO",
+[{'path': "HLT/FourVector/client/HLT_Jet30U/custom-eff/HLT_Jet30U_wrt__offEt_Eff_OnToOff", 'description':"Efficiency of HLT to RECO for path HLT_Jet50"}])
+
