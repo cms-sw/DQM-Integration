9c05da6ebc4e19c03135bf368169f6f1a12e79a0
Cms Dqm <sha1-ab433251379c0653a4d5fcb697bb9f400ca219d0@cern.ch>
2009-09-23 14:30:22 +0000

    test offline Gui configuration

---
+++ config/server-conf-offline-test.py
@@ -0,0 +1,34 @@
+import os.path
+global CONFIGDIR
+BASEDIR   = os.path.dirname(os.path.dirname(__file__))
+CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
+
+LAYOUTS = ["%s/shift_%s_T0_layout.py" % (CONFIGDIR, x) for x in
+           ("eb", "ee", "csc", "rpc", "hcal", "l1t", "l1temulator", "hlt", "pixel", "sistrip", "dt", "muons", "jetmet", "egamma")]
+LAYOUTS += ["%s/%s_T0_layouts.py" % (CONFIGDIR, x) for x in
+           ("pixel","sistrip","hcal", "eb", "ee")]
+LAYOUTS += [CONFIGDIR + "/shift_hlt_relval_layout.py"]
+LAYOUTS += [CONFIGDIR + "/hlt_relval-layouts.py"]
+
+modules = ("GuiDQM",)
+envsetup = "export QUIET_ASSERT=a"
+
+server.port        = 8070
+server.serverDir   = '/data/dqm/dqmtest/gui'
+server.baseUrl     = '/dqm/testing'
+server.title       = 'CMS data quality'
+server.serviceName = 'DQM Testing'
+
+server.plugin('render', BASEDIR + "/style/*.cc")
+server.extend('DQMFileAccess', '/dev/null', '/data/dqm/dqmtest/data',
+	      { 'Current_Offline': '/data/dqm/offline/data',
+	        'Test_1': '/data/dqm/dqmtest/datatest1',
+	        'Test_2': '/data/dqm/dqmtest/datatest2' })
+server.source('DQMUnknown', 'unknown', 8071)
+server.source('DQMArchive', 'file', '/data/dqm/dqmtest/idx', '^/Global/', '--listen 8071',
+              '--load ' + server.pathOfPlugin('render'))
+	      
+server.source('DQMLayout', 'layouts', *LAYOUTS)
+
+execfile(CONFIGDIR + "/dqm-services.py")
+execfile(CONFIGDIR + "/workspaces-tier-0.py")
