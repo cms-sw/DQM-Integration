490cce234671bcbdf9749a5de3ede54caf8ee2c8
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2009-09-29 21:42:27 +0000

    *** empty log message ***

--- python/test/hlt_dqm_sourceclient-playback_cfg.py
+++ python/test/hlt_dqm_sourceclient-playback_cfg.py
@@ -16,7 +16,7 @@ process.GlobalTrackingGeometryESProducer = cms.ESProducer( "GlobalTrackingGeomet
 
 process.load( "Configuration.StandardSequences.FrontierConditions_GlobalTag_cff" )
 process.GlobalTag.connect = "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"
-process.GlobalTag.globaltag = "GR09_31X_V4H::All"
+process.GlobalTag.globaltag = "GR09_H_V2::All"
 
 process.load("DQM.HLTEvF.HLTMonitor_cff")
 process.load("DQM.HLTEvF.HLTMonitorClient_cff")
