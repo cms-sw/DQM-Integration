29ed8d2bac919eb03214d407a97823703978424b
Cms Dqm <sha1-ab433251379c0653a4d5fcb697bb9f400ca219d0@cern.ch>
2009-10-19 12:01:48 +0000

    test hltmuon

--- config/server-conf-offline-test.py
+++ config/server-conf-offline-test.py
@@ -4,9 +4,9 @@ BASEDIR   = os.path.dirname(os.path.dirname(__file__))
 CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
 
 LAYOUTS = ["%s/shift_%s_T0_layout.py" % (CONFIGDIR, x) for x in
-           ("eb", "ee", "csc", "rpc", "hcal", "l1t", "l1temulator", "hlt", "pixel", "sistrip", "dt", "muons", "jetmet", "egamma")]
+           ("eb", "ee", "csc", "rpc", "hcal", "hlt","l1t", "l1temulator", "pixel", "sistrip", "dt", "muons", "jetmet", "egamma")]
 LAYOUTS += ["%s/%s_T0_layouts.py" % (CONFIGDIR, x) for x in
-           ("pixel","sistrip","hcal", "eb", "ee", "hltmuon")]
+           ("pixel","sistrip","hcal", "eb", "ee")]
 LAYOUTS += [CONFIGDIR + "/shift_hlt_relval_layout.py"]
 LAYOUTS += [CONFIGDIR + "/hlt_relval-layouts.py"]
 
@@ -19,7 +19,7 @@ server.baseUrl     = '/dqm/testing'
 server.title       = 'CMS data quality'
 server.serviceName = 'DQM Testing'
 
-server.plugin('render', BASEDIR + "/style/*.cc")
+server.plugin('render', "/data/dqm/dqmtest/style/*.cc")
 server.extend('DQMFileAccess', '/dev/null', '/data/dqm/dqmtest/data',
 	      { 'Current_Offline': '/data/dqm/offline/data',
 	        'Test_1': '/data/dqm/dqmtest/datatest1',
