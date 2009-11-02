0419fe4da6a0ff4ef0218013af5410b3ccf1e51a
neadam <sha1-94c70307ccce623c4c75bc33ad082dccd1c6c3c6@cern.ch>
2009-11-02 11:14:33 +0000

    Updated HLX shift layouts, added correct links and descriptions.

--- config/shift_hlx_layout.py
+++ config/shift_hlx_layout.py
@@ -1,22 +1,22 @@
 def shifthlxlayout(i, p, *rows): i["00 Shift/HLX/" + p] = DQMItem(layout=rows)
 
 shifthlxlayout(dqmitems, "Shifter HLX Lumi Summary",
-  [ {'path':"HLX/Luminosity/LumiIntegratedEtSum", 'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki_SystemOperations>Lumi Shift Histos</a>"},
-    {'path':"HLX/Luminosity/LumiIntegratedOccSet1",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki_SystemOperations>Lumi Shift Histos</a>"},
-    {'path':"HLX/Luminosity/LumiIntegratedOccSet2",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki_SystemOperations>Lumi Shift Histos</a>"} ])
+  [ {'path':"HLX/Luminosity/LumiIntegratedEtSum", 'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/HLXLumiDQMLuminosityMonitor>Integrated Luminosity ET Sum.</a>"},
+    {'path':"HLX/Luminosity/LumiIntegratedOccSet1",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/HLXLumiDQMLuminosityMonitor>Integrated Luminosity Tower Occ Set 1.</a>"},
+    {'path':"HLX/Luminosity/LumiIntegratedOccSet2",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/HLXLumiDQMLuminosityMonitor>Integrated Luminosity Tower Occ Set 2.</a>"} ])
 
-shifthlxlayout(dqmitems, "Shifter HLX HF Compare Summary",
-  [ {'path':"HLX/HFCompare/HFCompareEtSum",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki_SystemOperations>Lumi Shift Histos</a>"}],
-  [ {'path':"HLX/HFCompare/HFCompareOccBelowSet1",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki_SystemOperations>Lumi Shift Histos</a>"},
-    {'path':"HLX/HFCompare/HFCompareOccBelowSet2",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki_SystemOperations>Lumi Shift Histos</a>"}])
+shifthlxlayout(dqmitems, "Shifter HLX Lumi - HF Lumi Comparison Summary",
+  [ {'path':"HLX/HistoryLumi/HistInstantLumiEtSum",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/HLXLumiDQMLumiHistoryMonitor>Et Sum luminosity history - integrated luminosity for each LS in the run to date.</a>"}],
+  [ {'path':"HLX/HistoryLumi/HistInstantLumiOccSet1",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/HLXLumiDQMLumiHistoryMonitor>Tower occupancy set 1 luminosity history - integrated luminosity for each LS in the run to date.</a>"},
+    {'path':"HLX/HistoryLumi/HistInstantLumiOccSet2",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/HLXLumiDQMLumiHistoryMonitor>Tower occupancy set 2 luminosity history - integrated luminosity for each LS in the run to date.</a>"}])
 
-shifthlxlayout(dqmitems, "Shifter HLX Averages Summary",
-  [ {'path':"HLX/Average/AvgEtSum",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki_SystemOperations>Lumi Shift Histos</a>"}],
-  [ {'path':"HLX/Average/AvgOccBelowSet1",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki_SystemOperations>Lumi Shift Histos</a>"},
-    {'path':"HLX/Average/AvgOccBelowSet2",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki_SystemOperations>Lumi Shift Histos</a>"}])
+shifthlxlayout(dqmitems, "Shifter HLX Lumi Errors Summary",
+  [ {'path':"HLX/HistoryLumi/HistInstantLumiEtSumError",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/HLXLumiDQMLumiHistoryMonitor>Et Sum luminosity error history - error on the integrated luminosity for each LS in the run to date.</a>"}],
+  [ {'path':"HLX/HistoryLumi/HistInstantLumiOccSet1Error",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/HLXLumiDQMLumiHistoryMonitor>Tower occupancy set 1 luminosity error history - error on the integrated luminosity for each LS in the run to date.</a>"},
+    {'path':"HLX/HistoryLumi/HistInstantLumiOccSet2Error",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/HLXLumiDQMLumiHistoryMonitor>Tower occupancy set 2 luminosity error history - error on the integrated luminosity for each LS in the run to date.</a>"}])
 
 shifthlxlayout(dqmitems, "Shifter HLX CheckSum Summary",
-  [ {'path':"HLX/CheckSums/SumAllOccSet1",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki_SystemOperations>Lumi Shift Histos</a>"},
-    {'path':"HLX/CheckSums/SumAllOccSet2",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki_SystemOperations>Lumi Shift Histos</a>"} ])
+  [ {'path':"HLX/CheckSums/SumAllOccSet1",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/HLXLumiDQMCellSumMonitor>Cell's alive per wedge, occupancy set 1.</a>"},
+    {'path':"HLX/CheckSums/SumAllOccSet2",'description':"<a href=https://twiki.cern.ch/twiki/bin/view/CMS/HLXLumiDQMCellSumMonitor>Cell's alive per wedge, occupancy set 1.</a>"} ])
 
 
