79224c9ef67717dec994baf30bc05f0ad79c1043
Nuno Viegas Guerreiro Leonardo <sha1-c2958d5bf0c252791bec9eb3947de5286c342841@cern.ch>
2009-11-03 16:26:25 +0000

    use common global tag setting

--- python/test/l1temulator_dqm_sourceclient-file_cfg.py
+++ python/test/l1temulator_dqm_sourceclient-file_cfg.py
@@ -4,68 +4,53 @@ process = cms.Process("L1TEmuDQMfile")
 
 process.load("DQMServices.Core.DQM_cfg")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
+#process.load("DQM.Integration.test.environment_cfi")
 
 process.load("DQM.L1TMonitor.L1TEmulatorMonitor_cff")    
 process.load("DQM.L1TMonitorClient.L1TEMUMonitorClient_cff")    
 
-from L1Trigger.HardwareValidation.L1HardwareValidation_cff import *
-newHWSequence = cms.Sequence(deEcal+deHcal+deRct+deGct+deDt+deDttf+deCsc+deCsctf+deRpc+deGmt+deGt*l1compare)
-process.globalReplace("L1HardwareValidation", newHWSequence)
-
 #include geometry only at top level cfg
 process.load("Configuration.StandardSequences.Geometry_cff")
 
-
-##  Available data masks (case insensitive):
-##    all, gt, muons, jets, taujets, isoem, nonisoem, met
-process.l1temuEventInfoClient.dataMaskedSystems =cms.untracked.vstring("All")
-
-##  Available emulator masks (case insensitive):
-##    all, dttf, dttpg, csctf, csctpg, rpc, gmt, ecal, hcal, rct, gct, glt
-#process.l1temuEventInfoClient.emulatorMaskedSystems = cms.untracked.vstring("")
-
-
-#specify subsystems with qt's to be temporarily masked in summary map
-#sequence: dtf,dtp,ctf,ctp,rpc,gmt, etp,htp,rct,gct,gt
-#process.l1temuEventInfoClient.maskedSystems = [0,1,1,0,0,0, 1,1,1,0,1]
-
-#off-line
-#process.GlobalTag.globaltag = 'CRAFT_V2P::All'
-#process.GlobalTag.connect = 'frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'
-
-
-#online
-process.GlobalTag.connect = "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"   
-process.GlobalTag.globaltag = 'GR09_31X_V4H::All' # or any other appropriate
+### global tag
+process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
+##online
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+##off-line (overwite)
+process.GlobalTag.connect = "frontier://PromptProd/CMS_COND_31X_GLOBALTAG"
+process.GlobalTag.globaltag = "GR09_P_V2::All"
+#process.prefer("GlobalTag")
 process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 
+#from L1Trigger.HardwareValidation.L1HardwareValidation_cff import *
+#newHWSequence = cms.Sequence(deEcal+deHcal+deRct+deGct+deDt+deDttf+deCsc+deCsctf+deRpc+deGmt+deGt*l1compare)
+#process.globalReplace("L1HardwareValidation", newHWSequence)
+
+##  Subsystem masking in summary map (case insensitive)
+##  l1t: all, gt, muons, jets, taujets, isoem, nonisoem, met
+process.l1temuEventInfoClient.dataMaskedSystems =cms.untracked.vstring("All")
+##  emu: all, dttf, dttpg, csctf, csctpg, rpc, gmt, ecal, hcal, rct, gct, glt
+#process.l1tEventInfoClient.emulatorMaskedSystems = cms.untracked.vstring("All")
+process.l1temuEventInfoClient.emulatorMaskedSystems = cms.untracked.vstring("dttf", "dttpg", "csctf", "csctpg", "rpc", "ecal", "hcal", "rct", "glt")
+
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(10)
 )
 
-#process.source = cms.Source("PoolSource",  #"NewEventStreamFileReader",
-process.source = cms.Source("NewEventStreamFileReader",
-    fileNames = cms.untracked.vstring('file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat',
-                                      'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat',
-                                      'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat',
-                                      'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat',
-                                      'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat',
-                                      'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat',
-                                      'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat',
-                                      'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat',
-                                      'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat',
-                                      'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat',
-                                      'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat',
-                                      'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat',
-                                      'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat')
+process.source = cms.Source("PoolSource",
+#process.source = cms.Source("NewEventStreamFileReader",
+    fileNames = cms.untracked.vstring(
+    '/store/data/Commissioning09/Cosmics/RAW/v3/000/118/969/FEA12E2E-EEC5-DE11-8BD0-000423D98C20.root'
+    #'file:/lookarea_SM/MWGR_29.00106019.0036.A.storageManager.07.0000.dat'
+    )
 )
 
 process.DQMStore.verbose = 0
 process.DQM.collectorHost = "srv-c2d05-12"
 process.DQM.collectorPort = 9190
 #process.DQMStore.referenceFileName = "/cmsnfshome0/nfshome0/nuno/DQM_L1TEMU_Ref.root"
-
 
 process.MessageLogger = cms.Service("MessageLogger",
     detailedInfo = cms.untracked.PSet(
@@ -94,4 +79,3 @@ process.dqmSaver.producer = 'DQM'
 process.dqmEnv.subSystemFolder = 'L1TEMU'
 process.dqmSaver.saveByRun = 1
 process.dqmSaver.saveAtJobEnd = True
-
--- python/test/l1temulator_dqm_sourceclient-playback_cfg.py
+++ python/test/l1temulator_dqm_sourceclient-playback_cfg.py
@@ -2,43 +2,30 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("DQM")
 
-process.load("DQM.Integration.test.inputsource_playback_cfi")
 process.load("DQMServices.Core.DQM_cfg")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 process.load("DQM.Integration.test.environment_playback_cfi")
 
 process.load("DQM.L1TMonitor.L1TEmulatorMonitor_cff")    
 process.load("DQM.L1TMonitorClient.L1TEMUMonitorClient_cff")    
-#process.l1GtParameters.BstLengthBytes = 52
-
-from L1Trigger.HardwareValidation.L1HardwareValidation_cff import *
-newHWSequence = cms.Sequence(deEcal+deHcal+deRct+deGct+deDt+deDttf+deCsc+deCsctf+deRpc+deGmt+deGt*l1compare)
-process.globalReplace("L1HardwareValidation", newHWSequence)
 
 process.load("Configuration.StandardSequences.Geometry_cff")
 
+process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
 
-##  Available data masks (case insensitive):
-##    all, gt, muons, jets, taujets, isoem, nonisoem, met
+##  Subsystem masking in summary map (case insensitive)
+##  l1t: all, gt, muons, jets, taujets, isoem, nonisoem, met
 process.l1temuEventInfoClient.dataMaskedSystems =cms.untracked.vstring("All")
+##  emu: all, dttf, dttpg, csctf, csctpg, rpc, gmt, ecal, hcal, rct, gct, glt
+#process.l1tEventInfoClient.emulatorMaskedSystems = cms.untracked.vstring("All")
+#process.l1temuEventInfoClient.emulatorMaskedSystems = cms.untracked.vstring(")
 
-##  Available emulator masks (case insensitive):
-##    all, dttf, dttpg, csctf, csctpg, rpc, gmt, ecal, hcal, rct, gct, glt
-#process.l1temuEventInfoClient.emulatorMaskedSystems = cms.untracked.vstring("")
+#from L1Trigger.HardwareValidation.L1HardwareValidation_cff import *
+#newHWSequence = cms.Sequence(deEcal+deHcal+deRct+deGct+deDt+deDttf+deCsc+deCsctf+deRpc+deGmt+deGt*l1compare)
+#process.globalReplace("L1HardwareValidation", newHWSequence)
 
-#masking sequence: 
-#process.l1temuEventInfoClient.maskedSystems = [0,1,1,0,0,0, 1,1,1,0,1]
-
-#off-line
-#process.GlobalTag.globaltag = 'CRAFT_V2P::All'
-#process.GlobalTag.connect = 'frontier://FrontierProd/CMS_COND_21X_GLOBALTAG'
-
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.connect ="frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"   
-process.GlobalTag.globaltag = 'GR09_31X_V4H::All' # or any other appropriate
-#process.prefer("GlobalTag")
-process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
 
 #replace DQMStore.referenceFileName = "L1TEMU_reference.root"
+
 process.EventStreamHttpReader.consumerName = 'L1TEMU DQM Consumer'
 process.dqmEnv.subSystemFolder = 'L1TEMU'
