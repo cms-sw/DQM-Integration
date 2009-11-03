16c86d849b79af8bd3543ec9b0bc5c2b0ec2b62c
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2009-11-03 14:07:55 +0000

    add scaler and jetmet rate

--- config/shift_hlt_layout.py
+++ config/shift_hlt_layout.py
@@ -3,8 +3,8 @@ def hltlayout(i, p, *rows): i["00 Shift/HLT/" + p] = DQMItem(layout=rows)
 hltlayout(dqmitems,"00 Counts per muon HLT path",
   	[{'path': "HLT/HLTMonMuon/Summary/PassingBits_Summary", 'description': "event count per muon path. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
-hltlayout(dqmitems,"01 Path efficiencies wrt L1MuOpen",
-  	[{'path': "HLT/HLTMonMuon/Summary/Ratio_HLT_L1MuOpen", 'description': "Muon path efficiency with respect to the L1MuOpen. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+hltlayout(dqmitems,"01 HLT Rate w.r.t. L1 trigger path",
+  	[{'path': "HLT/HLTMonMuon/Summary/HLTRate_wrtL1", 'description': "Muon path efficiency with respect to base muon trigger (see histogram). For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
 hltlayout(dqmitems,"02 HLT Muon Occupancy",
   	[{'path': "HLT/HLTMonMuon/L1PassThrough/Level1/HLTMuonL1_etaphi", 'description': "Occupancy distribution for muon candidates passing any L1Muon path. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
@@ -14,4 +14,10 @@ hltlayout(dqmitems,"03 HLT Muon Eta",
 
 hltlayout(dqmitems,"04 HLT Muon Phi",
   	[{'path': "HLT/HLTMonMuon/L1PassThrough/Level1/HLTMuonL1_phi", 'description': "Phi distribution for muon candidates passing any L1Muon path. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+
+hltlayout(dqmitems,"05 HLT Scalers",
+  	[{'path': "HLT/HLTScalers_EvF/hltScalers", 'description': "HLT scaler for all hlt trigger paths. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+
+hltlayout(dqmitems,"06 Counts per JetMET HLT path",
+  	[{'path': "HLT/JetMET/All/rate_All", 'description': "Event triggered JetMET trigger paths . For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
