ef646254bcb96ea0d74965002bbb1143525fb93a
Dmytro Volyanskyy <sha1-ee228e6b0533b5d2566d9787bda6c58e75d066a4@cern.ch>
2009-11-04 14:49:56 +0000

    ---------------------------------------------------------------------- move to FrontierCondition_GT_cfi.py

--- python/test/beam_dqm_sourceclient-live_cfg.py
+++ python/test/beam_dqm_sourceclient-live_cfg.py
@@ -42,10 +42,7 @@ process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cf
 #--------------------------
 # Calibration
 #--------------------------
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.connect = "frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_GLOBALTAG"
-process.GlobalTag.globaltag = 'GR09_H_V2::All' # or any other appropriate
-process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
 
 #-----------------------
 #  Reconstruction Modules
