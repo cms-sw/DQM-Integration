00f38f533b41eb4c94aa8480353c94a6e41b4728
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2009-11-19 12:05:51 +0000

    add SiStrip stuff

--- python/test/hlt_dqm_sourceclient-live_cfg.py
+++ python/test/hlt_dqm_sourceclient-live_cfg.py
@@ -14,8 +14,8 @@ process.load("Configuration.StandardSequences.GeometryPilot2_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.GlobalTrackingGeometryESProducer = cms.ESProducer( "GlobalTrackingGeometryESProducer" ) # for muon hlt dqm
 #SiStrip Local Reco
-#process.SiStripDetInfoFileReader = cms.Service("SiStripDetInfoFileReader")
-#process.TkDetMap = cms.Service("TkDetMap")
+process.SiStripDetInfoFileReader = cms.Service("SiStripDetInfoFileReader")
+process.TkDetMap = cms.Service("TkDetMap")
 
 #---- for P5 (online) DB access
 process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
