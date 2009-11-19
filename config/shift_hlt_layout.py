dd59a707322c2dd5538e344f4887cd4429518948
Hwi Dong Yoo <sha1-643471636f9c8030d6c4344de45015d8db00367c@cern.ch>
2009-11-19 17:18:32 +0000

    add more hist

--- config/shift_hlt_layout.py
+++ config/shift_hlt_layout.py
@@ -1,10 +1,10 @@
 def hltlayout(i, p, *rows): i["00 Shift/HLT/" + p] = DQMItem(layout=rows)
   
 hltlayout(dqmitems,"00 Counts per muon HLT path",
-  	[{'path': "HLT/HLTMonMuon/Summary/PassingBits_Summary", 'description': "event count per muon path. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+  	[{'path': "HLT/HLTMonMuon/Summary/PassingBits_Summary_Muon", 'description': "event count per muon path. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
-hltlayout(dqmitems,"01 HLT Rate w.r.t. L1 trigger path",
-  	[{'path': "HLT/HLTMonMuon/Summary/HLTRate_wrtL1", 'description': "Muon path efficiency with respect to base muon trigger (see histogram). For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+hltlayout(dqmitems,"01 HLT Rate w.r.t. L1 Muon trigger path",
+  	[{'path': "HLT/HLTMonMuon/Summary/HLTRate_Muon", 'description': "Muon path efficiency with respect to base muon trigger (see histogram). For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
 hltlayout(dqmitems,"02 HLT Muon Occupancy",
   	[{'path': "HLT/HLTMonMuon/L1PassThrough/Level1/HLTMuonL1_etaphi", 'description': "Occupancy distribution for muon candidates passing any L1Muon path. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
@@ -21,3 +21,8 @@ hltlayout(dqmitems,"05 HLT Scalers",
 hltlayout(dqmitems,"06 Counts per JetMET HLT path",
   	[{'path': "HLT/JetMET/All/JetMET_rate_All", 'description': "Event triggered JetMET trigger paths . For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
+hltlayout(dqmitems,"07 HLT Rate w.r.t. L1 EG trigger path",
+  	[{'path': "HLT/HLTMonElectron/Summary/HLTRate_Electron", 'description': "EG path efficiency with respect to base EG trigger (see histogram). For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+
+hltlayout(dqmitems,"08 Counts per Tau HLT path",
+  	[{'path': "HLT/TauOnline/Inclusive/SingleTau/TriggerBits", 'description': "Event triggered Tau trigger paths . For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
