9c7b1718f5e3f26e975edc62e2fb0065498b0674
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2009-11-22 22:12:47 +0000

    marginal changes

--- python/test/physics_dqm_sourceclient-live_cfg.py
+++ python/test/physics_dqm_sourceclient-live_cfg.py
@@ -1,4 +1,4 @@
-# $Id: physics_dqm_sourceclient-live_cfg.py,v 1.1 2009/11/17 09:40:25 ameyer Exp $
+# $Id: physics_dqm_sourceclient-live_cfg.py,v 1.2 2009/11/19 17:04:15 ameyer Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -18,30 +18,23 @@ process.EventStreamHttpReader.consumerName = 'Physics DQM Consumer'
 
 process.load("DQMServices.Core.DQM_cfg")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
-
 process.load("DQM.Integration.test.environment_cfi")
 process.dqmEnv.subSystemFolder = 'Physics'
 
 #---- for P5 (online) DB access
 process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
-#---- for offline DB ----
 
-process.load('FWCore/MessageService/MessageLogger_cfi')
 process.load('Configuration/StandardSequences/GeometryIdeal_cff')
-process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+process.load('Configuration/StandardSequences/MagneticField_AutoFromDBCurrent_cff')
 process.load('DQM/Physics/qcdLowPtDQM_cfi')
 
 #process.dump = cms.EDAnalyzer('EventContentAnalyzer')
 
 process.p = cms.Path(
-#    process.dump *
     process.myRecoSeq1  *
-#    process.dump *
     process.myRecoSeq2  *
 #    process.dump *
     process.QcdLowPtDQM *
     process.dqmEnv *
     process.dqmSaver
 )
-
-
