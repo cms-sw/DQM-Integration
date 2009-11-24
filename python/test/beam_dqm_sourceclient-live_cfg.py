dd78b258ce677027c0088c5ab130b9f64c61e28e
Geng-Yuan Jeng <sha1-22504dd22d9623ddee36a32f0ec012f4644ca431@cern.ch>
2009-11-24 04:59:34 +0000

    Add pixelless tracking as an alternative to full tracking

--- python/test/beam_dqm_sourceclient-live_cfg.py
+++ python/test/beam_dqm_sourceclient-live_cfg.py
@@ -8,12 +8,6 @@ process = cms.Process("Beam Monitor")
 process.load("DQM.Integration.test.inputsource_cfi")
 
 #----------------------------
-# DQM Environment
-#-----------------------------
-process.load("DQMServices.Core.DQM_cfg")
-process.load("DQMServices.Components.DQMEnvironment_cfi")
-
-#----------------------------
 # DQM Live Environment
 #-----------------------------
 process.load("DQM.Integration.test.environment_cfi")
@@ -22,16 +16,16 @@ process.dqmEnv.subSystemFolder = 'BeamMonitor'
 #----------------------------
 # BeamMonitor
 #-----------------------------
-process.load("DQM.BeamMonitor.BeamMonitor_Cosmics_cff") # need input track collection in the event
-process.load("DQM.BeamMonitor.BeamConditionsMonitor_cff") # need beam spot collection in the event
+process.load("DQM.BeamMonitor.BeamMonitor_cff")
+process.load("DQM.BeamMonitor.BeamMonitor_PixelLess_cff")
+process.load("DQM.BeamMonitor.BeamConditionsMonitor_cff")
 
 ####  SETUP TRACKING RECONSTRUCTION ####
 
 #-------------------------------------------------
 # GEOMETRY
 #-------------------------------------------------
-#process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.GeometryPilot2_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
 
 #-----------------------------
 # Magnetic Field
@@ -47,42 +41,35 @@ process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
 #-----------------------
 #  Reconstruction Modules
 #-----------------------
-# Real data raw to digi
-process.load("EventFilter.SiStripRawToDigi.SiStripDigis_cfi")
-process.siStripDigis.ProductLabel = 'source'
-process.load("EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi")
-process.siPixelDigis.InputLabel = 'source'
+## Collision Reconstruction
+process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
 
-# Local and Track Reconstruction
-process.load("RecoLocalTracker.Configuration.RecoLocalTracker_Cosmics_cff")
-process.load("RecoTracker.Configuration.RecoTrackerP5_cff")
+## Pixelless Tracking
+process.load('RecoTracker/Configuration/RecoTrackerNotStandard_cff')
+process.MeasurementTracker.pixelClusterProducer = cms.string("")
 
-# offline beam spot
+# Offline Beam Spot
 process.load("RecoVertex.BeamSpotProducer.BeamSpot_cff")
 
 #### END OF TRACKING RECONSTRUCTION ####
 
 
+process.tracking = cms.Sequence(process.siPixelDigis*process.siStripDigis*process.trackerlocalreco*process.offlineBeamSpot*process.recopixelvertexing*process.ckftracks)
 
-process.tracking = cms.Path(process.siPixelDigis*process.siStripDigis*process.offlineBeamSpot*process.trackerlocalreco*process.ctftracksP5*process.cosmictracksP5)
-process.monitor = cms.Path(process.dqmBeamMonitor+process.dqmBeamCondMonitor+process.dqmEnv+process.dqmSaver)
+process.monitor = cms.Sequence(process.dqmBeamMonitor*process.dqmEnv*process.dqmSaver)
+
+process.tracking_pixelless = cms.Sequence(process.siPixelDigis*process.siStripDigis*process.trackerlocalreco*process.offlineBeamSpot*process.recopixelvertexing*process.ctfTracksPixelLess)
+
+process.monitor_pixelless = cms.Sequence(process.dqmBeamMonitor_pixelless*process.dqmEnv*process.dqmSaver)
 
 
-# # summary
+## Summary
 process.options = cms.untracked.PSet(
-    wantSummary = cms.untracked.bool(True)
+	wantSummary = cms.untracked.bool(True)
     )
 
-#process.out = cms.OutputModule("PoolOutputModule",
-#                               fileName = cms.untracked.string('test.root'),
-#			       outputCommands = cms.untracked.vstring(
-#				'drop *',
-#				'keep *_offlineBeamSpot_*_*',
-#				'keep *_ctfWithMaterialTracksP5_*_*',
-#				'keep *_cosmictrackfinderP5_*_*'
-#			       )
-#                              )
-#process.end = cms.EndPath(process.out)
+#process.p = cms.Path(process.tracking*process.monitor)
+process.p = cms.Path(process.tracking_pixelless*process.monitor_pixelless)
 
-process.schedule = cms.Schedule(process.tracking, process.monitor)
 
