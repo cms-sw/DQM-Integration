9fb9c700e255f231a3a82fc9f64c3f1c74de27a7
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2009-11-25 08:25:40 +0000

    *** empty log message ***

--- config/shift_pixel_layout.py
+++ config/shift_pixel_layout.py
@@ -1,12 +1,12 @@
 def shiftpixellayout(i, p, *rows): i["00 Shift/Pixel/" + p] = DQMItem(layout=rows)
-shiftpixellayout(dqmitems, "00 - Mean number of FED errors per FED",
-  [{ 'path': "Pixel/AdditionalPixelErrors/SUMRAW_NErrors_FEDErrors", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"}]
+shiftpixellayout(dqmitems, "00 - Type of last error per FED channel",
+  [{ 'path': "Pixel/AdditionalPixelErrors/FedChLErrArray", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"}]
 )
-shiftpixellayout(dqmitems, "01 - Mean number of FED errors Barrel",
-  [{ 'path': "Pixel/Barrel/SUMRAW_NErrors_Barrel", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"}]
+shiftpixellayout(dqmitems, "01 - Total number of errors per FED channel",
+  [{ 'path': "Pixel/AdditionalPixelErrors/FedChNErrArray", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"}]
 )
-shiftpixellayout(dqmitems, "02 - Mean number of FED errors Endcap",
-  [{ 'path': "Pixel/Endcap/SUMRAW_NErrors_Endcap", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"}]
+shiftpixellayout(dqmitems, "02 - Total number of errors per error type",
+  [{ 'path': "Pixel/AdditionalPixelErrors/FedETypeNErrArray", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"}]
 )
 shiftpixellayout(dqmitems, "03 - Mean digi charge Barrel",
   [{ 'path': "Pixel/Barrel/SUMDIG_adc_Barrel", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiPixel>Description for the Central DQM Shifter</a>"}]
