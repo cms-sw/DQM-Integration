055b158d2e72ce1d4a9494b596e5d10be4184b7b
Constantinos Loizides <sha1-5f4668d3c448f5fd9765e00db2ab69ebc21f8a50@cern.ch>
2009-11-29 10:51:53 +0000

    Enable filter

--- python/test/physics_dqm_sourceclient-live_cfg.py
+++ python/test/physics_dqm_sourceclient-live_cfg.py
@@ -1,4 +1,4 @@
-# $Id: physics_dqm_sourceclient-live_cfg.py,v 1.2 2009/11/19 17:04:15 ameyer Exp $
+# $Id: physics_dqm_sourceclient-live_cfg.py,v 1.3 2009/11/22 22:12:47 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -12,6 +12,11 @@ process.load("DQM.Integration.test.inputsource_cfi")
 process.EventStreamHttpReader.consumerName = 'Physics DQM Consumer'
 #process.EventStreamHttpReader.sourceURL = "http://localhost:50082/urn:xdaq-application:lid=29"
 
+#filter on specific trigger types
+process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(
+    SelectEvents = cms.vstring('HLT_Activity*','HLT_MinBias*','HLT_*BSC*','HLT_HFThreshold*','HLT_*SingleTrack')
+) 
+
 #----------------------------
 # DQM Environment
 #-----------------------------
@@ -20,6 +25,10 @@ process.load("DQMServices.Core.DQM_cfg")
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 process.load("DQM.Integration.test.environment_cfi")
 process.dqmEnv.subSystemFolder = 'Physics'
+
+# 0=random, 1=physics, 2=calibration, 3=technical
+process.load("HLTrigger.special.HLTTriggerTypeFilter_cfi")
+process.hltTriggerTypeFilter.SelectedTriggerType = 1
 
 #---- for P5 (online) DB access
 process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
@@ -31,6 +40,7 @@ process.load('DQM/Physics/qcdLowPtDQM_cfi')
 #process.dump = cms.EDAnalyzer('EventContentAnalyzer')
 
 process.p = cms.Path(
+    process.hltTriggerTypeFilter *
     process.myRecoSeq1  *
     process.myRecoSeq2  *
 #    process.dump *
