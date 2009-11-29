3918558cbd4b724c6922446999d7d7f4db8979ca
Andreas Meyer <sha1-50db6b7cf58ba47574a843ed1de05a28b0c6c126@cern.ch>
2009-11-29 15:10:27 +0000

    first version of overview layouts

--- config/overview-layouts.py
+++ config/overview-layouts.py
@@ -1,12 +1,12 @@
 def overviewlayout(i, p, *rows): i["Overview/" + p] = DQMItem(layout=rows)
 
 overviewlayout(dqmitems, "test1",
-           [{ 'path':"Hcal/EventInfo/reportSummaryMap",
+           [{ 'path':"EcalBarrel/EBSummaryClient/EB global summary",
              'description':"This shows ...."}]
            )
 
 overviewlayout(dqmitems, "test2",
-           [{ 'path':"CSC/EventInfo/CMSSW_Version",
+           [{ 'path':"DT/01-Digi/OccupancySummary",
 	     'description':"This shows ...."}]
            )
 
