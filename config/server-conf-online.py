61d2090d88dbbb6948918a42a6826bf79fa1520a
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2009-12-01 00:11:11 +0000

    Make host-independent for reuse on several servers. Remove link to merged files.

--- config/server-conf-online.py
+++ config/server-conf-online.py
@@ -1,7 +1,14 @@
-import os.path
+import os.path, socket
 global CONFIGDIR
-BASEDIR   = os.path.dirname(os.path.dirname(__file__))
+HOST      = socket.gethostname().lower()
+BASEDIR   = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
+if HOST.find("-c2d04-") > 0:
+  SRVDIR  = '/home/dqmlocal'
+  CHOST   = 'srv-c2d05-19'
+elif HOST.find("-c2d05-") > 0:
+  SRVDIR  = '/home/dqm'
+  CHOST   = 'localhost'
 
 LAYOUTS = ["%s/%s-layouts.py" % (CONFIGDIR, x) for x in
 	   ("castor","csc", "dt", "eb", "ee", "es","hcal", "hcalcalib", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip")]
@@ -16,22 +23,21 @@ envsetup = "export QUIET_ASSERT=a"
 #server.instrument  = 'valgrind --num-callers=999 `cmsvgsupp` --error-limit=no'
 #server.instrument  = 'valgrind --tool=helgrind --num-callers=999 --error-limit=no'
 #server.instrument  = 'igprof -t python -pp'
-server.serverDir   = '/home/dqm/gui'
+server.serverDir   = '%s/gui' % SRVDIR
 server.baseUrl     = '/dqm/online'
 server.title       = 'CMS data quality'
 server.serviceName = 'Online'
 
 server.plugin('render', BASEDIR + "/style/*.cc")
 server.extend('DQMFileAccess', None, "/dqmdata/dqm/uploads",
-              { "Merged": "/dqmdata/dqm/repository/merged",
-                "Original": "/dqmdata/dqm/repository/original",
+              { "Original": "/dqmdata/dqm/repository/original",
                 "iSpy": "/dqmdata/EventDisplay/done" })
 server.extend('DQMRenderLink', server.pathOfPlugin('render'))
 server.source('DQMUnknown', 'unknown')
 server.source('DQMOverlay', 'overlay')
 server.source('DQMStripChart', 'stripchart')
-server.source('DQMLive', 'dqm', 'localhost:9090')
-server.source('DQMArchive', 'file', '/home/dqm/ix', '^/Global/')
+server.source('DQMLive', 'dqm', '%s:9090' % CHOST)
+server.source('DQMArchive', 'file', '%s/ix' % SRVDIR, '^/Global/')
 server.source('DQMLayout', 'layouts', *LAYOUTS)
 
 execfile(CONFIGDIR + "/dqm-services.py")
