a52dea4d4eb3614d83ec69a68e68532a789bfad6
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2009-12-01 10:29:14 +0000

    overview plots for hcal

---
+++ config/hcal_overview_layouts.py
@@ -0,0 +1,24 @@
+def hcallayout(i, p, *rows): i["Overview/HcalFeedBack/"+p] = DQMItem(layout=rows)
+
+hcallayout(dqmitems, "01 - Hcal Parameters",
+           // HF plots
+           [{'path':"Hcal/RecHitMonitor_Hcal/rechit_info/HFweightedtimeDifference",
+             'description':"Difference in weighted times between HF+ and HF-, where times are weighted by rechit energies"},
+            {'path':"Hcal/RecHitMonitor_Hcal/rechit_info/HFtimeDifference",
+             'description':"Difference in average times between HF+ and HF-"},
+            {'path':"Hcal/RecHitMonitor_Hcal/rechit_info/HFenergyDifference",
+             'description':"Sum(E_HFplus - E_HFminus)/Sum(E_HFplus+E_HFminus)"},
+            {'path':"Hcal/RecHitMonitor_Hcal/rechit_info/HFaverageenergyDifference",
+             'description':"Difference in average energies between HF+ and HF-"}
+            ],
+           // HE plots
+           [{'path':"Hcal/RecHitMonitor_Hcal/rechit_info/HEweightedtimeDifference",
+             'description':"Difference in weighted times between HE+ and HE-, where times are weighted by rechit energies"},
+            {'path':"Hcal/RecHitMonitor_Hcal/rechit_info/HEtimeDifference",
+             'description':"Difference in average times between HE+ and HE-"},
+            {'path':"Hcal/RecHitMonitor_Hcal/rechit_info/HEenergyDifference",
+             'description':"Sum(E_HEplus - E_HEminus)/Sum(E_HEplus+E_HEminus)"},
+            {'path':"Hcal/RecHitMonitor_Hcal/rechit_info/HEaverageenergyDifference",
+             'description':"Difference in average energies between HE+ and HE-"}
+            ]
+           )
