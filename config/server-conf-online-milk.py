61d0014ce9c944f4caa8cc4c3873576625b27c87
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2009-12-02 09:44:02 +0000

    Add configuration for the 'milk' instance.

---
+++ config/server-conf-online-milk.py
@@ -0,0 +1,53 @@
+import os.path, socket; global CONFIGDIR
+CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
+
+# Modifiable parameters.
+LAYOUTS = ["%s/%s-layouts.py" % (CONFIGDIR, x) for x in
+	   ("castor","csc", "dt", "eb", "ee", "es","hcal", "hcalcalib", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip")]
+LAYOUTS += ["%s/%s_overview_layouts.py" % (CONFIGDIR, x) for x in
+            ("sistrip","ecal","hcal")]
+LAYOUTS += ["%s/shift_%s_layout.py" % (CONFIGDIR, x) for x in
+            ("castor","csc", "dt", "eb", "ee", "es","hcal", "hcalcalib", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip" , "fed" )]
+
+# Do not modify configuration below this line.
+HOST      = socket.gethostname().lower()
+HOSTADDR  = socket.getaddrinfo(HOST, None)[0][4][0]
+BASEDIR   = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+HOSTALIAS = HOST
+
+# Figure out a preferred alias for this out (if any)
+for alias in ["dqm-prod-local", "dqm-prod-offsite", "dqm-integration", "dqm-test"]:
+  if len([x for x in socket.getaddrinfo(alias, None) if x[4][0] == HOSTADDR]):
+    HOSTALIAS = alias
+    break
+
+# Determine installation directories.
+if HOST.find("-c2d04-") > 0:
+  SRVDIR   = '/home/dqmlocal'
+  COLLHOST = 'localhost'
+
+# Extension modules and environment to install.
+modules = ("GuiDQM",)
+envsetup = "export QUIET_ASSERT=a"
+
+# Server configuration.
+#server.instrument  = 'valgrind --num-callers=999 `cmsvgsupp` --error-limit=no'
+#server.instrument  = 'valgrind --tool=helgrind --num-callers=999 --error-limit=no'
+#server.instrument  = 'igprof -t python -pp'
+server.localBase   = HOSTALIAS
+server.serverDir   = '%s/gui' % SRVDIR
+server.baseUrl     = '/dqm/milk'
+server.title       = 'CMS data quality'
+server.serviceName = 'Milk'
+
+# Contents.
+server.plugin('render', BASEDIR + "/style/*.cc")
+server.extend('DQMRenderLink', server.pathOfPlugin('render'))
+server.source('DQMUnknown', 'unknown')
+server.source('DQMOverlay', 'overlay')
+server.source('DQMStripChart', 'stripchart')
+server.source('DQMLive', 'dqm', '%s:9090' % COLLHOST)
+server.source('DQMLayout', 'layouts', *LAYOUTS)
+
+execfile(CONFIGDIR + "/dqm-services.py")
+execfile(CONFIGDIR + "/workspaces-online.py")
