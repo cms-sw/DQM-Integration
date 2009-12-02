3f4a4e7c1d9d5c6f5082a49c143f37b4befd1bc9
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2009-12-02 21:10:26 +0000

    Added evd for COW, clean up cfgs and added full reconstruction

--- python/test/ispycow_dqm_sourceclient-live_cfg.py
+++ python/test/ispycow_dqm_sourceclient-live_cfg.py
@@ -22,22 +22,38 @@ process.load('Configuration/StandardSequences/VtxSmearedEarly10TeVCollision_cff'
 process.load("DQM/Integration/test/FrontierCondition_GT_cfi")
 process.load("ISpy/Analyzers/ISpy_Online_Producer_cff")
 from FWCore.MessageLogger.MessageLogger_cfi import *
-
+process.load('L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMenuConfig_cff')
+process.es_prefer_l1GtParameters = cms.ESPrefer('L1GtTriggerMenuXmlProducer','l1GtTriggerMenuXml')
+process.load('L1TriggerConfig.L1GtConfigProducers.Luminosity.lumi1031.L1Menu_MC2009_v2_L1T_Scales_20080922_Imp0_Unprescaled_cff')
 ####### Event Source
-process.load("DQM.Integration.test.inputsource_cfi")
-process.EventStreamHttpReader.consumerName = 'ISpy Event Display hltOutputDQM stream'
+process.source = cms.Source("EventStreamHttpReader",
+   sourceURL = cms.string('http://dvsrv-c2f37-01:43100/urn:xdaq-application:lid=50'),
+   consumerPriority = cms.untracked.string('normal'),
+   max_event_size = cms.int32(7000000),
+   consumerName = cms.untracked.string('ISpy COW Event Display '),
+   SelectHLTOutput = cms.untracked.string('outEVD'),
+   max_queue_depth = cms.int32(5),
+   maxEventRequestRate = cms.untracked.double(10.0),
+   SelectEvents = cms.untracked.PSet(
+       SelectEvents = cms.vstring('*')
+   ),
+   headerRetryInterval = cms.untracked.int32(3)
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
 
 ####### DQM Default File Location
 process.load("DQM.Integration.test.environment_cfi")
-
 ####### ISpy Service
 process.add_(
     cms.Service("ISpyService",
-    outputFileName = cms.untracked.string('%s/iSpy_BEAM%d_%s__hltOutputDQM_.ig' % (process.dqmSaver.dirName.value(),int(dt.date.today().strftime("%W"))+1,dt.date.today().strftime("%Y%m%d"))),
+    outputFileName = cms.untracked.string('%s/iSpy_BEAM%d_%s__COW_.ig' % (process.dqmSaver.dirName.value(),int(dt.date.today().strftime("%W"))+1,dt.date.today().strftime("%Y%m%d"))),
+    #outputFileName = cms.untracked.string('/home/lilopera/CMSSW/output/iSpy_MWGR%d_%s__hltOutputDQM_.ig' % (int(dt.date.today().strftime("%W"))+1,dt.date.today().strftime("%Y%m%d"))),
     outputESFileName=cms.untracked.string('/tmp/iSpy_ES.ig'),
     bufferSize = cms.untracked.uint32(1),
     outputHost = cms.untracked.string('localhost'),
-    outputPort = cms.untracked.uint32(9000),
+    outputPort = cms.untracked.uint32(9003),
     outputMaxEvents = cms.untracked.int32(100),
     online = cms.untracked.bool(True),
     debug = cms.untracked.bool(False)
@@ -61,17 +77,7 @@ process.ISpyTrack.iSpyTrackTags = cms.VInputTag(cms.InputTag('cosmicMuons'),
                                                 cms.InputTag('ctfPixelLess'),
                                                 cms.InputTag('pixelTracks')
                                                )
+####### Path                                             
+process.p= cms.Path(process.iSpy_online_sequence)
 
-
-####### Trigger Filters for physics events
-process.load("HLTrigger.special.HLTTriggerTypeFilter_cfi")
-# 0=random, 1=physics, 2=calibration, 3=technical
-process.hltTriggerTypeFilter.SelectedTriggerType = 1
-
-####### Path
-process.p= cms.Path(process.hltTriggerTypeFilter*
-                     process.RawToDigi*
-                     process.reconstruction_withPixellessTk*
-                     process.iSpy_online_sequence
-                    )
 
--- python/test/ispyexpress_dqm_sourceclient-live_cfg.py
+++ python/test/ispyexpress_dqm_sourceclient-live_cfg.py
@@ -1,7 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 import datetime as dt
 import socket
-process = cms.Process("IGUANA")
+process = cms.Process("iSpy EVD")
+
+####### Event processor configuration
 process.options=cms.untracked.PSet(
     SkipEvent = cms.untracked.vstring(["FatalRootError",
     		"Root_Error:  CosmicClusterProducer:cosmicBasicClusters",
@@ -11,25 +13,31 @@ process.options=cms.untracked.PSet(
 		"TUnixSystem::DispatchSignals()",
 		"DataCorrupt"])
   )
-process.load("Configuration.StandardSequences.Geometry_cff")
-process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
-process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
-process.load('Configuration.StandardSequences.ReconstructionCosmics_cff')
-process.load('Configuration.StandardSequences.VtxSmearedEarly10TeVCollision_cff')
-process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
 
+####### Components
+process.load("Configuration/StandardSequences/Geometry_cff")
+process.load("Configuration/StandardSequences/MagneticField_AutoFromDBCurrent_cff")
+process.load('Configuration/StandardSequences/Reconstruction_cff')
+process.load("Configuration/StandardSequences/RawToDigi_Data_cff")
+process.load('Configuration/StandardSequences/L1Reco_cff')
+process.load('Configuration/StandardSequences/VtxSmearedEarly10TeVCollision_cff')
+process.load("DQM/Integration/test/FrontierCondition_GT_cfi")
+process.load("ISpy/Analyzers/ISpy_Online_Producer_cff")
+from FWCore.MessageLogger.MessageLogger_cfi import *
+
+####### Event Source
 process.load("DQM.Integration.test.inputsource_cfi")
 process.EventStreamHttpReader.consumerName = 'ISpy Event Display Express Stream'
 process.EventStreamHttpReader.SelectHLTOutput = cms.untracked.string('hltOutputExpress')
 process.EventStreamHttpReader.sourceURL=cms.string('http://%s:24100/urn:xdaq-application:lid=30' % socket.gethostname())
-#process.EventStreamHttpReader.maxEventRequestRate = cms.untracked.double(0.5)
-process.load("DQM.Integration.test.environment_cfi")
-from FWCore.MessageLogger.MessageLogger_cfi import *
 
+####### DQM Default File Location
+process.load("DQM.Integration.test.environment_cfi")
+
+####### ISpy Service
 process.add_(
     cms.Service("ISpyService",
     outputFileName = cms.untracked.string('%s/iSpy_BEAM%d_%s__hltOutputExpress_.ig' % (process.dqmSaver.dirName.value(),int(dt.date.today().strftime("%W"))+1,dt.date.today().strftime("%Y%m%d"))),
-    #outputFileName = cms.untracked.string('/home/lilopera/CMSSW/output/iSpy_MWGR%d_%s__hltOutputDQM_.ig' % (int(dt.date.today().strftime("%W"))+1,dt.date.today().strftime("%Y%m%d"))),
     outputESFileName=cms.untracked.string('/tmp/iSpy_ES.ig'),
     bufferSize = cms.untracked.uint32(1),
     outputHost = cms.untracked.string('localhost'),
@@ -40,42 +48,7 @@ process.add_(
     )
 )
 
-process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
-)
-
-
-#process.load("ISpy.Analyzers.ISpyEventSetup_cfi")
-process.load("ISpy.Analyzers.ISpyEvent_cfi")
-process.load("ISpy.Analyzers.ISpyBasicCluster_cfi")
-process.load("ISpy.Analyzers.ISpyCSCSegment_cfi")
-process.load("ISpy.Analyzers.ISpyCSCStripDigi_cfi")
-process.load("ISpy.Analyzers.ISpyCSCWireDigi_cfi")
-process.load("ISpy.Analyzers.ISpyCaloTower_cfi")
-process.load('ISpy.Analyzers.ISpyDTDigi_cfi')
-process.load('ISpy.Analyzers.ISpyDTRecHit_cfi')
-process.load("ISpy.Analyzers.ISpyDTRecSegment4D_cfi")
-process.load("ISpy.Analyzers.ISpyEBRecHit_cfi")
-process.load("ISpy.Analyzers.ISpyEERecHit_cfi")
-process.load("ISpy.Analyzers.ISpyESRecHit_cfi")
-process.load("ISpy.Analyzers.ISpyHBRecHit_cfi")
-process.load("ISpy.Analyzers.ISpyHERecHit_cfi")
-process.load("ISpy.Analyzers.ISpyHFRecHit_cfi")
-process.load("ISpy.Analyzers.ISpyHORecHit_cfi")
-process.load("ISpy.Analyzers.ISpyJet_cfi")
-process.load("ISpy.Analyzers.ISpyL1GlobalTriggerReadoutRecord_cfi")
-process.load("ISpy.Analyzers.ISpyMET_cfi")
-process.load("ISpy.Analyzers.ISpyMuon_cfi")
-process.load("ISpy.Analyzers.ISpyPixelDigi_cfi")
-process.load('ISpy.Analyzers.ISpyRPCRecHit_cfi')
-process.load("ISpy.Analyzers.ISpySiPixelCluster_cfi")
-process.load("ISpy.Analyzers.ISpySiPixelRecHit_cfi")
-process.load("ISpy.Analyzers.ISpySiStripCluster_cfi")
-process.load("ISpy.Analyzers.ISpySiStripDigi_cfi")
-process.load("ISpy.Analyzers.ISpyTrack_cfi")
-process.load("ISpy.Analyzers.ISpyTrackingRecHit_cfi")
-process.load("ISpy.Analyzers.ISpyTriggerEvent_cfi")
-
+####### Producers input tags
 process.ISpyCSCSegment.iSpyCSCSegmentTag = cms.InputTag("cscSegments")
 process.ISpyCSCStripDigi.iSpyCSCStripDigiTag = cms.InputTag("muonCSCDigis:MuonCSCStripDigi")
 process.ISpyCSCWireDigi.iSpyCSCWireDigiTag = cms.InputTag("muonCSCDigis:MuonCSCWireDigi")
@@ -83,44 +56,25 @@ process.ISpyDTRecHit.iSpyDTRecHitTag = cms.InputTag("dt1DRecHits")
 process.ISpyRPCRecHit.iSpyRPCRecHitTag = cms.InputTag("rpcRecHits")
 process.ISpyMuon.iSpyMuonTag = cms.InputTag('muons')
 process.ISpySiStripDigi.iSpySiStripDigiTag = cms.InputTag('siStripDigis:ZeroSuppressed')
-process.ISpyTrack.iSpyTrackTag = cms.InputTag('cosmicMuons')
 process.ISpyTrackingRecHit.iSpyTrackingRecHitTag = cms.InputTag('cosmicMuons')
-process.ISpyTrack.iSpyTrackTags = cms.VInputTag(cms.InputTag('cosmicMuons'),cms.InputTag('cosmictrackfinderP5'),cms.InputTag('ctfWithMaterialTracksP5'))
-process.iSpy = cms.Path(process.ISpyEvent*
-                       #process.ISpyEventSetup*
-                       process.ISpyBasicCluster*
-                       process.ISpyCSCSegment*
-                       process.ISpyCSCStripDigi*
-                       process.ISpyCSCWireDigi*
-                       process.ISpyCaloTower*
-                       process.ISpyTrack*
-                       process.ISpyTrackingRecHit*
-                       process.ISpyDTRecSegment4D*
-                       process.ISpyDTDigi*
-                       process.ISpyDTRecHit*
-                       process.ISpyRPCRecHit*
-                       process.ISpyEBRecHit*
-                       process.ISpyEERecHit*
-                       process.ISpyESRecHit*
-                       process.ISpyHBRecHit*
-                       process.ISpyHERecHit*
-                       process.ISpyHFRecHit*
-                       process.ISpyHORecHit*
-                       process.ISpyJet*
-                       process.ISpyMET*
-                       process.ISpyMuon*
-                       process.ISpyPixelDigi*
-                       process.ISpySiPixelCluster*
-                       process.ISpySiPixelRecHit*
-                       process.ISpySiStripCluster*
-                       process.ISpySiStripDigi*
-                       process.ISpyL1GlobalTriggerReadoutRecord*
-                       process.ISpyTriggerEvent)
+process.ISpyTrack.iSpyTrackTag = cms.InputTag('cosmicMuons')
+process.ISpyTrack.iSpyTrackTags = cms.VInputTag(cms.InputTag('cosmicMuons'),
+                                                cms.InputTag('cosmictrackfinderP5'),
+                                                cms.InputTag('ctfWithMaterialTracksP5'),
+                                                cms.InputTag('generalTracks'),
+                                                cms.InputTag('ctfPixelLess'),
+                                                cms.InputTag('pixelTracks')
+                                               )
 
+
+####### Trigger Filters for physics events
 process.load("HLTrigger.special.HLTTriggerTypeFilter_cfi")
 # 0=random, 1=physics, 2=calibration, 3=technical
 process.hltTriggerTypeFilter.SelectedTriggerType = 1
 
-process.p3= cms.Path(process.hltTriggerTypeFilter*process.RawToDigi)
-process.p4= cms.Path(process.reconstructionCosmics)
-process.schedule = cms.Schedule(process.p3,process.p4,process.iSpy)
+####### Path
+process.p= cms.Path(process.hltTriggerTypeFilter*
+                     process.RawToDigi*
+                     process.reconstruction_withPixellessTk*
+                     process.iSpy_online_sequence
+                    )
