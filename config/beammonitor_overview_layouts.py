08f9e7adbee3b2d8d2fb8914dc2f406c86a4a2d3
Geng-Yuan Jeng <sha1-22504dd22d9623ddee36a32f0ec012f4644ca431@cern.ch>
2009-12-03 05:05:03 +0000

    Add layout file for BeamMonitor FeedBack

---
+++ config/beammonitor_overview_layouts.py
@@ -0,0 +1,16 @@
+def tklayout(i, p, *rows): i["Collisions/BeamMonitorFeedBack/" + p] = DQMItem(layout=rows)
+
+tklayout(dqmitems, "00 - BeamMonitor Results",
+         [{ 'path': "BeamMonitor/Fit/d0_phi0",
+            'description': "d0-phi0 correlation of selected tracks - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/BeamMonitorOnlineDQMInstructions>BeamMonitorOnlineDQMInstructions</a> "},
+          { 'path': "BeamMonitor/Fit/trk_z0",
+            'description': "Z0 distribution of selected tracks  - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/BeamMonitorOnlineDQMInstructions>BeamMonitorOnlineDQMInstructions</a> "}],
+         [{ 'path': "BeamMonitor/Fit/x0",
+            'description': "x coordinate of fitted beam spot"},
+          { 'path': "BeamMonitor/Fit/y0",
+            'description': "y coordinate of fitted beam spot"}],
+         [{ 'path': "BeamMonitor/Fit/z0",
+            'description': "z coordinate of fitted beam spot"},
+          { 'path': "BeamMonitor/Fit/sigmaZ0",
+            'description': "sigma z of fitted beam spot"}]
+         )
