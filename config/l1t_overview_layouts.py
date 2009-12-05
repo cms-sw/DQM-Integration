cd41a89dddb1d5482ae090d3f42efe9ff7a6ac4c
Darren Michael Puigh <sha1-c46d72cb3e01124e714a162bae3295f66e002006@cern.ch>
2009-12-05 15:29:49 +0000

    include tech bit rate ratio plots 41/40 and 33/32

--- config/l1t_overview_layouts.py
+++ config/l1t_overview_layouts.py
@@ -12,15 +12,21 @@ l1toverviewlayout(dqmitems,"02 Rate BSC splash left",
 l1toverviewlayout(dqmitems,"03 Rate BSCOR and BPTX",
   	[{'path': "L1T/L1TScalersSCAL/Level1TriggerRates/AlgorithmRates/Rate_AlgoBit_124", 'description': "Rate BSCOR and BPTX. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
-l1toverviewlayout(dqmitems,"04 Integ BSCL*BSCR Triggers vs LS",
+l1toverviewlayout(dqmitems,"04 Rate Ratio 33 over 32",
+  	[{'path': "L1T/L1TScalersSCAL/Level1TriggerRates/TechnicalRates/Rate_TechBit_Ratio_33_over_32", 'description': "Ratio of Tech Bit 33 rate to Tech Bit 32 rate. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+
+l1toverviewlayout(dqmitems,"05 Rate Ratio 41 over 40",
+  	[{'path': "L1T/L1TScalersSCAL/Level1TriggerRates/TechnicalRates/Rate_TechBit_Ratio_41_over_40", 'description': "Ratio of Tech Bit 41 rate to Tech Bit 40 rate. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+
+l1toverviewlayout(dqmitems,"06 Integ BSCL*BSCR Triggers vs LS",
   	[{'path': "L1T/L1TScalersSCAL/Level1TriggerRates/TechnicalRates/Integral_TechBit_040", 'description': "Integrated BSCL*BSCR Triggers vs LS. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
-l1toverviewlayout(dqmitems,"05 Integ BSCL or BSCR Triggers vs LS",
+l1toverviewlayout(dqmitems,"07 Integ BSCL or BSCR Triggers vs LS",
   	[{'path': "L1T/L1TScalersSCAL/Level1TriggerRates/TechnicalRates/Integral_TechBit_042_OR_043", 'description': "Integrated BSCL or BSCR Triggers vs LS. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
-l1toverviewlayout(dqmitems,"06 Integ HF Triggers vs LS",
+l1toverviewlayout(dqmitems,"08 Integ HF Triggers vs LS",
   	[{'path': "L1T/L1TScalersSCAL/Level1TriggerRates/TechnicalRates/Integral_TechBit_009", 'description': "Integrated HF Triggers vs LS. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
-l1toverviewlayout(dqmitems,"07 Integ BSCOR and BPTX",
+l1toverviewlayout(dqmitems,"09 Integ BSCOR and BPTX",
   	[{'path': "L1T/L1TScalersSCAL/Level1TriggerRates/AlgorithmRates/Integral_AlgoBit_124", 'description': "Integrated BSCOR and BPTX. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
 
