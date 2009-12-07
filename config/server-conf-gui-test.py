6f5e424b275c385b86e8e0a29f88e84cff63bd98
Lassi Tuura <sha1-92571bde2a1dfb51d2414b8dbe0d8e18611879de@cern.ch>
2009-12-07 13:33:02 +0000

    Update for recent changes, adjust to 5.1.5.

--- config/server-conf-gui-test.py
+++ config/server-conf-gui-test.py
@@ -5,6 +5,8 @@ CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
 
 LAYOUTS = ["%s/%s-layouts.py" % (CONFIGDIR, x) for x in
 	   ("castor","csc", "dt", "eb", "ee", "es","hcal", "hcalcalib", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip")]
+LAYOUTS += ["%s/%s_overview_layouts.py" % (CONFIGDIR, x) for x in
+            ("sistrip","ecal","hcal","beammonitor","l1t","hlt")]
 LAYOUTS += ["%s/shift_%s_layout.py" % (CONFIGDIR, x) for x in
             ("castor","csc", "dt", "eb", "ee", "es","hcal", "hcalcalib", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip" , "fed" )]
 
@@ -21,17 +23,15 @@ server.serviceName = 'GUI test'
 
 server.plugin('render', BASEDIR + "/style/*.cc")
 server.extend('DQMFileAccess', None, None,
-	      { "dqm": "/dqmdata/dqm/merged",
-	        "ispy": "/dqmdata/EventDisplay/dropbox/000" })
+              { "Original": "/dqmdata/dqm/repository/original",
+                "iSpy": "/dqmdata/EventDisplay/done" })
 server.extend('DQMRenderLink', server.pathOfPlugin('render'))
-#server.extend('EVDSnapshotUpload', '/home/dqm/iguana-snapshots')
-#server.source('EVDSnapshot', 'evd', '/home/dqm/iguana-snapshots')
-server.source('DQMUnknown', 'unknown')
-server.source('DQMOverlay', 'overlay')
-server.source('DQMStripChart', 'stripchart')
-server.source('DQMLive', 'dqm', 'localhost:9090')
-server.source('DQMArchive', 'file', '/home/dqm/ix', '^/Global/')
-server.source('DQMLayout', 'layouts', *LAYOUTS)
+server.source('DQMUnknown')
+server.source('DQMOverlay')
+server.source('DQMStripChart')
+server.source('DQMLive', 'dqm-integration:9090')
+server.source('DQMArchive', '/home/dqm/ix', '^/Global/')
+server.source('DQMLayout', *LAYOUTS)
 
 execfile(CONFIGDIR + "/dqm-services.py")
 execfile(CONFIGDIR + "/workspaces-online.py")
