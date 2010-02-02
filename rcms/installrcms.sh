c045336a81607285dd2cfe12114856bb5f75defd
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2010-02-02 16:55:15 +0000

    update playback servers and links

--- rcms/installrcms.sh
+++ rcms/installrcms.sh
@@ -41,23 +41,37 @@ if [ "$1" = "live" ] ; then
 elif [ "$1" = "playback" ] ; then
   echo playback
   echo "creating softlinks"
-  ln -s ee_dqm_sourceclient-playback_cfg.py           dqmfu09-1_cfg.py  
-  ln -s eb_dqm_sourceclient-playback_cfg.py           dqmfu09-2_cfg.py  
-  ln -s l1t_dqm_sourceclient-playback_cfg.py          dqmfu09-3_cfg.py  
-  ln -s l1temulator_dqm_sourceclient-playback_cfg.py  dqmfu09-4_cfg.py
-  ln -s ispy_dqm_sourceclient-playback_cfg.py         dqmfu09-5_cfg.py
-  ln -s hcal_dqm_sourceclient-playback_cfg.py         dqmfu10-1_cfg.py  
-  ln -s csc_dqm_sourceclient-playback_cfg.py          dqmfu10-2_cfg.py  
-  ln -s dt_dqm_sourceclient-playback_cfg.py           dqmfu10-3_cfg.py  
-  ln -s rpc_dqm_sourceclient-playback_cfg.py          dqmfu10-4_cfg.py  
-  ln -s fed_dqm_sourceclient-playback_cfg.py          dqmfu10-5_cfg.py
-  ln -s pixel_dqm_sourceclient-playback_cfg.py        dqmfu11-1_cfg.py  
-  ln -s hlt_dqm_sourceclient-playback_cfg.py          dqmfu11-2_cfg.py  
-  ln -s hcaltiming_dqm_sourceclient-playback_cfg.py   dqmfu11-3_cfg.py  
-  ln -s sistrip_dqm_sourceclient-playback_cfg.py      dqmfu11-4_cfg.py
-  ln -s es_dqm_sourceclient-playback_cfg.py           dqmfu11-5_cfg.py
+  ln -sfv eb_dqm_sourceclient-live_cfg.py                    dqmfu_06-1_cfg.py
+     ln -sfv fedtest_dqm_sourceclient-live_cfg.py               dqmfu_25-2_cfg.py
+     ln -sfv l1t_dqm_sourceclient-live_cfg.py                   dqmfu_25-3_cfg.py
+     ln -sfv l1temulator_dqm_sourceclient-live_cfg.py           dqmfu_25-4_cfg.py
+     ln -sfv hcal_dqm_sourceclient-live_cfg.py                  dqmfu_25-5_cfg.py
+     ln -sfv physics_dqm_sourceclient-live_cfg.py               dqmfu_25-6_cfg.py
+     ln -sfv info_dqm_sourceclient-live_cfg.py                  dqmfu_25-7_cfg.py 
+     
+     ln -sfv ee_dqm_sourceclient-live_cfg.py                    dqmfu_26-1_cfg.py
+     ln -sfv rpc_dqm_sourceclient-live_cfg.py                   dqmfu_26-2_cfg.py
+     ln -sfv csc_dqm_sourceclient-live_cfg.py                   dqmfu_26-3_cfg.py
+     ln -sfv es_dqm_sourceclient-live_cfg.py                    dqmfu_26-4_cfg.py
+     ln -sfv dt_dqm_sourceclient-live_cfg.py                    dqmfu_26-5_cfg.py
+     ln -sfv castor_dqm_sourceclient_live_cfg.py                dqmfu_26-6_cfg.py
+     
+     ln -sfv pixel_dqm_sourceclient-live_cfg.py                 dqmfu_27-1_cfg.py
+     ln -sfv sistrip_dqm_sourceclient-live_cfg.py               dqmfu_27-2_cfg.py
+     ln -sfv beam_dqm_sourceclient-live_cfg.py                  dqmfu_27-3_cfg.py
+     
+     
+     ln -sfv hlt_dqm_sourceclient-live_cfg.py                   dqmfu_28-1_cfg.py
+     ln -sfv hlx_dqm_sourceclient-live_cfg.py                   dqmfu_28-2_cfg.py
+     ln -sfv fed_dqm_sourceclient-live_cfg.py                   dqmfu_28-3_cfg.py
+     ln -sfv hcalcalib_dqm_sourceclient-live_cfg.py             dqmfu_28-4_cfg.py
+     
+     ln -sfv ispy_dqm_sourceclient-live_cfg.py                  dqmfu_29-1_cfg.py
+     ln -sfv ispycow_dqm_sourceclient-live_cfg.py               dqmfu_29-2_cfg.py
+     ln -sfv ispywide_dqm_sourceclient-live_cfg.py              dqmfu_29-3_cfg.py
+     ln -sfv ispyexpress_dqm_sourceclient-live_cfg.py           dqmfu_29-4_cfg.py
   echo "copying cfg from python test"
-  cp ../python/test/*playback_cfg.py .
+  cp ../python/test/*live_cfg.py .
 
   echo "done"
 
