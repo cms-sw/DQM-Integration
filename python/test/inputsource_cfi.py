0c8049c770d237ec6322d72d724edbf21d0d7730
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2010-02-09 14:52:53 +0000

    update to new machine sources

--- python/test/inputsource_cfi.py
+++ python/test/inputsource_cfi.py
@@ -4,16 +4,23 @@ maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
 source = cms.Source("EventStreamHttpReader",
-    sourceURL = cms.string('http://srv-c2d05-14.cms:22100/urn:xdaq-application:lid=30'),
-    #sourceURL = cms.string('http://localhost:50082/urn:xdaq-application:lid=29'),
-    #sourceURL = cms.string('http://%s:22100/urn:xdaq-application:lid=30' % socket.gethostname()),
-    #sourceURL = cms.string('http://srv-c2d05-05:50082/urn:xdaq-application:lid=29'),
+    #General porpouse SMProxy Server
+    #sourceURL = cms.string('http://srv-c2d05-30.cms:22100/urn:xdaq-application:lid=30'),
+    
+    #SMPlayback Server
+    #sourceURL = cms.string('http://srv-c2d05-30:50082/urn:xdaq-application:lid=29'),
+    
+    #local SMProxy Serve
+    sourceURL = cms.string('http://%s:22100/urn:xdaq-application:lid=30' % socket.gethostname()),
+    
+    #Storage Manager
+    #sourceURL = cms.string('http://srv-c2c06-13.cms:11100/urn:xdaq-application:lid=50'),
     consumerPriority = cms.untracked.string('normal'),
     max_event_size = cms.int32(7000000),
     consumerName = cms.untracked.string('DQM Source'),
     SelectHLTOutput = cms.untracked.string('hltOutputDQM'),
     max_queue_depth = cms.int32(5),
-    maxEventRequestRate = cms.untracked.double(10.0),
+    maxEventRequestRate = cms.untracked.double(100.0),
     SelectEvents = cms.untracked.PSet(
         SelectEvents = cms.vstring('*')
     ),
