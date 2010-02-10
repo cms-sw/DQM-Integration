6fb7f5381aec62da5740b0075fe59dcda290d271
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2010-02-10 11:08:01 +0000

    Fixed typo

--- rcms/installrcms.sh
+++ rcms/installrcms.sh
@@ -41,7 +41,7 @@ if [ "$1" = "live" ] ; then
 elif [ "$1" = "playback" ] ; then
   echo playback
   echo "creating softlinks"
-  ln -sfv eb_dqm_sourceclient-live_cfg.py                    dqmfu_06-1_cfg.py
+     ln -sfv eb_dqm_sourceclient-live_cfg.py                    dqmfu_25-1_cfg.py
      ln -sfv fedtest_dqm_sourceclient-live_cfg.py               dqmfu_25-2_cfg.py
      ln -sfv l1t_dqm_sourceclient-live_cfg.py                   dqmfu_25-3_cfg.py
      ln -sfv l1temulator_dqm_sourceclient-live_cfg.py           dqmfu_25-4_cfg.py
@@ -72,41 +72,6 @@ elif [ "$1" = "playback" ] ; then
      ln -sfv ispyexpress_dqm_sourceclient-live_cfg.py           dqmfu_29-4_cfg.py
   echo "copying cfg from python test"
   cp ../python/test/*live_cfg.py .
-
-  echo "done"
-
-elif [ "$1" = "fakelive" ] ; then
-  echo fakelive
-  echo "creating softlinks"
-
-    ln -sf fedtest_dqm_sourceclient-live_cfg.py     dqmfu09-1_cfg.py
-    ln -sf l1t_dqm_sourceclient-live_cfg.py         dqmfu09-2_cfg.py
-    ln -sf l1temulator_dqm_sourceclient-live_cfg.py dqmfu09-3_cfg.py
-    
-    ln -sf hcal_dqm_sourceclient-live_cfg.py        dqmfu10-1_cfg.py
-    ln -sf castor_dqm_sourceclient_live_cfg.py      dqmfu10-2_cfg.py
-    ln -sf hlx_dqm_sourceclient-live_cfg.py         dqmfu10-3_cfg.py
-    
-    ln -sf ee_dqm_sourceclient-live_cfg.py          dqmfu11-1_cfg.py
-    ln -sf rpc_dqm_sourceclient-live_cfg.py         dqmfu11-2_cfg.py
-    ln -sf csc_dqm_sourceclient-live_cfg.py         dqmfu11-3_cfg.py
-    
-    ln -sf eb_dqm_sourceclient-live_cfg.py          dqmfu15-1_cfg.py
-    ln -sf es_dqm_sourceclient-live_cfg.py          dqmfu15-2_cfg.py
-    ln -sf dt_dqm_sourceclient-live_cfg.py          dqmfu15-3_cfg.py
-    
-    ln -sf pixel_dqm_sourceclient-live_cfg.py       dqmfu16-1_cfg.py
-    ln -sf sistrip_dqm_sourceclient-live_cfg.py     dqmfu16-2_cfg.py
-    ln -sf ispy_dqm_sourceclient-live_cfg.py        dqmfu16-3_cfg.py
-    
-    ln -sf fed_dqm_sourceclient-live_cfg.py         dqmfu17-1_cfg.py
-    ln -sf hlt_dqm_sourceclient-live_cfg.py         dqmfu17-2_cfg.py
-    ln -sf daq_dqm_sourceclient-live_cfg.py         dqmfu17-3_cfg.py
-    ln -sf hcalcalib_dqm_sourceclient-live_cfg.py   dqmfu17-4_cfg.py
-
-  echo "copying cfg from python test"
-  cp ../python/test/*live_cfg.py .
-  
 
   echo "done"
 
