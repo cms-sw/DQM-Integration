b6a4109669910b6c2d9df18f968f4775ee8358f6
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2010-02-18 20:16:25 +0000

    Added ext Hcal monitoring plots

--- config/workspaces-tier-0.py
+++ config/workspaces-tier-0.py
@@ -38,7 +38,11 @@ server.workspace('DQMContent', 23, 'Calorimeter', 'EcalPreshower', '^EcalPreshow
 server.workspace('DQMContent', 24, 'Calorimeter', 'HCAL', '^Hcal/',
                  'Hcal/Layouts/01 HCAL Summaries',
                  'Hcal/Layouts/HCAL Dead Cell Check',
-                 'Hcal/Layouts/HCAL Hot Cell Check')
+                 'Hcal/Layouts/HCAL Hot Cell Check',
+                 'Hcal/Layouts/HCAL Data Format',
+                 'Hcal/Layouts/HCAL Lumi Problems',
+                 'Hcal/Layouts/HCAL Digi Problems',
+                 )
 
 server.workspace('DQMContent', 20, 'Calorimeter', 'CASTOR', '^Castor/',
                  'Castor/Layouts/CASTOR Pedestals',
