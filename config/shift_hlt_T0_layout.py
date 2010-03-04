be64fc499cfe045618e70459fc485cae6889c951
Dimitrije Maletic <sha1-dadf9b4a90fc13c50bb2966bf9e0fcc2233bd6cb@cern.ch>
2010-03-04 09:58:56 +0000

    Adding L1, On and Off histograms

--- config/shift_hlt_T0_layout.py
+++ config/shift_hlt_T0_layout.py
@@ -13,6 +13,17 @@ trigvalFVMuon(dqmitems,"Muon Eff HLT to L1",
 trigvalFVMuon(dqmitems,"Muon Eff HLT to RECO",
 [{'path': "HLT/FourVector/client/HLT_Mu9/custom-eff/HLT_Mu9_wrt__offEt_Eff_OnToOff", 'description':"Efficiency of HLT to RECO for path HLT_Mu9"}])
 
+trigvalFVMuon(dqmitems,"HLT_Mu9: Et of L1 Muon Objects",
+[{'path': "HLT/FourVector/source/HLT_Mu9/HLT_Mu9_wrt__l1EtL1", 'description':"Et of L1 Muon object for path HLT_Mu9"}])
+
+trigvalFVMuon(dqmitems,"HLT_Mu9: Et of HLT Muon Objects",
+[{'path': "HLT/FourVector/source/HLT_Mu9/HLT_Mu9_wrt__onEtOn", 'description':"Et of HLT Muon object for path HLT_Mu9"}])
+
+trigvalFVMuon(dqmitems,"HLT_Mu9: Et of RECO Muon Objects",
+[{'path': "HLT/FourVector/source/HLT_Mu9/HLT_Mu9_wrt__offEtOff", 'description':"Et of RECO Muon object for path HLT_Mu9"}])
+
+
+
 ###---- GENERIC - FourVector Electron
 def trigvalFVEle(i, p, *rows): i["00 Shift/HLT/FourVector/Electron/" + p] = DQMItem(layout=rows)
   
@@ -23,15 +34,35 @@ trigvalFVEle(dqmitems,"Electron Eff HLT to L1",
 trigvalFVEle(dqmitems,"Electron Eff HLT to RECO",
 [{'path': "HLT/FourVector/client/HLT_Ele10_LW_L1R/custom-eff/HLT_Ele10_LW_L1R_wrt__offEt_Eff_OnToOff", 'description':"Efficiency of HLT to RECO for path HLT_Ele10_LW_L1R"}])
 
+trigvalFVEle(dqmitems,"HLT_Ele10_LW_L1R: Et of L1 Electron Objects",
+     [{'path': "HLT/FourVector/source/HLT_Ele10_LW_L1R/HLT_Ele10_LW_L1R_wrt__l1EtL1", 'description':"Et of L1 Electron object for path HLT_Ele10_LW_L1R"}])
+
+trigvalFVEle(dqmitems,"HLT_Ele10_LW_L1R: Et of HLT Electron Objects",
+[{'path': "HLT/FourVector/source/HLT_Ele10_LW_L1R/HLT_Ele10_LW_L1R_wrt__onEtOn", 'description':"Et of HLT Electron object for path HLT_Ele10_LW_L1R"}])
+
+trigvalFVEle(dqmitems,"HLT_Ele10_LW_L1R: Et of RECO Electron Objects",
+[{'path': "HLT/FourVector/source/HLT_Ele10_LW_L1R/HLT_Ele10_LW_L1R_wrt__offEtOff", 'description':"Et of RECO Electron object for path HLT_Ele10_LW_L1R"}])
+
+             
+
 ###---- GENERIC - FourVector Jet
 def trigvalFVJet(i, p, *rows): i["00 Shift/HLT/FourVector/Jet/" + p] = DQMItem(layout=rows)
   
 
 trigvalFVJet(dqmitems,"Jet Eff HLT to L1",
-[{'path': "HLT/FourVector/client/HLT_Jet50/custom-eff/HLT_Jet50_wrt__l1Et_Eff_OnToL1", 'description':"Efficiency of HLT to L1 for path HLT_Jet50"}])
+[{'path': "HLT/FourVector/client/HLT_Jet30U/custom-eff/HLT_Jet30U_wrt__l1Et_Eff_OnToL1", 'description':"Efficiency of HLT to L1 for path HLT_Jet30U"}])
 
 trigvalFVJet(dqmitems,"Jet Eff HLT to RECO",
-[{'path': "HLT/FourVector/client/HLT_Jet50/custom-eff/HLT_Jet50_wrt__offEt_Eff_OnToOff", 'description':"Efficiency of HLT to RECO for path HLT_Jet50"}])
+[{'path': "HLT/FourVector/client/HLT_Jet30U/custom-eff/HLT_Jet30U_wrt__offEt_Eff_OnToOff", 'description':"Efficiency of HLT to RECO for path HLT_Jet30U"}])
+
+trigvalFVJet(dqmitems,"HLT_Jet30U: Et of L1 Jet Objects",
+[{'path': "HLT/FourVector/source/HLT_Jet30U/HLT_Jet30U_wrt__l1EtL1", 'description':"Et of L1 Jet object for path HLT_Jet30U"}])
+             
+trigvalFVJet(dqmitems,"HLT_Jet30U: Et of HLT Jet Objects",
+[{'path': "HLT/FourVector/source/HLT_Jet30U/HLT_Jet30U_wrt__onEtOn", 'description':"Et of HLT Jet object for path HLT_Jet30U"}])
+
+trigvalFVJet(dqmitems,"HLT_Jet30U: Et of RECO Jet Objects",
+[{'path': "HLT/FourVector/source/HLT_Jet30U/HLT_Jet30U_wrt__offEtOff", 'description':"Et of RECO Jet object for path HLT_Jet30U"}])
 
 ###---- GENERIC - FourVector Photon
 def trigvalFVPho(i, p, *rows): i["00 Shift/HLT/FourVector/Photon/" + p] = DQMItem(layout=rows)
@@ -43,6 +74,14 @@ trigvalFVPho(dqmitems,"Photon Eff HLT to L1",
 trigvalFVPho(dqmitems,"Photon Eff HLT to RECO",
 [{'path': "HLT/FourVector/client/HLT_Photon15_L1R/custom-eff/HLT_Photon15_L1R_wrt__offEt_Eff_OnToOff", 'description':"Efficiency of HLT to RECO for path HLT_Photon15_L1R"}])
 
+trigvalFVPho(dqmitems,"HLT_Photon15_L1R: Et of L1 Photon Objects",
+[{'path': "HLT/FourVector/source/HLT_Photon15_L1R/HLT_Photon15_L1R_wrt__l1EtL1", 'description':"Et of L1 Photon object for path HLT_Photon15_L1R"}])
+
+trigvalFVPho(dqmitems,"HLT_Photon15_L1R: Et of HLT Photon Objects",
+[{'path': "HLT/FourVector/source/HLT_Photon15_L1R/HLT_Photon15_L1R_wrt__onEtOn", 'description':"Et of HLT Photon object for path HLT_Photon15_L1R"}])
+
+trigvalFVPho(dqmitems,"HLT_Photon15_L1R: Et of RECO Photon objects",
+[{'path': "HLT/FourVector/source/HLT_Photon15_L1R/HLT_Photon15_L1R_wrt__offEtOff", 'description':"Et of RECO Photon object for path HLT_Photon15_L1R"}])
 
 
 def hltlayout(i, p, *rows): i["00 Shift/HLT/Cosmics/" + p] = DQMItem(layout=rows)
