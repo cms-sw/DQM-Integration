bedcee32ce6166a9a4b44a5714988fcc8e798f57
Geng-Yuan Jeng <sha1-22504dd22d9623ddee36a32f0ec012f4644ca431@cern.ch>
2010-04-16 13:25:23 +0000

    Update

--- python/test/beam_dqm_sourceclient-live_cfg.py
+++ python/test/beam_dqm_sourceclient-live_cfg.py
@@ -116,7 +116,7 @@ else:
 #process.dqmBeamMonitor.BeamFitter.SaveFitResults = False
 #process.dqmBeamMonitor.BeamFitter.OutputFileName = '/nfshome0/yumiceva/BeamMonitorDQM/BeamFitResults.root'
 
-process.dqmBeamMonitor.BeamFitter.InputBeamWidth = 0.006
+#process.dqmBeamMonitor.BeamFitter.InputBeamWidth = 0.006
 
 ## TKStatus
 process.dqmTKStatus = cms.EDFilter("TKStatus",
