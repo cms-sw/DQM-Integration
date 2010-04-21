3d273a83f453412c804e41ab29c7b448af183c57
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2010-04-21 21:02:45 +0000

    *** empty log message ***

--- config/pixel_T0_layouts.py
+++ config/pixel_T0_layouts.py
@@ -1,14 +1,18 @@
 def pixellayout(i, p, *rows): i["Pixel/Layouts/" + p] = DQMItem(layout=rows)
 
-pixellayout(dqmitems, "00 - Pixel_Error_Summary",
-  [{ 'path': "Pixel/AdditionalPixelErrors/SUMOFF_NErrors_FEDErrors",
-     'description': "Mean number of errors per FED crate (not associated with a specific module ID - should be empty!", 
-     'draw': { 'withref': "no" }}],
-  [{ 'path': "Pixel/Barrel/SUMOFF_NErrors_Barrel",
-     'description': "Mean number of errors per barrel module - should be empty!",
-     'draw': { 'withref': "no" }}],
-  [{ 'path': "Pixel/Endcap/SUMOFF_NErrors_Endcap",
-     'description': "Mean number of errors per endcap module - should be empty!",
+pixellayout(dqmitems, "00a - Pixel_Error_Summary",
+  [{ 'path': "Pixel/AdditionalPixelErrors/FedChLErrArray",
+     'description': "Error type of last error in a map of FED channels (y-axis) vs. FED (x-axis). Channel 0 is assigned for errors where the channel number is not known.", 
+     'draw': { 'withref': "no" }}]
+  )
+pixellayout(dqmitems, "00b - Pixel_Error_Summary",
+  [{ 'path': "Pixel/AdditionalPixelErrors/FedChNErrArray",
+     'description': "Total number of errors in a map of FED channels (y-axis) vs. FED (x-axis). Channel 0 is assigned for errors where the channel number is not known.",
+     'draw': { 'withref': "no" }}]
+  )
+pixellayout(dqmitems, "00c - Pixel_Error_Summary",
+  [{ 'path': "Pixel/AdditionalPixelErrors/FedETypeNErrArray",
+     'description': "Total number of errors per error type in a map of error type (y-axis) vs. FED (x-axis)",
      'draw': { 'withref': "no" }}]
   )
 pixellayout(dqmitems, "01 - Pixel_Digi_Summary",
@@ -26,24 +30,24 @@ pixellayout(dqmitems, "01 - Pixel_Digi_Summary",
      'draw': { 'withref': "yes" }}]
   )
 pixellayout(dqmitems, "02 - Pixel_Cluster_Summary",
-  [{ 'path': "Pixel/Barrel/SUMOFF_charge_Barrel",
-     'description': "Mean cluster charge in kilo electrons per barrel Ladder",
-     'draw': { 'withref': "yes" }},
-   { 'path': "Pixel/Barrel/SUMOFF_nclusters_Barrel",
-     'description': "Mean number of clusters per event per barrel Ladder",
-     'draw': { 'withref': "yes" }},
-   { 'path': "Pixel/Barrel/SUMOFF_size_Barrel",
-     'description': "Mean cluster size in number of pixels per barrel Ladder",
-     'draw': { 'withref': "yes" }}],
-  [{ 'path': "Pixel/Endcap/SUMOFF_charge_Endcap",
-     'description': "Mean cluster charge in kilo electrons per endcap Blade",
-     'draw': { 'withref': "yes" }},
-   { 'path': "Pixel/Endcap/SUMOFF_nclusters_Endcap",
-     'description': "Mean number of clusters per event per endcap Blade",
-     'draw': { 'withref': "yes" }},
-   { 'path': "Pixel/Endcap/SUMOFF_size_Endcap",
-     'description': "Mean cluster size in number of pixels per barrel Blade",
-     'draw': { 'withref': "yes" }}])
+  [{ 'path': "Pixel/Barrel/SUMOFF_charge_OnTrack_Barrel",
+     'description': "Mean cluster charge (OnTrack) in kilo electrons per barrel Ladder",
+     'draw': { 'withref': "no" }},
+   { 'path': "Pixel/Barrel/SUMOFF_nclusters_OnTrack_Barrel",
+     'description': "Mean number of clusters (OnTrack) per event per barrel Ladder",
+     'draw': { 'withref': "no" }},
+   { 'path': "Pixel/Barrel/SUMOFF_size_OnTrack_Barrel",
+     'description': "Mean cluster size (OnTrack) in number of pixels per barrel Ladder",
+     'draw': { 'withref': "no" }}],
+  [{ 'path': "Pixel/Endcap/SUMOFF_charge_OnTrack_Endcap",
+     'description': "Mean cluster charge (OnTrack) in kilo electrons per endcap Blade",
+     'draw': { 'withref': "no" }},
+   { 'path': "Pixel/Endcap/SUMOFF_nclusters_OnTrack_Endcap",
+     'description': "Mean number of clusters (OnTrack) per event per endcap Blade",
+     'draw': { 'withref': "no" }},
+   { 'path': "Pixel/Endcap/SUMOFF_size_OnTrack_Endcap",
+     'description': "Mean cluster size (OnTrack) in number of pixels per barrel Blade",
+     'draw': { 'withref': "no" }}])
 pixellayout(dqmitems, "03 - Pixel_Track_Summary",
   [{ 'path': "Pixel/Clusters/OnTrack/charge_siPixelClusters", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineSiPixel>Description for the Central DQM Shifter</a>"},
    { 'path': "Pixel/Clusters/OnTrack/size_siPixelClusters", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineSiPixel>Description for the Central DQM Shifter</a>"}],
@@ -51,17 +55,29 @@ pixellayout(dqmitems, "03 - Pixel_Track_Summary",
    { 'path': "Pixel/Clusters/OffTrack/size_siPixelClusters", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineSiPixel>Description for the Central DQM Shifter</a>"}],
   [{ 'path': "Pixel/Tracks/ntracks_generalTracks", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftOfflineSiPixel>Description for the Central DQM Shifter</a>"}]
 )
-pixellayout(dqmitems, "04 - Barrel_Digi_Summary_per_Shell",
-  [{ 'path': "Pixel/Barrel/Shell_mI/SUMOFF_adc_Shell_mI", 'description': "Mean digi charge in ADC counts per Barrel Shell_mI module"},
-   { 'path': "Pixel/Barrel/Shell_mO/SUMOFF_ndigis_Shell_mO", 'description': "Mean number of digis per event per Barrel Shell_mO module"}],
-  [{ 'path': "Pixel/Barrel/Shell_pI/SUMOFF_adc_Shell_pI", 'description': "Mean digi charge in ADC counts per Barrel Shell_pI module"},
-   { 'path': "Pixel/Barrel/Shell_pO/SUMOFF_ndigis_Shell_pO", 'description': "Mean number of digis per event per Barrel Shell_pO module"}]
+pixellayout(dqmitems, "04a - Barrel_DigiCharge_Summary_per_Shell",
+  [{ 'path': "Pixel/Barrel/Shell_mI/SUMOFF_adc_Shell_mI", 'description': "Mean digi charge in ADC counts per Barrel Shell_mI Ladder"},
+   { 'path': "Pixel/Barrel/Shell_mO/SUMOFF_adc_Shell_mO", 'description': "Mean digi charge in ADC counts per Barrel Shell_mO Ladder"}],
+  [{ 'path': "Pixel/Barrel/Shell_pI/SUMOFF_adc_Shell_pI", 'description': "Mean digi charge in ADC counts per Barrel Shell_pI Ladder"},
+   { 'path': "Pixel/Barrel/Shell_pO/SUMOFF_adc_Shell_pO", 'description': "Mean digi charge in ADC counts per Barrel Shell_pO Ladder"}]
 )     
-pixellayout(dqmitems, "05 - Endcap_Digi_Summary_per_HalfCylinder",
-  [{ 'path': "Pixel/Endcap/HalfCylinder_mI/SUMOFF_adc_HalfCylinder_mI", 'description': "Mean digi charge in ADC counts per Endcap HalfCylinder_mI module"},
-   { 'path': "Pixel/Endcap/HalfCylinder_mO/SUMOFF_ndigis_HalfCylinder_mO", 'description': "Mean number of digis per event per Endcap HalfCylinder_mO module"}],
-  [{ 'path': "Pixel/Endcap/HalfCylinder_pI/SUMOFF_adc_HalfCylinder_pI", 'description': "Mean digi charge in ADC counts per Endcap HalfCylinder_pI module"},
-   { 'path': "Pixel/Endcap/HalfCylinder_pO/SUMOFF_ndigis_HalfCylinder_pO", 'description': "Mean number of digis per event per Endcap HalfCylinder_pO module"}]
+pixellayout(dqmitems, "04b - Endcap_DigiCharge_Summary_per_HalfCylinder",
+  [{ 'path': "Pixel/Endcap/HalfCylinder_mI/SUMOFF_adc_HalfCylinder_mI", 'description': "Mean digi charge in ADC counts per Endcap HalfCylinder_mI Blade"},
+   { 'path': "Pixel/Endcap/HalfCylinder_mO/SUMOFF_adc_HalfCylinder_mO", 'description': "Mean digi charge in ADC counts per Endcap HalfCylinder_mO Blade"}],
+  [{ 'path': "Pixel/Endcap/HalfCylinder_pI/SUMOFF_adc_HalfCylinder_pI", 'description': "Mean digi charge in ADC counts per Endcap HalfCylinder_pI Blade"},
+   { 'path': "Pixel/Endcap/HalfCylinder_pO/SUMOFF_adc_HalfCylinder_pO", 'description': "Mean digi charge in ADC counts per Endcap HalfCylinder_pO Blade"}]
+)
+pixellayout(dqmitems, "05a - Barrel_NDigis_Summary_per_Shell",
+  [{ 'path': "Pixel/Barrel/Shell_mI/SUMOFF_ndigis_Shell_mI", 'description': "Mean ndigis per event per Barrel Shell_mI Ladder"},
+   { 'path': "Pixel/Barrel/Shell_mO/SUMOFF_ndigis_Shell_mO", 'description': "Mean ndigis per event per Barrel Shell_mO Ladder"}],
+  [{ 'path': "Pixel/Barrel/Shell_pI/SUMOFF_ndigis_Shell_pI", 'description': "Mean ndigis per event per Barrel Shell_pI Ladder"},
+   { 'path': "Pixel/Barrel/Shell_pO/SUMOFF_ndigis_Shell_pO", 'description': "Mean ndigis per event per Barrel Shell_pO Ladder"}]
+)     
+pixellayout(dqmitems, "05b - Endcap_NDigis_Summary_per_HalfCylinder",
+  [{ 'path': "Pixel/Endcap/HalfCylinder_mI/SUMOFF_ndigis_HalfCylinder_mI", 'description': "Mean ndigis per event per Endcap HalfCylinder_mI Blade"},
+   { 'path': "Pixel/Endcap/HalfCylinder_mO/SUMOFF_ndigis_HalfCylinder_mO", 'description': "Mean ndigis per event per Endcap HalfCylinder_mO Blade"}],
+  [{ 'path': "Pixel/Endcap/HalfCylinder_pI/SUMOFF_ndigis_HalfCylinder_pI", 'description': "Mean ndigis per event per Endcap HalfCylinder_pI Blade"},
+   { 'path': "Pixel/Endcap/HalfCylinder_pO/SUMOFF_ndigis_HalfCylinder_pO", 'description': "Mean ndigis per event per Endcap HalfCylinder_pO Blade"}]
 )
 pixellayout(dqmitems, "06 - Barrel OnTrack cluster positions",
   [{ 'path': "Pixel/Clusters/OnTrack/position_siPixelClusters_Layer_1", 'description': "Global position of OnTrack clusters in Barrel/Layer_1"}],
