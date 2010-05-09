8b6c69b8577763699bd5e1b6fcb3dbcbd9aa916d
Jason Michael Slaunwhite <sha1-f8e4660fc6d5a3c7bc6b3bf329fff136ec0c5225@cern.ch>
2010-05-09 11:25:03 +0000

    Updated TPG layout

--- config/hlt-layouts.py
+++ config/hlt-layouts.py
@@ -35,6 +35,8 @@ tpgSummary_l1t(dqmitems, "03.03 - Muon Timing DT vs RPC",
            [{'path': "L1T/L1TGMT/bx_CSC_vs_RPC", 'description':"Muon Timing"}])
 
 
+
+
 ######################## TPG HLT ################################3
 
 def tpgSummary_hlt(i, p, *rows): 
@@ -42,10 +44,57 @@ def tpgSummary_hlt(i, p, *rows):
 
 
 tpgSummary_hlt(dqmitems, "01 - HLT Postdeadtime Rate",
-           [{'path': "HLT/HLTScalers_SM/hltRate", 'description':"HLT Rate Postdeadtime"}])
+           [{'path': "HLT/HLTScalers_EvF/hltRateNorm", 'description':"HLT Rate Postdeadtime"}])
 
-tpgSummary_hlt(dqmitems, "02 - HLT MinBiasBSC Counts",
-           [{'path': "HLT/FourVector/PathsSummary/HLT LS/Paths/HLT_MinBiasBSC_count_per_LS", 'description':"HLT MinBias BSC Counts per lumi sec"}])
+tpgSummary_hlt(dqmitems, "02 - HLT MinBiasBSC Rate",
+           [{'path': "HLT/HLTScalers_EvF/RateHistory/norm_rate_p093", 'description':"HLT MinBias BSC Rate per lumi sec"}])
 
-tpgSummary_hlt(dqmitems, "03 - HLT MinBiasBSC Counts",
-           [{'path': "HLT/FourVector/PathsSummary/HLT LS/Paths/HLT_MinBiasPixel_SingleTrack_count_per_LS", 'description':"HLT MinBias Pixel Counts per lumi sec"}])
+tpgSummary_hlt(dqmitems, "03 - HLT ZeroBias Rate",
+           [{'path': "HLT/HLTScalers_EvF/RateHistory/norm_rate_p092", 'description':"HLT Zero Bias Rate per lumi sec"}])
+
+
+############  Muon
+
+tpgSummary_hlt(dqmitems, "04 - Muon POG HLT",
+           [{'path': "HLT/HLTMonMuon/L3Triggers/Level3/HLTMuonL3_etaphi", 'description':"Muons Passing HLT_Mu3 Occupancy phi vs eta"}])
+
+tpgSummary_hlt(dqmitems, "05 - Muon POG HLT",
+           [{'path': "HLT/HLTMonMuon/L3Triggers/Level3/HLTMuonL3_pt", 'description':"Muons Passing HLT_Mu3 Occpancy vs Pt"}])
+
+tpgSummary_hlt(dqmitems, "06 - Muon POG L1 Passthru",
+           [{'path': "HLT/HLTMonMuon/L1PassThrough/Level1/HLTMuonL1_etaphi", 'description':"Muons Passing HLT_L1Mu Occpancy phi vs eta"}])
+
+tpgSummary_hlt(dqmitems, "07 - Muon POG L1 Passthru",
+           [{'path': "HLT/HLTMonMuon/L1PassThrough/Level1/HLTMuonL1_pt", 'description':"Muons Passing HLT_L1Mu Occpancy vs Pt"}])
+
+######### Jet Met 
+
+tpgSummary_hlt(dqmitems, "08 - JET POG HLT",
+           [{'path': "HLT/JetMET/All/HLT_Jet15U/HLT_Jet15U_EtaPhi", 'description':"HLT_Jet15U occupancy eta vs phi"}])
+
+tpgSummary_hlt(dqmitems, "09 - JET POG HLT",
+           [{'path': "HLT/JetMET/All/HLT_Jet15U/HLT_Jet15U_Et", 'description':"HLT_Jet15U occupancy Et"}])
+
+tpgSummary_hlt(dqmitems, "10 - JET POG HLT L1 Passthru",
+           [{'path': "HLT/JetMET/All/HLT_L1Jet6U/HLT_L1Jet6U_EtaPhi", 'description':"HLT_L1Jet6U occupancy, eta vs phi"}])
+
+tpgSummary_hlt(dqmitems, "11 - JET POG HLT L1 Passthru",
+           [{'path': "HLT/JetMET/All/HLT_L1Jet6U/HLT_L1Jet6U_Et", 'description':"HLT_L1Jet6U occupancy, Et "}])
+
+######### EGamma
+
+tpgSummary_hlt(dqmitems, "12 - EG POG HLT ",
+           [{'path': "HLT/FourVector/paths/HLT_Ele10_LW_L1R/HLT_Ele10_LW_L1R_wrt__l1Etal1PhiL1On", 'description':"HLT_Ele10_LW_L1R occupancy, eta vs phi"}])
+
+tpgSummary_hlt(dqmitems, "13 - EG POG HLT",
+           [{'path': "HLT/FourVector/paths/HLT_Ele10_LW_L1R/HLT_Ele10_LW_L1R_wrt__l1EtL1", 'description':""}])
+
+
+######## Tau
+
+tpgSummary_hlt(dqmitems, "14 - Tau POG HLT",
+           [{'path': "HLT/FourVector/paths/HLT_SingleLooseIsoTau20/HLT_SingleLooseIsoTau20_wrt__l1EtL1", 'description':"SingleLooseIsoTau20 et"}])
+
+tpgSummary_hlt(dqmitems, "15 - Tau POG HLT",
+           [{'path': "HLT/FourVector/paths/HLT_SingleLooseIsoTau20/HLT_SingleLooseIsoTau20_wrt__l1Etal1PhiL1OnUM", 'description':"SingleLooseIsoTau20 eta vs phi"}])
+
