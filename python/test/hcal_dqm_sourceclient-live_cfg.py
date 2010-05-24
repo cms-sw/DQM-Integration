6e4af3a2fd02134989b683d017bda96db3bbe514
Jeff Temple <sha1-bc90b1124bed75af77dcd56b787d94711370513b@cern.ch>
2010-05-24 17:30:52 +0000

    removed trailing .cms from playback host names

--- python/test/hcal_dqm_sourceclient-live_cfg.py
+++ python/test/hcal_dqm_sourceclient-live_cfg.py
@@ -6,10 +6,10 @@ from DQM.HcalMonitorTasks.HcalMonitorTasks_cfi import SetTaskParams
 # Get Host information
 host = socket.gethostname().split('.')[0].lower()
 # These are playback servers, not hosts...
-#HcalPlaybackHost='dqm-c2d07-13.cms'.lower()
-#HcalCalibPlaybackHost='dqm-c2d07-16.cms'.lower()
-HcalPlaybackHost='srv-c2d04-25.cms'.lower()
-HcalCalibPlaybackHost='srv-c2d04-28.cms'.lower()
+#HcalPlaybackHost='dqm-c2d07-13'.lower()
+#HcalCalibPlaybackHost='dqm-c2d07-16'.lower()
+HcalPlaybackHost='srv-c2d04-25'.lower()
+HcalCalibPlaybackHost='srv-c2d04-28'.lower()
 
 playbackHCAL=False
 if (host==HcalPlaybackHost):
