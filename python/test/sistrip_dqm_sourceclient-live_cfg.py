87539871f6a2defc3f2bb1e5690a286d7631efd1
Suchandra Dutta <sha1-66dec20152d9caa90fb44e3138a83d284b0c6de4@cern.ch>
2010-05-27 09:10:13 +0000

    updating sistrip live configuration which is compatible with 36X

--- python/test/sistrip_dqm_sourceclient-live_cfg.py
+++ python/test/sistrip_dqm_sourceclient-live_cfg.py
@@ -89,42 +89,50 @@ process.load("Configuration.StandardSequences.Reconstruction_cff")
 ## TRACKING:
 ## Skip events with HV off
 process.newSeedFromTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
-process.newSeedFromPairs.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
+process.newSeedFromPairs.ClusterCheckPSet.MaxNumberOfCosmicClusters=20000
 process.secTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
-process.fifthSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters = 10000
-process.fourthPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
+process.fifthSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters = 20000
+process.fourthPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters= 20000
+process.thTripletsA.ClusterCheckPSet.MaxNumberOfPixelClusters = 5000
+process.thTripletsB.ClusterCheckPSet.MaxNumberOfPixelClusters = 5000
+
+## local tracker strip reconstruction
+process.OutOfTime.TOBlateBP=0.071
+process.OutOfTime.TIBlateBP=0.036
+
 ###### FIXES TRIPLETS FOR LARGE BS DISPLACEMENT ######
+
+### prevent bias in pixel vertex
+process.pixelVertices.useBeamConstraint = False
+
 ### pixelTracks
-process.pixelTracks.RegionFactoryPSet.RegionPSet.nSigmaZ = cms.double(4.06) # was originHalfLength = 15.9; translated assuming sigmaZ ~ 3.8
+#---- new parameters ----
+process.pixelTracks.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.06)
+process.pixelTracks.RegionFactoryPSet.RegionPSet.originHalfLength = cms.double(40.6)
+
 ### 0th step of iterative tracking
-#---- replaces ----
-process.newSeedFromTriplets.RegionFactoryPSet.ComponentName = 'GlobalRegionProducerFromBeamSpot' # was GlobalRegionProducer
 #---- new parameters ----
-process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ = cms.double(4.06) # was originHalfLength = 15.9; translated assuming sigmaZ ~ 3.8
-process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.beamSpot = cms.InputTag("offlineBeamSpot")
+process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ   = cms.double(4.06)
+process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.originHalfLength = 40.6
+
 ### 2nd step of iterative tracking
-#---- replaces ----
-process.secTriplets.RegionFactoryPSet.ComponentName = 'GlobalRegionProducerFromBeamSpot' # was GlobalRegionProducer
 #---- new parameters ----
-process.secTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ = cms.double(4.47) # was originHalfLength = 17.5; translated assuming sigmaZ ~ 3.8
-process.secTriplets.RegionFactoryPSet.RegionPSet.beamSpot = cms.InputTag("offlineBeamSpot")
+process.secTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ  = cms.double(4.47)
+process.secTriplets.RegionFactoryPSet.RegionPSet.originHalfLength = 44.7
 
 ## Primary Vertex
 process.offlinePrimaryVerticesWithBS.PVSelParameters.maxDistanceToBeam = 2
 process.offlinePrimaryVerticesWithBS.TkFilterParameters.maxNormalizedChi2 = 20
-process.offlinePrimaryVerticesWithBS.TkFilterParameters.minSiliconHits = 6
 process.offlinePrimaryVerticesWithBS.TkFilterParameters.maxD0Significance = 100
-process.offlinePrimaryVerticesWithBS.TkFilterParameters.minPixelHits = 1
-process.offlinePrimaryVerticesWithBS.TkClusParameters.zSeparation = 1
+process.offlinePrimaryVerticesWithBS.TkFilterParameters.minPixelLayersWithHits = 2
+process.offlinePrimaryVerticesWithBS.TkFilterParameters.minSiliconLayersWithHits = 5
+process.offlinePrimaryVerticesWithBS.TkClusParameters.TkGapClusParameters.zSeparation = 1
 process.offlinePrimaryVertices.PVSelParameters.maxDistanceToBeam = 2
 process.offlinePrimaryVertices.TkFilterParameters.maxNormalizedChi2 = 20
-process.offlinePrimaryVertices.TkFilterParameters.minSiliconHits = 6
 process.offlinePrimaryVertices.TkFilterParameters.maxD0Significance = 100
-process.offlinePrimaryVertices.TkFilterParameters.minPixelHits = 1
-process.offlinePrimaryVertices.TkClusParameters.zSeparation = 1
-
-## Pixel Vertex
-process.pixelVertices.useBeamConstraint = False
+process.offlinePrimaryVertices.TkFilterParameters.minPixelLayersWithHits = 2
+process.offlinePrimaryVertices.TkFilterParameters.minSiliconLayersWithHits = 5
+process.offlinePrimaryVertices.TkClusParameters.TkGapClusParameters.zSeparation = 1
 ##============================================================================
 ## Cosmic Track Reconstruction
 process.load("RecoTracker.Configuration.RecoTrackerP5_cff")
