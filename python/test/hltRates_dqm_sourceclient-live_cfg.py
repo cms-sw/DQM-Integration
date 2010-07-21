af357234dbbbe24f0ef9d11bd92d1ffdda8a61d0
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2010-07-21 12:46:06 +0000

    file to test TrigResRateMon

---
+++ python/test/hltRates_dqm_sourceclient-live_cfg.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("DQM")
+process.load("DQM.Integration.test.inputsource_cfi")
+process.EventStreamHttpReader.SelectHLTOutput = cms.untracked.string('hltOutputHLTDQM')
+
+process.load("DQMServices.Core.DQM_cfg")
+process.DQMStore.referenceFileName = "/dqmdata/dqm/reference/hlt_reference.root"
+
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+
+process.load("DQM.Integration.test.environment_cfi")
+
+process.load("Configuration.StandardSequences.GeometryPilot2_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.GlobalTrackingGeometryESProducer = cms.ESProducer( "GlobalTrackingGeometryESProducer" ) # for muon hlt dqm
+#SiStrip Local Reco
+process.SiStripDetInfoFileReader = cms.Service("SiStripDetInfoFileReader")
+process.TkDetMap = cms.Service("TkDetMap")
+
+#---- for P5 (online) DB access
+process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
+
+process.load("DQM.HLTEvF.TrigResRateMon_cfi")
+
+#process.p = cms.EndPath(process.hlts+process.hltsClient)
+process.p = cms.EndPath(process.hltSeedL1Logic)
+
+
+process.pp = cms.Path(process.dqmEnv+process.dqmSaver)
+process.EventStreamHttpReader.consumerName = 'HLT DQM Consumer'
+process.dqmEnv.subSystemFolder = 'HLT_Rate'
+#process.hltResults.plotAll = True
+
