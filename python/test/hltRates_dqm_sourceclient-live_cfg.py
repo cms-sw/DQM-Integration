1e804078de4f237dea97d54dc293d9650fbf5b43
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2010-07-26 09:03:13 +0000

    latest running version

--- python/test/hltRates_dqm_sourceclient-live_cfg.py
+++ python/test/hltRates_dqm_sourceclient-live_cfg.py
@@ -2,24 +2,25 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("DQM")
 process.load("DQM.Integration.test.inputsource_cfi")
-process.EventStreamHttpReader.SelectHLTOutput = cms.untracked.string('hltOutputHLTDQM')
-
+process.EventStreamHttpReader.SelectHLTOutput = cms.untracked.string('hltOutputHLTDQMResults')
+process.EventStreamHttpReader.maxEventRequestRate = cms.untracked.double(1000.0)
+#process.EventStreamHttpReader.sourceURL = cms.string('http://srv-c2c07-13.cms:11100/urn:xdaq-application:lid=50')
 process.load("DQMServices.Core.DQM_cfg")
-process.DQMStore.referenceFileName = "/dqmdata/dqm/reference/hlt_reference.root"
+#process.DQMStore.referenceFileName = "/dqmdata/dqm/reference/hlt_reference.root"
 
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 
 process.load("DQM.Integration.test.environment_cfi")
-
-process.load("Configuration.StandardSequences.GeometryPilot2_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.GlobalTrackingGeometryESProducer = cms.ESProducer( "GlobalTrackingGeometryESProducer" ) # for muon hlt dqm
+process.dqmSaver.version = 2
+#process.load("Configuration.StandardSequences.GeometryPilot2_cff")
+#process.load("Configuration.StandardSequences.MagneticField_cff")
+#process.GlobalTrackingGeometryESProducer = cms.ESProducer( "GlobalTrackingGeometryESProducer" ) # for muon hlt dqm
 #SiStrip Local Reco
-process.SiStripDetInfoFileReader = cms.Service("SiStripDetInfoFileReader")
-process.TkDetMap = cms.Service("TkDetMap")
+#process.SiStripDetInfoFileReader = cms.Service("SiStripDetInfoFileReader")
+#process.TkDetMap = cms.Service("TkDetMap")
 
 #---- for P5 (online) DB access
-process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
+#process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
 
 process.load("DQM.HLTEvF.TrigResRateMon_cfi")
 
@@ -28,7 +29,7 @@ process.p = cms.EndPath(process.trRateMon)
 
 
 process.pp = cms.Path(process.dqmEnv+process.dqmSaver)
-process.EventStreamHttpReader.consumerName = 'HLT DQM Consumer'
-process.dqmEnv.subSystemFolder = 'HLT'
+process.EventStreamHttpReader.consumerName = 'HLTTrigerResults DQM Consumer'
+process.dqmEnv.subSystemFolder = 'HLT/TrigResults'
 #process.hltResults.plotAll = True
 
