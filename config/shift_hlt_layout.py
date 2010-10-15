d358041ada48302435436d3b747566cf2ba54145
Vladimir Rekovic <sha1-e431a861e44c9b9ca054da7b12df34443cbd8eb0@cern.ch>
2010-10-15 07:13:31 +0000

    Separate Cosmics and Collisons. Cosmics folder has what we had originally

--- config/shift_hlt_layout.py
+++ config/shift_hlt_layout.py
@@ -1,4 +1,24 @@
-def hltlayout(i, p, *rows): i["00 Shift/HLT/" + p] = DQMItem(layout=rows)
+def hltlayout(i, p, *rows): i["00 Shift/HLT/Cosmics" + p] = DQMItem(layout=rows)
+  
+
+hltlayout(dqmitems,"01 HLT_Egamma_Pass_Any",
+    [{'path': "HLT/FourVector/PathsSummary/HLT_Egamma_Pass_Any", 'description': "Shows total number of HLT Egamma trigger accepts and the total number of any HLT accepts. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+
+hltlayout(dqmitems,"02 HLT_JetMet_Pass_Any",
+    [{'path': "HLT/FourVector/PathsSummary/HLT_JetMet_Pass_Any", 'description': "Shows total number of HLT JetMET trigger accepts and the total number of any HLT accepts. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+
+hltlayout(dqmitems,"03 HLT_Muon_Pass_Any",
+    [{'path': "HLT/FourVector/PathsSummary/HLT_Muon_Pass_Any", 'description': "Shows total number of HLT Muon trigger accepts and the total number of any HLT accepts. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+
+hltlayout(dqmitems,"04 HLT_Rest_Pass_Any",
+    [{'path': "HLT/FourVector/PathsSummary/HLT_Rest_Pass_Any", 'description': "Shows total number of HLT Rest trigger accepts and the total number of any HLT accepts. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+
+hltlayout(dqmitems,"05 HLT_Special_Pass_Any",
+    [{'path': "HLT/FourVector/PathsSummary/HLT_Special_Pass_Any", 'description': "Shows total number of HLT Special trigger accepts and the total number of any HLT accepts. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
+
+
+
+def hltlayout(i, p, *rows): i["00 Shift/HLT/Collisions" + p] = DQMItem(layout=rows)
   
 
 hltlayout(dqmitems,"01 HLT_BTau_Pass_Any", [{'path': "HLT/FourVector/PathsSummary/HLT_BTau_Pass_Any", 'description': "Shows total number of HLT Egamma trigger accepts and the total number of any HLT accepts in this PD. For more information please click <a href=\"https://twiki.cern.ch/twiki/bin/view/CMS/TriggerShiftHLTGuide\">here</a>."}])
