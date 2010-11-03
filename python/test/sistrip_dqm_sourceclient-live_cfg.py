94c6f2ec039c1137945bc4ad50e562be14605c3e
Laura Borrello <sha1-736be50663db2e78013a6cb75c8af9fa12816bc9@cern.ch>
2010-11-03 21:53:26 +0000

    modification for heavy ion run

--- python/test/sistrip_dqm_sourceclient-live_cfg.py
+++ python/test/sistrip_dqm_sourceclient-live_cfg.py
@@ -17,9 +17,8 @@ process.MessageLogger = cms.Service("MessageLogger",
 process.load("DQM.Integration.test.inputsource_cfi")
 process.EventStreamHttpReader.consumerName = 'SiStrip DQM Consumer'
 process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring(
-                 'HLT_MinBia*','HLT_Jet*','HLT_DiJet*','HLT_HT100U','HLT_MultiVertex*','HLT_Pixel*'))
+                 'HLT_HICentralityVeto','HLT_HIMinBiasBSC_Core'))   
 #process.EventStreamHttpReader.sourceURL = cms.string('http://dqm-c2d07-30.cms:22100/urn:xdaq-application:lid=30')
-
 
 #----------------------------
 # DQM Environment
@@ -48,14 +47,6 @@ process.dqmEnvTr = cms.EDAnalyzer("DQMEventInfo",
 #-----------------------------
 # Magnetic Field
 #-----------------------------
-# 0T field
-#process.load("Configuration.StandardSequences.MagneticField_0T_cff")
-# 3.8T field
-#process.load("Configuration.StandardSequences.MagneticField_38T_cff")
-# 4.0T field
-#process.load("Configuration.StandardSequences.MagneticField_40T_cff")
-#process.prefer("VolumeBasedMagneticFieldESProducer")
-
 process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
 
 #-------------------------------------------------
@@ -85,8 +76,8 @@ process.siStripQualityESProducer.ListOfRecordToMerge = cms.VPSet(
 #-----------------------
 ## Collision Reconstruction
 process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
-process.load("Configuration.StandardSequences.Reconstruction_cff")
-process.load("Configuration.GlobalRuns.reco_TLR_38X")
+process.load("Configuration.StandardSequences.ReconstructionHeavyIons_cff")
+process.load("Configuration.GlobalRuns.reco_TLR_39X")
 
 ## Cosmic Track Reconstruction
 process.load("RecoTracker.Configuration.RecoTrackerP5_cff")
@@ -98,7 +89,7 @@ process.load("RecoVertex.BeamSpotProducer.BeamSpot_cff")
 # Strip DQM Source and Client
 #--------------------------
 process.load("DQM.SiStripMonitorClient.SiStripSourceConfigP5_cff")
-process.TrackMon_gentk.doLumiAnalysis = False
+process.TrackMon_hi.doLumiAnalysis = False
 process.TrackMon_ckf.doLumiAnalysis = False
 
 process.load("DQM.SiStripMonitorClient.SiStripClientConfigP5_cff")
@@ -110,7 +101,7 @@ process.SiStripAnalyser.StaticUpdateFrequency = 5
 # Quality Test
 #--------------------------
 process.qTester = cms.EDAnalyzer("QualityTester",
-    qtList = cms.untracked.FileInPath('DQM/SiStripMonitorClient/data/sistrip_qualitytest_config.xml'),
+    qtList = cms.untracked.FileInPath('DQM/SiStripMonitorClient/data/sistrip_qualitytest_config_heavyion.xml'),
     prescaleFactor = cms.untracked.int32(5),                               
     getQualityTestsFromFile = cms.untracked.bool(True),
     qtestOnEndLumi = cms.untracked.bool(True),
@@ -123,6 +114,11 @@ process.qTester = cms.EDAnalyzer("QualityTester",
 process.ModuleWebRegistry = cms.Service("ModuleWebRegistry")
 
 process.AdaptorConfig = cms.Service("AdaptorConfig")
+
+# Simple filter for event
+process.eventFilter = cms.EDFilter("SimpleEventFilter",
+                   EventsToSkip = cms.untracked.int32(3)
+)
 
 #--------------------------
 # Producers
@@ -157,12 +153,12 @@ process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('NOT (36 OR 37 OR 
 # Scheduling
 #--------------------------
 process.SiStripSources_LocalReco = cms.Sequence(process.siStripFEDMonitor*process.SiStripMonitorDigi*process.SiStripMonitorClusterReal)
-process.SiStripSources_TrkReco   = cms.Sequence(process.SiStripMonitorTrack_gentk*process.MonitorTrackResiduals_gentk*process.TrackMon_gentk)
+process.SiStripSources_TrkReco   = cms.Sequence(process.SiStripMonitorTrack_hi*process.MonitorTrackResiduals_hi*process.TrackMon_hi)
 process.SiStripSources_TrkReco_cosmic = cms.Sequence(process.SiStripMonitorTrack_ckf*process.MonitorTrackResiduals_ckf*process.TrackMon_ckf)
 process.SiStripClients           = cms.Sequence(process.SiStripAnalyser)
 process.DQMCommon                = cms.Sequence(process.qTester*process.dqmEnv*process.dqmEnvTr*process.dqmSaver)
 process.RecoForDQM_LocalReco     = cms.Sequence(process.siPixelDigis*process.siStripDigis*process.gtDigis*process.trackerlocalreco)
-process.RecoForDQM_TrkReco       = cms.Sequence(process.offlineBeamSpot*process.recopixelvertexing*process.ckftracks)
+process.RecoForDQM_TrkReco       = cms.Sequence(process.offlineBeamSpot*process.heavyIonTracking)
 process.RecoForDQM_TrkReco_cosmic = cms.Sequence(process.offlineBeamSpot*process.ctftracksP5)
 
 process.p = cms.Path(process.scalersRawToDigi*
@@ -173,7 +169,7 @@ process.p = cms.Path(process.scalersRawToDigi*
                      process.DQMCommon*
                      process.SiStripClients*
                      process.SiStripSources_LocalReco*
-                     process.hltLevel1GTSeed*
+                     process.eventFilter*
                      process.RecoForDQM_TrkReco*
                      process.SiStripSources_TrkReco
 )
