4911d5c244f24960071e8aa6af895c7e2603b4e0
Evan Klose Friis <sha1-f89f3a5c387f49b31e2b955d9166ab6080f8b5ee@cern.ch>
2010-11-03 14:47:12 +0000

    Adding HI config

---
+++ python/test/beam_dqm_sourceclient-live_HI_cfg.py
@@ -0,0 +1,210 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("BeamMonitor")
+
+#----------------------------
+# Event Source
+#-----------------------------
+process.load("DQM.Integration.test.inputsource_cfi")
+process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('HLT_L1*','HLT_TrackerCosmics','HLT_Jet*'))
+
+#--------------------------
+# Filters
+#--------------------------
+# HLT Filter
+process.load("HLTrigger.special.HLTTriggerTypeFilter_cfi")
+# 0=random, 1=physics, 2=calibration, 3=technical
+process.hltTriggerTypeFilter.SelectedTriggerType = 1
+
+# L1 Trigger Bit Selection
+#process.load('L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_cff')
+#process.load('HLTrigger/HLTfilters/hltLevel1GTSeed_cfi')
+#process.hltLevel1GTSeed.L1TechTriggerSeeding = cms.bool(True)
+#process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('NOT (36 OR 37 OR 38 OR 39)')
+
+#----------------------------
+# DQM Live Environment
+#-----------------------------
+process.load("DQM.Integration.test.environment_cfi")
+process.dqmEnv.subSystemFolder = 'BeamMonitor'
+
+import DQMServices.Components.DQMEnvironment_cfi
+process.dqmEnvPixelLess = DQMServices.Components.DQMEnvironment_cfi.dqmEnv.clone()
+process.dqmEnvPixelLess.subSystemFolder = 'BeamMonitor_PixelLess'
+
+#----------------------------
+# BeamMonitor
+#-----------------------------
+process.load("DQM.BeamMonitor.BeamMonitor_cff")
+process.load("DQM.BeamMonitor.BeamMonitorBx_cff")
+process.load("DQM.BeamMonitor.BeamMonitor_PixelLess_cff")
+process.load("DQM.BeamMonitor.BeamConditionsMonitor_cff")
+process.dqmBeamMonitor.resetEveryNLumi = 5
+process.dqmBeamMonitor.resetPVEveryNLumi = 5
+process.dqmBeamMonitorBx.fitEveryNLumi = 60
+process.dqmBeamMonitorBx.resetEveryNLumi = 60
+####  SETUP TRACKING RECONSTRUCTION ####
+
+#-------------------------------------------------
+# GEOMETRY
+#-------------------------------------------------
+process.load("Configuration.StandardSequences.Geometry_cff")
+
+#-----------------------------
+# Magnetic Field
+#-----------------------------
+#process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+
+#--------------------------
+# Calibration
+#--------------------------
+process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
+
+# Using offline alignments
+import commands
+from os import environ
+environ["http_proxy"]="http://cmsproxy.cms:3128"
+process.GlobalTag.connect = "frontier://(proxyurl=http://localhost:3128)(serverurl=http://localhost:8000/FrontierOnProd)(serverurl=http://localhost:8000/FrontierOnProd)(retrieve-ziplevel=0)(failovertoserver=no)/CMS_COND_31X_GLOBALTAG"
+dasinfo = eval(commands.getoutput("wget -qO- 'http://vocms115.cern.ch:8304/tier0/express_config?run=&stream=Express'"))
+process.GlobalTag.globaltag=dasinfo[0]['global_tag']
+process.GlobalTag.pfnPrefix=cms.untracked.string('frontier://(proxyurl=http://localhost:3128)(serverurl=http://localhost:8000/FrontierOnProd)(serverurl=http://localhost:8000/FrontierOnProd)(retrieve-ziplevel=0)(failovertoserver=no)/')
+del environ["http_proxy"]
+
+#-----------------------
+#  Reconstruction Modules
+#-----------------------
+## Collision Reconstruction
+process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+
+## Heavy ION
+process.load("Configuration.StandardSequences.ReconstructionHeavyIons_cff") ## HI sequences
+# Select events based on the pixel cluster multiplicity
+import  HLTrigger.special.hltPixelActivityFilter_cfi
+process.multFilter = HLTrigger.special.hltPixelActivityFilter_cfi.hltPixelActivityFilter.clone(
+    inputTag  = cms.InputTag('siPixelClusters'),
+    minClusters = cms.uint32(150),
+    maxClusters = cms.uint32(50000)
+    )
+
+process.filter_step = cms.Sequence(
+    process.siPixelDigis*
+    process.siPixelClusters*
+    process.multFilter
+)
+
+process.HIRecoForDQM = cms.Sequence(
+    process.siPixelDigis*
+    process.siPixelClusters*
+    process.siPixelRecHits*
+    process.offlineBeamSpot*
+    process.hiPixelVertices*
+    process.hiPixel3PrimTracks
+ )
+
+# use HI pixel tracking and vertexing
+process.dqmBeamMonitor.BeamFitter.TrackCollection = cms.untracked.InputTag('hiPixel3PrimTracks')
+process.dqmBeamMonitor.primaryVertex = cms.untracked.InputTag('hiSelectedVertex')
+process.dqmBeamMonitor.PVFitter.VertexCollection = cms.untracked.InputTag('hiSelectedVertex')
+# Beamspot DQM options
+process.dqmBeamMonitor.OnlineMode = True                  ## in MC the LS are not ordered??
+process.dqmBeamMonitor.BeamFitter.MinimumTotalLayers = 3   ## using pixel triplets
+process.dqmBeamMonitor.resetEveryNLumi = 10                ## default is 20
+process.dqmBeamMonitor.resetPVEveryNLumi = 5               ## default is 5
+
+# make pixel vertexing less sensitive to incorrect beamspot
+process.hiPixel3ProtoTracks.RegionFactoryPSet.RegionPSet.originRadius = 0.2
+process.hiPixel3ProtoTracks.RegionFactoryPSet.RegionPSet.fixedError = 0.5
+process.hiSelectedProtoTracks.maxD0Significance = 100
+process.hiPixelAdaptiveVertex.TkFilterParameters.maxD0Significance = 100
+process.hiPixelAdaptiveVertex.useBeamConstraint = False
+process.hiPixelAdaptiveVertex.PVSelParameters.maxDistanceToBeam = 1.0
+
+
+## Pixelless Tracking
+process.load('RecoTracker/Configuration/RecoTrackerNotStandard_cff')
+process.MeasurementTracker.pixelClusterProducer = cms.string("")
+
+# Offline Beam Spot
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cff")
+
+## Offline PrimaryVertices
+import RecoVertex.PrimaryVertexProducer.OfflinePrimaryVertices_cfi
+process.offlinePrimaryVertices = RecoVertex.PrimaryVertexProducer.OfflinePrimaryVertices_cfi.offlinePrimaryVertices.clone()
+## Input track for PrimaryVertex reconstruction, uncomment the following line to use pixelLess tracks
+#process.offlinePrimaryVertices.TrackLabel = cms.InputTag("ctfPixelLess")
+# EK commented out for Heavy Ion
+#process.dqmBeamMonitor.BeamFitter.TrackCollection = cms.untracked.InputTag('firstStepTracksWithQuality')
+#process.dqmBeamMonitorBx.BeamFitter.TrackCollection = cms.untracked.InputTag('firstStepTracksWithQuality')
+#process.offlinePrimaryVertices.TrackLabel = cms.InputTag("firstStepTracksWithQuality")
+
+## Skip events with HV off/beam gas scraping events
+process.newSeedFromTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
+process.newSeedFromPairs.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
+process.secTriplets.ClusterCheckPSet.MaxNumberOfPixelClusters=2000
+process.fifthSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters = 10000
+process.fourthPLSeeds.ClusterCheckPSet.MaxNumberOfCosmicClusters=10000
+### 0th step of iterative tracking
+#---- replaces ----
+process.newSeedFromTriplets.RegionFactoryPSet.ComponentName = 'GlobalRegionProducerFromBeamSpot' # was GlobalRegionProducer
+#---- new parameters ----
+process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.nSigmaZ = cms.double(4.06) # was originHalfLength = 15.9; translated assuming sigmaZ ~ 3.8
+process.newSeedFromTriplets.RegionFactoryPSet.RegionPSet.beamSpot = cms.InputTag("offlineBeamSpot")
+
+#### END OF TRACKING RECONSTRUCTION ####
+
+# Change Beam Monitor variables
+if process.dqmSaver.producer.value() is "Playback":
+  process.dqmBeamMonitor.BeamFitter.WriteAscii = False
+  process.dqmBeamMonitor.BeamFitter.AsciiFileName = '/nfshome0/yumiceva/BeamMonitorDQM/BeamFitResults.txt'
+  process.dqmBeamMonitor.BeamFitter.WriteDIPAscii = True
+  process.dqmBeamMonitor.BeamFitter.DIPFileName = '/nfshome0/dqmdev/BeamMonitorDQM/BeamFitResults.txt'
+else:
+  process.dqmBeamMonitor.BeamFitter.WriteAscii = True
+  process.dqmBeamMonitor.BeamFitter.AsciiFileName = '/nfshome0/yumiceva/BeamMonitorDQM/BeamFitResults.txt'
+  process.dqmBeamMonitor.BeamFitter.WriteDIPAscii = True
+  process.dqmBeamMonitor.BeamFitter.DIPFileName = '/nfshome0/dqmpro/BeamMonitorDQM/BeamFitResults.txt'
+#process.dqmBeamMonitor.BeamFitter.SaveFitResults = False
+#process.dqmBeamMonitor.BeamFitter.OutputFileName = '/nfshome0/yumiceva/BeamMonitorDQM/BeamFitResults.root'
+  process.dqmBeamMonitorBx.BeamFitter.WriteAscii = True
+  process.dqmBeamMonitorBx.BeamFitter.AsciiFileName = '/nfshome0/yumiceva/BeamMonitorDQM/BeamFitResults_Bx.txt'
+
+#process.dqmBeamMonitor.BeamFitter.InputBeamWidth = 0.006
+process.dqmBeamMonitor.PVFitter.minNrVerticesForFit = 40
+
+## TKStatus
+process.dqmTKStatus = cms.EDAnalyzer("TKStatus",
+        BeamFitter = cms.PSet(
+        DIPFileName = process.dqmBeamMonitor.BeamFitter.DIPFileName
+        )
+)
+
+#--------------------------
+# Scheduling
+#--------------------------
+#process.phystrigger = cms.Sequence(process.hltTriggerTypeFilter*process.gtDigis*process.hltLevel1GTSeed)
+#process.dqmcommon = cms.Sequence(process.dqmEnv*process.dqmSaver)
+process.tracking = cms.Sequence(process.siPixelDigis*process.siStripDigis*process.trackerlocalreco*process.offlineBeamSpot*process.recopixelvertexing*process.ckftracks)
+process.monitor = cms.Sequence(process.dqmBeamMonitor+process.dqmBeamMonitorBx)
+#process.tracking_pixelless = cms.Sequence(process.siPixelDigis*process.siStripDigis*process.trackerlocalreco*process.offlineBeamSpot*process.ctfTracksPixelLess)
+#process.monitor_pixelless = cms.Sequence(process.dqmBeamMonitor_pixelless*process.dqmEnvPixelLess)
+process.tracking_FirstStep = cms.Sequence(process.siPixelDigis*process.siStripDigis*process.trackerlocalreco*process.offlineBeamSpot*process.recopixelvertexing*process.firstStep)
+
+#--------------------------
+# Path
+#--------------------------
+process.p = cms.Path(process.scalersRawToDigi*process.dqmTKStatus*process.hltTriggerTypeFilter*process.dqmcommon*process.tracking_FirstStep*process.offlinePrimaryVertices*process.monitor)
+process.hi = cms.Path(
+    process.scalersRawToDigi
+    *process.dqmTKStatus
+    *process.hltTriggerTypeFilter
+    *process.filter_step
+    *process.HIRecoForDQM
+    *process.dqmcommon
+    #*process.tracking_FirstStep
+    #*process.offlinePrimaryVertices
+    *process.monitor)
+
+#process.p = cms.Path(process.scalersRawToDigi*process.dqmTKStatus*process.hltTriggerTypeFilter*process.gtDigis*process.dqmcommon*process.hltLevel1GTSeed*process.tracking_FirstStep*process.offlinePrimaryVertices*process.monitor)
+
