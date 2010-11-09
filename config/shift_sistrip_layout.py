b36b6548ddabe33eba766946933547ac7945e211
Laura Borrello <sha1-736be50663db2e78013a6cb75c8af9fa12816bc9@cern.ch>
2010-11-09 09:50:05 +0000

    correcting the path for 2 histograms

--- config/shift_sistrip_layout.py
+++ config/shift_sistrip_layout.py
@@ -1,9 +1,9 @@
 def shiftsistriplayout(i, p, *rows): i["00 Shift/SiStrip/" + p] = DQMItem(layout=rows)
 
 shiftsistriplayout(dqmitems, "00 - SiStrip ReportSummary",
- [{ 'path': "SiStrip/EventInfo/detFractionReportMap",
+ [{ 'path': "SiStrip/MechanicalView/detFractionReportMap",
     'description': "Fraction of Good Detector Modules plotted in different parts of the Tracker - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiStrip>DQMShiftOnlineSiStrip</a> ", 'draw': { 'withref': "no" }},
-  { 'path': "SiStrip/EventInfo/sToNReportMap",
+  { 'path': "SiStrip/MechanicalView/sToNReportMap",
     'description': "Accepted S/N Ratios in different parts of the Tracker. The values are 1 if the ratio is within the accepted range otherwise it is 0  - <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiStrip>DQMShiftOnlineSiStrip</a> ", 'draw': { 'withref': "yes" }}],
  [{ 'path': "SiStrip/EventInfo/reportSummaryMap",
     'description': "Overall Report Summary where detector fraction and S/N flags are combined together -  <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftSiStrip>DQMShiftOnlineSiStrip</a> ", 'draw': { 'withref': "no" }}])
