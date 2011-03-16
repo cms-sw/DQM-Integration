4250b31d49b3310c1446a33b7deab79af9e35383
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2011-03-16 16:11:47 +0000

    included sistriplas

--- config/server-conf-online-playback.py
+++ config/server-conf-online-playback.py
@@ -3,7 +3,7 @@ CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
 
 # Modifiable parameters.
 LAYOUTS = ["%s/%s-layouts.py" % (CONFIGDIR, x) for x in
-	   ("castor","csc", "dt", "eb", "ee", "es","hcal", "hcalcalib", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip")]
+	   ("castor","csc", "dt", "eb", "ee", "es","hcal", "hcalcalib", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip", "sistriplas")]
 LAYOUTS += ["%s/%s_overview_layouts.py" % (CONFIGDIR, x) for x in
             ("sistrip","ecal","hcal","beammonitor","l1t","hlt")]
 LAYOUTS += ["%s/shift_%s_layout.py" % (CONFIGDIR, x) for x in
