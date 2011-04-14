a07a4bd03d4cf458a24102a0ab793d557f8e7a3c
Laura Borrello <sha1-736be50663db2e78013a6cb75c8af9fa12816bc9@cern.ch>
2011-04-14 14:12:44 +0000

    adding trigger for 2T run

--- python/test/sistrip_dqm_sourceclient-live_cfg.py
+++ python/test/sistrip_dqm_sourceclient-live_cfg.py
@@ -16,7 +16,7 @@ process.MessageLogger = cms.Service("MessageLogger",
 #-----------------------------
 process.load("DQM.Integration.test.inputsource_cfi")
 process.EventStreamHttpReader.consumerName = 'SiStrip DQM Consumer'
-process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('HLT_Jet*','HLT_*Cosmic*','HLT_L1*','HLT_HT*'))  
+process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('HLT_Jet*','HLT_*Cosmic*','HLT_L1*','HLT_HT*','HLT_MinBias_Part*','HLT_Physics_v1')) 
 #process.EventStreamHttpReader.sourceURL = cms.string('http://dqm-c2d07-30.cms:22100/urn:xdaq-application:lid=30')
 
 #----------------------------
