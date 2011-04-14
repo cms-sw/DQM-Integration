f8960bf5b8018cd17a002faed99047ede3a5d485
Sushil Chauhan <sha1-dc853c26adfd296df98de885cec220b751df39de@cern.ch>
2011-04-14 15:55:23 +0000

    removed Random Calibr HLT path for 2T Run

--- python/test/beam_dqm_sourceclient-live_cfg.py
+++ python/test/beam_dqm_sourceclient-live_cfg.py
@@ -7,7 +7,7 @@ process = cms.Process("BeamMonitor")
 #-----------------------------
 process.load("DQM.Integration.test.inputsource_cfi")
 process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(
-    SelectEvents = cms.vstring('HLT_L1*','HLT_L1TrackerCosmics_v2','HLT_Jet*','HLT_MinBias_*','HLT_Physics_v1','HLT_Random_v1','HLT_Calibration_v1'))
+    SelectEvents = cms.vstring('HLT_L1*','HLT_L1TrackerCosmics_v2','HLT_Jet*','HLT_MinBias_*','HLT_Physics_v1'))
 
 #--------------------------
 # Filters
--- python/test/beampixel_dqm_sourceclient-live_cfg.py
+++ python/test/beampixel_dqm_sourceclient-live_cfg.py
@@ -9,7 +9,7 @@ process = cms.Process("BeamPixel")
 ### @@@@@@ Comment when running locally @@@@@@ ###
 process.load("DQM.Integration.test.inputsource_cfi")
 process.EventStreamHttpReader.consumerName = "Beam Pixel DQM Consumer"
-process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('HLT_L1*','HLT_L1TrackerCosmics_v2','HLT_Jet*','HLT_MinBias*','HLT_Physics_v1','HLT_Random_v1','HLT_Calibration_v1'))
+process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('HLT_L1*','HLT_L1TrackerCosmics_v2','HLT_Jet*','HLT_MinBias*','HLT_Physics_v1'))
 ### @@@@@@ Comment when running locally @@@@@@ ###
 
 
