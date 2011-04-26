ad9bd30c8919815e8d9bc86e578eb9ae9172035d
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2011-04-26 13:56:01 +0000

    Updated for new menu

--- config/hltmuon_T0_layouts.py
+++ config/hltmuon_T0_layouts.py
@@ -10,37 +10,37 @@ def trigHltMuonOfflineSummary(i, p, *rows):
 
 ######################################################
 
-trigHltMuonOfflineSummary(dqmitems,"01 - HLT_Mu3",
-   [{'path': "HLT/Muon/Distributions/HLT_Mu3/allMuons/recEffPt_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}],
-   [{'path': "HLT/Muon/Distributions/HLT_Mu3/allMuons/recEffPhiVsEta_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}])
+trigHltMuonOfflineSummary(dqmitems,"01 - HLT_Mu3_v4",
+   [{'path': "HLT/Muon/Distributions/HLT_Mu3_v4/allMuons/recEffPt_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}],
+   [{'path': "HLT/Muon/Distributions/HLT_Mu3_v4/allMuons/recEffPhiVsEta_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}])
 
 ######################################################
 
-trigHltMuonOfflineSummary(dqmitems,"02 - HLT_Mu5",
-   [{'path': "HLT/Muon/Distributions/HLT_Mu5/allMuons/recEffPt_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}],
-   [{'path': "HLT/Muon/Distributions/HLT_Mu5/allMuons/recEffPhiVsEta_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}])
+trigHltMuonOfflineSummary(dqmitems,"02 - HLT_Mu5_v4",
+   [{'path': "HLT/Muon/Distributions/HLT_Mu5_v4/allMuons/recEffPt_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}],
+   [{'path': "HLT/Muon/Distributions/HLT_Mu5_v4/allMuons/recEffPhiVsEta_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}])
 
 ######################################################
 
-trigHltMuonOfflineSummary(dqmitems,"03 - HLT_Mu9",
-   [{'path': "HLT/Muon/Distributions/HLT_Mu9/allMuons/recEffPt_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}],
-   [{'path': "HLT/Muon/Distributions/HLT_Mu9/allMuons/recEffPhiVsEta_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}])
+trigHltMuonOfflineSummary(dqmitems,"03 - HLT_Mu12_v2",
+   [{'path': "HLT/Muon/Distributions/HLT_Mu12_v2/allMuons/recEffPt_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}],
+   [{'path': "HLT/Muon/Distributions/HLT_Mu12_v2/allMuons/recEffPhiVsEta_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}])
 
 ######################################################
 
-trigHltMuonOfflineSummary(dqmitems,"04 - HLT_Mu11",
-   [{'path': "HLT/Muon/Distributions/HLT_Mu11/allMuons/recEffPt_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}],
-   [{'path': "HLT/Muon/Distributions/HLT_Mu11/allMuons/recEffPhiVsEta_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}])
+trigHltMuonOfflineSummary(dqmitems,"04 - HLT_Mu30_v2",
+   [{'path': "HLT/Muon/Distributions/HLT_Mu30_v2/allMuons/recEffPt_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}],
+   [{'path': "HLT/Muon/Distributions/HLT_Mu30_v2/allMuons/recEffPhiVsEta_L3Filtered", 'description':"Efficiency for RECO muons to match HLT"}])
 
 ######################################################
 
-trigHltMuonOfflineSummary(dqmitems,"05 - HLT_L2Mu9",
-   [{'path': "HLT/Muon/Distributions/HLT_L2Mu9/allMuons/recEffPt_L2Filtered", 'description':"Efficiency for RECO muons to match HLT"}],
-   [{'path': "HLT/Muon/Distributions/HLT_L2Mu9/allMuons/recEffPhiVsEta_L2Filtered", 'description':"Efficiency for RECO muons to match HLT"}])
+trigHltMuonOfflineSummary(dqmitems,"05 - HLT_L2Mu10_v2",
+   [{'path': "HLT/Muon/Distributions/HLT_L2Mu10_v2/allMuons/recEffPt_L2Filtered", 'description':"Efficiency for RECO muons to match HLT"}],
+   [{'path': "HLT/Muon/Distributions/HLT_L2Mu10_v2/allMuons/recEffPhiVsEta_L2Filtered", 'description':"Efficiency for RECO muons to match HLT"}])
 
 ######################################################
 
-trigHltMuonOfflineSummary(dqmitems,"06 - HLT_L2Mu11",
-   [{'path': "HLT/Muon/Distributions/HLT_L2Mu11/allMuons/recEffPt_L2Filtered", 'description':"Efficiency for RECO muons to match HLT"}],
-   [{'path': "HLT/Muon/Distributions/HLT_L2Mu11/allMuons/recEffPhiVsEta_L2Filtered", 'description':"Efficiency for RECO muons to match HLT"}])
+trigHltMuonOfflineSummary(dqmitems,"06 - HLT_L2Mu20_v2",
+   [{'path': "HLT/Muon/Distributions/HLT_L2Mu20_v2/allMuons/recEffPt_L2Filtered", 'description':"Efficiency for RECO muons to match HLT"}],
+   [{'path': "HLT/Muon/Distributions/HLT_L2Mu20_v2/allMuons/recEffPhiVsEta_L2Filtered", 'description':"Efficiency for RECO muons to match HLT"}])
 
