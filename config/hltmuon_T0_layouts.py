c0411ae5398524fc3c219f6f8b37a272f9ea82b3
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2011-04-26 19:41:14 +0000

    Added HLT/Layouts/Muon (SingleMu) in preparation for 4.2.X

--- config/hltmuon_T0_layouts.py
+++ config/hltmuon_T0_layouts.py
@@ -4,6 +4,28 @@
 ######################################################
 
 ###---- GENERIC - FourVector Muon
+def trigHltMuonOfflineSummaryNew(i, p, *rows):
+    i["HLT/Layouts/" + p] = DQMItem(layout=rows)
+
+## ## Layout for MuMonitor sample
+## paths = ["HLT_L1Mu7_v1", "HLT_L2Mu7_v1", "HLT_Mu3_v2",
+##          "HLT_L1DoubleMuOpen", "HLT_L2DoubleMu0", "HLT_DoubleMu0"]
+## for i, path in enumerate(paths):
+##     trigHltMuonOfflineSummaryNew(dqmitems, "Muon (MuMonitor)/%02d - %s" % (i, path),
+##         [{'path': "HLT/Muon/DistributionsVbtf/%s/efficiencyTurnOn" % path, 'description':"Efficiency for VBTF muons to match HLT"}],
+##         [{'path': "HLT/Muon/DistributionsVbtf/%s/efficiencyPhiVsEta" % path, 'description':"Efficiency for VBTF muons to match HLT"}])
+
+## Layout for SingleMu sample
+trigHltMuonOfflineSummaryNew(dqmitems, "Muon (SingleMu)/Tag and Probe",
+    [{'path': "HLT/Muon/DistributionsVbtf/HLT_Mu15_v1/massVsEta_efficiency", 'description':"Tag and probe"}])
+paths = ["HLT_Mu24_v2", "HLT_Mu30_v2", "HLT_IsoMu17_v6"]
+for i, path in enumerate(paths):
+    trigHltMuonOfflineSummaryNew(dqmitems, "Muon (Mu)/%02d - %s" % (i, path),
+        [{'path': "HLT/Muon/DistributionsVbtf/%s/efficiencyTurnOn" % path, 'description':"Efficiency for VBTF muons to match HLT"}],
+        [{'path': "HLT/Muon/DistributionsVbtf/%s/efficiencyPhiVsEta" % path, 'description':"Efficiency for VBTF muons to match HLT"}])
+                  
+
+###---- GENERIC - FourVector Muon
 def trigHltMuonOfflineSummary(i, p, *rows): 
    i["HLT/Muon/MuonHLTSummary/" + p] = DQMItem(layout=rows)
 
