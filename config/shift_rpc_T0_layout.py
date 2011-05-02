08ce65827298adee5aa01b8e6fc0611c4f85055c
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2011-05-02 13:09:35 +0000

    improved rpc shift layouts

--- config/shift_rpc_T0_layout.py
+++ config/shift_rpc_T0_layout.py
@@ -1,28 +1,37 @@
 def shiftrpclayout(i, p, *rows): i["00 Shift/RPC/" + p] = DQMItem(layout=rows)
-eff = "RPC Efficiency, Roll vs Sector ";
+
+
+fed = "FED Fatal Errors. Entries MUST be ZERO at all times. If not, report the problem and ask to stop the run."
+rpcevents = "Number of processed events."
+eff = "RPC Efficiency distribution. Make sure average values is greater than 80.";
 rpclink = "   >>> <a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>";
+quality = "Overview of system quality. Expressed in percentage of chambers."
+occupancy = "Occupancy per sector"
 
-shiftrpclayout(dqmitems, "00-BarrelOccupancy",
-  [{ 'path': "RPC/RecHits/SummaryHistograms/Occupancy_for_Barrel", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }],
-  [{ 'path': "RPC/RecHits/SummaryHistograms/Occupancy_for_EndcapNegative", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" },
- { 'path': "RPC/RecHits/SummaryHistograms/Occupancy_for_EndcapPositive", 'description': "<a href=https://twiki.cern.ch/twiki/bin/view/CMS/DQMShiftRPC>Description</a>" }])
-
-shiftrpclayout(dqmitems, "01-BarrelEfficiency",
-          [{ 'path': "RPC/RPCEfficiency/Efficiency_Roll_vs_Sector_Wheel_-2", 'description': eff + rpclink },
-           { 'path': "RPC/RPCEfficiency/Efficiency_Roll_vs_Sector_Wheel_-1", 'description': eff + rpclink  }],
+shiftrpclayout(dqmitems, "00-FED_Fatal_Errors",
+               [{ 'path': "RPC/FEDIntegrity/FEDFatal", 'description': fed + rpclink }])
           
-          [{ 'path': "RPC/RPCEfficiency/Efficiency_Roll_vs_Sector_Wheel_0", 'description': eff + rpclink  },
-           { 'path': "RPC/RPCEfficiency/Efficiency_Roll_vs_Sector_Wheel_+1", 'description': eff + rpclink  },
-           { 'path': "RPC/RPCEfficiency/Efficiency_Roll_vs_Sector_Wheel_+2", 'description': eff + rpclink  }]
-          )     
-shiftrpclayout(dqmitems, "01-EndCapEfficiency",
-          [{ 'path': "RPC/RPCEfficiency/Efficiency_Roll_vs_Segment_Disk_-3", 'description': eff + rpclink },
-           { 'path': "RPC/RPCEfficiency/Efficiency_Roll_vs_Segment_Disk_-2", 'description': eff + rpclink },
-           { 'path': "RPC/RPCEfficiency/Efficiency_Roll_vs_Segment_Disk_-1", 'description': eff + rpclink }],
 
-          [{ 'path': "RPC/RPCEfficiency/Efficiency_Roll_vs_Segment_Disk_1", 'description': eff + rpclink },
-           { 'path': "RPC/RPCEfficiency/Efficiency_Roll_vs_Segment_Disk_2", 'description': eff + rpclink },
-           { 'path': "RPC/RPCEfficiency/Efficiency_Roll_vs_Segment_Disk_3", 'description': eff + rpclink }],
-          
-         
-          )     
+shiftrpclayout(dqmitems, "01-RPC_Events",
+               [{ 'path': "RPC/RecHits/SummaryHistograms/RPCEvents", 'description': rpcevents + rpclink }])
+
+
+shiftrpclayout(dqmitems, "02-Quality_State_Overview",
+               [{ 'path': "RPC/RecHits/SummaryHistograms/RPC_System_Quality_Overview", 'description': quality + rpclink }])
+
+
+shiftrpclayout(dqmitems, "03-RPC_Occupancy",
+               [{ 'path': "RPC/RecHits/SummaryHistograms/Occupancy_for_Barrel", 'description': occupancy + rpclink  }],
+               [{ 'path': "RPC/RecHits/SummaryHistograms/Occupancy_for_EndcapNegative", 'description': occupancy + rpclink  },
+                { 'path': "RPC/RecHits/SummaryHistograms/Occupancy_for_EndcapPositive", 'description':  occupancy + rpclink }])
+
+
+shiftrpclayout(dqmitems, "04-Statistics",
+               [{ 'path': "RPC/RPCEfficiency/EffBarrelRoll", 'description': eff + rpclink }])
+
+
+shiftrpclayout(dqmitems, "05-Efficiency_Distribution",
+               [{ 'path': "RPC/RPCEfficiency/EffBarrelRoll", 'description': eff + rpclink  }],
+               [{ 'path': "RPC/RPCEfficiency/EffEndcapPlusRoll", 'description': eff + rpclink  },
+                { 'path': "RPC/RPCEfficiency/EffEndcapMinusRoll", 'description':  eff + rpclink }])
+
