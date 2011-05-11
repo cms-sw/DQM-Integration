2310c04e8ac85670c3246a1d4c84d09a7ac27a5a
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2011-05-11 09:52:20 +0000

    Chnaged global tag to GR_H_V19::All and Enable save at runEnd

--- python/test/environment_cfi.py
+++ python/test/environment_cfi.py
@@ -14,6 +14,6 @@ dqmSaver.producer = 'DQM'
 dqmSaver.saveByTime = 1
 dqmSaver.saveByLumiSection = -1
 dqmSaver.saveByMinute = 8
-dqmSaver.saveByRun = -1
+dqmSaver.saveByRun = 1
 dqmSaver.saveAtJobEnd = False
 
