7e113ec3c295e73d491f662a6cee11d162e4538c
Anna Cimmino <sha1-afe53aef2ff6db42fff967e60ac6346900548455@cern.ch>
2011-05-12 11:54:52 +0000

    fixes for 42x

--- python/test/rpc_dqm_sourceclient-live_cfg.py
+++ python/test/rpc_dqm_sourceclient-live_cfg.py
@@ -1,108 +1,88 @@
 
 import FWCore.ParameterSet.Config as cms
 
+## Use RECO Muons flag
+useMuons = False
+isOfflineDQM = False
+
 process = cms.Process("RPCDQM")
 
-
-############## Event Source ####################
+############## Event Source #####################
 process.load("DQM.Integration.test.inputsource_cfi")
 process.EventStreamHttpReader.consumerName = 'RPC DQM Consumer'
-#process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('HLT_MinBia*','HLT_CSCBeamHaloOverlapRing2','HLT_CSCBeamHaloRing2or3', 'HLT_L1Mu*','HLT_L1Mu','HLT_TrackerCosmics'))
+ #process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('HLT_MinBia*','HLT_CSCBeamHaloOverlapRing2','HLT_CSCBeamHaloRing2or3', 'HLT_L1Mu*','HLT_L1Mu','HLT_TrackerCosmics'))
 process.EventStreamHttpReader.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('*'))
 
-################ HLT Filter######################
+############### HLT Filter#######################
 process.load("HLTrigger.special.HLTTriggerTypeFilter_cfi")
 # 0=random, 1=physics, 2=calibration, 3=technical
 process.hltTriggerTypeFilter.SelectedTriggerType = 1
 
-
-############### Scaler Producer #################
-process.load("EventFilter.ScalersRawToDigi.ScalersRawToDigi_cfi")
-
-
-################# Geometry  ######################
+################# Geometry  #####################
 process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
 process.load("Geometry.RPCGeometry.rpcGeometry_cfi")
 process.load("Geometry.MuonNumbering.muonNumberingInitialization_cfi")
 
+################ Condition ######################
 process.load("CondCore.DBCommon.CondDBSetup_cfi")
 
-################# RPC Unpacker  ######################
-process.rpcunpacker = cms.EDProducer("RPCUnpackingModule",
-    InputLabel = cms.InputTag("source"),
-    doSynchro = cms.bool(False)
-)
-
-
+############# DQM Cetral Modules ################
+process.load("DQMServices.Core.DQM_cfg")
 process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
 process.GlobalTag.RefreshEachRun = cms.untracked.bool(True)
 
-## process.RPCCabling = cms.ESSource("PoolDBESSource",
-##     DBParameters = cms.PSet(
-##         messageLevel = cms.untracked.int32(0),
-##         authenticationPath = cms.untracked.string('/nfshome0/hltpro/cmssw/cfg/')
-##     ),
-##     timetype = cms.string('runnumber'),
-##     toGet = cms.VPSet(cms.PSet(
-##         record = cms.string('RPCEMapRcd'),
-##         tag = cms.string('RPCEMap_v2')
-##     )),
-##     connect = cms.string('frontier://(proxyurl=http://localhost:3128)(serverurl=http://frontier1.cms:8000/FrontierOnProd)(serverurl=http://frontier2.cms:8000/FrontierOnProd)(retrieve-ziplevel=0)/CMS_COND_31X_RPC'),
-##     siteLocalConfig = cms.untracked.bool(False)
-## )
-
-################# RPC Rec Hits  ######################
-process.load("RecoLocalMuon.RPCRecHit.rpcRecHits_cfi")
-process.rpcRecHits.rpcDigiLabel = 'rpcunpacker'
-
-################# DQM Cetral Modules ######################
-process.load("DQMServices.Core.DQM_cfg")
-
-
-######## DQM Enviroment ###################
+############## DQM Enviroment ###################
 process.load("DQM.Integration.test.environment_cfi")
 process.dqmEnv.subSystemFolder = 'RPC'
 process.load("DQMServices.Components.DQMEnvironment_cfi")
 
 process.DQMStore.referenceFileName = '/dqmdata/dqm/reference/rpc_reference.root'
 
-################# DQM Digi Module ######################
-process.load("DQM.RPCMonitorDigi.RPCDigiMonitoring_cfi")
-process.rpcdigidqm.DigiEventsInterval = 100
-process.rpcdigidqm.dqmshifter = True
-process.rpcdigidqm.dqmexpert = True
-process.rpcdigidqm.dqmsuperexpert = False
-process.rpcdigidqm.DigiDQMSaveRootFile = False
-process.rpcdigidqm.DigiLabel = cms.InputTag("rpcunpacker")
+############### Scaler Producer #################
+process.load("EventFilter.ScalersRawToDigi.ScalersRawToDigi_cfi")
 
+############## RPC Unpacker  ####################
+process.rpcunpacker = cms.EDProducer("RPCUnpackingModule",
+    InputLabel = cms.InputTag("source"),
+    doSynchro = cms.bool(False)
+)
+
+################# RPC Rec Hits  #################
+process.load("RecoLocalMuon.RPCRecHit.rpcRecHits_cfi")
+process.rpcRecHits.rpcDigiLabel = 'rpcunpacker'
+
+
+################ DQM Digi Module ################
+process.load("DQM.RPCMonitorDigi.RPCDigiMonitoring_cfi")
+process.rpcdigidqm.UseMuon =  cms.untracked.bool(useMuons)
+process.rpcdigidqm.NoiseFolder  = cms.untracked.string("AllHits")
 
 ################# DCS Info ######################
 process.load("DQM.RPCMonitorDigi.RPCDcsInfo_cfi")
 
-
-################# DQM Client Modules ####################
+################# DQM Client Modules ############
 process.load("DQM.RPCMonitorClient.RPCDqmClient_cfi")
-process.rpcdqmclient.RPCDqmClientList = cms.untracked.vstring("RPCNoisyStripTest","RPCOccupancyTest","RPCClusterSizeTest","RPCDeadChannelTest","RPCMultiplicityTest")
-process.rpcdqmclient.DiagnosticPrescale = cms.untracked.int32(5)
-process.rpcdqmclient.MinimumRPCEvents = cms.untracked.int32(10)
-process.rpcdqmclient.OfflineDQM = cms.untracked.bool(False)
-
-################# Other Clients ############################
+process.rpcdqmclient.RPCDqmClientList = cms.untracked.vstring("RPCMultiplicityTest", "RPCDeadChannelTest", "RPCClusterSizeTest", "RPCOccupancyTest","RPCNoisyStripTest")
+process.rpcdqmclient.DiagnosticPrescale = cms.untracked.int32(1)
+process.rpcdqmclient.MinimumRPCEvents  = cms.untracked.int32(100)
+process.rpcdqmclient.OfflineDQM = cms.untracked.bool(isOfflineDQM)
+process.rpcdqmclient.RecHitTypeFolder = cms.untracked.string("AllHits")
+    
+################# Other Clients #################
 #process.load("DQM.RPCMonitorClient.RPCMon_SS_Dbx_Global_cfi")
 
-
-################### FED ##################################
+################### FED #########################
 process.load("DQM.RPCMonitorClient.RPCMonitorRaw_cfi")
 process.load("DQM.RPCMonitorClient.RPCFEDIntegrity_cfi")
 process.load("DQM.RPCMonitorClient.RPCMonitorLinkSynchro_cfi")
 
-################# RPC Event Summary Module ####################
+########### RPC Event Summary Module ############
 process.load("DQM.RPCMonitorClient.RPCEventSummary_cfi")
-process.rpcEventSummary.EventInfoPath = 'RPC/EventInfo'
-process.rpcEventSummary.PrescaleFactor = 5
-process.rpcEventSummary.OfflineDQM = cms.untracked.bool(False)
+process.rpcEventSummary.OfflineDQM = cms.untracked.bool(isOfflineDQM )
+process.rpcEventSummary.MinimumRPCEvents  = cms.untracked.int32(10000)
+process.rpcEventSummary.RecHitTypeFolder = cms.untracked.string("AllHits")
 
-################# Quality Tests #########################
+################# Quality Tests #################
 process.qTesterRPC = cms.EDAnalyzer("QualityTester",
     qtList = cms.untracked.FileInPath('DQM/RPCMonitorClient/test/RPCQualityTests.xml'),
     prescaleFactor = cms.untracked.int32(5),
@@ -110,17 +90,14 @@ process.qTesterRPC = cms.EDAnalyzer("QualityTester",
     qtestOnEndRun = cms.untracked.bool(True)
 )
 
-################ Chamber Quality ##################
+############## Chamber Quality ##################
 process.load("DQM.RPCMonitorClient.RPCChamberQuality_cfi")
-process.rpcChamberQuality.MinimumRPCEvents = cms.untracked.int32(10000)
-process.rpcChamberQuality.PrescaleFactor = cms.untracked.int32(5) 
-process.rpcChamberQuality.OfflineDQM = cms.untracked.bool(False)
+process.rpcChamberQuality.OfflineDQM = cms.untracked.bool(isOfflineDQM )
+process.rpcChamberQuality.RecHitTypeFolder = cms.untracked.string("AllHits")
+process.rpcChamberQuality.MinimumRPCEvents  = cms.untracked.int32(10000)
                              
-
-################  Sequences ############################
-process.rpcDigi = cms.Sequence(process.rpcunpacker*process.rpcRecHits*process.scalersRawToDigi*process.rpcdigidqm*process.rpcMonitorRaw*process.rpcDcsInfo)
-process.rpcClient = cms.Sequence(process.qTesterRPC*process.rpcdqmclient*process.rpcChamberQuality*process.rpcEventSummary*process.dqmEnv*process.dqmSaver)
+###############  Sequences ######################
+process.rpcSource = cms.Sequence(process.rpcunpacker*process.rpcRecHits*process.scalersRawToDigi*process.rpcdigidqm*process.rpcMonitorRaw*process.rpcDcsInfo*process.qTesterRPC)
+process.rpcClient = cms.Sequence(process.rpcdqmclient*process.rpcChamberQuality*process.rpcEventSummary*process.dqmEnv*process.dqmSaver)
+process.p = cms.Path(process.rpcSource*process.rpcClient)
 process.p = cms.Path(process.hltTriggerTypeFilter*process.rpcDigi*process.rpcClient)
-
-
-
