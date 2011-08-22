e337ffecca1668a44c8bfe1384b0c7e7e7edc79e
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2011-08-22 13:14:36 +0000

    Enabled file Upload and registration on the GUI

--- config/server-conf-online-playback.py
+++ config/server-conf-online-playback.py
@@ -3,7 +3,7 @@ CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
 
 # Modifiable parameters.
 LAYOUTS = ["%s/%s-layouts.py" % (CONFIGDIR, x) for x in
-	   ("castor","csc", "dt", "eb", "ee", "es","hcal", "hcalcalib", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip", "sistriplas")]
+	   ("castor","csc", "dt", "eb", "ee", "ecalcalib", "es","hcal", "hcalcalib", "hlt", "hlx", "l1t", "l1temulator", "rpc", "pixel", "sistrip", "sistriplas")]
 LAYOUTS += ["%s/%s_overview_layouts.py" % (CONFIGDIR, x) for x in
             ("sistrip","ecal","hcal","beammonitor","l1t","hlt")]
 LAYOUTS += ["%s/shift_%s_layout.py" % (CONFIGDIR, x) for x in
@@ -45,7 +45,7 @@ server.plugin('render', BASEDIR + "/style/*.cc")
 # Extensions.
 server.extend('DQMRenderLink', server.pathOfPlugin('render'))
 server.extend('DQMToJSON')
-server.extend('DQMFileAccess', None, None,
+server.extend('DQMFileAccess', None, "/dqmdata/dqmdev/uploads",
               { "Original": "/dqmdata/dqm/repository/original" })
 
 # Sources.
