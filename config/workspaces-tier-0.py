8aca0ad212408721f3fca811cd227c3ac277b1bd
Tomas Hreus <sha1-3b7f95655a4528fda85af562e681c07c81f0445e@cern.ch>
2011-11-17 15:54:14 +0000

    include pp for rereco

--- config/workspaces-tier-0.py
+++ config/workspaces-tier-0.py
@@ -90,8 +90,9 @@ server.workspace('DQMContent', 44, 'POG', 'Btag', '^Btag/',
 
 server.workspace('DQMContent', 45, 'POG', 'Tracking', '^(Tracking|AlcaBeamMonitor|OfflinePV)/',
                                   'Tracking/Layouts/01 - Tracking ReportSummary',
-                                  'Tracking/Layouts/02 - Tracks (HI collisions)',
-                                  'Tracking/Layouts/03 - Tracks (Cosmic Tracking)'
+                                  'Tracking/Layouts/02 - Tracks (pp collisions)',
+                                  'Tracking/Layouts/03 - Tracks (HI collisions)',
+                                  'Tracking/Layouts/04 - Tracks (Cosmic Tracking)'
                                   )
 
 server.workspace('DQMContent', 51,'FeedBack for Collisions', 'Tracking FeedBack', '^(Collisions|SiStrip|Tracking|Pixel|AlcaBeamMonitor|OfflinePV)/',
