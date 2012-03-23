e8b79c13172521a4c45c63875e049fa4355ef642
lilopera <sha1-30ba17dc973fc9681a5c97b15977c1775ca9b4fb@cern.ch>
2012-03-23 18:01:24 +0000

    relaxed the check for report summary map, to allow subsytem splits

--- scripts/filecollector/filechk.C
+++ scripts/filecollector/filechk.C
@@ -9,6 +9,33 @@ int filechk(string fname)
   }
   else
   {
+    if ( fname.find("_SiStrip_") != string::npos ){
+      TString rnStr = fname.substr(fname.find("_R") + 2, 9);
+      TString runDirStr("Run ");
+      runDirStr += rnStr.Atoi();
+      TDirectoryFile* runDir = (TDirectoryFile*)f.FindObjectAny(runDirStr);
+      if ( runDir == 0 )
+        return 0; 
+      
+      TDirectoryFile* tracking = (TDirectoryFile*)runDir->FindObjectAny("Tracking");
+      if ( tracking == 0 )
+        return 0;
+      
+      TDirectoryFile* sistrip = (TDirectoryFile*)runDir->FindObjectAny("SiStrip");
+      if ( sistrip == 0 )
+        return 0;  
+       
+      TDirectoryFile* hist = (TDirectoryFile*)tracking->FindObjectAny("reportSummaryMap");
+      if ( hist == 0 )
+        return 0;
+       
+      hist = (TDirectoryFile*)sistrip->FindObjectAny("reportSummaryMap");
+      if ( hist == 0 )
+        return 0; 
+      
+      return 1;
+    }
+    
     //TH2F* hist;
     //hist = (TH2F*)f.FindObjectAny("reportSummaryMap");
     TDirectoryFile* hist;	
@@ -16,6 +43,11 @@ int filechk(string fname)
     hist = (TDirectoryFile*)f.FindObjectAny("reportSummaryMap");
     if (0 == hist) 
     {
+      TDirectoryFile* hist2;
+      hist2 = (TDirectoryFile*)f.FindObjectAny("EventInfo");
+      if (0 != hist2)
+        return 1;
+
       //cout << "File is incomplete" << endl;
       return 0;
     }
