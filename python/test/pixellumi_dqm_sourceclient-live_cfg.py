7f5221d11c5eec6d3a3415b6e11608ede9617654
Amita Raval <sha1-0dabfdc0c489a7ec18ae2fd9a092d20a84886309@cern.ch>
2012-07-25 08:46:07 +0000

    modified lumi db connection string according to prescriptions by DQM resps

--- python/test/pixellumi_dqm_sourceclient-live_cfg.py
+++ python/test/pixellumi_dqm_sourceclient-live_cfg.py
@@ -50,15 +50,6 @@ process.load("DQMServices.Components.DQMEnvironment_cfi")
 #-----------------------------------------------------------------------------
 process.load("DQM.Integration.test.environment_cfi")
 process.dqmEnv.subSystemFolder    = "PixelLumi"
-process.DQM.collectorHost='dqm-c2d07-19'
-process.DQM.collectorPort=9190
-process.dqmSaver.dirName = './'
-process.dqmSaver.convention = 'Online'
-process.dqmSaver.producer = 'DQM'
-process.dqmEnv.subSystemFolder = 'PixelLumi'
-process.dqmSaver.saveByLumiSection = 1
-process.dqmSaver.saveByRun = 1
-process.dqmSaver.saveAtJobEnd = True
 
 #------------------------
 #  Reconstruction Modules
@@ -110,11 +101,11 @@ process.PixelLumiDqmHFGetter = cms.EDAnalyzer("HFLumiGetter")
 # Lumi from "DIP"
 #----------------
 process.DBService=cms.Service('DBService',
-                              authPath= cms.untracked.string('./')       
+                              authPath= cms.untracked.string('/nfshome0/popcondev/conddb')       
                               )
-
+process.GlobalTag.DBParameters.authenticationPath = process.DBService.authPath
 process.DIPLumiProducer=cms.ESSource("DIPLumiProducer",
-                                     connect=cms.string('oracle://cms_orcon_prod/cms_runtime_logger')
+                                     connect=cms.string('oracle://cms_omds_lb/CMS_RUNTIME_LOGGER')
                                      # NB: Zhen's recommendion below does NOT work for Online;
                                      #     Crashes at end of LS!!!
                                      #     connect=cms.string('oracle://cms_orcon_prod/cms_lumi_prod')
@@ -130,38 +121,11 @@ process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100)
 #-------------
 # Input Source
 #-------------
-process.source = cms.Source("EventStreamHttpReader",
-                            # General purpose SMProxy Server
-                            #------------------------------- 
-                            sourceURL = cms.string('http://dqm-c2d07-30.cms:22100/urn:xdaq-application:lid=30'),
-                            # SMPlayback Server
-                            #------------------
-                            #sourceURL = cms.string('http://dqm-c2d07-30:50082/urn:xdaq-application:lid=29'),
-                            # Local SMProxy Server
-                            #---------------------
-                            #sourceURL = cms.string('http://%s:22100/urn:xdaq-application:lid=30' % socket.gethostname()),
-                            # Storage Manager
-                            #---------------
-                            #sourceURL = cms.string('http://srv-c2c06-13.cms:11100/urn:xdaq-application:lid=50'),
-                            # Other Stuff
-                            #------------
-                            consumerPriority = cms.untracked.string('normal'),
-                            max_event_size = cms.int32(7000000),
-                            consumerName = cms.untracked.string('DQM Source'),
-                            # Trigger
-                            #--------
-                            #SelectHLTOutput = cms.untracked.string('hltOutputDQM'),
-                            SelectHLTOutput = cms.untracked.string('hltOutputALCALUMIPIXELS'),
-                            max_queue_depth = cms.int32(5),
-                            maxEventRequestRate = cms.untracked.double(200.0),
-                            #maxEventRequestRate = cms.untracked.double(100.0),
-                            SelectEvents = cms.untracked.PSet(
-    SelectEvents = cms.vstring('AlCa_LumiPixels_ZeroBias_v*')
-    ),
-                            headerRetryInterval = cms.untracked.int32(3),
-                            dropOldLumisectionEvents = cms.untracked.bool(True)
-                            )
-
+process.load("DQM.Integration.test.inputsource_cfi")
+process.DQMEventStreamHttpReader.consumerName = 'DQM Pixel Luminosity Consumer'
+process.DQMEventStreamHttpReader.SelectHLTOutput = cms.untracked.string('hltOutputALCALUMIPIXELS')
+process.DQMEventStreamHttpReader.maxEventRequestRate = cms.untracked.double(200.0)
+process.DQMEventStreamHttpReader.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('AlCa_LumiPixels_ZeroBias_v*'))
 
 # Only unpack pixel data; there is nothing else, really.
 process.MyRawToDigi = cms.Sequence(process.siPixelDigis)
