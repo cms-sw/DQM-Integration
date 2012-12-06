1603af256b5d82afbf42853f5c25a1907be5a30a
Daniel Duggan <sha1-a38f4466a54a4858eaba6132046a9c87013d90e8@cern.ch>
2012-12-06 13:14:39 +0000

    updated HI triggers and cleaned all commented code

--- python/test/pixel_dqm_sourceclient-live_cfg.py
+++ python/test/pixel_dqm_sourceclient-live_cfg.py
@@ -7,7 +7,6 @@ process.MessageLogger = cms.Service("MessageLogger",
                                          #'siPixelClusters', 
                                          'SiPixelRawDataErrorSource', 
                                          'SiPixelDigiSource', 
-                                         #'SiPixelClusterSource',
 					 'sipixelEDAClient'),
     cout = cms.untracked.PSet(threshold = cms.untracked.string('ERROR')),
     destinations = cms.untracked.vstring('cout')
@@ -20,11 +19,9 @@ QTestfile = 'DQM/SiPixelMonitorClient/test/sipixel_qualitytest_config.xml'
 process.load("DQM.Integration.test.inputsource_cfi")
 process.DQMEventStreamHttpReader.consumerName = 'Pixel DQM Consumer'
 process.DQMEventStreamHttpReader.SelectEvents = cms.untracked.PSet(
-    SelectEvents = cms.vstring('HLT_L1*','HLT_Jet*','HLT_*Cosmic*','HLT_HT*','HLT_MinBias_*','HLT_Physics*', 'HLT_ZeroBias*'))
+    SelectEvents = cms.vstring('HLT_L1*','HLT_Jet*','HLT_*Cosmic*','HLT_HT*','HLT_MinBias_*',
+                               'HLT_Physics*','HLT_ZeroBias*','HLT_PAL1*','HLT_PAZeroBias_*'))
 
-#process.DQMEventStreamHttpReader.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('HLT_Rando*'))
-#process.DQMEventStreamHttpReader.SelectEvents = cms.untracked.PSet(SelectEvents = cms.vstring('*'))
-#process.DQMEventStreamHttpReader.sourceURL = cms.string('http://srv-c2c05-07.cms:22100/urn:xdaq-application:lid=30')
 ##
 #----------------------------
 # DQM Environment
@@ -47,11 +44,7 @@ if (process.runType.getRunType() == process.runType.cosmic_run):
 #-----------------------------
 # Magnetic Field
 #-----------------------------
-# 0T field
-#process.load("Configuration.StandardSequences.MagneticField_0T_cff")
 # 3.8T field
-#process.load("Configuration.StandardSequences.MagneticField_38T_cff")
-#process.prefer("VolumeBasedMagneticFieldESProducer")
 process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
 
 #-------------------------------------------------
@@ -70,30 +63,12 @@ process.load("DQM.Integration.test.FrontierCondition_GT_cfi")
 #-----------------------
 #  Reconstruction Modules
 #-----------------------
-###process.load("Configuration.StandardSequences.Reconstruction_cff")
-##process.load("Configuration.StandardSequences.ReconstructionCosmics_cff")
 # Real data raw to digi
 process.load("EventFilter.SiPixelRawToDigi.SiPixelRawToDigi_cfi")
-#process.load("Configuration.StandardSequences.RawToDigi_cff")
-process.siPixelDigis.InputLabel = 'source'
 process.siPixelDigis.IncludeErrors = True
 
 # Local Reconstruction
 process.load("RecoLocalTracker.SiPixelClusterizer.SiPixelClusterizer_cfi")
-
-#process.load("EventFilter.SiStripRawToDigi.SiStripRawToDigis_standard_cff")
-#process.siStripDigis.ProductLabel = 'source'
-
-#process.load("RecoLocalTracker.SiStripClusterizer.SiStripClusterizer_cfi")
-#process.load("RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitConverter_cfi")
-#process.load("RecoLocalTracker.SiStripRecHitConverter.SiStripRecHitMatcher_cfi")
-#process.load("RecoLocalTracker.SiStripRecHitConverter.StripCPEfromTrackAngle_cfi")
-#process.load("RecoLocalTracker.SiStripZeroSuppression.SiStripZeroSuppression_cfi")
-
-#process.load("RecoVertex.BeamSpotProducer.BeamSpot_cff")
-#process.load("RecoPixelVertexing.Configuration.RecoPixelVertexing_cff")
-##process.load("RecoLocalTracker.Configuration.RecoLocalTracker_Cosmics_cff")
-#process.load("RecoTracker.Configuration.RecoTrackerP5_cff")
 
 #----------------------------------
 # High Pileup Configuration Changes
@@ -152,22 +127,13 @@ process.hltTriggerTypeFilter.SelectedTriggerType = 1
 #--------------------------
 # Scheduling
 #--------------------------
-#process.Reco = cms.Sequence(process.siPixelDigis)
 process.Reco = cms.Sequence(process.siPixelDigis*process.siPixelClusters)
-#process.RecoStrips = cms.Sequence(process.siStripDigis*process.siStripClusters)
-#process.siPixelLocalReco = cms.Sequence(process.siPixelRecHits) 
-#process.siStripLocalReco = cms.Sequence(process.siStripMatchedRecHits)
-#process.trackerLocalReco = cms.Sequence(process.siPixelLocalReco*process.siStripLocalReco)
-#process.trackReco = cms.Sequence(process.trackerLocalReco*process.offlineBeamSpot*process.recopixelvertexing*process.ckftracksP5) #*process.rstracks 
 process.DQMmodules = cms.Sequence(process.dqmEnv*process.qTester*process.dqmSaver)
 
 process.SiPixelDigiSource.layOn = True
 process.SiPixelDigiSource.diskOn = True
 
-
 process.p = cms.Path(process.Reco*process.DQMmodules*process.SiPixelRawDataErrorSource*process.SiPixelDigiSource*process.SiPixelClusterSource*process.PixelP5DQMClientWithDataCertification)
-####process.p = cms.Path(process.hltTriggerTypeFilter*process.Reco*process.DQMmodules*process.SiPixelRawDataErrorSource*process.SiPixelDigiSource*process.SiPixelClusterSource*process.PixelP5DQMClientWithDataCertification)
-#process.p = cms.Path(process.Reco*process.RecoStrips*process.trackReco*process.DQMmodules*process.siPixelP5DQM_cosmics_source*process.PixelP5DQMClientWithDataCertification)
 
 #--------------------------------------------------
 # Heavy Ion Specific Fed Raw Data Collection Label
