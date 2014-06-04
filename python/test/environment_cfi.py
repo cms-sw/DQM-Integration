fe7e9d8813629fd0537b2a1200e3ded53ea38433
federico de guio <federico.de.guio@cern.ch>
2014-06-04 19:27:20 +0200

    fix a minor bug and update the DQMService in environment_cfi.py

--- python/test/environment_cfi.py
+++ python/test/environment_cfi.py
@@ -1,9 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-from DQMServices.Core.DQM_cfg import *
+#from DQMServices.Core.DQM_cfg import *
 
-DQM.collectorHost = 'dqm-prod-local.cms'
-DQM.collectorPort = 9090
+DQM = cms.Service("DQM",                                                                                                                                                                           
+                  debug = cms.untracked.bool(False),
+                  publishFrequency = cms.untracked.double(5.0),
+                  collectorPort = cms.untracked.int32(9090),
+                  collectorHost = cms.untracked.string('dqm-prod-local.cms'),
+                  filter = cms.untracked.string('')
+                  )      
 
 from DQMServices.Components.DQMEnvironment_cfi import *
 
