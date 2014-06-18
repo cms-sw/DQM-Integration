9112141e42a3c319a45702b216394842d8099066
Laura Borrello <laura.borrello@cern.ch>
2014-06-18 18:46:15 +0200

    File for test in lxplus cluster

---
+++ python/test/FrontierCondition_GT_Offline_cfi.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import * 
+from Configuration.AlCa.GlobalTag import GlobalTag
+
+GlobalTag = GlobalTag(GlobalTag, 'auto:com10', '')
+
